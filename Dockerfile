891dfdae0c2b1ea17a827d271763dd1af7474062
Harry Marr <harry.marr@gmail.com>
2015-10-17 12:42:50 +0100

    Config for deployment

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,12 @@ FROM ruby:2.2.3
 
 RUN apt-get update -qq && apt-get install -y build-essential
 
+# Needed to clone GitHub repos without interactive prompts
+RUN mkdir -p /root/.ssh/ && ssh-keyscan github.com > /root/.ssh/github.pub
+# Verify RSA identity of github.com
+RUN [ "$(ssh-keygen -lf /root/.ssh/github.pub | awk '{print $2}')" = "16:27:ac:a5:76:28:2d:36:63:1b:56:4d:eb:df:a6:48" ] && \
+    mv /root/.ssh/github.pub /root/.ssh/known_hosts || (echo "Wrong Github Fingerprint" && exit 1)
+
 ENV APP_HOME /app
 RUN mkdir $APP_HOME
 WORKDIR $APP_HOME
--- app/boot.rb
+++ app/boot.rb
@@ -1,11 +1,29 @@
 require "prius"
+require "hutch"
 require "raven"
 require "./lib/null_logger"
 
 Prius.load(:bump_github_token)
 Prius.load(:sentry_dsn, required: false)
+Prius.load(:amqp_host)
+Prius.load(:amqp_api_host)
+Prius.load(:amqp_api_port)
+Prius.load(:amqp_vhost)
+Prius.load(:amqp_api_ssl, type: :bool)
+Prius.load(:amqp_username)
+Prius.load(:amqp_password)
 
 Raven.configure do |config|
   config.dsn = Prius.get(:sentry_dsn) if Prius.get(:sentry_dsn)
   config.logger = NullLogger.new(STDOUT) unless Prius.get(:sentry_dsn)
 end
+
+Hutch::Config.set(:mq_host, Prius.get(:amqp_host))
+Hutch::Config.set(:mq_api_host, Prius.get(:amqp_api_host))
+Hutch::Config.set(:mq_api_port, Prius.get(:amqp_api_port))
+Hutch::Config.set(:mq_vhost, Prius.get(:amqp_vhost))
+Hutch::Config.set(:mq_api_ssl, Prius.get(:amqp_api_ssl))
+Hutch::Config.set(:mq_username, Prius.get(:amqp_username))
+Hutch::Config.set(:mq_password, Prius.get(:amqp_password))
+Hutch.connect
+
