ba9382b75b23c7c77e297013fc6fdcd333e6e0be
Grey Baker <greysteil@gmail.com>
2015-10-25 18:55:14 +0000

    Move dummy-env to config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "rspec/its"
 require "webmock/rspec"
 require "dotenv"
 
-Dotenv.load("dummy-env")
+Dotenv.load(File.expand_path("../../config/dummy-env", __FILE__))
 
 require "./app/boot"
 
