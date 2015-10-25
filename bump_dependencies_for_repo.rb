27ac9d361e0e3dad7f4aa9b653ba45c37ab001f6
Grey Baker <greysteil@gmail.com>
2015-10-25 15:13:21 +0000

    Make user entry script foolproof

--- bump_dependencies_for_repo.rb
+++ bump_dependencies_for_repo.rb
@@ -1,12 +1,32 @@
 #!/usr/bin/env ruby
 
 require "./app/workers/dependency_file_fetcher"
+require "./lib/github"
 require "highline/import"
 require "dotenv"
 Dotenv.load
 
-repo     = ask "Which repo would you like to bump dependencies for? "
-language = ask "Which language? "
+repo = ask("Which repo would you like to bump dependencies for? ") do |question|
+  question.validate = lambda { |repo_name|
+    begin
+      Github.client.repository(repo_name)
+      true
+    rescue Octokit::NotFound
+      false
+    end
+  }
+
+  question.responses[:invalid_type] =
+    "Could not access that repo. Make sure you use the format "\
+    "'gocardless/bump', and that your GitHub token has read/write "\
+    "access to the given repo."
+end
+
+language = choose do |menu|
+  menu.index = :none
+  menu.header = "Which language would you like to bump dependencies for?"
+  menu.choices(:ruby, :node)
+end
 
 Workers::DependencyFileFetcher.
   perform_async("repo" => { "language" => language, "name" => repo })
