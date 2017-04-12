0c4b243d4da6c9efa2b509fba35bdcae9a289b2e
Grey Baker <greysteil@gmail.com>
2017-04-12 23:42:06 +0100

    Better requirements.txt handling
    Previously, parsing and recreating a Python `requirements.txt` file only handled
    the simple case of all dependencies being fully specified. This commit:
    - Parses Python requirement files in the general case
    - Adjusts the Python file update strategy to only alter a single line in the
      dependency file when regenerating it
    - Ensures any comments and version specificity is preserved when a line in
      `requirements.txt` is updated

--- lib/bump/dependency_file_parsers/python.rb
+++ lib/bump/dependency_file_parsers/python.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require "bump/dependency"
-require "bump/python_helpers"
 
 module Bump
   module DependencyFileParsers
@@ -14,9 +13,59 @@ module Bump
       end
 
       def parse
-        PythonHelpers.
-          parse_requirements(@requirements.content).
-          map { |dep| Dependency.new(name: dep[0], version: dep[1]) }
+        @requirements.
+          content.
+          each_line.
+          each_with_object([]) do |line, dependencies|
+            dependency = LineParser.new(line).parse
+
+            next if dependency.nil?
+            next if dependency[:requirements].length.zero?
+
+            # Ignore dependencies with multiple requirements, since they would
+            # cause trouble at the dependency update step
+            next if dependency[:requirements].length > 1
+
+            version =
+              dependency[:requirements].first.match(LineParser::VERSION)[0]
+
+            dependencies << Dependency.new(
+              name: dependency[:name],
+              version: version,
+              language: "python"
+            )
+          end
+      end
+
+      class LineParser
+        NAME = /[a-zA-Z0-9\-_\.]+/
+        EXTRA = /[a-zA-Z0-9\-_\.]+/
+        COMPARISON = /===|==|>=|<=|<|>|~=|!=/
+        VERSION = /[a-zA-Z0-9\-_\.]+/
+        REQUIREMENT = /#{COMPARISON}\s*#{VERSION}/
+
+        REQUIREMENT_LINE =
+          /^\s*(?<name>#{NAME})
+            \s*(\[\s*(?<extras>#{EXTRA}(\s*,\s*#{EXTRA})*)\s*\])?
+            \s*(?<requirements>#{REQUIREMENT}(\s*,\s*#{REQUIREMENT})*)?
+            \s*#*\s*(?<comment>.+)?$
+          /x
+
+        attr_reader :line
+
+        def initialize(line)
+          @line = line
+        end
+
+        def parse
+          requirement = line.chomp.match(REQUIREMENT_LINE)
+          return if requirement.nil?
+
+          {
+            name: requirement[:name],
+            requirements: (requirement[:requirements] || "").scan(REQUIREMENT)
+          }
+        end
       end
     end
   end
--- spec/lib/dependency_file_parsers/python_spec.rb
+++ spec/lib/dependency_file_parsers/python_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Bump::DependencyFileParsers::Python do
       content: requirements_body
     )
   end
-  let(:requirements_body) { fixture("requirements", "requirements.txt") }
+  let(:requirements_body) { fixture("requirements", "version_specified.txt") }
   let(:parser) { described_class.new(dependency_files: files) }
 
   describe "parse" do
@@ -27,6 +27,51 @@ RSpec.describe Bump::DependencyFileParsers::Python do
         its(:name) { is_expected.to eq("psycopg2") }
         its(:version) { is_expected.to eq("2.6.1") }
       end
+    end
+
+    context "with comments" do
+      let(:requirements_body) { fixture("requirements", "comments.txt") }
+      its(:length) { is_expected.to eq(2) }
+
+      describe "the first dependency" do
+        subject { dependencies.first }
+
+        it { is_expected.to be_a(Bump::Dependency) }
+        its(:name) { is_expected.to eq("psycopg2") }
+        its(:version) { is_expected.to eq("2.6.1") }
+      end
+    end
+
+    context "with extras" do
+      let(:requirements_body) { fixture("requirements", "extras.txt") }
+
+      describe "the first dependency" do
+        subject { dependencies.first }
+
+        it { is_expected.to be_a(Bump::Dependency) }
+        its(:name) { is_expected.to eq("psycopg2") }
+        its(:version) { is_expected.to eq("2.6.1") }
+      end
+    end
+
+    context "with no version specified" do
+      let(:requirements_body) do
+        fixture("requirements", "version_not_specified.txt")
+      end
+
+      # If no version is specified, Python will always use the latest, and we
+      # don't need to attempt to bump the dependency.
+      its(:length) { is_expected.to eq(1) }
+    end
+
+    context "with a version specified as between two constraints" do
+      let(:requirements_body) do
+        fixture("requirements", "version_between_bounds.txt")
+      end
+
+      # TODO: For now we ignore dependencies with multiple requirements, because
+      # they'd cause trouble at the dependency update step.
+      its(:length) { is_expected.to eq(1) }
     end
   end
 end
