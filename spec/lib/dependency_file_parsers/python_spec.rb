a4865fd8b9a718906ec9c3e266e9c1a87db944d9
Grey Baker <greysteil@gmail.com>
2017-04-13 19:25:43 +0100

    Python parser unit tests

--- spec/lib/dependency_file_parsers/python_spec.rb
+++ spec/lib/dependency_file_parsers/python_spec.rb
@@ -87,4 +87,60 @@ RSpec.describe Bump::DependencyFileParsers::Python do
       its(:length) { is_expected.to eq(1) }
     end
   end
+
+  describe Bump::DependencyFileParsers::Python::LineParser do
+    subject(:parser) { described_class.new(line) }
+
+    describe "parse" do
+      subject { parser.parse }
+
+      context "with a blank line" do
+        let(:line) { "" }
+        it { is_expected.to be_nil }
+      end
+
+      context "with just a line break" do
+        let(:line) { "\n" }
+        it { is_expected.to be_nil }
+      end
+
+      context "with a non-requirement line" do
+        let(:line) { "# This is just a comment" }
+        it { is_expected.to be_nil }
+      end
+
+      context "with no specification" do
+        let(:line) { "luigi" }
+        its([:name]) { is_expected.to eq "luigi" }
+        its([:requirements]) { is_expected.to eq [] }
+
+        context "with a comment" do
+          let(:line) { "luigi # some comment" }
+          its([:name]) { is_expected.to eq "luigi" }
+          its([:requirements]) { is_expected.to eq [] }
+        end
+      end
+
+      context "with a simple specification" do
+        let(:line) { "luigi == 0.1.0" }
+        its([:requirements]) { is_expected.to eq ["== 0.1.0"] }
+
+        context "without spaces" do
+          let(:line) { "luigi==0.1.0" }
+          its([:name]) { is_expected.to eq "luigi" }
+          its([:requirements]) { is_expected.to eq ["==0.1.0"] }
+        end
+      end
+
+      context "with multiple specifications" do
+        let(:line) { "luigi == 0.1.0, <= 1" }
+        its([:requirements]) { is_expected.to eq ["== 0.1.0", "<= 1"] }
+
+        context "with a comment" do
+          let(:line) { "luigi == 0.1.0, <= 1 # some comment" }
+          its([:requirements]) { is_expected.to eq ["== 0.1.0", "<= 1"] }
+        end
+      end
+    end
+  end
 end
