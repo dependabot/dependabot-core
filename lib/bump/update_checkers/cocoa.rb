d453992f3d3ef22a14d65750b08654e119c9d422
Grey Baker <greysteil@gmail.com>
2017-05-18 21:58:12 +0100

    Consider resolvability when calculating latest_version (Cocoa)

--- lib/bump/update_checkers/cocoa.rb
+++ lib/bump/update_checkers/cocoa.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 require "cocoapods"
+require "gemnasium/parser"
 require "bump/update_checkers/base"
 require "bump/shared_helpers"
 require "bump/errors"
+require "bump/dependency_file_updaters/cocoa"
 
 module Bump
   module UpdateCheckers
@@ -19,20 +21,37 @@ module Bump
 
         return nil if pod.external_source
 
-        source_manager = Pod::Config.instance.sources_manager
-        analyzer = Pod::Installer::Analyzer.new(nil, parsed_podfile, nil)
-        analyzer.config.silent = true
-        analyzer.update_repositories
+        specs = pod_analyzer.analyze.specifications
 
-        sources =
-          if pod.podspec_repo
-            [source_manager.find_or_create_source_with_url(pod.podspec_repo)]
-          else
-            analyzer.sources
+        Gem::Version.new(specs.find { |d| d.name == dependency.name }.version)
+      end
+
+      def pod_analyzer
+        @pod_analyzer =
+          begin
+            lockfile_hash =
+              Pod::YAMLHelper.load_string(lockfile_for_update_check)
+            parsed_lockfile = Pod::Lockfile.new(lockfile_hash)
+
+            evaluated_podfile =
+              Pod::Podfile.from_ruby(nil, podfile_for_update_check)
+
+            pod_sandbox = Pod::Sandbox.new("tmp")
+
+            analyzer = Pod::Installer::Analyzer.new(
+              pod_sandbox,
+              evaluated_podfile,
+              parsed_lockfile
+            )
+
+            analyzer.installation_options.integrate_targets = false
+            analyzer.update = { pods: ["Alamofire"] }
+
+            analyzer.config.silent = true
+            analyzer.update_repositories
+
+            analyzer
           end
-
-        set = Pod::Specification::Set.new(pod.name, sources)
-        Gem::Version.new(set.highest_version)
       end
 
       def lockfile
@@ -45,6 +64,42 @@ module Bump
         podfile = dependency_files.find { |f| f.name == "Podfile" }
         raise "No Podfile!" unless podfile
         podfile
+      end
+
+      def podfile_for_update_check
+        podfile_content = podfile.content
+        podfile_content = remove_dependency_requirement(podfile_content)
+        prepend_git_auth_details(podfile_content)
+      end
+
+      def lockfile_for_update_check
+        lockfile_content = lockfile.content
+        prepend_git_auth_details(lockfile_content)
+      end
+
+      # Replace the original pod requirements with nothing, to fully "unlock"
+      # the pod during version checking
+      def remove_dependency_requirement(podfile_content)
+        podfile_content.
+          to_enum(:scan, Bump::DependencyFileUpdaters::Cocoa::POD_CALL).
+          find { Regexp.last_match[:name] == dependency.name }
+
+        original_pod_declaration_string = Regexp.last_match.to_s
+        updated_pod_declaration_string =
+          original_pod_declaration_string.
+          sub(/,[ \t]*#{Gemnasium::Parser::Patterns::REQUIREMENTS}/, "")
+
+        podfile_content.gsub(
+          original_pod_declaration_string,
+          updated_pod_declaration_string
+        )
+      end
+
+      def prepend_git_auth_details(podfile_content)
+        podfile_content.gsub(
+          "git@github.com:",
+          "https://#{github_access_token}:x-oauth-basic@github.com/"
+        )
       end
     end
   end
