9924b2ba8cde24a8bd92a51401375a4f80031a47
Grey Baker <greysteil@gmail.com>
2017-05-20 20:30:41 +0100

    Add DependencyFileParsers::Php

--- lib/bump/file_parsers.rb
+++ lib/bump/file_parsers.rb
@@ -2,6 +2,7 @@
 require "bump/file_parsers/ruby/bundler"
 require "bump/file_parsers/python/pip"
 require "bump/file_parsers/java_script/yarn"
+require "bump/file_parsers/php/composer"
 
 module Bump
   module FileParsers
@@ -10,6 +11,7 @@ module Bump
       when "bundler" then FileParsers::Ruby::Bundler
       when "yarn" then FileParsers::JavaScript::Yarn
       when "pip" then FileParsers::Python::Pip
+      when "composer" then FileParsers::Php::Composer
       else raise "Unsupported package_manager #{package_manager}"
       end
     end
