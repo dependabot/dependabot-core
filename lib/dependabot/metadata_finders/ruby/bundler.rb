1f170d008a7e4dea2084cb3919ac40c5ee6119aa
Grey Baker <greysteil@gmail.com>
2017-06-27 11:20:05 +0100

    More options for Bundler source code lookup

--- lib/dependabot/metadata_finders/ruby/bundler.rb
+++ lib/dependabot/metadata_finders/ruby/bundler.rb
@@ -12,6 +12,9 @@ module Dependabot
           wiki_uri
           bug_tracker_uri
           documentation_uri
+          changelog_uri
+          mailing_list_uri
+          download_uri
         ).freeze
 
         private
