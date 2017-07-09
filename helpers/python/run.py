f7ff25f7664efc1f3facd5d2447fbe3368927efa
Grey Baker <greysteil@gmail.com>
2017-07-09 18:00:29 +0100

    Four spaces for Python, not two

--- helpers/python/run.py
+++ helpers/python/run.py
@@ -4,7 +4,7 @@ import json
 from lib import parser
 
 if __name__ == "__main__":
-  args = json.loads(sys.stdin.read())
+    args = json.loads(sys.stdin.read())
 
-  if args["function"] == "parse":
-    print(parser.parse(args["args"][0]))
+    if args["function"] == "parse":
+        print(parser.parse(args["args"][0]))
