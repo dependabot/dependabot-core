bb485580b2e91db04cd39b963360c47bb1f465b6
Grey Baker <greysteil@gmail.com>
2017-07-09 23:10:30 +0100

    Add CocoaPods file fetcher

--- lib/dependabot/file_fetchers.rb
+++ lib/dependabot/file_fetchers.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require "dependabot/file_fetchers/ruby/bundler"
+require "dependabot/file_fetchers/cocoa/cocoa_pods"
 require "dependabot/file_fetchers/python/pip"
 require "dependabot/file_fetchers/java_script/yarn"
 require "dependabot/file_fetchers/php/composer"
@@ -9,6 +10,7 @@ module Dependabot
     def self.for_package_manager(package_manager)
       case package_manager
       when "bundler" then FileFetchers::Ruby::Bundler
+      when "cocoapods" then FileFetchers::Cocoa::CocoaPods
       when "yarn" then FileFetchers::JavaScript::Yarn
       when "pip" then FileFetchers::Python::Pip
       when "composer" then FileFetchers::Php::Composer
