c2bdc8ea1563b51b4561df3faac9ccd1355a5fed
Grey Baker <greysteil@gmail.com>
2017-08-15 14:37:43 +0100

    Replace MetadataFinders::Ruby::Gemspec completely

--- lib/dependabot/metadata_finders.rb
+++ lib/dependabot/metadata_finders.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require "dependabot/metadata_finders/ruby/bundler"
-require "dependabot/metadata_finders/ruby/gemspec"
 require "dependabot/metadata_finders/python/pip"
 require "dependabot/metadata_finders/java_script/yarn"
 require "dependabot/metadata_finders/php/composer"
@@ -9,8 +8,7 @@ module Dependabot
   module MetadataFinders
     def self.for_package_manager(package_manager)
       case package_manager
-      when "bundler" then MetadataFinders::Ruby::Bundler
-      when "gemspec" then MetadataFinders::Ruby::Gemspec
+      when "bundler", "gemspec" then MetadataFinders::Ruby::Bundler
       when "yarn" then MetadataFinders::JavaScript::Yarn
       when "pip" then MetadataFinders::Python::Pip
       when "composer" then MetadataFinders::Php::Composer
