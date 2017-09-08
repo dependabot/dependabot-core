7b70000a81a100ada9ab89390183f5acffc96112
Grey Baker <greysteil@gmail.com>
2017-09-08 21:52:42 +0100

    Add file update for git submodules

---
+++ spec/dependabot/file_updaters/git/submodules_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+require "spec_helper"
+require "dependabot/dependency"
+require "dependabot/dependency_file"
+require "dependabot/file_updaters/git/submodules"
+require_relative "../shared_examples_for_file_updaters"
+
+RSpec.describe Dependabot::FileUpdaters::Git::Submodules do
+  it_behaves_like "a dependency file updater"
+
+  let(:updater) do
+    described_class.new(
+      dependency_files: [gitmodules, submodule],
+      dependency: dependency,
+      github_access_token: "token"
+    )
+  end
+  let(:gitmodules) do
+    Dependabot::DependencyFile.new(
+      content: fixture("git", "gitmodules", ".gitmodules"),
+      name: ".gitmodules"
+    )
+  end
+  let(:submodule) do
+    Dependabot::DependencyFile.new(
+      content: "sha1",
+      name: "manifesto"
+    )
+  end
+
+  let(:dependency) do
+    Dependabot::Dependency.new(
+      name: "manifesto",
+      version: "sha2",
+      previous_version: "sha1",
+      requirements: [
+        {
+          file: ".gitmodules",
+          requirement: {
+            url: "https://github.com/example/manifesto.git",
+            branch: "master"
+          },
+          groups: []
+        }
+      ],
+      previous_requirements: [
+        {
+          file: ".gitmodules",
+          requirement: {
+            url: "https://github.com/example/manifesto.git",
+            branch: "master"
+          },
+          groups: []
+        }
+      ],
+      package_manager: "submodules"
+    )
+  end
+
+  describe "#updated_dependency_files" do
+    subject(:updated_files) { updater.updated_dependency_files }
+
+    it "returns DependencyFile objects" do
+      updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+    end
+
+    its(:length) { is_expected.to eq(1) }
+
+    describe "the updated file" do
+      subject(:updated_submodule) { updated_files.first }
+
+      its(:name) { is_expected.to eq("manifesto") }
+      its(:content) { is_expected.to eq("sha2") }
+    end
+  end
+end
