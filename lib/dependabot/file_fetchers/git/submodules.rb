9b75433435a814f6317a38b3cebcbb78e33a9cbb
Grey Baker <greysteil@gmail.com>
2017-09-08 19:35:44 +0100

    Add parser for git submodules (and improve file fetcher)

--- lib/dependabot/file_fetchers/git/submodules.rb
+++ lib/dependabot/file_fetchers/git/submodules.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
+require "parseconfig"
 require "dependabot/file_fetchers/base"
+require "dependabot/shared_helpers"
 
 module Dependabot
   module FileFetchers
@@ -16,11 +18,42 @@ module Dependabot
         private
 
         def fetch_files
-          [gitmodules_file]
+          fetched_files = []
+          fetched_files << gitmodules_file
+          fetched_files += submodule_refs
+          fetched_files
         end
 
         def gitmodules_file
           @gitmodules_file ||= fetch_file_from_github(".gitmodules")
+        end
+
+        def submodule_refs
+          submodule_paths.map { |path| fetch_submodule_ref_from_github(path) }
+        end
+
+        def submodule_paths
+          SharedHelpers.in_a_temporary_directory do
+            File.write(".gitmodules", gitmodules_file.content)
+
+            ParseConfig.new(".gitmodules").params.map do |_, params|
+              params["path"]
+            end
+          end
+        end
+
+        def fetch_submodule_ref_from_github(submodule_path)
+          path = Pathname.new(File.join(directory, submodule_path)).
+                 cleanpath.to_path
+          sha = github_client.contents(repo, path: path, ref: commit).sha
+
+          DependencyFile.new(
+            name: Pathname.new(submodule_path).cleanpath.to_path,
+            content: sha,
+            directory: directory
+          )
+        rescue Octokit::NotFound
+          raise Dependabot::DependencyFileNotFound, path
         end
       end
     end
--- spec/dependabot/file_fetchers/git/submodules_spec.rb
+++ spec/dependabot/file_fetchers/git/submodules_spec.rb
@@ -4,4 +4,65 @@ require_relative "../shared_examples_for_file_fetchers"
 
 RSpec.describe Dependabot::FileFetchers::Git::Submodules do
   it_behaves_like "a dependency file fetcher"
+
+  context "with submodules" do
+    let(:github_client) { Octokit::Client.new(access_token: "token") }
+    let(:file_fetcher_instance) do
+      described_class.new(repo: "gocardless/bump", github_client: github_client)
+    end
+
+    let(:url) { "https://api.github.com/repos/gocardless/bump/contents/" }
+
+    before do
+      allow(file_fetcher_instance).to receive(:commit).and_return("sha")
+
+      stub_request(:get, url + ".gitmodules?ref=sha").
+        to_return(
+          status: 200,
+          body: fixture("github", "gitmodules.json"),
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    context "that are fetchable" do
+      before do
+        stub_request(:get, url + "about/documents?ref=sha").
+          to_return(
+            status: 200,
+            body: fixture("github", "submodule.json"),
+            headers: { "content-type" => "application/json" }
+          )
+        stub_request(:get, url + "manifesto?ref=sha").
+          to_return(
+            status: 200,
+            body: fixture("github", "submodule.json"),
+            headers: { "content-type" => "application/json" }
+          )
+      end
+
+      it "fetches the submodules" do
+        expect(file_fetcher_instance.files.count).to eq(3)
+        expect(file_fetcher_instance.files.last.name).to eq("manifesto")
+        expect(file_fetcher_instance.files.last.content).
+          to eq("d70e943e00a09a3c98c0e4ac9daab112b749cf62")
+        expect(file_fetcher_instance.files[1].name).to eq("about/documents")
+        expect(file_fetcher_instance.files.last.content).
+          to eq("d70e943e00a09a3c98c0e4ac9daab112b749cf62")
+      end
+    end
+
+    context "that has an unfetchable path" do
+      before do
+        stub_request(:get, url + "about/documents?ref=sha").
+          to_return(status: 404)
+        stub_request(:get, url + "manifesto?ref=sha").
+          to_return(status: 404)
+      end
+
+      it "raises a DependencyFileNotFound error with details" do
+        expect { file_fetcher_instance.files }.
+          to raise_error(Dependabot::DependencyFileNotFound)
+      end
+    end
+  end
 end
