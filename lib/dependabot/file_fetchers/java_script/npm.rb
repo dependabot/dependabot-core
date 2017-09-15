71571c4763ece5c5746cce5dc7460bbea25b6556
Grey Baker <greysteil@gmail.com>
2017-09-15 17:53:17 +0100

    Add file fetcher for npm5

---
+++ lib/dependabot/file_fetchers/java_script/npm.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+require "dependabot/file_fetchers/base"
+require "dependabot/file_parsers/java_script/yarn"
+
+module Dependabot
+  module FileFetchers
+    module JavaScript
+      class Npm < Dependabot::FileFetchers::Base
+        def self.required_files_in?(filenames)
+          (%w(package.json package-json.lock) - filenames).empty?
+        end
+
+        def self.required_files_message
+          "Repo must contain a package.json and a package-lock.json."
+        end
+
+        private
+
+        def fetch_files
+          fetched_files = []
+          fetched_files << package_json
+          fetched_files << package_lock
+          fetched_files += path_dependencies
+          fetched_files
+        end
+
+        def package_json
+          @package_json ||= fetch_file_from_github("package.json")
+        end
+
+        def package_lock
+          @package_lock ||= fetch_file_from_github("package-lock.json")
+        end
+
+        def path_dependencies
+          package_json_files = []
+          unfetchable_deps = []
+
+          types = Dependabot::FileParsers::JavaScript::Yarn::DEPENDENCY_TYPES
+          parsed_package_json.values_at(*types).compact.each do |deps|
+            deps.map do |name, version|
+              next unless version.start_with?("file:")
+
+              path = version.sub(/^file:/, "")
+              file = File.join(path, "package.json")
+
+              begin
+                package_json_files << fetch_file_from_github(file)
+              rescue Dependabot::DependencyFileNotFound
+                unfetchable_deps << name
+              end
+            end
+          end
+
+          if unfetchable_deps.any?
+            raise Dependabot::PathDependenciesNotReachable, unfetchable_deps
+          end
+
+          package_json_files
+        end
+
+        def parsed_package_json
+          JSON.parse(package_json.content)
+        rescue JSON::ParserError
+          raise Dependabot::DependencyFileNotParseable, package_json.path
+        end
+      end
+    end
+  end
+end
---
+++ spec/dependabot/file_fetchers/java_script/npm_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+require "dependabot/file_fetchers/java_script/npm"
+require_relative "../shared_examples_for_file_fetchers"
+
+RSpec.describe Dependabot::FileFetchers::JavaScript::Npm do
+  it_behaves_like "a dependency file fetcher"
+
+  context "with a path dependency" do
+    let(:github_client) { Octokit::Client.new(access_token: "token") }
+    let(:file_fetcher_instance) do
+      described_class.new(repo: "gocardless/bump", github_client: github_client)
+    end
+
+    let(:url) { "https://api.github.com/repos/gocardless/bump/contents/" }
+
+    before do
+      allow(file_fetcher_instance).to receive(:commit).and_return("sha")
+
+      stub_request(:get, url + "?ref=sha").
+        to_return(
+          status: 200,
+          body: fixture("github", "business_files_no_gemspec.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "package.json?ref=sha").
+        to_return(
+          status: 200,
+          body: fixture("github", "package_json_with_path_content.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "package-lock.json?ref=sha").
+        to_return(
+          status: 200,
+          body: fixture("github", "package_lock_content.json"),
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    context "with a bad package.json" do
+      before do
+        stub_request(:get, url + "package.json?ref=sha").
+          to_return(
+            status: 200,
+            body: fixture("github", "gemfile_content.json"),
+            headers: { "content-type" => "application/json" }
+          )
+      end
+
+      it "raises a DependencyFileNotParseable error" do
+        expect { file_fetcher_instance.files }.
+          to raise_error(Dependabot::DependencyFileNotParseable) do |error|
+            expect(error.file_name).to eq("package.json")
+          end
+      end
+    end
+
+    context "that has a fetchable path" do
+      before do
+        stub_request(:get, url + "deps/etag/package.json?ref=sha").
+          to_return(
+            status: 200,
+            body: fixture("github", "package_json_content.json"),
+            headers: { "content-type" => "application/json" }
+          )
+      end
+
+      it "fetches package.json from path dependency" do
+        expect(file_fetcher_instance.files.count).to eq(3)
+        expect(file_fetcher_instance.files.map(&:name)).
+          to include("deps/etag/package.json")
+      end
+    end
+
+    context "that has an unfetchable path" do
+      before do
+        stub_request(:get, url + "deps/etag/package.json?ref=sha").
+          to_return(status: 404)
+      end
+
+      it "raises a PathDependenciesNotReachable error with details" do
+        expect { file_fetcher_instance.files }.
+          to raise_error(
+            Dependabot::PathDependenciesNotReachable,
+            "The following path based dependencies could not be retrieved: " \
+            "etag"
+          )
+      end
+    end
+  end
+end
