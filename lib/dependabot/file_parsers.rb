d528683126088f259e23f9259959e6c0676b4d2f
Grey Baker <greysteil@gmail.com>
2017-09-15 18:37:16 +0100

    Add npm to for_package_manager methods

--- lib/dependabot/file_parsers.rb
+++ lib/dependabot/file_parsers.rb
@@ -2,6 +2,7 @@
 
 require "dependabot/file_parsers/ruby/bundler"
 require "dependabot/file_parsers/python/pip"
+require "dependabot/file_parsers/java_script/npm"
 require "dependabot/file_parsers/java_script/yarn"
 require "dependabot/file_parsers/php/composer"
 require "dependabot/file_parsers/git/submodules"
@@ -13,6 +14,7 @@ module Dependabot
     def self.for_package_manager(package_manager)
       case package_manager
       when "bundler" then FileParsers::Ruby::Bundler
+      when "npm" then FileParsers::JavaScript::Npm
       when "yarn" then FileParsers::JavaScript::Yarn
       when "pip" then FileParsers::Python::Pip
       when "composer" then FileParsers::Php::Composer
