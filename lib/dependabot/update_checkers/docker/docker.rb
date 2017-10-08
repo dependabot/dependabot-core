7f8fd0304c5211e3db8af3722debf82897f937e3
Grey Baker <greysteil@gmail.com>
2017-10-08 14:30:48 +0100

    Add basic UpdateChecker for docker

---
+++ lib/dependabot/update_checkers/docker/docker.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "dependabot/update_checkers/base"
+require "docker_registry2"
+
+module Dependabot
+  module UpdateCheckers
+    module Docker
+      class Docker < Dependabot::UpdateCheckers::Base
+        VERSION_REGEX = /^(?<version>[0-9]+\.[0-9]+(?:\.[a-zA-Z0-9]+)*)$/
+
+        def latest_version
+          @latest_version ||= fetch_latest_version
+        end
+
+        def latest_resolvable_version
+          # Resolvability isn't an issue for Docker containers.
+          latest_version
+        end
+
+        def updated_requirements
+          dependency.requirements
+        end
+
+        private
+
+        def fetch_latest_version
+          return nil unless dependency.version.match?(VERSION_REGEX)
+
+          # TODO: How can we get details to connect to private registries?
+          registry = DockerRegistry2.connect
+
+          # TODO: The "library" bit probably doesn't belong here
+          tags = registry.tags("library/#{dependency.name}")
+
+          tags.fetch("tags").
+            select { |tag| tag.match?(VERSION_REGEX) }.
+            map { |tag| Gem::Version.new(tag) }.
+            max
+        end
+      end
+    end
+  end
+end
---
+++ spec/dependabot/update_checkers/docker/docker_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/dependency"
+require "dependabot/update_checkers/docker/docker"
+require_relative "../shared_examples_for_update_checkers"
+
+RSpec.describe Dependabot::UpdateCheckers::Docker::Docker do
+  it_behaves_like "an update checker"
+
+  let(:checker) do
+    described_class.new(
+      dependency: dependency,
+      dependency_files: [],
+      github_access_token: "token"
+    )
+  end
+
+  let(:dependency) do
+    Dependabot::Dependency.new(
+      name: "ubuntu",
+      version: version,
+      requirements: [],
+      package_manager: "docker"
+    )
+  end
+  let(:version) { "17.04" }
+  let(:registry_tags) { fixture("docker", "registry_tags", "ubuntu.json") }
+
+  before do
+    ping_url = "https://registry.hub.docker.com/v2/"
+    stub_request(:get, ping_url).and_return(status: 200)
+
+    auth_url = "https://auth.docker.io/token?service=registry.docker.io"
+    stub_request(:get, auth_url).
+      and_return(status: 200, body: { token: "token" }.to_json)
+
+    tags_url = "https://registry.hub.docker.com/v2/library/ubuntu/tags/list"
+    stub_request(:get, tags_url).
+      and_return(status: 200, body: registry_tags)
+  end
+
+  describe "#needs_update?" do
+    subject { checker.needs_update? }
+
+    context "given an outdated dependency" do
+      let(:version) { "17.04" }
+      it { is_expected.to be_truthy }
+    end
+
+    context "given an up-to-date dependency" do
+      let(:version) { "17.10" }
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe "#latest_version" do
+    subject { checker.latest_version }
+
+    it { is_expected.to eq(Gem::Version.new("17.10")) }
+
+    context "when the dependency has a non-numeric version" do
+      let(:version) { "artful" }
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe "#latest_resolvable_version" do
+    subject { checker.latest_resolvable_version }
+
+    before { allow(checker).to receive(:latest_version).and_return("delegate") }
+    it { is_expected.to eq("delegate") }
+  end
+
+  describe "#updated_requirements" do
+    subject { checker.updated_requirements }
+    it { is_expected.to eq(dependency.requirements) }
+  end
+end
