88b839b74159191ec00df2a25ed7f92b94154233
Grey Baker <greysteil@gmail.com>
2017-11-01 14:54:35 +0000

    Add basic parser for Maven

--- lib/dependabot/file_parsers/java/maven.rb
+++ lib/dependabot/file_parsers/java/maven.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "nokogiri"
+
 require "dependabot/dependency"
 require "dependabot/file_parsers/base"
 
@@ -8,11 +10,34 @@ module Dependabot
     module Java
       class Maven < Dependabot::FileParsers::Base
         def parse
-          # TODO: Return an array of `Dependency` objects, representing each
-          # of the project's dependencies
+          doc = Nokogiri::XML(pom.content)
+          doc.css("dependencies dependency").map do |dependency_node|
+            Dependency.new(
+              name: dependency_name(dependency_node),
+              version: dependency_node.at_css("version").content,
+              package_manager: "maven",
+              requirements: [{
+                requirement: dependency_node.at_css("version").content,
+                file: "pom.xml",
+                groups: [],
+                source: nil
+              }]
+            )
+          end
         end
 
         private
+
+        def dependency_name(dependency_node)
+          [
+            dependency_node.at_css("groupId").content,
+            dependency_node.at_css("artifactId").content
+          ].join("/")
+        end
+
+        def pom
+          @pom ||= get_original_file("pom.xml")
+        end
 
         def check_required_files
           %w(pom.xml).each do |filename|
--- spec/dependabot/file_parsers/java/maven_spec.rb
+++ spec/dependabot/file_parsers/java/maven_spec.rb
@@ -1,9 +1,48 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "dependabot/dependency_file"
 require "dependabot/file_parsers/java/maven"
 require_relative "../shared_examples_for_file_parsers"
 
 RSpec.describe Dependabot::FileParsers::Java::Maven do
   it_behaves_like "a dependency file parser"
+
+  let(:files) { [pom] }
+  let(:pom) do
+    Dependabot::DependencyFile.new(
+      name: "pom.xml",
+      content: pom_body
+    )
+  end
+  let(:pom_body) do
+    fixture("java", "poms", "basic_pom.xml")
+  end
+  let(:parser) { described_class.new(dependency_files: files) }
+
+  describe "parse" do
+    subject(:dependencies) { parser.parse }
+
+    its(:length) { is_expected.to eq(2) }
+
+    describe "the first dependency" do
+      subject(:dependency) { dependencies.first }
+
+      it "has the right details" do
+        expect(dependency).to be_a(Dependabot::Dependency)
+        expect(dependency.name).to eq("com.google.guava/guava")
+        expect(dependency.version).to eq("23.3-jre")
+        expect(dependency.requirements).to eq(
+          [
+            {
+              requirement: "23.3-jre",
+              file: "pom.xml",
+              groups: [],
+              source: nil
+            }
+          ]
+        )
+      end
+    end
+  end
 end
