0cb19d393d02338d045daf9b518eb4b3d0f47869
Grey Baker <greysteil@gmail.com>
2017-11-03 00:16:08 +0000

    Add crude MetadataFinder for Java

--- lib/dependabot/metadata_finders/java/maven.rb
+++ lib/dependabot/metadata_finders/java/maven.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "nokogiri"
 require "dependabot/metadata_finders/base"
 
 module Dependabot
@@ -9,11 +10,32 @@ module Dependabot
         private
 
         def look_up_source
-          # TODO: return a `Source` object with a github, gitlab or bitbucket
-          # host, and details of the repo. Use SOURCE_REGEX (on the base class)
-          # to check if any candidate URLs include these (candidate URLs
-          # normally come from hitting the registry's API for details of the
-          # dependency)
+          potential_source_urls = [
+            pom_file.at_css("project url")&.content,
+            pom_file.at_css("project scm url")&.content,
+            pom_file.at_css("project scm issueManagement")&.content
+          ].compact
+
+          source_url = potential_source_urls.find { |url| url =~ SOURCE_REGEX }
+
+          return nil unless source_url
+          captures = source_url.match(SOURCE_REGEX).named_captures
+          Source.new(host: captures.fetch("host"), repo: captures.fetch("repo"))
+        end
+
+        def pom_file
+          return @pom_file unless @pom_file.nil?
+
+          artifact_id = dependency.name.split("/").last
+          response = Excon.get(
+            "https://search.maven.org/remotecontent?filepath="\
+            "#{dependency.name.tr('.', '/')}/#{dependency.version}/"\
+            "#{artifact_id}-#{dependency.version}.pom",
+            idempotent: true,
+            middlewares: SharedHelpers.excon_middleware
+          )
+
+          @pom_file = Nokogiri::XML(response.body)
         end
       end
     end
