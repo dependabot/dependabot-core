dd39b855207ac86c3b17a31bf7383647494f333f
Grey Baker <greysteil@gmail.com>
2017-11-04 12:37:26 +0000

    Add spec for additional dependency declaration types

--- spec/dependabot/file_parsers/java/maven_spec.rb
+++ spec/dependabot/file_parsers/java/maven_spec.rb
@@ -10,38 +10,89 @@ RSpec.describe Dependabot::FileParsers::Java::Maven do
 
   let(:files) { [pom] }
   let(:pom) do
-    Dependabot::DependencyFile.new(
-      name: "pom.xml",
-      content: pom_body
-    )
+    Dependabot::DependencyFile.new(name: "pom.xml", content: pom_body)
   end
-  let(:pom_body) do
-    fixture("java", "poms", "basic_pom.xml")
-  end
+  let(:pom_body) { fixture("java", "poms", "basic_pom.xml") }
   let(:parser) { described_class.new(dependency_files: files) }
 
   describe "parse" do
     subject(:dependencies) { parser.parse }
 
-    its(:length) { is_expected.to eq(2) }
+    context "for top-level dependencies" do
+      its(:length) { is_expected.to eq(2) }
 
-    describe "the first dependency" do
-      subject(:dependency) { dependencies.first }
+      describe "the first dependency" do
+        subject(:dependency) { dependencies.first }
 
-      it "has the right details" do
-        expect(dependency).to be_a(Dependabot::Dependency)
-        expect(dependency.name).to eq("com.google.guava:guava")
-        expect(dependency.version).to eq("23.3-jre")
-        expect(dependency.requirements).to eq(
-          [
-            {
-              requirement: "23.3-jre",
-              file: "pom.xml",
-              groups: [],
-              source: nil
-            }
-          ]
-        )
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("com.google.guava:guava")
+          expect(dependency.version).to eq("23.3-jre")
+          expect(dependency.requirements).to eq(
+            [
+              {
+                requirement: "23.3-jre",
+                file: "pom.xml",
+                groups: [],
+                source: nil
+              }
+            ]
+          )
+        end
+      end
+    end
+
+    context "for dependencyManagement dependencies" do
+      let(:pom_body) do
+        fixture("java", "poms", "dependency_management_pom.xml")
+      end
+
+      its(:length) { is_expected.to eq(2) }
+
+      describe "the first dependency" do
+        subject(:dependency) { dependencies.first }
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("com.google.guava:guava")
+          expect(dependency.version).to eq("23.3-jre")
+          expect(dependency.requirements).to eq(
+            [
+              {
+                requirement: "23.3-jre",
+                file: "pom.xml",
+                groups: [],
+                source: nil
+              }
+            ]
+          )
+        end
+      end
+    end
+
+    context "for plugin dependencies" do
+      let(:pom_body) { fixture("java", "poms", "plugin_dependencies_pom.xml") }
+
+      its(:length) { is_expected.to eq(2) }
+
+      describe "the first dependency" do
+        subject(:dependency) { dependencies.first }
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("com.google.guava:guava")
+          expect(dependency.version).to eq("23.3-jre")
+          expect(dependency.requirements).to eq(
+            [
+              {
+                requirement: "23.3-jre",
+                file: "pom.xml",
+                groups: [],
+                source: nil
+              }
+            ]
+          )
+        end
       end
     end
   end
