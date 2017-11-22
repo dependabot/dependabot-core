cdee7aaa8a317ba6a1535f501f7b8123c4d0284a
Grey Baker <greysteil@gmail.com>
2017-11-22 23:17:13 +0000

    Add specs for UpdateCheckers::Base.up_to_date?

--- spec/dependabot/update_checkers/base_spec.rb
+++ spec/dependabot/update_checkers/base_spec.rb
@@ -55,6 +55,112 @@ RSpec.describe Dependabot::UpdateCheckers::Base do
       and_return(updated_requirements)
   end
 
+  describe "#up_to_date?" do
+    subject(:up_to_date) { updater_instance.up_to_date? }
+
+    context "when the dependency is outdated" do
+      let(:latest_version) { Gem::Version.new("1.6.0") }
+
+      it { is_expected.to be_falsey }
+
+      context "but cannot resolve to the new version" do
+        let(:latest_resolvable_version) { Gem::Version.new("1.5.0") }
+        it { is_expected.to be_falsey }
+      end
+    end
+
+    context "when the dependency is up-to-date" do
+      let(:latest_version) { Gem::Version.new("1.5.0") }
+      it { is_expected.to be_truthy }
+
+      it "doesn't attempt to resolve the dependency" do
+        expect(updater_instance).to_not receive(:latest_resolvable_version)
+        up_to_date
+      end
+    end
+
+    context "when the dependency couldn't be found" do
+      let(:latest_version) { nil }
+      it { is_expected.to be_falsey }
+    end
+
+    context "when the dependency has a SHA-1 hash version" do
+      let(:dependency) do
+        Dependabot::Dependency.new(
+          name: "business",
+          version: "5bfb6d149c410801f194da7ceb3b2bdc5e8b75f3",
+          requirements: [
+            { file: "Gemfile", requirement: ">= 0", groups: [], source: nil }
+          ],
+          package_manager: "bundler"
+        )
+      end
+
+      context "that matches the latest version" do
+        let(:latest_version) { "5bfb6d149c410801f194da7ceb3b2bdc5e8b75f3" }
+        it { is_expected.to be_truthy }
+      end
+
+      context "that does not match the latest version" do
+        let(:latest_version) { "4bfb6d149c410801f194da7ceb3b2bdc5e8b75f3" }
+        it { is_expected.to eq(false) }
+
+        context "but the latest latest_resolvable_version does" do
+          let(:latest_resolvable_version) do
+            "5bfb6d149c410801f194da7ceb3b2bdc5e8b75f3"
+          end
+          it { is_expected.to eq(false) }
+        end
+      end
+    end
+
+    context "when updating a requirement file" do
+      let(:dependency) do
+        Dependabot::Dependency.new(
+          name: "business",
+          requirements: requirements,
+          package_manager: "bundler"
+        )
+      end
+      let(:requirements) do
+        [{ file: "Gemfile", requirement: "~> 1", groups: [], source: nil }]
+      end
+
+      context "that already permits the latest version" do
+        let(:updated_requirements) { requirements }
+        it { is_expected.to be_truthy }
+      end
+
+      context "that doesn't yet permit the latest version" do
+        let(:updated_requirements) do
+          [
+            {
+              file: "Gemfile",
+              requirement: ">= 1, < 3",
+              groups: [],
+              source: nil
+            }
+          ]
+        end
+        it { is_expected.to be_falsey }
+      end
+
+      context "that we don't know how to fix" do
+        let(:updated_requirements) do
+          [
+            {
+              file: "Gemfile",
+              requirement: :unfixable,
+              groups: [],
+              source: nil
+            }
+          ]
+        end
+        it { is_expected.to be_falsey }
+      end
+    end
+  end
+
   describe "#can_update?" do
     subject(:can_update) { updater_instance.can_update? }
 
@@ -106,7 +212,9 @@ RSpec.describe Dependabot::UpdateCheckers::Base do
         it { is_expected.to eq(true) }
 
         context "but the latest latest_resolvable_version does" do
-          let(:latest_version) { "5bfb6d149c410801f194da7ceb3b2bdc5e8b75f3" }
+          let(:latest_resolvable_version) do
+            "5bfb6d149c410801f194da7ceb3b2bdc5e8b75f3"
+          end
           it { is_expected.to eq(false) }
         end
       end
