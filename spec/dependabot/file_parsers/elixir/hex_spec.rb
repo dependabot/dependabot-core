bf7ff570f79021307bb3338e53f381fae1302685
Steve Domin <steve@stevedomin.com>
2017-12-01 16:52:37 +0300

    Fix specs

--- spec/dependabot/file_parsers/elixir/hex_spec.rb
+++ spec/dependabot/file_parsers/elixir/hex_spec.rb
@@ -6,7 +6,7 @@ require "dependabot/file_parsers/elixir/hex"
 require_relative "../shared_examples_for_file_parsers"
 
 RSpec.describe Dependabot::FileParsers::Elixir::Hex do
-  # it_behaves_like "a dependency file parser"
+  it_behaves_like "a dependency file parser"
 
   let(:files) { [mixfile, lockfile] }
   let(:mixfile) do
@@ -21,7 +21,7 @@ RSpec.describe Dependabot::FileParsers::Elixir::Hex do
   let(:lockfile_body) do
     fixture("elixir", "lockfiles", "minor_version_specified")
   end
-  let(:parser) { described_class.new(dependency_files: files) }
+  let(:parser) { described_class.new(dependency_files: files, repo: "org/nm") }
 
   describe "parse" do
     subject(:dependencies) { parser.parse }
@@ -33,8 +33,8 @@ RSpec.describe Dependabot::FileParsers::Elixir::Hex do
         subject { dependencies.first }
 
         it { is_expected.to be_a(Dependabot::Dependency) }
-        its(:name) { is_expected.to eq("phoenix") }
-        its(:version) { is_expected.to eq("1.2.4") }
+        its(:name) { is_expected.to eq("phoenix_live_reload") }
+        its(:version) { is_expected.to eq("1.0.8") }
       end
     end
   end
