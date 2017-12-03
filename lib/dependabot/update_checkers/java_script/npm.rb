693b66a643c1698c82fc9caf66371cc5507167a0
Grey Baker <greysteil@gmail.com>
2017-12-03 13:39:20 +0000

    Move JS update checkers to base class structure

--- lib/dependabot/update_checkers/java_script/npm.rb
+++ lib/dependabot/update_checkers/java_script/npm.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-require "dependabot/update_checkers/java_script/yarn"
+require "dependabot/update_checkers/java_script/base"
 
 module Dependabot
   module UpdateCheckers
     module JavaScript
-      class Npm < Dependabot::UpdateCheckers::JavaScript::Yarn
-        # Identical logic to Yarn, since we're just talking about the
-        # package.json here, and neither npm nor Yarn use a resolver by default
+      class Npm < Dependabot::UpdateCheckers::JavaScript::Base
       end
     end
   end
--- spec/dependabot/update_checkers/shared_examples_for_update_checkers.rb
+++ spec/dependabot/update_checkers/shared_examples_for_update_checkers.rb
@@ -8,36 +8,56 @@ RSpec.shared_examples "an update checker" do
     subject { described_class }
     let(:base_class) { Dependabot::UpdateCheckers::Base }
 
-    its(:superclass) { is_expected.to eq(base_class) }
+    def recent_ancestors
+      ancestors = described_class.ancestors.take_while { |a| a != base_class }
+      [described_class] + ancestors
+    end
+
+    def own_public_methods(include_ancestor_methods)
+      (recent_ancestors + [described_class]).
+        map { |cls| cls.public_instance_methods(include_ancestor_methods) }.
+        flatten.
+        uniq
+    end
+
+    def own_private_methods(include_ancestor_methods)
+      (recent_ancestors + [described_class]).
+        map { |cls| cls.private_instance_methods(include_ancestor_methods) }.
+        flatten
+    end
+
+    it "inherits from the base class" do
+      expect(described_class.ancestors).to include(base_class)
+    end
 
     it "implements updated_requirements" do
-      expect(described_class.public_instance_methods(false)).
+      expect(own_public_methods(false)).
         to include(:updated_requirements)
     end
 
     it "implements latest_version" do
-      expect(described_class.public_instance_methods(false)).
+      expect(own_public_methods(false)).
         to include(:latest_version)
     end
 
     it "implements latest_resolvable_version" do
-      expect(described_class.public_instance_methods(false)).
+      expect(own_public_methods(false)).
         to include(:latest_resolvable_version)
     end
 
     it "implements latest_version_resolvable_with_full_unlock?" do
-      expect(described_class.private_instance_methods(false)).
+      expect(own_private_methods(false)).
         to include(:latest_version_resolvable_with_full_unlock?)
     end
 
     it "implements updated_dependencies_after_full_unlock" do
-      expect(described_class.private_instance_methods(false)).
+      expect(own_private_methods(false)).
         to include(:updated_dependencies_after_full_unlock)
     end
 
     it "doesn't define any additional public instance methods" do
-      expect(described_class.public_instance_methods).
-        to match_array(base_class.public_instance_methods)
+      expect(own_public_methods(true)).
+        to match_array(base_class.public_instance_methods(true))
     end
   end
 end
