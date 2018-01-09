c354991411c7e7329cd948ca92edc3673157c87f
Grey Baker <greysteil@gmail.com>
2018-01-09 12:37:41 +0000

    Update FileFetcher for new setup

--- lib/dependabot/file_fetchers/elixir/hex.rb
+++ lib/dependabot/file_fetchers/elixir/hex.rb
@@ -6,8 +6,29 @@ module Dependabot
   module FileFetchers
     module Elixir
       class Hex < Dependabot::FileFetchers::Base
-        def self.required_files
-          %w(mix.exs mix.lock)
+        def self.required_files_in?(filenames)
+          (%w(mix.exs mix.lock) - filenames).empty?
+        end
+
+        def self.required_files_message
+          "Repo must contain a mix.exs and a mix.lock."
+        end
+
+        private
+
+        def fetch_files
+          fetched_files = []
+          fetched_files << mixfile
+          fetched_files << lockfile
+          fetched_files
+        end
+
+        def mixfile
+          @mixfile ||= fetch_file_from_github("mix.exs")
+        end
+
+        def lockfile
+          @lockfile ||= fetch_file_from_github("mix.lock")
         end
       end
     end
