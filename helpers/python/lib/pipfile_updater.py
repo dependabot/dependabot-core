47d3622e0a0d7786349dd88a01fd873b338fae14
Grey Baker <greysteil@gmail.com>
2018-02-25 00:06:03 +0000

    Python: Use Pipenv's new --keep-outdated option insted of freezing Pipfile

--- helpers/python/lib/pipfile_updater.py
+++ helpers/python/lib/pipfile_updater.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def update(directory):
-   os.system("cd {0} && pipenv lock".format(directory))
+   os.system("cd {0} && pipenv lock --keep-outdated".format(directory))
 
    lockfile = open(directory + "/Pipfile.lock", "r").read()
    return json.dumps({ "result": { "Pipfile.lock": lockfile } })
