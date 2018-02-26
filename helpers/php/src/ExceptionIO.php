3a15530529c511b2c56b976daa5cd8f8601c1a42
Grey Baker <greysteil@gmail.com>
2018-02-26 15:51:56 +0000

    PHP: Handle array errors in ExceptionIO

--- helpers/php/src/ExceptionIO.php
+++ helpers/php/src/ExceptionIO.php
@@ -10,6 +10,9 @@ class ExceptionIO extends NullIO
 {
     public function writeError($messages, $newline = true, $verbosity = self::NORMAL): void
     {
+        if (is_array($messages)) {
+          return;
+        }
         if (strpos($messages, 'Your requirements could not be resolved') !== false) {
             throw new \RuntimeException('Requirements could not be resolved');
         }
