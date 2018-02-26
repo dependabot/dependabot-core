2610cb8cc4aa9ef6f68613a2cf20b85e9c49c08a
Grey Baker <greysteil@gmail.com>
2018-02-26 15:57:07 +0000

    Fix PHP formatting

--- helpers/php/src/ExceptionIO.php
+++ helpers/php/src/ExceptionIO.php
@@ -11,7 +11,7 @@ class ExceptionIO extends NullIO
     public function writeError($messages, $newline = true, $verbosity = self::NORMAL): void
     {
         if (is_array($messages)) {
-          return;
+            return;
         }
         if (strpos($messages, 'Your requirements could not be resolved') !== false) {
             throw new \RuntimeException('Requirements could not be resolved');
