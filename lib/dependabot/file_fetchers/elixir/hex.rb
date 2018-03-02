370bbb62e1a65824df14033798ea0dabffb21d91
Grey Baker <greysteil@gmail.com>
2018-03-02 09:42:50 +0000

    Elixir: Extend FileFetcher to handle Umbrella apps

--- lib/dependabot/file_fetchers/elixir/hex.rb
+++ lib/dependabot/file_fetchers/elixir/hex.rb
@@ -20,6 +20,7 @@ module Dependabot
           fetched_files = []
           fetched_files << mixfile
           fetched_files << lockfile
+          fetched_files += subapp_mixfiles
           fetched_files
         end
 
@@ -29,6 +30,22 @@ module Dependabot
 
         def lockfile
           @lockfile ||= fetch_file_from_host("mix.lock")
+        end
+
+        def subapp_mixfiles
+          return [] unless repo_contents.any? { |f| f.name == "apps" }
+          app_directories =
+            repo_contents(dir: "apps").
+            select { |f| f.type == "dir" }
+
+          app_directories.map do |dir|
+            fetch_file_from_host("#{dir.path}/mix.exs")
+          rescue Dependabot::DependencyFileNotFound
+            # If the folder doesn't have a mix.exs is *might* be because it's
+            # not an app. Ignore the fact we couldn't fetch one and proceed with
+            # updating (it will blow up later if there are problems)
+            nil
+          end.compact
         end
       end
     end
--- spec/dependabot/file_fetchers/elixir/hex_spec.rb
+++ spec/dependabot/file_fetchers/elixir/hex_spec.rb
@@ -5,4 +5,97 @@ require_relative "../shared_examples_for_file_fetchers"
 
 RSpec.describe Dependabot::FileFetchers::Elixir::Hex do
   it_behaves_like "a dependency file fetcher"
+
+  let(:source) { { host: "github", repo: "gocardless/bump" } }
+  let(:file_fetcher_instance) do
+    described_class.new(source: source, credentials: credentials)
+  end
+  let(:url) { "https://api.github.com/repos/gocardless/bump/contents/" }
+  let(:credentials) do
+    [
+      {
+        "host" => "github.com",
+        "username" => "x-access-token",
+        "password" => "token"
+      }
+    ]
+  end
+
+  before { allow(file_fetcher_instance).to receive(:commit).and_return("sha") }
+
+  context "with an umbrella app" do
+    before do
+      stub_request(:get, url + "?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_umbrella.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "mix.exs?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_mixfile.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "mix.lock?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_lockfile.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "apps?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_umbrella_apps.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, url + "apps/bank/mix.exs?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_bank_mixfile.json"),
+          headers: { "content-type" => "application/json" }
+        )
+      stub_request(:get, url + "apps/bank_web/mix.exs?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_elixir_bank_web_mixfile.json"),
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    it "fetches the mixfiles for the sub-apps" do
+      expect(file_fetcher_instance.files.count).to eq(4)
+      expect(file_fetcher_instance.files.map(&:name)).
+        to include("apps/bank/mix.exs")
+    end
+
+    context "when one of the folders isn't an app" do
+      before do
+        stub_request(:get, url + "apps/bank_web/mix.exs?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 404,
+            headers: { "content-type" => "application/json" }
+          )
+      end
+
+      it "fetches the mixfiles for the other sub-apps" do
+        expect(file_fetcher_instance.files.count).to eq(3)
+        expect(file_fetcher_instance.files.map(&:name)).
+          to include("apps/bank/mix.exs")
+        expect(file_fetcher_instance.files.map(&:name)).
+          to_not include("apps/bank_web/mix.exs")
+      end
+    end
+  end
 end
