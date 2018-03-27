a7f75bff86af64408f6d59306f51c66b13a59715
Grey Baker <greysteil@gmail.com>
2018-03-27 16:37:42 +0100

    Rust: Fetch path dependency Cargo.toml files

--- lib/dependabot/file_fetchers/rust/cargo.rb
+++ lib/dependabot/file_fetchers/rust/cargo.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require "toml-rb"
+
 require "dependabot/file_fetchers/base"
+require "dependabot/file_parsers/rust/cargo"
 
 module Dependabot
   module FileFetchers
@@ -20,8 +23,52 @@ module Dependabot
           fetched_files = []
           fetched_files << cargo_toml
           fetched_files << cargo_lock if cargo_lock
+          fetched_files += path_dependency_files
           # TODO: Read the Cargo.toml and get any workspaces
           fetched_files
+        end
+
+        def path_dependency_files
+          @path_dependency_files ||=
+            fetch_path_dependency_files(
+              file: cargo_toml,
+              previously_fetched_files: []
+            )
+        end
+
+        def fetch_path_dependency_files(file:, previously_fetched_files:)
+          current_dir = file.name.split("/")[0..-2].join("/")
+
+          path_dependency_paths_from_file(file).flat_map do |path|
+            path = File.join(current_dir, path) unless current_dir.nil?
+            path = Pathname.new(path).cleanpath.to_path
+
+            next if previously_fetched_files.map(&:name).include?(path)
+            next if file.name == path
+
+            fetched_file = fetch_file_from_host(path)
+            grandchild_requirement_files = fetch_path_dependency_files(
+              file: fetched_file,
+              previously_fetched_files: previously_fetched_files + [file]
+            )
+            [fetched_file, *grandchild_requirement_files]
+          end.compact
+        end
+
+        def path_dependency_paths_from_file(file)
+          FileParsers::Rust::Cargo::DEPENDENCY_TYPES.flat_map do |type|
+            parsed_file(file).fetch(type, {}).map do |_, details|
+              next unless details.is_a?(Hash)
+              next unless details["path"]
+              File.join(details["path"], "Cargo.toml")
+            end
+          end.compact
+        end
+
+        def parsed_file(file)
+          TomlRB.parse(file.content)
+        rescue TomlRB::ParseError
+          raise Dependabot::DependencyFileNotParseable, file.path
         end
 
         def cargo_toml
--- spec/dependabot/file_fetchers/rust/cargo_spec.rb
+++ spec/dependabot/file_fetchers/rust/cargo_spec.rb
@@ -76,6 +76,121 @@ RSpec.describe Dependabot::FileFetchers::Rust::Cargo do
     end
   end
 
+  context "with a path dependency" do
+    before do
+      stub_request(:get, url + "?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_cargo_without_lockfile.json"),
+          headers: json_header
+        )
+      stub_request(:get, url + "Cargo.toml?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(status: 200, body: parent_fixture, headers: json_header)
+    end
+    let(:parent_fixture) do
+      fixture("github", "contents_cargo_manifest_path_deps.json")
+    end
+
+    context "that is fetchable" do
+      before do
+        stub_request(:get, url + "src/s3/Cargo.toml?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(status: 200, body: path_dep_fixture, headers: json_header)
+      end
+      let(:path_dep_fixture) do
+        fixture("github", "contents_cargo_manifest.json")
+      end
+
+      it "fetches the path dependency's Cargo.toml" do
+        expect(file_fetcher_instance.files.map(&:name)).
+          to match_array(%w(Cargo.toml /src/s3/Cargo.toml))
+      end
+
+      context "with a trailing slash in the path" do
+        let(:parent_fixture) do
+          fixture(
+            "github",
+            "contents_cargo_manifest_path_deps_trailing_slash.json"
+          )
+        end
+
+        it "fetches the path dependency's Cargo.toml" do
+          expect(file_fetcher_instance.files.map(&:name)).
+            to match_array(%w(Cargo.toml /src/s3/Cargo.toml))
+        end
+      end
+
+      context "with a directory" do
+        let(:file_fetcher_instance) do
+          described_class.new(
+            source: source,
+            credentials: credentials,
+            directory: "my_dir/"
+          )
+        end
+
+        let(:url) do
+          "https://api.github.com/repos/gocardless/bump/contents/my_dir/"
+        end
+        before do
+          stub_request(:get, "https://api.github.com/repos/gocardless/bump/"\
+                             "contents/my_dir?ref=sha").
+            with(headers: { "Authorization" => "token token" }).
+            to_return(
+              status: 200,
+              body: fixture("github", "contents_cargo_without_lockfile.json"),
+              headers: json_header
+            )
+        end
+
+        it "fetches the path dependency's Cargo.toml" do
+          expect(file_fetcher_instance.files.map(&:name)).
+            to match_array(%w(Cargo.toml /src/s3/Cargo.toml))
+          expect(file_fetcher_instance.files.map(&:path)).
+            to match_array(%w(/my_dir/Cargo.toml /my_dir/src/s3/Cargo.toml))
+        end
+      end
+
+      context "and includes another path dependency" do
+        let(:path_dep_fixture) do
+          fixture("github", "contents_cargo_manifest_path_deps.json")
+        end
+
+        before do
+          stub_request(:get, url + "src/s3/src/s3/Cargo.toml?ref=sha").
+            with(headers: { "Authorization" => "token token" }).
+            to_return(
+              status: 200,
+              body: fixture("github", "contents_cargo_manifest.json"),
+              headers: json_header
+            )
+        end
+
+        it "fetches the nested path dependency's Cargo.toml" do
+          expect(file_fetcher_instance.files.map(&:name)).
+            to match_array(
+              %w(Cargo.toml /src/s3/Cargo.toml /src/s3/src/s3/Cargo.toml)
+            )
+        end
+      end
+    end
+
+    context "that is not fetchable" do
+      before do
+        stub_request(:get, url + "src/s3/Cargo.toml?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(status: 404, headers: json_header)
+      end
+
+      it "raises a DependencyFileNotFound error" do
+        expect { file_fetcher_instance.files }.
+          to raise_error(Dependabot::DependencyFileNotFound)
+      end
+    end
+  end
+
   context "without a Cargo.toml" do
     before do
       stub_request(:get, url + "?ref=sha").
