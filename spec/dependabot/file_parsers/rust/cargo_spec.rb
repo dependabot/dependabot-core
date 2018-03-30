551481ed54499a6b7260f504006a6b5e2a015970
Grey Baker <greysteil@gmail.com>
2018-03-30 16:22:13 +0100

    Rust: Test file parsing for complicated workspace setup

--- spec/dependabot/file_parsers/rust/cargo_spec.rb
+++ spec/dependabot/file_parsers/rust/cargo_spec.rb
@@ -109,6 +109,149 @@ RSpec.describe Dependabot::FileParsers::Rust::Cargo do
         end
       end
 
+      context "with workspaces" do
+        let(:manifest_fixture_name) { "workspace_root" }
+        let(:lockfile_fixture_name) { "workspace" }
+        let(:files) { [manifest, lockfile, workspace_child] }
+        let(:workspace_child) do
+          Dependabot::DependencyFile.new(
+            name: "lib/sub_crate/Cargo.toml",
+            content: fixture("rust", "manifests", "workspace_child")
+          )
+        end
+
+        its(:length) { is_expected.to eq(13) }
+
+        describe "top level dependencies" do
+          subject(:top_level_dependencies) { dependencies.select(&:top_level?) }
+
+          its(:length) { is_expected.to eq(2) }
+
+          describe "the first dependency" do
+            subject(:dependency) { top_level_dependencies.first }
+
+            it "has the right details" do
+              expect(dependency).to be_a(Dependabot::Dependency)
+              expect(dependency.name).to eq("regex")
+              expect(dependency.version).to eq("0.2.10")
+              expect(dependency.requirements).to eq(
+                [{
+                  requirement: "=0.2.10",
+                  file: "Cargo.toml",
+                  groups: ["dependencies"],
+                  source: nil
+                }]
+              )
+            end
+          end
+
+          describe "the last dependency" do
+            subject(:dependency) { top_level_dependencies.last }
+
+            it "has the right details" do
+              expect(dependency).to be_a(Dependabot::Dependency)
+              expect(dependency.name).to eq("log")
+              expect(dependency.version).to eq("0.4.0")
+              expect(dependency.requirements).to eq(
+                [{
+                  requirement: "=0.4.0",
+                  file: "lib/sub_crate/Cargo.toml",
+                  groups: ["dependencies"],
+                  source: nil
+                }]
+              )
+            end
+          end
+        end
+
+        context "with a virtual workspace root" do
+          let(:manifest_fixture_name) { "virtual_workspace_root" }
+          let(:lockfile_fixture_name) { "virtual_workspace" }
+          let(:files) do
+            [
+              manifest,
+              lockfile,
+              workspace_child,
+              workspace_child2,
+              workspace_child3
+            ]
+          end
+          let(:workspace_child) do
+            Dependabot::DependencyFile.new(
+              name: "lib/sub_crate/Cargo.toml",
+              content: fixture("rust", "manifests", "workspace_child")
+            )
+          end
+          let(:workspace_child2) do
+            Dependabot::DependencyFile.new(
+              name: "lib/sub_crate2/Cargo.toml",
+              content: workspace_child2_body
+            )
+          end
+          let(:workspace_child3) do
+            Dependabot::DependencyFile.new(
+              name: "lib/sub_crate3/Cargo.toml",
+              content: workspace_child2_body
+            )
+          end
+          let(:workspace_child2_body) do
+            fixture("rust", "manifests", "workspace_child_with_path_dependency")
+          end
+
+          describe "top level dependencies" do
+            subject(:top_level_dependencies) do
+              dependencies.select(&:top_level?)
+            end
+
+            its(:length) { is_expected.to eq(2) }
+
+            describe "the first dependency" do
+              subject(:dependency) { top_level_dependencies.first }
+
+              it "has the right details" do
+                expect(dependency).to be_a(Dependabot::Dependency)
+                expect(dependency.name).to eq("log")
+                expect(dependency.version).to eq("0.4.0")
+                expect(dependency.requirements).to eq(
+                  [{
+                    requirement: "=0.4.0",
+                    file: "lib/sub_crate/Cargo.toml",
+                    groups: ["dependencies"],
+                    source: nil
+                  }]
+                )
+              end
+            end
+
+            describe "the last dependency" do
+              subject(:dependency) { top_level_dependencies.last }
+
+              it "has the right details" do
+                expect(dependency).to be_a(Dependabot::Dependency)
+                expect(dependency.name).to eq("dependabot_sub_crate")
+                expect(dependency.version).to eq("0.2.0")
+                expect(dependency.requirements).to eq(
+                  [
+                    {
+                      requirement: nil,
+                      file: "lib/sub_crate2/Cargo.toml",
+                      groups: ["dependencies"],
+                      source: { type: "path" }
+                    },
+                    {
+                      requirement: nil,
+                      file: "lib/sub_crate3/Cargo.toml",
+                      groups: ["dependencies"],
+                      source: { type: "path" }
+                    }
+                  ]
+                )
+              end
+            end
+          end
+        end
+      end
+
       context "with a git dependency" do
         let(:manifest_fixture_name) { "git_dependency" }
 
