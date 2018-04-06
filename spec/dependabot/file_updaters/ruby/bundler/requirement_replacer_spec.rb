f005e03c708829b42b724152ab2299784136ab0f
Grey Baker <greysteil@gmail.com>
2018-04-06 12:09:00 +0100

    Additional spec

--- spec/dependabot/file_updaters/ruby/bundler/requirement_replacer_spec.rb
+++ spec/dependabot/file_updaters/ruby/bundler/requirement_replacer_spec.rb
@@ -92,6 +92,11 @@ RSpec.describe Dependabot::FileUpdaters::Ruby::Bundler::RequirementReplacer do
         end
       end
 
+      context "with a conditional" do
+        let(:content) { %(gem "rouge", ENV["ROUGE"] if ENV["ROUGE"]) }
+        it { is_expected.to eq(content) }
+      end
+
       context "with a dependency that uses single quotes" do
         let(:content) { %(gem "business", '~> 1.0') }
         it { is_expected.to eq(%(gem "business", '~> 1.5.0')) }
