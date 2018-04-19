765b12a32491ec7f1f26049ec13e85454ff7a472
Grey Baker <greysteil@gmail.com>
2018-04-19 18:15:16 +0100

    Java: Fetch all poms for multimodule projects

--- lib/dependabot/file_fetchers/java/maven.rb
+++ lib/dependabot/file_fetchers/java/maven.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require "nokogiri"
 require "dependabot/file_fetchers/base"
 
 module Dependabot
   module FileFetchers
     module Java
       class Maven < Dependabot::FileFetchers::Base
+        MODULE_SELECTOR = "project > modules > module"
+
         def self.required_files_in?(filenames)
           (%w(pom.xml) - filenames).empty?
         end
@@ -19,11 +22,43 @@ module Dependabot
         def fetch_files
           fetched_files = []
           fetched_files << pom
+          fetched_files += child_poms
           fetched_files
         end
 
         def pom
           @pom ||= fetch_file_from_host("pom.xml")
+        end
+
+        def child_poms
+          recursively_fetch_child_poms(pom, fetched_filenames: ["pom.xml"])
+        end
+
+        def recursively_fetch_child_poms(pom, fetched_filenames:)
+          base_path = pom.name.gsub(/pom\.xml$/, "")
+          doc = Nokogiri::XML(pom.content)
+
+          doc.css(MODULE_SELECTOR).flat_map do |module_node|
+            relative_path = module_node.content.strip
+            name_parts = [
+              base_path,
+              relative_path,
+              relative_path.end_with?("pom.xml") ? nil : "pom.xml"
+            ].compact.reject(&:empty?)
+            path = Pathname.new(File.join(*name_parts)).cleanpath.to_path
+
+            next [] if fetched_filenames.include?(path)
+
+            child_pom = fetch_file_from_host(File.join(*name_parts))
+            fetched_filenames += [child_pom.name]
+            [
+              child_pom,
+              recursively_fetch_child_poms(
+                child_pom,
+                fetched_filenames: fetched_filenames
+              )
+            ].flatten
+          end
         end
       end
     end
--- spec/dependabot/file_fetchers/java/maven_spec.rb
+++ spec/dependabot/file_fetchers/java/maven_spec.rb
@@ -5,4 +5,156 @@ require_relative "../shared_examples_for_file_fetchers"
 
 RSpec.describe Dependabot::FileFetchers::Java::Maven do
   it_behaves_like "a dependency file fetcher"
+
+  let(:source) { { host: "github", repo: "gocardless/bump" } }
+  let(:file_fetcher_instance) do
+    described_class.new(source: source, credentials: credentials)
+  end
+  let(:github_url) { "https://api.github.com/" }
+  let(:url) { github_url + "repos/gocardless/bump/contents/" }
+  let(:credentials) do
+    [{
+      "host" => "github.com",
+      "username" => "x-access-token",
+      "password" => "token"
+    }]
+  end
+
+  before { allow(file_fetcher_instance).to receive(:commit).and_return("sha") }
+
+  context "with a basic pom" do
+    before do
+      stub_request(:get, File.join(url, "pom.xml?ref=sha")).
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_java_basic_pom.json"),
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    it "fetches the pom" do
+      expect(file_fetcher_instance.files.count).to eq(1)
+      expect(file_fetcher_instance.files.map(&:name)).
+        to match_array(%w(pom.xml))
+    end
+  end
+
+  context "with a multimodule pom" do
+    before do
+      stub_request(:get, File.join(url, "pom.xml?ref=sha")).
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_java_multimodule_pom.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, File.join(url, "util/pom.xml?ref=sha")).
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_java_basic_pom.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, File.join(url, "business-app/pom.xml?ref=sha")).
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_java_basic_pom.json"),
+          headers: { "content-type" => "application/json" }
+        )
+
+      stub_request(:get, File.join(url, "legacy/pom.xml?ref=sha")).
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_java_basic_pom.json"),
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    it "fetches the poms" do
+      expect(file_fetcher_instance.files.count).to eq(4)
+      expect(file_fetcher_instance.files.map(&:name)).
+        to match_array(
+          %w(pom.xml util/pom.xml business-app/pom.xml legacy/pom.xml)
+        )
+    end
+
+    context "with a nested multimodule pom" do
+      before do
+        stub_request(:get, File.join(url, "util/pom.xml?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_java_multimodule_pom.json"),
+            headers: { "content-type" => "application/json" }
+          )
+
+        stub_request(:get, File.join(url, "util/util/pom.xml?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_java_basic_pom.json"),
+            headers: { "content-type" => "application/json" }
+          )
+
+        stub_request(:get, File.join(url, "util/business-app/pom.xml?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_java_basic_pom.json"),
+            headers: { "content-type" => "application/json" }
+          )
+
+        stub_request(:get, File.join(url, "util/legacy/pom.xml?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_java_basic_pom.json"),
+            headers: { "content-type" => "application/json" }
+          )
+      end
+
+      it "fetches the poms" do
+        expect(file_fetcher_instance.files.count).to eq(7)
+        expect(file_fetcher_instance.files.map(&:name)).
+          to match_array(
+            %w(
+              pom.xml util/pom.xml business-app/pom.xml legacy/pom.xml
+              util/util/pom.xml util/legacy/pom.xml util/business-app/pom.xml
+            )
+          )
+      end
+
+      context "where multiple poms require the same file" do
+        before do
+          stub_request(:get, File.join(url, "util/legacy/pom.xml?ref=sha")).
+            with(headers: { "Authorization" => "token token" }).
+            to_return(
+              status: 200,
+              body: fixture("github", "contents_java_relative_module_pom.json"),
+              headers: { "content-type" => "application/json" }
+            )
+        end
+
+        it "fetches the poms uniquely" do
+          expect(file_fetcher_instance.files.count).to eq(7)
+          expect(file_fetcher_instance.files.map(&:name)).
+            to match_array(
+              %w(
+                pom.xml util/pom.xml business-app/pom.xml legacy/pom.xml
+                util/util/pom.xml util/legacy/pom.xml util/business-app/pom.xml
+              )
+            )
+          expect(WebMock).to have_requested(
+            :get,
+            File.join(url, "util/util/pom.xml?ref=sha")
+          ).once
+        end
+      end
+    end
+  end
 end
