3ec363a9769b1599aba843719d17e3d131cf19f8
Juliano Solanho <juliano.solanho@gmail.com>
2018-05-24 17:20:28 -0300

    Elm file fetcher

---
+++ lib/dependabot/file_fetchers/elm/elm_package.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "dependabot/file_fetchers/base"
+
+module Dependabot
+  module FileFetchers
+    module Elm
+      class ElmPackage < Dependabot::FileFetchers::Base
+        def self.required_files_in?(filenames)
+          (required_files - filenames).empty?
+        end
+
+        def self.required_files_message
+          "Repo must contain an " + required_files.join(" and an ")
+        end
+
+        private
+
+        def fetch_files
+          required_files.
+          	map {|filename| fetch_file_from_host(filename)}
+        end
+
+        def required_files
+        	[elm_package, exact_deps]
+        end
+
+        def elm_package
+        	"elm-package.json"
+        end
+
+        def exact_deps
+        	"elm-stuff/exact-dependencies.json"
+        end
+      end
+    end
+  end
+end
