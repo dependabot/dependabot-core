e4c93a791aa411e9c0ab70d77ad48ff7f258868c
Grey Baker <greysteil@gmail.com>
2018-06-21 13:32:05 -0400

    .NET: Allow repos with just a .sln file at the top level

--- lib/dependabot/file_fetchers/dotnet/nuget.rb
+++ lib/dependabot/file_fetchers/dotnet/nuget.rb
@@ -11,6 +11,7 @@ module Dependabot
 
         def self.required_files_in?(filenames)
           return true if filenames.any? { |f| f.match?(/^packages\.config$/i) }
+          return true if filenames.any? { |f| f.end_with?(".sln") }
           filenames.any? { |name| name.match?(%r{^[^/]*\.(cs|vb|fs)proj$}) }
         end
 
