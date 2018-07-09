b70f4283bebbf9ae73f9588995a5c286a115f288
Grey Baker <greysteil@gmail.com>
2018-07-09 17:31:00 -0400

    Fix JS style

--- helpers/yarn/lib/lockfile-parser.js
+++ helpers/yarn/lib/lockfile-parser.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
 const path = require("path");
-const parseLockfile = require("@dependabot/yarn-lib/lib/lockfile/parse").default;
+const parseLockfile = require("@dependabot/yarn-lib/lib/lockfile/parse")
+  .default;
 
 // Inspired by yarn-tools. Altered to ensure the latest version is always used
 // for version ranges which allow it.
 async function parse(directory) {
   const readFile = fileName =>
     fs.readFileSync(path.join(directory, fileName)).toString();
-  const data = readFile("yarn.lock")
+  const data = readFile("yarn.lock");
   return parseLockfile(data).object;
-};
+}
 
 module.exports = { parse };
