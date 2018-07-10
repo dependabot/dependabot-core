4dbbc3f53c420918ae96880bcdb4f73802f6d1c4
Grey Baker <greysteil@gmail.com>
2018-07-10 15:25:03 -0400

    Remove unused code

--- helpers/yarn/lib/lockfile-parser.js
+++ helpers/yarn/lib/lockfile-parser.js
@@ -1,10 +1,16 @@
+/* YARN.LOCK PARSER
+ *
+ * Inputs:
+ *  - directory containing a yarn.lock
+ *
+ * Outputs:
+ *  - JSON formatted yarn.lock
+ */
 const fs = require("fs");
 const path = require("path");
 const parseLockfile = require("@dependabot/yarn-lib/lib/lockfile/parse")
   .default;
 
-// Inspired by yarn-tools. Altered to ensure the latest version is always used
-// for version ranges which allow it.
 async function parse(directory) {
   const readFile = fileName =>
     fs.readFileSync(path.join(directory, fileName)).toString();
