febf2dc4d7eb310c1a250641765c1a2c2ba0833d
Grey Baker <greysteil@gmail.com>
2018-07-22 14:30:32 +0100

    Dep: Move UpdateCheckers::Go::Dep#latest_version logic into separate class

--- lib/dependabot/update_checkers/go/dep.rb
+++ lib/dependabot/update_checkers/go/dep.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
-require "toml-rb"
-
-require "dependabot/source"
 require "dependabot/update_checkers/base"
-require "dependabot/git_commit_checker"
 
 module Dependabot
   module UpdateCheckers
     module Go
       class Dep < Dependabot::UpdateCheckers::Base
+        require_relative "dep/latest_version_finder"
+
         def latest_version
           @latest_version ||=
-            if git_dependency? then latest_version_for_git_dependency
-            else latest_release_tag_version
-            end
+            LatestVersionFinder.new(
+              dependency: dependency,
+              dependency_files: dependency_files,
+              credentials: credentials,
+              ignored_versions: ignored_versions
+            ).latest_version
         end
 
         def latest_resolvable_version
@@ -46,90 +47,6 @@ module Dependabot
           raise NotImplementedError
         end
 
-        def latest_release_tag_version
-          if @latest_release_tag_lookup_attempted
-            return @latest_release_tag_version
-          end
-
-          @latest_release_tag_lookup_attempted = true
-
-          latest_release_version_str = fetch_latest_release_tag&.sub(/^v?/, "")
-          return unless latest_release_version_str
-          return unless version_class.correct?(latest_release_version_str)
-
-          @latest_release_tag_version =
-            version_class.new(latest_release_version_str)
-        end
-
-        def fetch_latest_release_tag
-          # If this is a git dependency then getting the latest tag is trivial
-          if git_dependency?
-            return git_commit_checker.local_tag_for_latest_version&.fetch(:tag)
-          end
-
-          # If not, we need to find the URL for the source code.
-          path = dependency.requirements.
-                 map { |r| r.dig(:source, :source) }.
-                 compact.first
-          path ||= dependency.name
-
-          updated_path = path.gsub(%r{^golang\.org/x}, "github.com/golang")
-          # Currently, Dependabot::Source.new will return `nil` if it can't find
-          # a git SCH associated with a path. If it is ever extended to handle
-          # non-git sources we'll need to add an additional check here.
-          source = Source.from_url(updated_path)
-          return unless source
-
-          # Given a source, we want to find the latest tag. Piggy-back off the
-          # logic in GitCommitChecker to do so.
-          git_dep = Dependency.new(
-            name: dependency.name,
-            version: dependency.version,
-            requirements: [{
-              file: "Gopkg.toml",
-              groups: [],
-              requirement: nil,
-              source: { type: "git", url: source.url }
-            }],
-            package_manager: dependency.package_manager
-          )
-
-          GitCommitChecker.
-            new(dependency: git_dep, credentials: credentials).
-            local_tag_for_latest_version&.fetch(:tag)
-        end
-
-        def latest_version_for_git_dependency
-          latest_release = latest_release_tag_version
-
-          # If there's been a release that includes the current pinned ref or
-          # that the current branch is behind, we switch to that release.
-          return latest_release if git_branch_or_ref_in_release?(latest_release)
-
-          # Otherwise, if the gem isn't pinned, the latest version is just the
-          # latest commit for the specified branch.
-          unless git_commit_checker.pinned?
-            return git_commit_checker.head_commit_for_current_branch
-          end
-
-          # If the dependency is pinned to a tag that looks like a version then
-          # we want to update that tag. The latest version will be the tag name
-          # (NOT the tag SHA, unlike in other package managers).
-          if git_commit_checker.pinned_ref_looks_like_version?
-            latest_tag = git_commit_checker.local_tag_for_latest_version
-            return latest_tag&.fetch(:tag)
-          end
-
-          # If the dependency is pinned to a tag that doesn't look like a
-          # version then there's nothing we can do.
-          nil
-        end
-
-        def git_branch_or_ref_in_release?(release)
-          return false unless release
-          git_commit_checker.branch_or_ref_in_release?(release)
-        end
-
         def dependencies_to_import
           # There's no way to tell whether dependencies that appear in the
           # lockfile are there because they're imported themselves or because
@@ -142,19 +59,6 @@ module Dependabot
           parsed_file(lockfile).fetch("required").map do |detail|
             detail["name"]
           end
-        end
-
-        def git_dependency?
-          git_commit_checker.git_dependency?
-        end
-
-        def git_commit_checker
-          @git_commit_checker ||=
-            GitCommitChecker.new(
-              dependency: dependency,
-              credentials: credentials,
-              ignored_versions: ignored_versions
-            )
         end
 
         def parsed_file(file)
--- spec/dependabot/update_checkers/go/dep_spec.rb
+++ spec/dependabot/update_checkers/go/dep_spec.rb
@@ -72,102 +72,16 @@ RSpec.describe Dependabot::UpdateCheckers::Go::Dep do
 
   describe "#latest_version" do
     subject { checker.latest_version }
-    it { is_expected.to eq(Gem::Version.new("0.3.0")) }
 
-    context "with a sub-dependency" do
-      let(:requirements) { [] }
-      it { is_expected.to eq(Gem::Version.new("0.3.0")) }
-    end
+    it "delegates to LatestVersionFinder" do
+      expect(described_class::LatestVersionFinder).to receive(:new).with(
+        dependency: dependency,
+        dependency_files: dependency_files,
+        credentials: credentials,
+        ignored_versions: ignored_versions
+      ).and_call_original
 
-    context "with a git source" do
-      context "that specifies a branch" do
-        let(:manifest_fixture_name) { "branch.toml" }
-        let(:lockfile_fixture_name) { "branch.lock" }
-
-        let(:source) do
-          {
-            type: "git",
-            url: "https://github.com/golang/text",
-            branch: "master",
-            ref: nil
-          }
-        end
-
-        before do
-          repo_url = "https://api.github.com/repos/golang/text"
-          stub_request(:get, repo_url + "/compare/v0.3.0...master").
-            to_return(
-              status: 200,
-              body: commit_compare_response,
-              headers: { "Content-Type" => "application/json" }
-            )
-        end
-
-        context "that is behind the latest release" do
-          let(:commit_compare_response) do
-            fixture("github", "commit_compare_behind.json")
-          end
-
-          it { is_expected.to eq(Gem::Version.new("0.3.0")) }
-        end
-
-        context "that is diverged from the latest release" do
-          let(:commit_compare_response) do
-            fixture("github", "commit_compare_diverged.json")
-          end
-
-          it { is_expected.to eq("0605a8320aceb4207a5fb3521281e17ec2075476") }
-        end
-      end
-
-      context "that specifies a tag" do
-        let(:manifest_fixture_name) { "tag_as_revision.toml" }
-        let(:lockfile_fixture_name) { "tag_as_revision.lock" }
-
-        let(:source) do
-          {
-            type: "git",
-            url: "https://github.com/golang/text",
-            branch: nil,
-            ref: "v0.2.0"
-          }
-        end
-
-        before do
-          repo_url = "https://api.github.com/repos/golang/text"
-          stub_request(:get, repo_url + "/compare/v0.3.0...v0.2.0").
-            to_return(
-              status: 200,
-              body: commit_compare_response,
-              headers: { "Content-Type" => "application/json" }
-            )
-        end
-        let(:commit_compare_response) do
-          fixture("github", "commit_compare_behind.json")
-        end
-
-        it { is_expected.to eq(Gem::Version.new("0.3.0")) }
-
-        context "that is up-to-date" do
-          let(:commit_compare_response) do
-            fixture("github", "commit_compare_identical.json")
-          end
-
-          # Still make an update as we wish to switch source declaration style.
-          # (Could decide not to do this if it causes trouble.)
-          it { is_expected.to eq(Gem::Version.new("0.3.0")) }
-        end
-
-        context "when the new version isn't a direct update to the old one" do
-          let(:commit_compare_response) do
-            fixture("github", "commit_compare_diverged.json")
-          end
-
-          # Still make an update as we wish to switch source declaration style.
-          # (Could decide not to do this if it causes trouble.)
-          it { is_expected.to eq("v0.3.0") }
-        end
-      end
+      expect(checker.latest_version).to eq(Gem::Version.new("0.3.0"))
     end
   end
 end
