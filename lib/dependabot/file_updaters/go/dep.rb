6cd36b98cc535c49493a1fec774804c26514c33d
Grey Baker <greysteil@gmail.com>
2018-07-25 10:40:02 +0100

    Dep: First version of FileUpdater (Gopkg.toml only)

--- lib/dependabot/file_updaters/go/dep.rb
+++ lib/dependabot/file_updaters/go/dep.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "dependabot/shared_helpers"
 require "dependabot/file_updaters/base"
 
 module Dependabot
@@ -24,8 +25,12 @@ module Dependabot
               )
           end
 
-          updated_files <<
-            updated_file(file: lockfile, content: updated_lockfile_content)
+          if lockfile
+            updated_files <<
+              updated_file(file: lockfile, content: updated_lockfile_content)
+          end
+
+          raise "No files changed!" if updated_files.none?
 
           updated_files
         end
@@ -33,9 +38,7 @@ module Dependabot
         private
 
         def check_required_files
-          %w(Gopkg.toml Gopkg.lock).each do |filename|
-            raise "No #{filename}!" unless get_original_file(filename)
-          end
+          raise "No Gopkg.toml!" unless get_original_file("Gopkg.toml")
         end
 
         def manifest
@@ -47,12 +50,111 @@ module Dependabot
         end
 
         def updated_manifest_content
-          # TODO: This can normally be written using regexs
+          dependencies.
+            select { |dep| requirement_changed?(manifest, dep) }.
+            reduce(manifest.content.dup) do |content, dep|
+              updated_content = content
+              updated_content = update_requirements(
+                content: updated_content,
+                filename: manifest.name,
+                dependency: dep
+              )
+              updated_content = update_git_pin(
+                content: updated_content,
+                filename: manifest.name,
+                dependency: dep
+              )
+
+              raise "Expected content to change!" if content == updated_content
+              updated_content
+            end
         end
 
         def updated_lockfile_content
           # TODO: This normally needs to be written in the native language.
           # We do so by shelling out to a helper method (see other languages)
+          lockfile.content
+        end
+
+        def update_requirements(content:, filename:, dependency:)
+          updated_content = content.dup
+
+          # The UpdateChecker ensures the order of requirements is preserved
+          # when updating, so we can zip them together in new/old pairs.
+          reqs = dependency.requirements.zip(dependency.previous_requirements).
+                 reject { |new_req, old_req| new_req == old_req }
+
+          # Loop through each changed requirement
+          reqs.each do |new_req, old_req|
+            raise "Bad req match" unless new_req[:file] == old_req[:file]
+            next if new_req[:requirement] == old_req[:requirement]
+            next unless new_req[:file] == filename
+
+            updated_content = update_manifest_req(
+              content: updated_content,
+              dep: dependency,
+              old_req: old_req.fetch(:requirement),
+              new_req: new_req.fetch(:requirement)
+            )
+          end
+
+          updated_content
+        end
+
+        def update_git_pin(content:, filename:, dependency:)
+          updated_pin =
+            dependency.requirements.
+            find { |r| r[:file] == filename }&.
+            dig(:source, :ref)
+
+          old_pin =
+            dependency.previous_requirements.
+            find { |r| r[:file] == filename }&.
+            dig(:source, :ref)
+
+          return content unless old_pin
+
+          update_manifest_pin(
+            content: content,
+            dep: dependency,
+            old_pin: old_pin,
+            new_pin: updated_pin
+          )
+        end
+
+        def update_manifest_req(content:, dep:, old_req:, new_req:)
+          simple_declaration = content.scan(declaration_regex(dep)).
+                               find { |m| m.include?(old_req) }
+
+          if simple_declaration
+            content.gsub(simple_declaration) do |line|
+              line.gsub(old_req, new_req)
+            end
+          else
+            content
+          end
+        end
+
+        def update_manifest_pin(content:, dep:, old_pin:, new_pin:)
+          simple_declaration = content.scan(declaration_regex(dep)).
+                               find { |m| m.include?(old_pin) }
+
+          if simple_declaration
+            content.gsub(simple_declaration) do |line|
+              line.gsub(old_pin, new_pin)
+            end
+          else
+            content
+          end
+        end
+
+        def declaration_regex(dep)
+          /
+            (?<=\]\])
+            (?:(?!^\[).)*
+            name\s*=\s*["']#{Regexp.escape(dep.name)}["']
+            (?:(?!^\[).)*
+          /mx
         end
       end
     end
--- spec/dependabot/file_updaters/go/dep_spec.rb
+++ spec/dependabot/file_updaters/go/dep_spec.rb
@@ -1,8 +1,113 @@
 # frozen_string_literal: true
 
+require "dependabot/dependency"
+require "dependabot/dependency_file"
 require "dependabot/file_updaters/go/dep"
 require_relative "../shared_examples_for_file_updaters"
 
 RSpec.describe Dependabot::FileUpdaters::Go::Dep do
   it_behaves_like "a dependency file updater"
+
+  let(:updater) do
+    described_class.new(
+      dependency_files: files,
+      dependencies: [dependency],
+      credentials: [{
+        "type" => "git_source",
+        "host" => "github.com",
+        "username" => "x-access-token",
+        "password" => "token"
+      }]
+    )
+  end
+
+  let(:files) { [manifest, lockfile] }
+  let(:manifest) do
+    Dependabot::DependencyFile.new(name: "Gopkg.toml", content: manifest_body)
+  end
+  let(:lockfile) do
+    Dependabot::DependencyFile.new(name: "Gopkg.lock", content: lockfile_body)
+  end
+  let(:manifest_body) { fixture("go", "gopkg_tomls", manifest_fixture_name) }
+  let(:lockfile_body) { fixture("go", "gopkg_locks", lockfile_fixture_name) }
+  let(:manifest_fixture_name) { "bare_version.toml" }
+  let(:lockfile_fixture_name) { "bare_version.lock" }
+
+  let(:dependency) do
+    Dependabot::Dependency.new(
+      name: dependency_name,
+      version: dependency_version,
+      requirements: requirements,
+      previous_version: dependency_previous_version,
+      previous_requirements: previous_requirements,
+      package_manager: "dep"
+    )
+  end
+  let(:dependency_name) { "github.com/dgrijalva/jwt-go" }
+  let(:dependency_version) { "3.2.0" }
+  let(:dependency_previous_version) { "1.0.1" }
+  let(:requirements) { previous_requirements }
+  let(:previous_requirements) do
+    [{
+      file: "Gopkg.toml",
+      requirement: "1.0.0",
+      groups: [],
+      source: {
+        type: "default",
+        source: "github.com/dgrijalva/jwt-go"
+      }
+    }]
+  end
+  let(:tmp_path) { Dependabot::SharedHelpers::BUMP_TMP_DIR_PATH }
+
+  before { Dir.mkdir(tmp_path) unless Dir.exist?(tmp_path) }
+
+  describe "#updated_dependency_files" do
+    subject(:updated_files) { updater.updated_dependency_files }
+
+    it "doesn't store the files permanently, and returns DependencyFiles" do
+      expect { updated_files }.to_not(change { Dir.entries(tmp_path) })
+      updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+    end
+
+    it { expect { updated_files }.to_not output.to_stdout }
+
+    context "without a lockfile" do
+      let(:files) { [manifest] }
+
+      context "if no files have changed" do
+        it "raises a helpful error" do
+          expect { updater.updated_dependency_files }.
+            to raise_error("No files changed!")
+        end
+      end
+
+      context "when the manifest has changed" do
+        let(:requirements) do
+          [{
+            file: "Gopkg.toml",
+            requirement: ">= 1.0.0, < 4.0.0",
+            groups: [],
+            source: {
+              type: "default",
+              source: "github.com/dgrijalva/jwt-go"
+            }
+          }]
+        end
+
+        its(:length) { is_expected.to eq(1) }
+
+        describe "the updated manifest" do
+          subject(:updated_manifest_content) do
+            updated_files.find { |f| f.name == "Gopkg.toml" }.content
+          end
+
+          it "includes the new requirement" do
+            expect(updated_manifest_content).
+              to include(%(version = ">= 1.0.0, < 4.0.0"))
+          end
+        end
+      end
+    end
+  end
 end
