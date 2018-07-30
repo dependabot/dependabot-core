e4468400201f5b439e16c6cbec4712b94533753c
Juliano Solanho <juliano.solanho@gmail.com>
2018-07-30 19:04:15 -0300

    Bunch of comments as I start to understand things
    VersionResolver et al will follow.

--- lib/dependabot/update_checkers/elm/elm_package.rb
+++ lib/dependabot/update_checkers/elm/elm_package.rb
@@ -8,7 +8,19 @@ require "dependabot/errors"
 module Dependabot
   module UpdateCheckers
     module Elm
+      # TODO:
+      # * latest_resolvable_version_with_no_unlock: what's the latest version something can go to without modifying elm-package.json?
+      # => this probably doesn't make sense for elm but :shrug:
+      # => elm-package install, bring back dep modifications, return current version if any diff between exact-dependencies.json <> elm-package.json
+      # * latest_resolvable_version: what's the latest version something can go to without modifying other deps in elm-package.json?
+      # => elm-package install, bring back dep modifications, return current version if other deps modified
+      # => if we could build a map of version dependencies somehow, that'd be awesome.. I wonder what elm-package uses..
+      # * latest_version_resolvable_with_full_unlock:
+      # => elm-package install, bring back dep modifications, give out latest version
       class ElmPackage < Dependabot::UpdateCheckers::Base
+        # require_relative "elm_package/requirements_updater"
+        require_relative "elm_package/version_resolver"
+
         VERSION_REGEX = /(\d+)\.(\d+)\.(\d+)/
         VERSIONS_REGEX = /versions: \[("#{VERSION_REGEX}",?)+\]/
         def latest_version
@@ -16,18 +28,42 @@ module Dependabot
         end
 
         def latest_resolvable_version
-          # TODO: how do we deal with resolvability in elm?
-          latest_version
+          # what's the latest version something can go to without modifying other deps in elm-package.json?
+          # => elm-package install, bring back dep modifications, return current version if other deps modified
+          VersionResolver.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            unlock_requirement: true,
+            versions: versions
+          ).latest_resolvable_version
         end
 
         def latest_resolvable_version_with_no_unlock
           # No concept of "unlocking" for elm-packages
-          # elm-package sort of on always-unlock mode
+          # Elm-package installs whatever it wants
+          # to satisfy the minimum dependencies you set
+          #
+          # To complicate things more, it's not advised
+          # to commit the `exact-dependencies.json` file
+          # so no dependency `appears_in_lockfile?`.
+          #
+          # Given what's in base.rb, I imagine this will
+          # never be called
+          #
+          # Nevertheless, let's leave it the same as
+          # dependency.version
           dependency.version
         end
 
         def updated_requirements
-
+          # TODO: Delegate to RequirementsUpdater
+          # it should return the .requirements field on Dependency
+          # updated to the `latest_resolvable_version`
+          RequirementsUpdater.new(
+            requirements: dependency.requirements,
+            latest_version: latest_version&.to_s,
+            latest_resolvable_version: latest_resolvable_version&.to_s
+          ).updated_requirements
         end
 
         private
@@ -37,7 +73,14 @@ module Dependabot
         end
 
         def latest_version_resolvable_with_full_unlock?
-          # Full unlock checks aren't relevant for elm-packages
+          # Elm-package doesn't have locking.
+          # If we can't upgrade with the other methods
+          # it must mean some dependency doesn't yet
+          # support `latest_version`.
+          #
+          # Elm package doesn't let us get to this
+          # state unless we manually try to update
+          # a requirement.
           false
         end
 
