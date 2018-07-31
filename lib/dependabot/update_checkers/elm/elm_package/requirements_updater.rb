35bbe79a938eb429abb4b568bf467c29b5c908eb
Juliano Solanho <juliano.solanho@gmail.com>
2018-07-31 19:14:37 -0300

    Requirement updater for Elm

---
+++ lib/dependabot/update_checkers/elm/elm_package/requirements_updater.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "dependabot/utils/elm/version"
+require "dependabot/utils/elm/requirement"
+require "dependabot/update_checkers/elm/elm_package"
+
+module Dependabot
+  module UpdateCheckers
+    module Elm
+      class ElmPackage
+        class RequirementsUpdater
+          # DEPENDENCY_REGEX = ->(name) /"#{name}": ("[^\"]+")/
+
+          def initialize(requirements:, latest_resolvable_version:)
+            @requirements = requirements
+
+            return unless latest_resolvable_version
+            unless Utils::Elm::Version.correct?(latest_resolvable_version)
+              return
+            end
+            @latest_resolvable_version =
+              Utils::Elm::Version.new(latest_resolvable_version)
+          end
+
+          def updated_requirements
+            if @latest_resolvable_version
+              requirements.each { |req| req[:requirement] = require_exactly(@latest_resolvable_version) }
+            end
+            requirements
+          end
+
+          private
+
+          attr_reader :requirements, :latest_resolvable_version
+
+          def require_exactly(version)
+            # Elm recommends folks to use exact versions
+            # and Elm 0.19 won't support other requirement
+            # specifications, so lets force everyone towards it
+            "#{version} <= v <= #{version}"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/dependabot/update_checkers/elm/elm_package/requirements_updater_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/update_checkers/elm/elm_package/requirements_updater"
+
+RSpec.describe Dependabot::UpdateCheckers::Elm::ElmPackage::RequirementsUpdater do
+  let(:updater) do
+    described_class.new(
+      requirements: requirements,
+      latest_resolvable_version: latest_resolvable_version
+    )
+  end
+
+  let(:requirements) { [elm_package_req] }
+  let(:updated_source) { nil }
+  let(:elm_package_req) do
+    {
+      file: "elm-package.json",
+      requirement: requirement_string,
+      groups: [],
+      source: nil
+    }
+  end
+  let(:requirement_string) { "1.4.0 <= v < 1.4.0" }
+
+  let(:latest_resolvable_version) { "1.5.0" }
+
+  describe "#updated_requirements" do
+    subject { updater.updated_requirements.first }
+
+    specify { expect(updater.updated_requirements.count).to eq(1) }
+
+    let(:requirement_string) { "1.4.0 <= v <= 1.4.0" }
+    let(:latest_resolvable_version) { "1.4.0" }
+
+    context "when there is no resolvable version" do
+      let(:latest_resolvable_version) { nil }
+      its([:requirement]) { is_expected.to eq(requirement_string) }
+    end
+
+    context "when there is a resolvable version" do
+      let(:latest_resolvable_version) { "1.5.0" }
+
+      context "with exact requirement" do
+        let(:requirement_string) { "1.2.3 <= v <= 1.2.3" }
+        its([:requirement]) { is_expected.to eq("1.5.0 <= v <= 1.5.0") }
+      end
+
+      context "with range requirement" do
+        let(:requirement_string) { "1.0.0 <= v < 1.4.0" }
+        its([:requirement]) { is_expected.to eq("1.5.0 <= v <= 1.5.0") }
+      end
+    end
+  end
+end
