f23ef3ad9ea3c9e5146ae8c8ab62bd69327cdbc4
Grey Baker <greysteil@gmail.com>
2018-08-23 15:13:12 +0100

    Terraform: Use Ruby version and requirement classes

--- lib/dependabot/utils.rb
+++ lib/dependabot/utils.rb
@@ -26,7 +26,7 @@ module Dependabot
   module Utils
     def self.version_class_for_package_manager(package_manager)
       case package_manager
-      when "bundler", "submodules", "docker" then Gem::Version
+      when "bundler", "submodules", "docker", "terraform" then Gem::Version
       when "nuget" then Utils::Dotnet::Version
       when "maven" then Utils::Java::Version
       when "gradle" then Utils::Java::Version
@@ -43,7 +43,8 @@ module Dependabot
 
     def self.requirement_class_for_package_manager(package_manager)
       case package_manager
-      when "bundler", "submodules", "docker" then Utils::Ruby::Requirement
+      when "bundler", "submodules", "docker", "terraform" then
+        Utils::Ruby::Requirement
       when "nuget" then Utils::Dotnet::Requirement
       when "maven" then Utils::Java::Requirement
       when "gradle" then Utils::Java::Requirement
