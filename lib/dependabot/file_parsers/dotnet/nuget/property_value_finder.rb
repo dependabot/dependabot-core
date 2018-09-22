b692287b3f302b297e23b1390ab4f7a39d13b3db
Grey Baker <greysteil@gmail.com>
2018-09-22 18:16:13 +0100

    .NET: Handle inherited properties in PropertyValueFinder

--- lib/dependabot/file_parsers/dotnet/nuget/property_value_finder.rb
+++ lib/dependabot/file_parsers/dotnet/nuget/property_value_finder.rb
@@ -16,18 +16,82 @@ module Dependabot
           end
 
           def property_details(property_name:, callsite_file:)
-            doc = Nokogiri::XML(callsite_file.content)
-            doc.remove_namespaces!
+            node_details = deep_find_prop_node(
+              property: property_name,
+              file: callsite_file
+            )
 
-            node = doc.at_xpath("/Project/PropertyGroup/#{property_name}")
-            return unless node
+            node_details ||=
+              find_property_in_directory_build_props(
+                property: property_name,
+                callsite_file: callsite_file
+              )
 
-            { file: callsite_file.name, node: node, value: node.content.strip }
+            node_details
           end
 
           private
 
           attr_reader :dependency_files
+
+          def deep_find_prop_node(property:, file:)
+            doc = Nokogiri::XML(file.content)
+            doc.remove_namespaces!
+            node = doc.at_xpath(property_xpath(property))
+
+            # If we found a value for the property, return it
+            return node_details(file: file, node: node) if node
+
+            # Otherwise, we need to look in an imported file
+            import_path_finder =
+              FileFetchers::Dotnet::Nuget::ImportPathsFinder.
+              new(project_file: file)
+
+            import_paths = [
+              *import_path_finder.import_paths,
+              *import_path_finder.project_reference_paths
+            ]
+
+            file = import_paths.
+                   map { |p| dependency_files.find { |f| f.name == p } }.
+                   compact.
+                   find { |f| deep_find_prop_node(property: property, file: f) }
+
+            return unless file
+
+            deep_find_prop_node(property: property, file: file)
+          end
+
+          def find_property_in_directory_build_props(property:, callsite_file:)
+            file = buildfile_for_project(callsite_file)
+            return unless file
+
+            deep_find_prop_node(property: property, file: file)
+          end
+
+          def buildfile_for_project(project_file)
+            dir = File.dirname(project_file.name)
+
+            possible_paths = dir.split("/").map.with_index do |_, i|
+              dir.split("/").first(i + 1).join("/") + "/Directory.Build.props"
+            end.reverse
+
+            path =
+              possible_paths.map { |p| Pathname.new(p).cleanpath.to_path }.
+              find { |p| dependency_files.find { |f| f.name == p } }
+
+            return unless path
+
+            dependency_files.find { |f| f.name == path }
+          end
+
+          def property_xpath(property_name)
+            "/Project/PropertyGroup/#{property_name}"
+          end
+
+          def node_details(file:, node:)
+            { file: file.name, node: node, value: node.content.strip }
+          end
         end
       end
     end
--- spec/dependabot/file_parsers/dotnet/nuget/property_value_finder_spec.rb
+++ spec/dependabot/file_parsers/dotnet/nuget/property_value_finder_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe Dependabot::FileParsers::Dotnet::Nuget::PropertyValueFinder do
     Dependabot::DependencyFile.new(name: "my.csproj", content: file_body)
   end
   let(:file_body) { fixture("dotnet", "csproj", "property_version.csproj") }
-  let(:finder) { described_class.new(dependency_files: [file]) }
+  let(:finder) { described_class.new(dependency_files: files) }
+  let(:files) { [file] }
   let(:property_name) { "NukeVersion" }
 
   describe "property_details" do
@@ -21,11 +22,38 @@ RSpec.describe Dependabot::FileParsers::Dotnet::Nuget::PropertyValueFinder do
     context "with a property that can be found" do
       let(:property_name) { "NukeVersion" }
       its([:value]) { is_expected.to eq("0.1.434") }
+      its([:file]) { is_expected.to eq("my.csproj") }
     end
 
     context "with a property that can't be found" do
       let(:property_name) { "UnknownVersion" }
       it { is_expected.to be_nil }
+    end
+
+    context "from a directory.build.props file" do
+      let(:files) { [file, build_file, imported_file] }
+
+      let(:build_file) do
+        Dependabot::DependencyFile.new(
+          name: "Directory.Build.props",
+          content: build_file_body
+        )
+      end
+      let(:build_file_body) { fixture("dotnet", "property_files", "imports") }
+      let(:imported_file) do
+        Dependabot::DependencyFile.new(
+          name: "build/dependencies.props",
+          content: imported_file_body
+        )
+      end
+      let(:imported_file_body) do
+        fixture("dotnet", "property_files", "dependency.props")
+      end
+
+      let(:property_name) { "XunitPackageVersion" }
+
+      its([:value]) { is_expected.to eq("2.3.1") }
+      its([:file]) { is_expected.to eq("build/dependencies.props") }
     end
   end
 end
