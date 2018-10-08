831d46adb546152a071d44eaf94e0ab011cad7ab
Grey Baker <greysteil@gmail.com>
2018-10-08 21:21:01 +0100

    Prefer lower versions when parsing dependencies into DependencySet

--- lib/dependabot/file_parsers/base/dependency_set.rb
+++ lib/dependabot/file_parsers/base/dependency_set.rb
@@ -2,6 +2,7 @@
 
 require "dependabot/dependency"
 require "dependabot/file_parsers/base"
+require "dependabot/utils"
 
 module Dependabot
   module FileParsers
@@ -25,20 +26,15 @@ module Dependabot
 
           existing_dependency = dependencies.find { |d| d.name == dep.name }
 
-          if existing_dependency
-            return self if existing_dependency.to_h == dep.to_h
+          return self if existing_dependency&.to_h == dep.to_h
 
+          if existing_dependency
             dependencies[dependencies.index(existing_dependency)] =
-              Dependency.new(
-                name: existing_dependency.name,
-                version: existing_dependency.version || dep.version,
-                requirements:
-                  (existing_dependency.requirements + dep.requirements).uniq,
-                package_manager: existing_dependency.package_manager
-              )
+              combined_dependency(existing_dependency, dep)
           else
             dependencies << dep
           end
+
           self
         end
 
@@ -49,6 +45,31 @@ module Dependabot
 
           other.dependencies.each { |dep| self << dep }
           self
+        end
+
+        private
+
+        def combined_dependency(old_dep, new_dep)
+          package_manager = old_dep.package_manager
+          v_cls = Utils.version_class_for_package_manager(package_manager)
+
+          # If we already have a requirement use the existing version
+          # (if present). Otherwise, use whatever the lowest version is
+          new_version =
+            if old_dep.requirements.any? then old_dep.version || new_dep.version
+            elsif !v_cls.correct?(new_dep.version) then old_dep.version
+            elsif !v_cls.correct?(old_dep.version) then new_dep.version
+            elsif v_cls.new(new_dep.version) > v_cls.new(old_dep.version)
+              old_dep.version
+            else new_dep.version
+            end
+
+          Dependency.new(
+            name: old_dep.name,
+            version: new_version,
+            requirements: (old_dep.requirements + new_dep.requirements).uniq,
+            package_manager: package_manager
+          )
         end
       end
     end
