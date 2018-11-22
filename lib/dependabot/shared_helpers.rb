2d009d0e9ad2bf864acfc1c8af17482b79955b5c
Harry Marr <harry.marr@gmail.com>
2018-11-22 10:16:34 +0000

    Handle a nil that previously wasn't being handled

--- lib/dependabot/shared_helpers.rb
+++ lib/dependabot/shared_helpers.rb
@@ -176,6 +176,7 @@ module Dependabot
     end
 
     def self.reset_global_git_config(backup_path)
+      return if backup_path.nil?
       return unless File.exist?(backup_path)
 
       FileUtils.mv(backup_path, GIT_CONFIG_GLOBAL_PATH)
