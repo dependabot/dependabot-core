0dba02cf3c8438411b56dc677a0199745e61c00c
Harry Marr <harry.marr@gmail.com>
2018-12-07 16:43:52 +0000

    Fix core tests following python migration

--- spec/dependabot/metadata_finders/base/release_finder_spec.rb
+++ spec/dependabot/metadata_finders/base/release_finder_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Dependabot::MetadataFinders::Base::ReleaseFinder do
       requirements:
         [{ file: "Gemfile", requirement: ">= 0", groups: [], source: nil }],
       previous_version: dependency_previous_version,
-      package_manager: "pip"
+      package_manager: "bundler"
     )
   end
   let(:dependency_name) { "business" }
