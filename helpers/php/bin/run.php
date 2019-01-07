75ce5dac8553afbd7f656d3a7ac5963fb6f69a09
Grey Baker <greysteil@gmail.com>
2019-01-07 13:25:33 +0000

    PHP style fix

--- helpers/php/bin/run.php
+++ helpers/php/bin/run.php
@@ -17,7 +17,7 @@ $request = json_decode(file_get_contents('php://stdin'), true);
 $memoryInBytes = function ($value) {
     $unit = strtolower(substr($value, -1, 1));
     $value = (int) $value;
-    switch($unit) {
+    switch ($unit) {
         case 'g':
             $value *= 1024;
         // no break (cumulative multiplier)
