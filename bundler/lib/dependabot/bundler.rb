378d8d89380ec5a5a4b757ca711a59e489cfa355
Grey Baker <greysteil@gmail.com>
2019-02-01 13:15:21 +0000

    Remove blank line

--- bundler/lib/dependabot/bundler.rb
+++ bundler/lib/dependabot/bundler.rb
@@ -10,7 +10,6 @@ require "dependabot/bundler/metadata_finder"
 require "dependabot/bundler/requirement"
 require "dependabot/bundler/version"
 
-
 require "dependabot/pull_request_creator/labeler"
 Dependabot::PullRequestCreator::Labeler.
   register_label_details("bundler", name: "ruby", colour: "ce2d2d")
