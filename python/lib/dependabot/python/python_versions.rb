7be0c83c1cb74eca3de4e6f7276b9f53358e22b7
Grey Baker <greysteil@gmail.com>
2019-04-09 18:54:57 +0100

    Python: Use pyenv v1.2.11 in Dockerfile, and update available Python versions

--- python/lib/dependabot/python/python_versions.rb
+++ python/lib/dependabot/python/python_versions.rb
@@ -10,9 +10,9 @@ module Dependabot
       # Due to an OpenSSL issue we can only install the following versions in
       # the Dependabot container.
       SUPPORTED_VERSIONS = %w(
-        3.7.2 3.7.1 3.7.0
+        3.7.3 3.7.2 3.7.1 3.7.0
         3.6.8 3.6.7 3.6.6 3.6.5 3.6.4 3.6.3 3.6.2 3.6.1 3.6.0
-        3.5.6 3.5.5 3.5.4 3.5.3
+        3.5.7 3.5.6 3.5.5 3.5.4 3.5.3
         2.7.16 2.7.15 2.7.14 2.7.13
       ).freeze
 
