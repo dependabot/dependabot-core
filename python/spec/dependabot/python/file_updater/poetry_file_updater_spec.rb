5fae99a0711028d414c0c37578edb1c130c26fd0
Grey Baker <greysteil@gmail.com>
2019-04-28 11:52:36 +0100

    More spec Python fixes

--- python/spec/dependabot/python/file_updater/poetry_file_updater_spec.rb
+++ python/spec/dependabot/python/file_updater/poetry_file_updater_spec.rb
@@ -77,7 +77,7 @@ RSpec.describe Dependabot::Python::FileUpdater::PoetryFileUpdater do
       expect(pytest["version"]).to eq("3.5.0")
 
       expect(lockfile_obj["metadata"]["content-hash"]).
-        to start_with("82505f37a0da79b1e0f8d5c715d5435ef9318adf4df0e7372bde")
+        to start_with("8cea4ecb5b2230fbd4a33a67a4da004f1ccabad48352aaf040")
     end
 
     context "with a specified Python version" do
@@ -167,7 +167,7 @@ RSpec.describe Dependabot::Python::FileUpdater::PoetryFileUpdater do
         expect(pytest["version"]).to eq("3.5.0")
 
         expect(lockfile_obj["metadata"]["content-hash"]).
-          to start_with("82505f37a0da79b1e0f8d5c715d5435ef9318adf4df0e7372bde")
+          to start_with("8cea4ecb5b2230fbd4a33a67a4da004f1ccabad48352aaf040a1d")
       end
 
       context "with a sub-dependency" do
@@ -194,7 +194,7 @@ RSpec.describe Dependabot::Python::FileUpdater::PoetryFileUpdater do
           expect(certifi["version"]).to eq("2018.11.29")
 
           expect(lockfile_obj["metadata"]["content-hash"]).
-            to start_with("82505f37a0da79b1e0f8d5c715d5435ef9318adf4df0e73")
+            to start_with("8cea4ecb5b2230fbd4a33a67a4da004f1ccabad48352aaf040a")
         end
       end
     end
