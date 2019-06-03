024175ef92c02dcd87fcbd743d8ca0dd38589676
Grey Baker <greysteil@gmail.com>
2019-06-03 20:25:33 +0100

    Update Python specs for new Django

--- python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
+++ python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
@@ -286,7 +286,7 @@ RSpec.describe namespace::PipenvVersionResolver do
         end
         let(:updated_requirement) { ">= 1.1.14, <= 2.1.4" }
 
-        it { is_expected.to eq(Gem::Version.new("1.11.20")) }
+        it { is_expected.to eq(Gem::Version.new("1.11.21")) }
       end
 
       context "for a resolution that has caused trouble in the past" do
