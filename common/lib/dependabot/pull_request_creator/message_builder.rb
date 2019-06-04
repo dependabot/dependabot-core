0f6b64a211c327efce8fd6c5abed08c38621b579
Grey Baker <greysteil@gmail.com>
2019-06-04 22:02:44 +0100

    Better mention sanitizing (handle codeblocks)

--- common/lib/dependabot/pull_request_creator/message_builder.rb
+++ common/lib/dependabot/pull_request_creator/message_builder.rb
@@ -11,6 +11,7 @@ module Dependabot
   class PullRequestCreator
     class MessageBuilder
       require_relative "message_builder/issue_linker"
+      require_relative "message_builder/link_and_mention_sanitizer"
 
       ANGULAR_PREFIXES = %w(build chore ci docs feat fix perf refactor style
                             test).freeze
@@ -29,11 +30,6 @@ module Dependabot
                             see_no_evil sparkles speech_balloon tada truck
                             twisted_rightwards_arrows whale wheelchair
                             white_check_mark wrench zap).freeze
-      GITHUB_REF_REGEX = %r{
-        (?:https?://)?
-        github\.com/[^/\s]+/[^/\s]+/
-        (?:issue|pull)s?/(?<number>\d+)
-      }x.freeze
 
       attr_reader :source, :dependencies, :files, :credentials,
                   :pr_message_footer, :author_details, :vulnerabilities_fixed,
@@ -716,41 +712,9 @@ module Dependabot
       end
 
       def sanitize_links_and_mentions(text)
-        text = sanitize_mentions(text)
-        sanitize_links(text)
-      end
-
-      def sanitize_mentions(text)
-        text.gsub(%r{(?<![A-Za-z0-9`])@[\w][\w.-/]*}) do |mention|
-          next mention if mention.include?("/")
-
-          last_match = Regexp.last_match
-
-          sanitized_mention = mention.gsub("@", "@&#8203;")
-          if last_match.pre_match.chars.last == "[" &&
-             last_match.post_match.chars.first == "]"
-            sanitized_mention
-          else
-            "[#{sanitized_mention}](https://github.com/#{mention.tr('@', '')})"
-          end
-        end
-      end
-
-      def sanitize_links(text)
-        text.gsub(GITHUB_REF_REGEX) do |ref|
-          last_match = Regexp.last_match
-          previous_char = last_match.pre_match.chars.last
-          next_char = last_match.post_match.chars.first
-
-          sanitized_url =
-            ref.gsub("github.com", github_redirection_service || "github.com")
-          if (previous_char.nil? || previous_char.match?(/\s/)) &&
-             (next_char.nil? || next_char.match?(/\s/))
-            "[##{last_match.named_captures.fetch('number')}](#{sanitized_url})"
-          else
-            sanitized_url
-          end
-        end
+        LinkAndMentionSanitizer.
+          new(github_redirection_service: github_redirection_service).
+          sanitize_links_and_mentions(text: text)
       end
 
       def sanitize_template_tags(text)
