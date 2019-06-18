60900a31cfae3849a1cbb8a20a0585bd68632d10
Grey Baker <greysteil@gmail.com>
2019-06-18 11:04:28 -0700

    Fix rubocop

--- python/lib/dependabot/python/file_parser/setup_file_parser.rb
+++ python/lib/dependabot/python/file_parser/setup_file_parser.rb
@@ -142,13 +142,13 @@ module Dependabot
         def get_regexed_req_array(regex)
           return unless (mch = setup_file.content.match(regex))
 
-          "[#{mch.post_match[0..closing_bracket_index(mch.post_match, "[")]}"
+          "[#{mch.post_match[0..closing_bracket_index(mch.post_match, '[')]}"
         end
 
         def get_regexed_req_dict(regex)
           return unless (mch = setup_file.content.match(regex))
 
-          "{#{mch.post_match[0..closing_bracket_index(mch.post_match, "{")]}"
+          "{#{mch.post_match[0..closing_bracket_index(mch.post_match, '{')]}"
         end
 
         def closing_bracket_index(string, bracket)
