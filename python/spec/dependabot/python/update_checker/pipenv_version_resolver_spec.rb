565b806d6662e2e3366c3937ae443e96100cbdc6
Grey Baker <greysteil@gmail.com>
2019-08-02 15:03:42 +0100

    Fix python tests

--- python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
+++ python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
@@ -302,7 +302,7 @@ RSpec.describe namespace::PipenvVersionResolver do
         end
         let(:updated_requirement) { ">= 1.1.14, <= 2.1.4" }
 
-        it { is_expected.to eq(Gem::Version.new("1.11.22")) }
+        it { is_expected.to eq(Gem::Version.new("1.11.23")) }
       end
 
       context "for a resolution that has caused trouble in the past" do
