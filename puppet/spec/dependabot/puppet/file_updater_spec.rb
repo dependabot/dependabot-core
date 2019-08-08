63e45ea14515314b66c7bf5329e33a64e5f7aa68
Grey Baker <greysteil@gmail.com>
2019-08-08 13:27:24 +0100

    Additional specs for puppetfile updating

--- puppet/spec/dependabot/puppet/file_updater_spec.rb
+++ puppet/spec/dependabot/puppet/file_updater_spec.rb
@@ -25,7 +25,10 @@ RSpec.describe Dependabot::Puppet::FileUpdater do
 
   let(:files) { [puppet_file] }
   let(:puppet_file) do
-    Dependabot::DependencyFile.new(name: "Puppetfile", content: puppet_file_body)
+    Dependabot::DependencyFile.new(
+      name: "Puppetfile",
+      content: puppet_file_body
+    )
   end
   let(:puppet_file_body) { fixture("puppet", puppet_file_fixture_name) }
   let(:puppet_file_fixture_name) { "Puppetfile" }
@@ -82,27 +85,22 @@ RSpec.describe Dependabot::Puppet::FileUpdater do
       package_manager: "puppet"
     )
   end
-  let(:tmp_path) { Dependabot::SharedHelpers::BUMP_TMP_DIR_PATH }
-
-  before { Dir.mkdir(tmp_path) unless Dir.exist?(tmp_path) }
 
   describe "#updated_dependency_files" do
     subject(:updated_files) { updater.updated_dependency_files }
 
-    it "doesn't store the files permanently, and returns DependencyFiles" do
-      expect { updated_files }.to_not(change { Dir.entries(tmp_path) })
-      updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+    it "returns ana rray of DependencyFiles" do
+      updated_files.each do |f|
+        expect(f).to be_a(Dependabot::DependencyFile)
+      end
     end
 
-    it { expect { updated_files }.to_not output.to_stdout }
-
-    it "includes an updated Puppetfile" do
-      expect(updated_files.find { |f| f.name == "Puppetfile" }).to_not be_nil
+    it "updates the Puppetfile correctly" do
+      updated_puppetfile = updated_files.find { |f| f.name == "Puppetfile" }
+      expect(updated_puppetfile.content).
+        to include(%{mod "puppetlabs/dsc", '1.9.0'\n})
+      expect(updated_puppetfile.content).
+        to include(%{mod "puppet/windowsfeature",  "3.2.0"\n})
     end
-
-    # it "only changes 1 line" do
-    #   expect(changed_file).to_not be_nil
-    # end
-
   end
 end
