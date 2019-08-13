927a8d454867b455555eefe890dd47f77e740cbc
Philip Harrison <philip@mailharrison.com>
2019-08-13 16:47:16 +0100

    JS: Add failing test for dependencies with latest

--- npm_and_yarn/spec/dependabot/npm_and_yarn/file_updater_spec.rb
+++ npm_and_yarn/spec/dependabot/npm_and_yarn/file_updater_spec.rb
@@ -1487,6 +1487,58 @@ RSpec.describe Dependabot::NpmAndYarn::FileUpdater do
       end
     end
 
+    context "when 'latest' is specified as version requirement" do
+      let(:manifest_fixture_name) { "latest_package_requirement.json" }
+      let(:npm_lock_fixture_name) { "latest_package_requirement.json" }
+      let(:yarn_lock_fixture_name) { "latest_package_requirement.lock" }
+
+      let(:dependency_name) { "extend" }
+      let(:version) { "3.0.2" }
+      let(:previous_version) { "2.0.1" }
+      let(:requirements) do
+        [{
+          file: "package.json",
+          requirement: "^3.0.2",
+          groups: ["dependencies"],
+          source: nil
+        }]
+      end
+      let(:previous_requirements) do
+        [{
+          file: "package.json",
+          requirement: "^2.0.1",
+          groups: ["dependencies"],
+          source: nil
+        }]
+      end
+
+      context "with an npm lock file" do
+        let(:files) { [package_json, package_lock] }
+
+        it "only updates extend and locks etag" do
+          expect(updated_files.map(&:name)).
+            to match_array(%w(package.json package-lock.json))
+          expect(updated_npm_lock.content).
+            to include("extend/-/extend-3.0.2.tgz")
+          expect(updated_npm_lock.content).
+            to include("etag/-/etag-1.7.0.tgz")
+        end
+      end
+
+      context "with a yarn lockfile" do
+        let(:files) { [package_json, yarn_lock] }
+
+        it "only updates extend and locks etag" do
+          expect(updated_files.map(&:name)).
+            to match_array(%w(package.json yarn.lock))
+          expect(updated_yarn_lock.content).
+            to include("extend@^3.0.2:\n  version \"3.0.2\"")
+          expect(updated_yarn_lock.content).
+            to include("etag@latest:\n  version \"1.7.0\"")
+        end
+      end
+    end
+
     context "when the exact version we're updating from is still requested" do
       let(:files) { [package_json, yarn_lock] }
       let(:manifest_fixture_name) { "typedoc-plugin-ui-router.json" }
