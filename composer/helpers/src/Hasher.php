02f7bd6033247dc0806e987642be78e159ae422f
Andreas Möller <am@localheinz.com>
2019-08-18 22:57:29 +0200

    Fix: Remove unused local variables

--- composer/helpers/src/Hasher.php
+++ composer/helpers/src/Hasher.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Dependabot\Composer;
 
-use Composer\Factory;
 use Composer\Package\Locker;
 
 class Hasher
@@ -14,10 +13,6 @@ class Hasher
         [$workingDirectory] = $args;
 
         $config = $workingDirectory . '/composer.json';
-
-        $io = new ExceptionIO();
-        $composer = Factory::create($io, $config);
-        $locker = $composer->getLocker();
 
         return Locker::getContentHash(file_get_contents($config));
     }
