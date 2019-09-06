07377aa79997e95cf9ccfed55b1bfb653ba96aaf
Philip Harrison <philip@mailharrison.com>
2019-09-06 13:56:03 +0100

    Fix js helper tests

--- npm_and_yarn/helpers/test/npm/updater.test.js
+++ npm_and_yarn/helpers/test/npm/updater.test.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+const rimraf = require("rimraf");
 const nock = require("nock");
 const {
   updateDependencyFiles,
@@ -12,29 +13,36 @@ describe("updater", () => {
   let tempDir;
   beforeEach(() => {
     nock("https://registry.npmjs.org")
+      .persist()
       .get("/left-pad")
-      .reply(200, helpers.loadFixture("npm-left-pad.json"));
+      .replyWithFile(
+        200,
+        path.join(__dirname, "fixtures", "npm-left-pad.json"),
+        {
+          "Content-Type": "application/json"
+        }
+      );
 
     tempDir = fs.mkdtempSync(os.tmpdir() + path.sep);
   });
-  afterEach(() => fs.rmdirSync(tempDir));
+  afterEach(() => rimraf.sync(tempDir));
 
-  async function copyDependencies(sourceDir, destDir) {
+  function copyDependencies(sourceDir, destDir) {
     const srcPackageJson = path.join(
       __dirname,
       `fixtures/updater/${sourceDir}/package.json`
     );
-    await fs.copyFile(srcPackageJson, `${destDir}/package.json`);
+    fs.copyFileSync(srcPackageJson, `${destDir}/package.json`);
 
     const srcLockfile = path.join(
       __dirname,
       `fixtures/updater/${sourceDir}/package-lock.json`
     );
-    await fs.copyFile(srcLockfile, `${destDir}/package-lock.json`);
+    fs.copyFileSync(srcLockfile, `${destDir}/package-lock.json`);
   }
 
   it("generates an updated package-lock.json", async () => {
-    await copyDependencies("original", tempDir);
+    copyDependencies("original", tempDir);
 
     const result = await updateDependencyFiles(tempDir, "package-lock.json", [
       {
