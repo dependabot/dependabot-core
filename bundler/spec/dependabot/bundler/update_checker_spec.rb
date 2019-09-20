9ce2ff2d9539a21a2a6aabbaa228e308eee8544b
Josh Soref <jsoref@users.noreply.github.com>
2019-09-20 00:37:21 -0400

    spelling: constant

--- bundler/spec/dependabot/bundler/update_checker_spec.rb
+++ bundler/spec/dependabot/bundler/update_checker_spec.rb
@@ -116,7 +116,7 @@ RSpec.describe Dependabot::Bundler::UpdateChecker do
         let(:required_file) do
           Dependabot::DependencyFile.new(
             name: "../some_other_file.rb",
-            content: "SOME_CONTANT = 5",
+            content: "SOME_CONSTANT = 5",
             directory: directory
           )
         end
