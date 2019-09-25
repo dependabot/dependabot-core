604796df0adc6e4e31a3c604341af684d008a83e
Harry Marr <harry.marr@gmail.com>
2019-09-25 12:34:08 +0100

    Add Go 1.13 spec

--- go_modules/spec/dependabot/go_modules/file_updater/go_mod_updater_spec.rb
+++ go_modules/spec/dependabot/go_modules/file_updater/go_mod_updater_spec.rb
@@ -89,6 +89,13 @@ RSpec.describe Dependabot::GoModules::FileUpdater::GoModUpdater do
           it { is_expected.to include("go 1.12") }
         end
 
+        context "for a go 1.13 go.mod" do
+          let(:go_mod_body) do
+            fixture("go_mods", go_mod_fixture_name).sub(/go 1.12/, "go 1.13")
+          end
+          it { is_expected.to include("go 1.13") }
+        end
+
         context "with a go.sum" do
           let(:go_sum) do
             Dependabot::DependencyFile.new(name: "go.sum", content: go_sum_body)
