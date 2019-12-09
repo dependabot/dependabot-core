dependabot/dependabot-core

commit 6fa27f79834e2ed7a84e7d2565492115854b13f8

NPM and Yarn: Fetch tarball path dependencies
Fetch tarballed path dependencies in the file fetcher.

https://docs.npmjs.com/cli/pack.html

npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -125,7 +125,12 @@ module Dependabot
 
         path_dependency_details(fetched_files).each do |name, path|
           path = path.gsub(PATH_DEPENDENCY_CLEAN_REGEX, "")
-          filename = File.join(path, "package.json")
+          filename = path
+          # NPM/Yarn support loading path dependencies from tarballs:
+          # https://docs.npmjs.com/cli/pack.html
+          unless filename.end_with?(".tgz")
+            filename = File.join(filename, "package.json")
+          end
           cleaned_name = Pathname.new(filename).cleanpath.to_path
           next if fetched_files.map(&:name).include?(cleaned_name)
 

npm_and_yarn/spec/dependabot/npm_and_yarn/file_fetcher_spec.rb
@@ -390,6 +390,48 @@ RSpec.describe Dependabot::NpmAndYarn::FileFetcher do
       end
     end
 
+    context "with a tarball path dependency" do
+      before do
+        stub_request(:get, File.join(url, "package.json?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "package_json_with_tarball_path.json"),
+            headers: json_header
+          )
+        stub_request(:get, "https://api.github.com/repos/gocardless/bump/"\
+                           "contents/deps/etag.tgz?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 403,
+            body: fixture("github", "file_too_large.json"),
+            headers: json_header
+          )
+        stub_request(:get, "https://api.github.com/repos/gocardless/bump/"\
+                            "contents/deps?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_js_tarball.json"),
+            headers: json_header
+          )
+        stub_request(:get, "https://api.github.com/repos/gocardless/bump/git/"\
+                           "blobs/2393602fac96cfe31d64f89476014124b4a13b85").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "blob_js_tarball.json"),
+            headers: json_header
+          )
+      end
+
+      it "fetches the tarball path dependency" do
+        expect(file_fetcher_instance.files.map(&:name)).to eq(
+          ["package.json", "package-lock.json", "deps/etag.tgz"]
+        )
+      end
+    end
+
     context "that has an unfetchable path" do
       before do
         stub_request(:get, File.join(url, "deps/etag/package.json?ref=sha")).

commit 440100c46a84a393a70546996c6dd053d05ca85c

JS: Fix unfetchable tarball path deps ∞ loop
Fixes a regression introduced by
https://github.com/dependabot/dependabot-core/commit/6fa27f79834e2ed7a84e7d2565492115854b13f8

When a path dependency is a tarball it shouldn't be fetched as a
package (appending/package.json to the end assuming it's a directory).

Commit #6fa27f79834e2ed7a84e7d2565492115854b13f8 only handled the
initial fetch but not the retry which caused an infinite retry loop as
the path dependency was re-fetched with path.tgz/package.json after each
time it wasn't found as path.tgz.

npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -138,7 +138,8 @@ module Dependabot
             file = fetch_file_from_host(filename, fetch_submodules: true)
             package_json_files << file
           rescue Dependabot::DependencyFileNotFound
-            unfetchable_deps << [name, path]
+            # Unfetchable tarballs should not be re-fetched as a package
+            unfetchable_deps << [name, path] unless path.end_with?(".tgz")
           end
         end
 

npm_and_yarn/spec/dependabot/npm_and_yarn/file_fetcher_spec.rb
@@ -432,6 +432,36 @@ RSpec.describe Dependabot::NpmAndYarn::FileFetcher do
       end
     end
 
+    context "that has an unfetchable tarball path dependency" do
+      before do
+        stub_request(:get, File.join(url, "package.json?ref=sha")).
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "package_json_with_tarball_path.json"),
+            headers: json_header
+          )
+        stub_request(:get, "https://api.github.com/repos/gocardless/bump/"\
+                            "contents/deps?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(
+            status: 200,
+            body: fixture("github", "contents_js_tarball.json"),
+            headers: json_header
+          )
+        stub_request(:get, "https://api.github.com/repos/gocardless/bump/"\
+                          "contents/deps/etag.tgz?ref=sha").
+          with(headers: { "Authorization" => "token token" }).
+          to_return(status: 404)
+      end
+
+      it "doesn't try to fetch the tarball as a package" do
+        expect(file_fetcher_instance.files.map(&:name)).to eq(
+          ["package.json", "package-lock.json"]
+        )
+      end
+    end
+
     context "that has an unfetchable path" do
       before do
         stub_request(:get, File.join(url, "deps/etag/package.json?ref=sha")).