e8bd345f62b5d34e8753987fbb0b0708a0711035
Pix4DJanus <platform_ci_team@pix4d.com>
2020-04-03 17:34:59 +0200

    Style changes due to rubocop

--- docker/dependabot-main.rb
+++ docker/dependabot-main.rb
@@ -118,13 +118,12 @@ input_files_path.each do |file_path|
 
     puts pull_request.html_url
 
-    if ENV["FEATURE_PACKAGE"] == "docker"
-      auto_merge(pull_request.number,
-                 pull_request.head.ref,
-                 ENV["PROJECT_PATH"],
-                 ENV["GITHUB_ACCESS_TOKEN"])
-    end
+    next unless ENV["FEATURE_PACKAGE"] == "docker"
 
+    auto_merge(pull_request.number,
+               pull_request.head.ref,
+               ENV["PROJECT_PATH"],
+               ENV["GITHUB_ACCESS_TOKEN"])
   end
 end
 puts "Done"
