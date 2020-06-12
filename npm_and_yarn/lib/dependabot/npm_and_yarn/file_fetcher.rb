64b6310a3494b153379651da615e4b43ffb8ae3b
Milind Pincha <milind009pincha@gmail.com>
2020-06-12 17:21:07 +0530

    added missing comma

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -135,7 +135,7 @@ module Dependabot
           fetch_file_if_present("common/config/rush/.npmrc"),
           fetch_file_if_present("common/scripts/install-run-rush.js"),
           fetch_file_if_present("common/scripts/install-run.js"),
-          fetch_file_if_present("common/config/pnpmfile-dependencies.json")
+          fetch_file_if_present("common/config/pnpmfile-dependencies.json"),
           fetch_file_if_present("common/config/rush/common-versions.json")
         ].compact        
       end
