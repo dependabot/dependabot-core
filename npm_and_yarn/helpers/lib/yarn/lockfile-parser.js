d57768cca194f96a2cb7c6bea642fd587b428cf2
Young Min Kin <mail@ylem.kim>
2020-08-07 13:32:36 +0900

    Add Yarn 2 support for parseLockfile

--- npm_and_yarn/helpers/lib/yarn/lockfile-parser.js
+++ npm_and_yarn/helpers/lib/yarn/lockfile-parser.js
@@ -10,11 +10,15 @@ const fs = require("fs");
 const path = require("path");
 const parseLockfile = require("@dependabot/yarn-lib/lib/lockfile/parse")
   .default;
+const { parseSyml } = require("@yarnpkg/parsers");
 
 async function parse(directory) {
   const readFile = (fileName) =>
     fs.readFileSync(path.join(directory, fileName)).toString();
   const data = readFile("yarn.lock");
+  if (data.indexOf("__metadata:\n") > -1) {
+    return parseSyml(data);
+  }
   return parseLockfile(data).object;
 }
 
