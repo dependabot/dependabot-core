73d1e37e955afc810754aa448fc5091d8368f947
jurre <jurre@github.com>
2020-09-10 10:36:58 +0200

    Remove nuget registry namespace check
    I'm not quite sure why we need this, and removing it does not fail any
    tests. As long as we can get all the data we need out of the XML doc I
    don't think we should be fussy about the namespace.

--- nuget/lib/dependabot/nuget/update_checker/repository_finder.rb
+++ nuget/lib/dependabot/nuget/update_checker/repository_finder.rb
@@ -11,7 +11,6 @@ module Dependabot
     class UpdateChecker
       class RepositoryFinder
         DEFAULT_REPOSITORY_URL = "https://api.nuget.org/v3/index.json"
-        ATOM_PUB_NAMESPACE = "http://www.w3.org/2007/app"
 
         def initialize(dependency:, credentials:, config_files: [])
           @dependency  = dependency
@@ -94,8 +93,6 @@ module Dependabot
 
         def build_v2_url(response, repo_details)
           doc = Nokogiri::XML(response.body)
-          return unless doc.root.namespace&.href&.casecmp(ATOM_PUB_NAMESPACE)&.
-                        zero?
 
           doc.remove_namespaces!
           base_url = doc.at_xpath("service")&.attributes&.
