11f1367cfd5976339097a375954090b685cecaf1
Philip Harrison <philip@mailharrison.com>
2020-09-29 17:38:45 +0100

    Fix gemfile declaration finder

--- bundler/lib/dependabot/bundler/file_parser/gemfile_declaration_finder.rb
+++ bundler/lib/dependabot/bundler/file_parser/gemfile_declaration_finder.rb
@@ -20,7 +20,7 @@ module Dependabot
         def enhanced_req_string
           return unless gemfile_includes_dependency?
 
-          fallback_string = dependency.requirement.to_s
+          fallback_string = dependency.requirement
           req_nodes = declaration_node.children[3..-1]
           req_nodes = req_nodes.reject { |child| child.type == :hash }
 
@@ -28,7 +28,9 @@ module Dependabot
           return fallback_string unless req_nodes.all? { |n| n.type == :str }
 
           original_req_string = req_nodes.map { |n| n.children.last }
-          if dependency.requirement == Gem::Requirement.new(original_req_string)
+          fallback_requirement =
+            Gem::Requirement.new(fallback_string.split(", "))
+          if fallback_requirement == Gem::Requirement.new(original_req_string)
             original_req_string.join(", ")
           else
             fallback_string
