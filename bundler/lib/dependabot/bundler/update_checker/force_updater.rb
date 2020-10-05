d993627970e78794adc82d9c93b609fc41d4b99b
Philip Harrison <philip@mailharrison.com>
2020-10-05 15:47:41 +0100

    Remove monkey patch require from force updater

--- bundler/lib/dependabot/bundler/update_checker/force_updater.rb
+++ bundler/lib/dependabot/bundler/update_checker/force_updater.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require "dependabot/monkey_patches/bundler/definition_bundler_version_patch"
-require "dependabot/monkey_patches/bundler/definition_ruby_version_patch"
-require "dependabot/monkey_patches/bundler/git_source_patch"
-
 require "dependabot/bundler/file_parser"
 require "dependabot/bundler/file_updater/lockfile_updater"
 require "dependabot/bundler/native_helpers"
