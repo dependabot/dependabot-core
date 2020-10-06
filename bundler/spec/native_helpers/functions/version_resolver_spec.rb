abab357c886b2abe0347f9fbfa2dc419655ee04f
Barry Gordon <brrygrdn@github.com>
2020-10-06 20:40:31 +0100

    Wrap test execution in a temp dir

--- bundler/spec/native_helpers/functions/version_resolver_spec.rb
+++ bundler/spec/native_helpers/functions/version_resolver_spec.rb
@@ -7,30 +7,30 @@ require "dependabot/dependency"
 require_relative "../../../helpers/lib/functions/version_resolver"
 
 RSpec.describe Functions::VersionResolver do
+  let(:gemfile_fixture) do
+    fixture("ruby", "gemfiles", gemfile_fixture_name)
+  end
+  let(:lockfile_fixture) do
+    fixture("ruby", "lockfiles", lockfile_fixture_name)
+  end
+
+  let(:tmp_path) do
+    dir = Dir.mktmpdir("native_helper_spec_", "tmp")
+    Pathname.new(dir).expand_path
+  end
+
+  before do
+    File.write(File.join(tmp_path, "Gemfile"), gemfile_fixture)
+    File.write(File.join(tmp_path, "Gemfile.lock"), lockfile_fixture)
+  end
+
   let(:version_resolver) do
     described_class.new(
       dependency_name: dependency_name,
       dependency_requirements: dependency_requirements,
-      gemfile_name: gemfile_name,
-      lockfile_name: lockfile_name
+      gemfile_name: "Gemfile",
+      lockfile_name: "Gemfile.lock"
     )
-  end
-
-  let(:gemfile_fixture_directory) do
-    File.join(
-      File.dirname(__FILE__), "..", "..", "fixtures", "ruby", "gemfiles"
-    )
-  end
-  let(:gemfile_name) do
-    File.join(gemfile_fixture_directory, gemfile_fixture_name)
-  end
-  let(:lockfile_fixture_directory) do
-    File.join(
-      File.dirname(__FILE__), "..", "..", "fixtures", "ruby", "lockfiles"
-    )
-  end
-  let(:lockfile_name) do
-    File.join(lockfile_fixture_directory, lockfile_fixture_name)
   end
 
   let(:dependency_name) { "business" }
@@ -49,9 +49,11 @@ RSpec.describe Functions::VersionResolver do
   include_context "stub rubygems compact index"
 
   describe "#version_details" do
-    subject { version_resolver.version_details }
-
-    include_context "stub rubygems compact index"
+    subject do
+      Dir.chdir(tmp_path) do
+        version_resolver.version_details
+      end
+    end
 
     context "with a private gemserver source" do
       let(:gemfile_fixture_name) { "specified_source" }
@@ -76,56 +78,53 @@ RSpec.describe Functions::VersionResolver do
 
       its([:version]) { is_expected.to eq(Gem::Version.new("1.9.0")) }
     end
-  end
 
-  # TODO: Wrap tests in a temp folder isolation and unpend
-  context "with a legacy Ruby which disallows the latest version" do
-    subject { version_resolver.version_details }
+    context "with a legacy Ruby which disallows the latest version" do
+      let(:gemfile_fixture_name) { "legacy_ruby" }
+      let(:lockfile_fixture_name) { "legacy_ruby.lock" }
+      let(:dependency_name) { "public_suffix" }
+      let(:requirement_string) { ">= 0" }
 
-    let(:gemfile_fixture_name) { "legacy_ruby" }
-    let(:lockfile_fixture_name) { "legacy_ruby.lock" }
-    let(:dependency_name) { "public_suffix" }
-    let(:requirement_string) { ">= 0" }
+      # The latest version of public_suffix is 2.0.5, but requires Ruby 2.0.0
+      # or greater.
+      its([:version]) { is_expected.to eq(Gem::Version.new("1.4.6")) }
 
-    # The latest version of public_suffix is 2.0.5, but requires Ruby 2.0.0
-    # or greater.
-    its([:version]) { is_expected.to eq(Gem::Version.new("1.4.6")) }
+      context "when Bundler's compact index is down" do
+        before do
+          old_index_url = "https://index.rubygems.org/api/v1/dependencies"
+          stub_request(:get, "https://index.rubygems.org/versions").
+            to_return(status: 500, body: "We'll be back soon")
+          stub_request(:get, "https://index.rubygems.org/info/public_suffix").
+            to_return(status: 500, body: "We'll be back soon")
+          stub_request(:get, old_index_url).to_return(status: 200)
+          stub_request(:get, old_index_url + "?gems=public_suffix").
+            to_return(
+              status: 200,
+              body: fixture("ruby",
+                            "rubygems_responses",
+                            "dependencies-public_suffix")
+            )
 
-    context "when Bundler's compact index is down" do
-      before do
-        old_index_url = "https://index.rubygems.org/api/v1/dependencies"
-        stub_request(:get, "https://index.rubygems.org/versions").
-          to_return(status: 500, body: "We'll be back soon")
-        stub_request(:get, "https://index.rubygems.org/info/public_suffix").
-          to_return(status: 500, body: "We'll be back soon")
-        stub_request(:get, old_index_url).to_return(status: 200)
-        stub_request(:get, old_index_url + "?gems=public_suffix").
-          to_return(
-            status: 200,
-            body: fixture("ruby",
-                          "rubygems_responses",
-                          "dependencies-public_suffix")
-          )
-
-        stub_request(:get, rubygems_url + "versions/public_suffix.json").
-          to_return(status: 200, body: rubygems_versions)
-      end
-      let(:rubygems_versions) do
-        fixture("ruby", "rubygems_responses", "versions-public_suffix.json")
-      end
-
-      it { is_expected.to be_nil }
-
-      context "and the dependency doesn't have a required Ruby version" do
+          stub_request(:get, rubygems_url + "versions/public_suffix.json").
+            to_return(status: 200, body: rubygems_versions)
+        end
         let(:rubygems_versions) do
-          fixture(
-            "ruby",
-            "rubygems_responses",
-            "versions-public_suffix.json"
-          ).gsub(/"ruby_version": .*,/, '"ruby_version": null,')
+          fixture("ruby", "rubygems_responses", "versions-public_suffix.json")
         end
 
-        its([:version]) { is_expected.to eq(Gem::Version.new("3.0.2")) }
+        it { is_expected.to be_nil }
+
+        context "and the dependency doesn't have a required Ruby version" do
+          let(:rubygems_versions) do
+            fixture(
+              "ruby",
+              "rubygems_responses",
+              "versions-public_suffix.json"
+            ).gsub(/"ruby_version": .*,/, '"ruby_version": null,')
+          end
+
+          its([:version]) { is_expected.to eq(Gem::Version.new("3.0.2")) }
+        end
       end
     end
   end
