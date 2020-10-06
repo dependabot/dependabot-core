65e83854ac26675a9e935563cb92cdee84a8af4a
jurre <jurre@github.com>
2020-10-06 16:11:34 +0200

    Add test coverage for VendorUpdater

--- common/lib/dependabot/file_updaters/vendor_updater.rb
+++ common/lib/dependabot/file_updaters/vendor_updater.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "dependabot/dependency_file"
+
 module Dependabot
   module FileUpdaters
     class VendorUpdater
---
+++ common/spec/dependabot/file_fetchers/vendor_updater_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/file_updaters/vendor_updater"
+
+RSpec.describe Dependabot::FileUpdaters::VendorUpdater do
+  let(:updater) do
+    described_class.new(
+      repo_contents_path: repo_contents_path,
+      vendor_dir: vendor_dir
+    )
+  end
+
+  let(:vendor_dir) { File.join(repo_contents_path, "vendor/cache") }
+  let(:project_name) { "vendor_gems" }
+  let(:repo_contents_path) { build_tmp_repo(project_name) }
+
+  let(:updated_files) do
+    updater.updated_vendor_cache_files(base_directory: repo_contents_path)
+  end
+
+  before do
+    in_cloned_repository(repo_contents_path) do
+      `mv vendor/cache/business-1.4.0.gem vendor/cache/business-1.5.0.gem`
+    end
+  end
+
+  after do
+    FileUtils.remove_entry repo_contents_path
+  end
+
+  describe "#updated_vendor_cache_files" do
+    it "returns the updated files" do
+      expect(updated_files.map(&:name)).to eq(
+        %w(
+          vendor/cache/business-1.4.0.gem
+          vendor/cache/business-1.5.0.gem
+        )
+      )
+    end
+
+    it "marks binary files as such" do
+      expect(updated_files.first.binary?).to be_truthy
+    end
+
+    it "marks deleted files as such" do
+      file = updated_files.find do |f|
+        f.name == "vendor/cache/business-1.4.0.gem"
+      end
+
+      expect(file).to be_deleted
+    end
+
+    it "base64 encodes binary files" do
+      file = updated_files.find do |f|
+        f.name == "vendor/cache/business-1.5.0.gem"
+      end
+
+      expect(file.content_encoding).to eq("base64")
+    end
+
+    it "ignores files that are in the .gitignore" do
+      in_cloned_repository(repo_contents_path) do
+        `touch vendor/cache/ignored.txt`
+      end
+
+      expect(updated_files.map(&:name)).to_not include(
+        "vendor/cache/ignored.txt"
+      )
+    end
+
+    it "ignores files that are not in the vendor directory" do
+      in_cloned_repository(repo_contents_path) do
+        `touch some-file.txt`
+      end
+
+      expect(updated_files.map(&:name)).to_not include(
+        "some-file.txt"
+      )
+    end
+  end
+
+  private
+
+  def in_cloned_repository(repo_contents_path, &block)
+    Dir.chdir(repo_contents_path) { block.call }
+  end
+end
