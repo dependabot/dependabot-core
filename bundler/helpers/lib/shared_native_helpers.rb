3e698c87ff82d0f451e0574c26fb9abe74960919
Philip Harrison <philip@mailharrison.com>
2020-10-07 17:28:33 +0100

    Update bundler/helpers/lib/shared_native_helpers.rb

--- bundler/helpers/lib/shared_native_helpers.rb
+++ bundler/helpers/lib/shared_native_helpers.rb
@@ -4,6 +4,7 @@ require "excon"
 
 module SharedNativeHelpers
   # Duplicated in lib/dependabot/bundler/file_updater/lockfile_updater.rb
+  # TODO: Stop sanitizing the lockfile once we have bundler 2 installed
   LOCKFILE_ENDING = /(?<ending>\s*(?:RUBY VERSION|BUNDLED WITH).*)/m.freeze
 
   USER_AGENT = "dependabot-core/bundler-helper "\
