b33e6ec445226eb05c9e4eee9c74b565da616a05
Philip Harrison <philip@mailharrison.com>
2020-10-08 09:19:45 +0100

    Remove excon_defaults from SharedNativeHelpers

--- bundler/helpers/lib/shared_native_helpers.rb
+++ bundler/helpers/lib/shared_native_helpers.rb
@@ -6,36 +6,4 @@ module SharedNativeHelpers
   # Duplicated in lib/dependabot/bundler/file_updater/lockfile_updater.rb
   # TODO: Stop sanitizing the lockfile once we have bundler 2 installed
   LOCKFILE_ENDING = /(?<ending>\s*(?:RUBY VERSION|BUNDLED WITH).*)/m.freeze
-
-  USER_AGENT = "dependabot-core/bundler-helper "\
-               "#{Excon::USER_AGENT} ruby/#{RUBY_VERSION} "\
-               "(#{RUBY_PLATFORM}) "\
-               "(+https://github.com/dependabot/dependabot-core)"
-
-  def self.excon_middleware
-    Excon.defaults[:middlewares] +
-      [Excon::Middleware::Decompress] +
-      [Excon::Middleware::RedirectFollower]
-  end
-
-  def self.excon_headers(headers = nil)
-    headers ||= {}
-    {
-      "User-Agent" => USER_AGENT
-    }.merge(headers)
-  end
-
-  # Duplicated in common/lib/dependabot/shared_helpers.rb
-  def self.excon_defaults(options = nil)
-    options ||= {}
-    headers = options.delete(:headers)
-    {
-      connect_timeout: 5,
-      write_timeout: 5,
-      read_timeout: 20,
-      omit_default_port: true,
-      middlewares: excon_middleware,
-      headers: excon_headers(headers)
-    }.merge(options)
-  end
 end
