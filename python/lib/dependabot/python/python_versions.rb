045a8ee1392a8002220af79d35f205cac309e46f
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2020-10-12 00:40:11 +0300

    Add Python 3.9

--- python/lib/dependabot/python/python_versions.rb
+++ python/lib/dependabot/python/python_versions.rb
@@ -4,12 +4,13 @@ module Dependabot
   module Python
     module PythonVersions
       PRE_INSTALLED_PYTHON_VERSIONS = %w(
-        3.8.6 2.7.18
+        3.9.0 2.7.18
       ).freeze
 
       # Due to an OpenSSL issue we can only install the following versions in
       # the Dependabot container.
       SUPPORTED_VERSIONS = %w(
+        3.9.0
         3.8.6 3.8.5 3.8.4 3.8.3 3.8.2 3.8.1 3.8.0
         3.7.9 3.7.8 3.7.7 3.7.6 3.7.5 3.7.4 3.7.3 3.7.2 3.7.1 3.7.0
         3.6.9 3.6.8 3.6.7 3.6.6 3.6.5 3.6.4 3.6.3 3.6.2 3.6.1 3.6.0
