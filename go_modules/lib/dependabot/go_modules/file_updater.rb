43b4047b1f18f55c48fd1d568f63b5d7f0202706
Jason Rudolph <jasonrudolph@github.com>
2020-10-20 10:16:31 -0400

    ♻️ Refactor: Extract method with intention-revealing name

--- go_modules/lib/dependabot/go_modules/file_updater.rb
+++ go_modules/lib/dependabot/go_modules/file_updater.rb
@@ -13,21 +13,8 @@ module Dependabot
       def initialize(dependencies:, dependency_files:, repo_contents_path: nil,
                      credentials:, options: {})
         super
-        return unless repo_contents_path.nil?
 
-        # Create a placeholder repository at repo_contents_path
-        tmp = Dir.mktmpdir
-        Dir.chdir(tmp) do
-          dependency_files.each do |file|
-            File.write(file.name, file.content)
-          end
-          `git config --global user.email "no-reply@github.com"`
-          `git config --global user.name "Dependabot"`
-          `git init .`
-          `git add .`
-          `git commit -m'fake repo_contents_path'`
-        end
-        @repo_contents_path = tmp
+        @repo_contents_path = repo_contents_path || create_placeholder_repo
       end
 
       def self.updated_files_regex
@@ -73,6 +60,21 @@ module Dependabot
         return if go_mod
 
         raise "No go.mod!"
+      end
+
+      def create_placeholder_repo
+        tmp = Dir.mktmpdir
+        Dir.chdir(tmp) do
+          dependency_files.each do |file|
+            File.write(file.name, file.content)
+          end
+          `git config --global user.email "no-reply@github.com"`
+          `git config --global user.name "Dependabot"`
+          `git init .`
+          `git add .`
+          `git commit -m'fake repo_contents_path'`
+        end
+        tmp
       end
 
       def go_mod
