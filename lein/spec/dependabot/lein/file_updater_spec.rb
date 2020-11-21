3ba12ab53e37e2b9303b3ca9e6bb7f9898f9a83d
Christian Gregg <christian@bissy.io>
2020-11-21 15:02:16 +0000

    Add test for fully qualified dependency (containing group and artifact)
    Signed-off-by: Christian Gregg <christian@bissy.io>

--- lein/spec/dependabot/lein/file_updater_spec.rb
+++ lein/spec/dependabot/lein/file_updater_spec.rb
@@ -32,42 +32,90 @@ RSpec.describe Dependabot::Lein::FileUpdater do
       name: "project.clj"
     )
   end
-  let(:dependency) do
-    Dependabot::Dependency.new(
-      name: "commons-io:commons-io",
-      version: "2.7",
-      requirements:
-      [{ file: "pom.xml", requirement: "2.7", groups: [], source: nil }],
-      previous_version: "2.6",
-      previous_requirements:
-        [{ file: "pom.xml", requirement: "2.6", groups: [], source: nil }],
-      package_manager: "lein"
-    )
-  end
 
-  describe "#updated_dependency_files" do
-    subject(:updated_files) { updater.updated_dependency_files }
-
-    it "returns DependencyFile objects" do
-      updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+  describe "an abbreviated dependency" do
+    let(:dependency) do
+      Dependabot::Dependency.new(
+        name: "commons-io:commons-io",
+        version: "2.7",
+        requirements:
+        [{ file: "pom.xml", requirement: "2.7", groups: [], source: nil }],
+        previous_version: "2.6",
+        previous_requirements:
+          [{ file: "pom.xml", requirement: "2.6", groups: [], source: nil }],
+        package_manager: "lein"
+      )
     end
 
-    it "returns the project.clj" do
-      expect(updated_files.count).to eq(1)
-      expect(updated_files.first.name).to eq("project.clj")
-    end
+    describe "#updated_dependency_files" do
+      subject(:updated_files) { updater.updated_dependency_files }
 
-    describe "updated project.clj" do
-      subject(:updated_project_file) { updated_files.find { |f| f.name == "project.clj" } }
-
-      it "has updated commons-io" do
-        expect(updated_project_file.content).to include(%([commons-io "2.7"]))
+      it "returns DependencyFile objects" do
+        updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
       end
 
-      it "has not changed anything else" do
-        expected_content = updated_project_file.content.gsub(%([commons-io "2.7"]), %([commons-io "2.6"]))
+      it "returns the project.clj" do
+        expect(updated_files.count).to eq(1)
+        expect(updated_files.first.name).to eq("project.clj")
+      end
 
-        expect(expected_content).to eq(project_file.content)
+      describe "updated project.clj" do
+        subject(:updated_project_file) { updated_files.find { |f| f.name == "project.clj" } }
+
+        it "has updated commons-io" do
+          expect(updated_project_file.content).to include(%([commons-io "2.7"]))
+        end
+
+        it "has not changed anything else" do
+          expected_content = updated_project_file.content.gsub(%([commons-io "2.7"]), %([commons-io "2.6"]))
+
+          expect(project_file.content).to eq(expected_content)
+        end
+      end
+    end
+  end
+
+  describe "a fully qualified dependency" do
+    let(:dependency) do
+      Dependabot::Dependency.new(
+        name: "org.apache.httpcomponents:httpmime",
+        version: "4.5.15",
+        requirements:
+        [{ file: "pom.xml", requirement: "4.5.15", groups: [], source: nil }],
+        previous_version: "4.5.13",
+        previous_requirements:
+        [{ file: "pom.xml", requirement: "4.5.13", groups: [], source: nil }],
+        package_manager: "lein"
+      )
+    end
+
+    describe "#updated_dependency_files" do
+      subject(:updated_files) { updater.updated_dependency_files }
+
+      it "returns DependencyFile objects" do
+        updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+      end
+
+      it "returns the project.clj" do
+        expect(updated_files.count).to eq(1)
+        expect(updated_files.first.name).to eq("project.clj")
+      end
+
+      describe "updated project.clj" do
+        subject(:updated_project_file) { updated_files.find { |f| f.name == "project.clj" } }
+
+        it "has updated commons-io" do
+          expect(updated_project_file.content).to include(%([org.apache.httpcomponents/httpmime "4.5.15"]))
+        end
+
+        it "has not changed anything else" do
+          expected_content = updated_project_file.content.gsub(
+            %([org.apache.httpcomponents/httpmime "4.5.15"]),
+            %([org.apache.httpcomponents/httpmime "4.5.13"])
+          )
+
+          expect(project_file.content).to eq(expected_content)
+        end
       end
     end
   end
