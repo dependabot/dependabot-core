ec04bd94d68d49f883ae324d7cbb0e2d3632a7c8
Philip Harrison <philip@mailharrison.com>
2021-01-18 14:58:59 +0000

    Add npm7 js tests

---
+++ npm_and_yarn/helpers/test/npm7/updater.test.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const rimraf = require("rimraf");
+const { updateDependencyFiles } = require("../../lib/npm7/updater");
+const helpers = require("./helpers");
+
+describe("updater", () => {
+  let tempDir;
+  beforeEach(() => {
+    tempDir = fs.mkdtempSync(os.tmpdir() + path.sep);
+  });
+  afterEach(() => rimraf.sync(tempDir));
+
+  it("generates an updated package-lock.json", async () => {
+    helpers.copyDependencies("updater/original", tempDir);
+
+    const result = await updateDependencyFiles(tempDir, "package-lock.json", [
+      {
+        name: "left-pad",
+        version: "1.1.3",
+        requirements: [{ file: "package.json", groups: ["dependencies"] }],
+      },
+    ]);
+    expect(result["package-lock.json"]).toEqual(
+      helpers.loadFixture("updater/updated/package-lock.json").trim()
+    );
+  });
+});
