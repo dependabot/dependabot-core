1091677dd72486204f47a605277cf294f31f8cf2
aliculPix4D <aleksandar.licul_ext@pix4d.com>
2021-02-08 17:48:50 +0100

    PCI-1724 Add regression test

--- docker/dependabot-main_spec.rb
+++ docker/dependabot-main_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "dependabot-main"
+require "spec_helper"
 
 # rubocop:disable Metrics/BlockLength
-RSpec.describe "main function", :pix4d do
+RSpec.describe "describe main function", :pix4d do
   def fixture(*name)
     File.read(File.join("spec", "fixtures", *name))
   end
@@ -54,6 +55,7 @@ RSpec.describe "main function", :pix4d do
   let(:dependency_file) do
     Dependabot::DependencyFile.new(name: file_name, content: fixture_file, directory: dependency_dir)
   end
+  let(:github_url) { "https://api.github.com/" }
 
   let(:expected_commit) { "c1a68d7" }
   let(:file_name) { "public-simple.yml" }
@@ -91,6 +93,60 @@ RSpec.describe "main function", :pix4d do
       allow(self).to receive(:requirements).and_return(":own")
       allow(self).to receive(:checker_updated_dependencies).and_return([updated_dependency_instance])
       allow(self).to receive(:pr_creator).and_return(pull_request)
+
+      actual = main(project_data, fake_token, docker_cred)
+      expect(actual).to equal("Success")
+    end
+  end
+
+  context "for docker module" do
+    let(:project_data) do
+      {
+        "module" => "docker",
+        "repo" => "Pix4D/test_repo",
+        "branch" => "staging",
+        "dependency_dir" => "dockerfiles/"
+      }
+    end
+
+    let(:github_sha) { "76abc" }
+    let(:repo) { project_data["repo"] }
+    let(:branch) { project_data["branch"] }
+    let(:url1) { github_url + "repos/#{repo}/branches/#{branch}" }
+    let(:url2) do
+      github_url +
+        "repos/#{repo}/git/trees/#{github_sha}?recursive=true"
+    end
+    let(:fixture_file) { fixture("pipelines", file_name) }
+    let(:dependency_dir) { project_data["dependency_dir"] }
+    let(:repo) { project_data["repo"] }
+    let(:branch) { project_data["branch"] }
+    before do
+      stub_request(:get, url1).
+        to_return(
+          status: 200,
+          body: { "name": branch, "commit": { "sha": github_sha } }.to_json,
+          headers: { "content-type" => "application/json" }
+        )
+      stub_request(:get, url2).
+        to_return(
+          status: 200,
+          body: { "sha": github_sha, "tree": [
+            { "path": "dockerfiles/folder-1/Dockerfile" }
+          ] }.to_json,
+          headers: { "content-type" => "application/json" }
+        )
+    end
+
+    it "when directory tree for staging branch is wanted" do
+
+      allow(self).to receive(:file_fetcher).and_return([[dependency_file], expected_commit])
+      allow(self).to receive(:file_parser).and_return([dependency_instance])
+      allow(self).to receive(:checker_up_to_date).and_return(false)
+      allow(self).to receive(:requirements).and_return(":own")
+      allow(self).to receive(:checker_updated_dependencies).and_return([updated_dependency_instance])
+      allow(self).to receive(:pr_creator).and_return(pull_request)
+      allow(self).to receive(:auto_merge).and_return("")
 
       actual = main(project_data, fake_token, docker_cred)
       expect(actual).to equal("Success")
