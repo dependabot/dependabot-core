b197ac7c4eed8c44a937fd62ef31400f1bb67ee5
Barry Gordon <brrygrdn@github.com>
2021-02-22 10:25:39 +0000

    Revert to block_range

--- bundler/spec/support/project_fixtures/autocorrector.rb
+++ bundler/spec/support/project_fixtures/autocorrector.rb
@@ -30,23 +30,16 @@ module ProjectFixtures
         # TODO: This messes up the indentation, rubocop can autofix this for us,
         # but would be nice to figure out how to determine it. Breadcrumb:
         # https://github.com/rubocop-hq/rubocop/blob/751edc7bf3df93d7ec5d59f5ac5b501627a6b723/lib/rubocop/cop/layout/heredoc_indentation.rb#L144-L155
-        its_block = node.loc.expression.source.start_with?("its")
-        if its_block
-          replace(
-            block_range(node),
-            node.loc.expression.source.split("\n").tap do |lines|
-              lines.insert(0, "let(:dependency_files) { project_dependency_files(\"#{@project_name}\") }\n")
-            end.join("\n")
-          )
-        else
-          byebug
-          replace(
-            block_range(node.parent),
-            node.parent.loc.expression.source.split("\n").tap do |lines|
-              lines.insert(1, "let(:dependency_files) { project_dependency_files(\"#{@project_name}\") }")
-            end.join("\n")
-          )
-        end
+        #
+        # child = node.children[-1].children.select { |n| n.type.equal?(:block) }.first
+        insert_before(
+          # This is gross, but we know this is a block, so the second child will
+          # be the actual block. Its children are the content of that block, and
+          # we want to insert before the first line in the block, ensuring that
+          # the first thing the block defines is a files definition.
+          block_range(node),
+          "let(:dependency_files) { project_dependency_files(\"#{@project_name}\") }\n"
+        )
         return super
       end
 
