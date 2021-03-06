f94051a9d83cabf8e76bb4a55b891fc30058033d
David McIntosh <804610+mctofu@users.noreply.github.com>
2021-03-05 17:34:03 -0800

    Renamed packages cause DependencyFileNotResolvable with go 1.16
    Previously this error was only occuring with `go mod tidy` and was being
    ignored. With go 1.16 the error is triggered during `go get` so we need to
    handle it again.
    
    Distinguishing the error from GitDependenciesNotReachable also took some
    extra effort. The error message has changed from:
    github.com/dependabot/vgotest imports
    	github.com/googleapis/gnostic/OpenAPIv2: module github.com/googleapis/gnostic@latest found (v0.5.1), but does not contain package github.com/googleapis/gnostic/OpenAPIv2
    
    to:
    github.com/dependabot/vgotest imports
            github.com/googleapis/gnostic/OpenAPIv2: cannot find module providing package github.com/googleapis/gnostic/OpenAPIv2
    
    We now use `go list` to check that github.com/googleapis/gnostic is a
    reachable repo/module.

--- go_modules/lib/dependabot/go_modules/resolvability_errors.rb
+++ go_modules/lib/dependabot/go_modules/resolvability_errors.rb
@@ -15,16 +15,17 @@ module Dependabot
           SharedHelpers.with_git_configured(credentials: credentials) do
             File.write("go.mod", "module dummy\n")
 
-            env = { "GOPRIVATE" => "*" }
-            _, _, status = Open3.capture3(env, SharedHelpers.escape_command("go get #{mod_path}"))
-            raise Dependabot::DependencyFileNotResolvable, message if status.success?
-
             mod_split = mod_path.split("/")
             repo_path = if mod_split.size > 3
                           mod_split[0..2].join("/")
                         else
                           mod_path
                         end
+
+            env = { "GOPRIVATE" => "*" }
+            _, _, status = Open3.capture3(env, SharedHelpers.escape_command("go list -m -versions #{repo_path}"))
+            raise Dependabot::DependencyFileNotResolvable, message if status.success?
+
             raise Dependabot::GitDependenciesNotReachable, [repo_path]
           end
         end
