00c58291eb631e0d9d3e231c8538b6433f2e875f
milind009 <milind009pincha@gmail.com>
2021-03-16 15:28:41 +0530

    Update file_parser.rb

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_parser.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_parser.rb
@@ -124,7 +124,7 @@ module Dependabot
       end
 
       def check_required_files
-        raise "No package.json!" unless get_original_file("package.json")
+        raise "No manifest file!" unless get_original_file("package.json") || get_original_file("rush.json")
       end
 
       def ignore_requirement?(requirement)
