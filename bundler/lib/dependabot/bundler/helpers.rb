c3071cf123d5cb5be3841af004bbd5888369720b
Jurre Stender <jurrestender@gmail.com>
2021-03-16 14:39:35 +0100

    Instrument bundler package manager version

--- bundler/lib/dependabot/bundler/helpers.rb
+++ bundler/lib/dependabot/bundler/helpers.rb
@@ -11,6 +11,13 @@ module Dependabot
       def self.bundler_version(_lockfile)
         V1
       end
+
+      def self.detected_bundler_version(lockfile)
+        return "unknown" unless lockfile
+        return V2 if lockfile.content.match?(/BUNDLED WITH\s+2/m)
+
+        V1
+      end
     end
   end
 end
