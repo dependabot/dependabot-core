1975d594695ab94f1e696d1f7a532329420a10a7
Philip Harrison <philip@mailharrison.com>
2021-03-22 11:59:55 +0000

    Set up matrix builds for bundler1 and bundler2

--- bundler/spec/spec_helper.rb
+++ bundler/spec/spec_helper.rb
@@ -9,3 +9,19 @@ def require_common_spec(path)
 end
 
 require "#{common_dir}/spec/spec_helper.rb"
+
+def bundler_2_available?
+  ENV["SUITE_NAME"] == "bundler2"
+end
+
+RSpec.configure do |config|
+  config.around do |example|
+    if bundler_2_available? && example.metadata[:bundler_v1_only]
+      example.skip
+    elsif !bundler_2_available? && example.metadata[:bundler_v2_only]
+      example.skip
+    else
+      example.run
+    end
+  end
+end
