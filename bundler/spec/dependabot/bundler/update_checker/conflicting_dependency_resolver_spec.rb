2f9aa03f93a38dfb9bcac1f2b9627044506803d7
Philip Harrison <philip@mailharrison.com>
2021-03-23 16:06:26 +0000

    Bundler [Prerelease]: Add conflicting dep resolver
    Adding Bundler v2 native helpers for conflicting dependency resolver.

--- bundler/spec/dependabot/bundler/update_checker/conflicting_dependency_resolver_spec.rb
+++ bundler/spec/dependabot/bundler/update_checker/conflicting_dependency_resolver_spec.rb
@@ -6,9 +6,7 @@ require "dependabot/dependency"
 require "dependabot/dependency_file"
 require "dependabot/bundler/update_checker/conflicting_dependency_resolver"
 
-RSpec.describe(
-  Dependabot::Bundler::UpdateChecker::ConflictingDependencyResolver
-) do
+RSpec.describe(Dependabot::Bundler::UpdateChecker::ConflictingDependencyResolver) do
   include_context "stub rubygems compact index"
 
   let(:resolver) do
@@ -21,7 +19,7 @@ RSpec.describe(
         "username" => "x-access-token",
         "password" => "token"
       }],
-      options: {}
+      options: { bundler_2_available: bundler_2_available? }
     )
   end
   let(:dependency_files) { [gemfile, lockfile] }
@@ -44,10 +42,8 @@ RSpec.describe(
   let(:lockfile) do
     Dependabot::DependencyFile.new(content: lockfile_body, name: "Gemfile.lock")
   end
-  let(:gemfile_body) { fixture("ruby", "gemfiles", "subdep_blocked_by_subdep") }
-  let(:lockfile_body) do
-    fixture("ruby", "lockfiles", "subdep_blocked_by_subdep.lock")
-  end
+  let(:gemfile_body) { fixture("ruby", "projects", "bundler1", "subdep_blocked_by_subdep", "Gemfile") }
+  let(:lockfile_body) { fixture("ruby", "projects", "bundler1", "subdep_blocked_by_subdep", "Gemfile.lock") }
 
   describe "#conflicting_dependencies" do
     subject(:conflicting_dependencies) do
