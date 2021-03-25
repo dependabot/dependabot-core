b92608c7d6ecf1b7004f80c26c5cdca066bdaca1
Barry Gordon <brrygrdn@github.com>
2021-03-25 13:41:09 +0000

    Explicitly raise if attempting to load a missing project fixture

--- bundler/helpers/v2/spec/native_spec_helper.rb
+++ bundler/helpers/v2/spec/native_spec_helper.rb
@@ -26,7 +26,11 @@ end
 LOCKFILE_ENDING = /(?<ending>\s*(?:RUBY VERSION|BUNDLED WITH).*)/m.freeze
 
 def project_dependency_files(project)
+  # TODO: Retrieve files from bundler2 folder once it is fully up to date
   project_path = File.expand_path(File.join("../../spec/fixtures/projects/bundler1", project))
+
+  raise "Fixture does not exist for project: '#{project}'" unless Dir.exist?(project_path)
+
   Dir.chdir(project_path) do
     # NOTE: Include dotfiles (e.g. .npmrc)
     files = Dir.glob("**/*", File::FNM_DOTMATCH)
