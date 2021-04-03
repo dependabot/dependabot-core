14387dd6d81d3933ae3502f1f17e0254b5808a5d
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2021-04-03 16:19:22 +0200

    Add pub file parser

---
+++ pub/lib/dependabot/pub/file_parser.rb
@@ -0,0 +1,193 @@
+# frozen_string_literal: true
+
+require "cgi"
+require "excon"
+require "nokogiri"
+require "open3"
+require "yaml"
+require "dependabot/dependency"
+require "dependabot/file_parsers"
+require "dependabot/file_parsers/base"
+require "dependabot/git_commit_checker"
+require "dependabot/shared_helpers"
+require "dependabot/errors"
+require "dependabot/pub/native_helpers"
+
+module Dependabot
+  module Pub
+    class FileParser < Dependabot::FileParsers::Base
+      require "dependabot/file_parsers/base/dependency_set"
+
+      DEPENDENCY_TYPES =
+        %w(dependencies dev_dependencies).freeze
+
+      def parse
+        dependency_set = DependencySet.new
+        dependency_set += pubspec_yaml_dependencies
+        dependency_set += pubspec_lock_dependencies
+        dependency_set.dependencies
+      end
+
+      private
+
+      def pubspec_yaml_dependencies
+        dependency_set = DependencySet.new
+
+        pubspec_yaml_files.each do |file|
+          DEPENDENCY_TYPES.each do |type|
+            deps = YAML.load(file.content)[type] || {}
+            deps.each do |name, requirement|
+              dep = build_dependency(
+                file: file, type: type, name: name, requirement: requirement
+              )
+              next unless dep
+
+              dependency_set << dep if dep
+            end
+          end
+        end
+
+        dependency_set
+      end
+
+      def pubspec_lock_dependencies
+        dependency_set = DependencySet.new
+
+        pubspec_lock_files.each do |file|
+          lock_file_packages(file).each do |name, details|
+            dependency_set << Dependency.new(
+              name: name,
+              version: details["version"],
+              package_manager: "pub",
+              requirements: []
+            )
+          end
+        end
+
+        dependency_set
+      end
+
+      def build_dependency(file:, type:, name:, requirement:)
+        version = version_for(name: name, manifest_name: file.name)
+        req = requirement_for(requirement)
+        return nil unless req
+
+        Dependency.new(
+          name: name,
+          version: version,
+          package_manager: "pub",
+          requirements: [{
+            requirement: req,
+            file: file.name,
+            groups: [type],
+            source: source_for(name: name, manifest_name: file.name)
+          }]
+        )
+      end
+
+      def requirement_for(requirement)
+        if requirement.is_a?(String)
+          return SharedHelpers.in_a_temporary_directory do
+            SharedHelpers.run_helper_subprocess(
+              command: "#{NativeHelpers.helper_path} version --version \"#{requirement}\"",
+              function: "version_parser",
+              args: ["--version", requirement]
+            )
+          end
+        elsif requirement.is_a?(Hash) && requirement.key?("git")
+          git = requirement["git"]
+
+          return "git:#{git}" if git.is_a?(String)
+
+          url = git["url"]
+          path = git["path"]
+          ref = git["ref"]
+
+          req = "git:#{url}"
+          req += "/#{path}" if path
+          req += "##{ref}" if ref
+
+          return req
+        end
+
+        nil
+      end
+
+      def version_for(name:, manifest_name:)
+        version = lockfile_details(
+          name: name,
+          manifest_name: manifest_name
+        )&.fetch("version", nil)
+        version
+      end
+
+      def source_for(name:, manifest_name:)
+        details = lockfile_details(
+          name: name,
+          manifest_name: manifest_name
+        )
+        source_type = details["source"]
+        description = details["description"] || {}
+
+        if source_type == "hosted"
+          return {
+            type: source_type,
+            url: description["url"]
+          }
+        end
+        if source_type == "git"
+          return {
+            type: source_type,
+            url: description["url"],
+            path: description["path"],
+            branch: nil,
+            ref: description["ref"],
+            resolved_ref: description["resolved-ref"]
+          }
+        end
+        if source_type == "path"
+          return {
+            type: source_type,
+            path: description["path"],
+            relative: description["relative"]
+          }
+        end
+
+        nil
+      end
+
+      def lockfile_details(name:, manifest_name:)
+        lock_file_name = lockfile_name_for(manifest_name)
+        packages = lock_file_packages(lock_file_name)
+        dep = packages[name] || {}
+        dep
+      end
+
+      def lockfile_name_for(manifest_filename)
+        dir_name = File.dirname(manifest_filename)
+        lock_file = Pathname.new(File.join(dir_name, "pubspec.lock")).cleanpath.to_path
+
+        pubspec_lock_files.find { |f| f.name == lock_file }
+      end
+
+      def lock_file_packages(file)
+        YAML.load(file.content)["packages"] || {}
+      end
+
+      def pubspec_yaml_files
+        dependency_files.select { |f| f.name.end_with?("pubspec.yaml") }
+      end
+
+      def pubspec_lock_files
+        dependency_files.select { |f| f.name.end_with?("pubspec.lock") }
+      end
+
+      def check_required_files
+        raise "No pubspec.yaml!" unless get_original_file("pubspec.yaml")
+      end
+    end
+  end
+end
+
+Dependabot::FileParsers.
+  register("pub", Dependabot::Pub::FileParser)
---
+++ pub/spec/dependabot/pub/file_parser_spec.rb
@@ -0,0 +1,550 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/dependency_file"
+require "dependabot/source"
+require "dependabot/pub/file_parser"
+require_common_spec "file_parsers/shared_examples_for_file_parsers"
+
+RSpec.describe Dependabot::Pub::FileParser do
+  it_behaves_like "a dependency file parser"
+
+  let(:files) { [pubspec_yaml_file, pubspec_lock_file] }
+  let(:pubspec_yaml_file) do
+    Dependabot::DependencyFile.new(name: "pubspec.yaml", content: pubspec_yaml_body)
+  end
+  let(:pubspec_lock_file) do
+    Dependabot::DependencyFile.new(name: "pubspec.lock", content: pubspec_lock_body)
+  end
+  let(:pubspec_yaml_body) do
+    fixture("pubspec_yamlfiles", pubspec_fixture_name + ".yaml")
+  end
+  let(:pubspec_lock_body) do
+    fixture("pubspec_lockfiles", pubspec_fixture_name + ".lock")
+  end
+  let(:pubspec_fixture_name) { "hosted" }
+  let(:parser) { described_class.new(dependency_files: files, source: source) }
+  let(:source) do
+    Dependabot::Source.new(
+      provider: "github",
+      repo: "gocardless/bump",
+      directory: "/"
+    )
+  end
+
+  describe "parse" do
+    subject(:dependencies) { parser.parse }
+
+    context "with hosted sources" do
+      let(:pubspec_fixture_name) { "hosted" }
+
+      its(:length) { is_expected.to eq(2) }
+
+      # context "that are invalid" do
+      #   let(:pubspec_fixture_name) { "invalid_registry.tf" }
+
+      #   it "raises a helpful error" do
+      #     expect { parser.parse }.
+      #       to raise_error(Dependabot::DependencyFileNotEvaluatable) do |err|
+      #         expect(err.message).
+      #           to eq("Invalid registry source specified: 'consul/aws'")
+      #       end
+      #   end
+      # end
+
+      # context "that can't be parsed" do
+      #   let(:pubspec_fixture_name) { "unparseable.tf" }
+
+      #   it "raises a helpful error" do
+      #     expect { parser.parse }.
+      #       to raise_error(Dependabot::DependencyFileNotParseable) do |err|
+      #         expect(err.file_path).to eq("/main.tf")
+      #         expect(err.message).to eq(
+      #           "unable to parse HCL: object expected closing RBRACE got: EOF"
+      #         )
+      #       end
+      #   end
+      # end
+
+      describe "the first dependency (direct main with caret version)" do
+        subject(:dependency) { dependencies.first }
+        let(:expected_requirements) do
+          [{
+            requirement: ">= 1.7.0 and < 2.0.0-0",
+            groups: ["dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "hosted",
+              url: "https://pub.dartlang.org"
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("path")
+          expect(dependency.version).to eq("1.8.0")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+
+      describe "the second dependency (direct dev with caret version)" do
+        subject(:dependency) { dependencies[1] }
+        let(:expected_requirements) do
+          [{
+            requirement: ">= 1.9.0 and < 2.0.0-0",
+            groups: ["dev_dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "hosted",
+              url: "https://pub.dartlang.org"
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("pedantic")
+          expect(dependency.version).to eq("1.11.0")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+    end
+
+    context "with git sources via ssh" do
+      let(:pubspec_fixture_name) { "git_ssh" }
+
+      its(:length) { is_expected.to eq(2) }
+
+      describe "the first dependency (direct main)" do
+        subject(:dependency) { dependencies.first }
+        let(:expected_requirements) do
+          [{
+            requirement: "git:git@github.com:dart-lang/path.git",
+            groups: ["dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "git",
+              url: "git@github.com:dart-lang/path.git",
+              resolved_ref: "49a015d612541e549cfbe657ef48145ca32a98f8",
+              path: "."
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("path")
+          expect(dependency.version).to eq("1.8.1-dev")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+
+      describe "the second dependency (direct dev)" do
+        subject(:dependency) { dependencies[1] }
+        let(:expected_requirements) do
+          [{
+            requirement: "git:git@github.com:google/pedantic.git",
+            groups: ["dev_dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "git",
+              url: "git@github.com:google/pedantic.git",
+              resolved_ref: "66f2f6c27581c7936482e83be80b27be2719901c",
+              path: "."
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("pedantic")
+          expect(dependency.version).to eq("1.11.0")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+
+      context "with path" do
+        let(:pubspec_fixture_name) { "git_ssh_with_path" }
+
+        its(:length) { is_expected.to eq(34) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:rrousselGit/river_pod.git/packages/riverpod",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:rrousselGit/river_pod.git",
+                resolved_ref: "843adaa56bc34d617b07b14bdf4570afb907ee77",
+                path: "packages/riverpod"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("riverpod")
+            expect(dependency.version).to eq("0.13.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:rrousselGit/freezed.git/packages/freezed",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:rrousselGit/freezed.git",
+                resolved_ref: "cd29a64c3369bff6ccbe794c323b995adf15ac6a",
+                path: "packages/freezed"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("freezed")
+            expect(dependency.version).to eq("0.14.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+
+      context "with ref" do
+        let(:pubspec_fixture_name) { "git_ssh_with_ref" }
+
+        its(:length) { is_expected.to eq(3) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:dart-lang/path.git#1.7.0",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:dart-lang/path.git",
+                ref: "1.7.0",
+                resolved_ref: "10c778c799b2fc06036cbd0aa0e399ad4eb1ff5b",
+                path: "."
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("path")
+            expect(dependency.version).to eq("1.7.0")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:google/pedantic.git#v1.9.1",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:google/pedantic.git",
+                resolved_ref: "d7fe6f0ca73a10542f3d7abed1818dd0a0693dd4",
+                path: "."
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("pedantic")
+            expect(dependency.version).to eq("1.9.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+
+      context "with path and ref" do
+        let(:pubspec_fixture_name) { "git_ssh_with_path_and_ref" }
+
+        its(:length) { is_expected.to eq(34) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:rrousselGit/river_pod.git/packages/riverpod#v0.12.4",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:rrousselGit/river_pod.git",
+                ref: "v0.12.4",
+                resolved_ref: "97e31d3481b68e4293408b4eef99dc7916dc8147",
+                path: "packages/riverpod"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("riverpod")
+            expect(dependency.version).to eq("0.12.4")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:git@github.com:rrousselGit/freezed.git/packages/freezed#v0.12.7",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "git@github.com:rrousselGit/freezed.git",
+                resolved_ref: "2fe149026c3edf4735641255923157fded532b0b",
+                path: "packages/freezed"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("freezed")
+            expect(dependency.version).to eq("0.12.7")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+    end
+
+    context "with git sources via https" do
+      let(:pubspec_fixture_name) { "git_https" }
+
+      its(:length) { is_expected.to eq(2) }
+
+      describe "the first dependency (direct main)" do
+        subject(:dependency) { dependencies.first }
+        let(:expected_requirements) do
+          [{
+            requirement: "git:https://github.com/dart-lang/path.git",
+            groups: ["dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "git",
+              url: "https://github.com/dart-lang/path.git",
+              resolved_ref: "49a015d612541e549cfbe657ef48145ca32a98f8",
+              path: "."
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("path")
+          expect(dependency.version).to eq("1.8.1-dev")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+
+      describe "the second dependency (direct dev)" do
+        subject(:dependency) { dependencies[1] }
+        let(:expected_requirements) do
+          [{
+            requirement: "git:https://github.com/google/pedantic.git",
+            groups: ["dev_dependencies"],
+            file: "pubspec.yaml",
+            source: {
+              type: "git",
+              url: "https://github.com/google/pedantic.git",
+              resolved_ref: "66f2f6c27581c7936482e83be80b27be2719901c",
+              path: "."
+            }
+          }]
+        end
+
+        it "has the right details" do
+          expect(dependency).to be_a(Dependabot::Dependency)
+          expect(dependency.name).to eq("pedantic")
+          expect(dependency.version).to eq("1.11.0")
+          expect(dependency.requirements).to eq(expected_requirements)
+        end
+      end
+
+      context "with path" do
+        let(:pubspec_fixture_name) { "git_https_with_path" }
+
+        its(:length) { is_expected.to eq(34) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/rrousselGit/river_pod.git/packages/riverpod",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/rrousselGit/river_pod.git",
+                resolved_ref: "843adaa56bc34d617b07b14bdf4570afb907ee77",
+                path: "packages/riverpod"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("riverpod")
+            expect(dependency.version).to eq("0.13.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/rrousselGit/freezed.git/packages/freezed",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/rrousselGit/freezed.git",
+                resolved_ref: "cd29a64c3369bff6ccbe794c323b995adf15ac6a",
+                path: "packages/freezed"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("freezed")
+            expect(dependency.version).to eq("0.14.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+
+      context "with ref" do
+        let(:pubspec_fixture_name) { "git_https_with_ref" }
+
+        its(:length) { is_expected.to eq(3) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/dart-lang/path.git#1.7.0",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/dart-lang/path.git",
+                ref: "1.7.0",
+                resolved_ref: "10c778c799b2fc06036cbd0aa0e399ad4eb1ff5b",
+                path: "."
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("path")
+            expect(dependency.version).to eq("1.7.0")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/google/pedantic.git#v1.9.1",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/google/pedantic.git",
+                resolved_ref: "d7fe6f0ca73a10542f3d7abed1818dd0a0693dd4",
+                path: "."
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("pedantic")
+            expect(dependency.version).to eq("1.9.1")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+
+      context "with path and ref" do
+        let(:pubspec_fixture_name) { "git_https_with_path_and_ref" }
+
+        its(:length) { is_expected.to eq(34) }
+
+        describe "the first dependency (direct main)" do
+          subject(:dependency) { dependencies.first }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/rrousselGit/river_pod.git/packages/riverpod#v0.12.4",
+              groups: ["dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/rrousselGit/river_pod.git",
+                ref: "v0.12.4",
+                resolved_ref: "97e31d3481b68e4293408b4eef99dc7916dc8147",
+                path: "packages/riverpod"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("riverpod")
+            expect(dependency.version).to eq("0.12.4")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+
+        describe "the second dependency (direct dev)" do
+          subject(:dependency) { dependencies[1] }
+          let(:expected_requirements) do
+            [{
+              requirement: "git:https://github.com/rrousselGit/freezed.git/packages/freezed#v0.12.7",
+              groups: ["dev_dependencies"],
+              file: "pubspec.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/rrousselGit/freezed.git",
+                resolved_ref: "2fe149026c3edf4735641255923157fded532b0b",
+                path: "packages/freezed"
+              }
+            }]
+          end
+
+          it "has the right details" do
+            expect(dependency).to be_a(Dependabot::Dependency)
+            expect(dependency.name).to eq("freezed")
+            expect(dependency.version).to eq("0.12.7")
+            expect(dependency.requirements).to eq(expected_requirements)
+          end
+        end
+      end
+    end
+  end
+end
