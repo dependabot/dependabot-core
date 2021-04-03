8caa8b1902a5e6e003687ad79b1cae0590dbee01
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2021-04-03 16:16:29 +0200

    Register pub

---
+++ pub/lib/dependabot/pub.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# These all need to be required so the various classes can be registered in a
+# lookup table of package manager names to concrete classes.
+require "dependabot/pub/file_fetcher"
+require "dependabot/pub/file_parser"
+require "dependabot/pub/update_checker"
+require "dependabot/pub/file_updater"
+require "dependabot/pub/metadata_finder"
+require "dependabot/pub/requirement"
+require "dependabot/pub/version"
+
+require "dependabot/pull_request_creator/labeler"
+Dependabot::PullRequestCreator::Labeler.
+  register_label_details("pub", name: "dart", colour: "02569B")
+
+require "dependabot/dependency"
+Dependabot::Dependency.
+  register_production_check("pub", ->(_) { true })
