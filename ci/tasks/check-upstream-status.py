7c32b28b180458df80975c5e30028adf53374e77
aliculPix4D <aleksandar.licul_ext@pix4d.com>
2021-04-16 17:23:54 +0200

    PCI-1839 Run black on our ci/tasks Python files

--- ci/tasks/check-upstream-status.py
+++ ci/tasks/check-upstream-status.py
@@ -32,35 +32,44 @@ while num_check < max_checks:
     status_filter = []
     data = requests.get(
         f"https://api.github.com/repos/dependabot/dependabot-core/commits/{args.commit}/check-runs",
-        headers={"authorization":f"Bearer {args.token}", "Accept": "application/vnd.github.antiope-preview+json"},
+        headers={
+            "authorization": f"Bearer {args.token}",
+            "Accept": "application/vnd.github.antiope-preview+json",
+        },
     )
     if data.status_code != requests.codes.ok:
         logger.error("HTTP request failed ")
-        logger.info(f"Waiting before checking upstream status again. This was attempt number: {num_check}")
+        logger.info(
+            f"Waiting before checking upstream status again. This was attempt number: {num_check}"
+        )
         time.sleep(WAIT_TIME)
         continue
 
     response = data.json()
-    job_status = [ (check_run["status"],check_run["conclusion"]) for check_run in response["check_runs"] ]
-    all_ok = all(item == ('completed', 'success') for item in job_status)
-    
+    job_status = [
+        (check_run["status"], check_run["conclusion"]) for check_run in response["check_runs"]
+    ]
+    all_ok = all(item == ("completed", "success") for item in job_status)
+
     if all_ok:
         msg = f"Check the details here: https://github.com/dependabot/dependabot-core/commit/{args.commit}"
         logger.info(msg)
         logger.info("All " + str(response["total_count"]) + " checks finished successfully")
         sys.exit(0)
 
-    logger.info(f"Waiting before checking upstream status again. This was attempt number: {num_check}")
+    logger.info(
+        f"Waiting before checking upstream status again. This was attempt number: {num_check}"
+    )
     time.sleep(WAIT_TIME)
 
 for check_run in response["check_runs"]:
     if check_run["status"] != "completed" or check_run["conclusion"] != "success":
-        logger.error(f"Job name: {check_run['name']} status: {check_run['status']}, conclusion: {check_run['conclusion']}")
+        logger.error(
+            f"Job name: {check_run['name']} status: {check_run['status']}, conclusion: {check_run['conclusion']}"
+        )
 
-msg = f"Check the details here: https://github.com/dependabot/dependabot-core/commit/{args.commit}" 
+msg = f"Check the details here: https://github.com/dependabot/dependabot-core/commit/{args.commit}"
 logger.error("Some checks failed in the upstream repository")
 logger.error(msg)
 
 sys.exit(1)
-
-
