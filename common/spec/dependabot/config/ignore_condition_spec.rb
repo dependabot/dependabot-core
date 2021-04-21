4d6a275e0ece2e64e00d429dc8c79cbacee7fde8
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-04-21 12:11:46 -0400

    TDD dynamic ignore conditions

--- common/spec/dependabot/config/ignore_condition_spec.rb
+++ common/spec/dependabot/config/ignore_condition_spec.rb
@@ -23,10 +23,57 @@ RSpec.describe Dependabot::Config::IgnoreCondition do
       )
     end
 
+    def expect_allowed(*versions)
+      req = Gem::Requirement.new(ignored_versions.flat_map { |s| s.split(",").map(&:strip) })
+      versions.map do |v|
+        expect(req.satisfied_by?(Gem::Version.new(v))).
+          to eq(false), "Expected #{v} to be allowed, but was ignored"
+      end
+    end
+
+    def expect_ignored(*versions)
+      req = Gem::Requirement.new(ignored_versions.flat_map { |s| s.split(",").map(&:strip) })
+      versions.map do |v|
+        expect(req.satisfied_by?(Gem::Version.new(v))).
+          to eq(true), "Expected #{v} to be ignored, but was allowed"
+      end
+    end
+
     context "with static ignored versions" do
       let(:versions) { [">= 2.0.0"] }
       it "returns the versions" do
         expect(ignored_versions).to eq([">= 2.0.0"])
+      end
+
+      it "ignores expected versions" do
+        expect_allowed("1.0.0", "1.1.0", "1.1.1")
+        expect_ignored("2.0", "2.0.0")
+      end
+    end
+
+    context "with patch versions ignored" do
+      let(:update_types) { [:ignore_patch_versions] }
+
+      it "ignores expected versions" do
+        expect_allowed("1.3.0", "2.0.0")
+        expect_ignored("1.2.3", "1.2.4", "1.2.5")
+      end
+    end
+
+    context "with minor versions ignored" do
+      let(:update_types) { [:ignore_minor_versions] }
+
+      it "ignores expected versions" do
+        expect_allowed("2.0.0")
+        expect_ignored("1.2.3", "1.2.4", "1.3.0")
+      end
+    end
+
+    context "with major versions ignored" do
+      let(:update_types) { [:ignore_major_versions] }
+
+      it "ignores expected versions" do
+        expect_ignored("1.2.3", "1.2.4", "1.3.0", "2.0.0")
       end
     end
   end
