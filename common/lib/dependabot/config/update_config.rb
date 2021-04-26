8c8859b93cda9974d9afed84c75ef093602eaeb1
Philip Harrison <philip@mailharrison.com>
2021-04-26 15:04:27 +0100

    Add update config ignored update

--- common/lib/dependabot/config/update_config.rb
+++ common/lib/dependabot/config/update_config.rb
@@ -24,6 +24,20 @@ module Dependabot
           uniq
       end
 
+      def ignored_update?(updated_dependency, security_updates_only: false)
+        # NOTE: Build a dependency and ignore condition with the previous_version
+        # that will ignore the updated dependency version
+        version_class = version_class_for(updated_dependency)
+        updated_version = updated_dependency.version
+        if version_class.correct?(updated_dependency.version)
+          updated_version = version_class.new(updated_dependency.version)
+        end
+        previous_dependency = build_previous_dependency(updated_dependency)
+        ignored_versions_for(previous_dependency, security_updates_only: security_updates_only).
+          flat_map { |version| requirement_class_for(previous_dependency).requirements_array(version) }.
+          any? { |req| req.satisfied_by?(updated_version) }
+      end
+
       def self.wildcard_match?(wildcard_string, candidate_string)
         return false unless wildcard_string && candidate_string
 
@@ -35,6 +49,23 @@ module Dependabot
       end
 
       private
+
+      def requirement_class_for(dependency)
+        Utils.version_class_for_package_manager(dependency.package_manager)
+      end
+
+      def version_class_for(dependency)
+        Utils.version_class_for_package_manager(dependency.package_manager)
+      end
+
+      def build_previous_dependency(previous_dependency)
+        Dependabot::Dependency.new(
+          name: previous_dependency.name,
+          version: previous_dependency.previous_version,
+          requirements: previous_dependency.previous_requirements || [],
+          package_manager: previous_dependency.package_manager
+        )
+      end
 
       def name_normaliser_for(dep)
         name_normaliser ||= {}
