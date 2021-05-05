b696a412a9c552bb398d9f703fedcf387f624b46
mo khan <mo@mokhan.ca>
2021-05-05 16:22:58 -0600

    test: add example of failing upgrade

--- terraform/spec/dependabot/terraform/file_updater_spec.rb
+++ terraform/spec/dependabot/terraform/file_updater_spec.rb
@@ -22,6 +22,52 @@ RSpec.describe Dependabot::Terraform::FileUpdater do
   describe "#updated_dependency_files" do
     subject { updater.updated_dependency_files }
 
+    context "when updating to the latest version of the aws provider" do
+      let(:files) { project_dependency_files("aws") }
+      let(:dependencies) do
+        [
+          Dependabot::Dependency.new(
+            name: "aws",
+            version: "3.38.0",
+            previous_version: "3.27.0",
+            requirements: [],
+            previous_requirements: [],
+            package_manager: "terraform"
+          )
+        ]
+      end
+
+      specify { expect(subject.count).to eql(1) }
+
+      it "updates the `.terraform.lock.hcl` file" do
+        lock_file = subject.find { |file| file.name == ".terraform.lock.hcl" }
+
+        expect(lock_file.content).to eql(<<~LOCKFILE)
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "3.38.0"
+  constraints = "~> 3.27"
+  hashes = [
+    "h1:qKEjN/EM56XT46vGY33eoq7nD6JuGqRqFp7tkzTrRM0=",
+    "zh:20476d4c1b0c0efc55226bcbd85fbd948638fd9860a0edcdb7875cbb2b449e46",
+    "zh:7102622e6549cc3fc46b9ad68cbf4c50b162ce1013d4da817d05d1edf1f12fae",
+    "zh:74ff7f1610065e14c043cd9d74b3d5e0de4474f09a1a81e0b126b920b5cf6a27",
+    "zh:800e1b168149d507d23845f7a8b7e598c7dc16d2ee0f47848cf85d3e7458884f",
+    "zh:81ac3c68d6230b77740ca367e0c05a32ebb9be0fe5478c836573218a84eb3e46",
+    "zh:86536598796ba65539816f08351ac0ab32988ab84fa8f100049579996fafc800",
+    "zh:b9985c64f0f0b5bafb7067a60381fd807f7c3dd952c5d9f531385e464867bdd5",
+    "zh:c19c692896469724c6320fa7d87532ec3935e14e0e0fa0a8a0f1cf28ae7a0b0a",
+    "zh:cb8b14f246953a275ada562f5275a0d1a4938b7d20597e62fabe264012410f53",
+    "zh:cdbfa0ad87ff4d7451cfb89e53692a651d4c9cadece6845e60d986fd454b52b3",
+    "zh:ed5c4c8ae5adda37942bb15ef058c0811a95cb4c87259ae822627756dcb90efc",
+  ]
+}
+        LOCKFILE
+      end
+    end
+
     context "with a valid dependency file" do
       let(:files) { project_dependency_files("git_tags") }
       let(:dependencies) do
---
+++ terraform/spec/fixtures/projects/aws/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
