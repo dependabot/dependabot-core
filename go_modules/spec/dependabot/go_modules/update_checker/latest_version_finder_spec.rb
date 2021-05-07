73e91185fde8a9d67098ef16f9b971c7eb847129
Jason Rudolph <jasonrudolph@github.com>
2021-05-07 12:28:14 -0400

    🎨 Move `let` blocks into the `describe` block that uses them

--- go_modules/spec/dependabot/go_modules/update_checker/latest_version_finder_spec.rb
+++ go_modules/spec/dependabot/go_modules/update_checker/latest_version_finder_spec.rb
@@ -6,41 +6,41 @@ require "dependabot/go_modules/native_helpers"
 require "dependabot/go_modules/update_checker/latest_version_finder"
 
 RSpec.describe Dependabot::GoModules::UpdateChecker::LatestVersionFinder do
-  let(:dependency_name) { "github.com/dependabot-fixtures/go-modules-lib" }
-
-  let(:dependency_version) { "1.0.0" }
-
-  let(:dependency) do
-    Dependabot::Dependency.new(
-      name: dependency_name,
-      version: dependency_version,
-      package_manager: "go_modules",
-      requirements: [{
-        file: "go.mod",
-        requirement: dependency_version,
-        groups: [],
-        source: { type: "default", source: dependency_name }
-      }]
-    )
-  end
-
-  let(:go_mod_content) do
-    <<~GOMOD
-      module foobar
-      require #{dependency_name} v#{dependency_version}
-    GOMOD
-  end
-
-  let(:dependency_files) do
-    [
-      Dependabot::DependencyFile.new(
-        name: "go.mod",
-        content: go_mod_content
-      )
-    ]
-  end
-
   describe "#latest_version" do
+    let(:dependency_name) { "github.com/dependabot-fixtures/go-modules-lib" }
+
+    let(:dependency_version) { "1.0.0" }
+
+    let(:dependency) do
+      Dependabot::Dependency.new(
+        name: dependency_name,
+        version: dependency_version,
+        package_manager: "go_modules",
+        requirements: [{
+          file: "go.mod",
+          requirement: dependency_version,
+          groups: [],
+          source: { type: "default", source: dependency_name }
+        }]
+      )
+    end
+
+    let(:go_mod_content) do
+      <<~GOMOD
+        module foobar
+        require #{dependency_name} v#{dependency_version}
+      GOMOD
+    end
+
+    let(:dependency_files) do
+      [
+        Dependabot::DependencyFile.new(
+          name: "go.mod",
+          content: go_mod_content
+        )
+      ]
+    end
+
     context "when the latest version is an '+incompatible' version" do # https://golang.org/ref/mod#incompatible-versions
       let(:dependency_name) { "github.com/dependabot-fixtures/go-modules-incompatible" }
       let(:dependency_version) { "2.0.0+incompatible" }
