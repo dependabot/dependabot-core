c295ebe6b99cda831b0954454958fed6204af2ad
Jurre Stender <jurrestender@gmail.com>
2021-05-21 12:16:43 +0200

    Terraform: Improve updating provider requirements
    Previously we would match for any `version` string, and this would end
    up matching on `required_version`, and we'd fail to update these
    configs.

--- terraform/spec/fixtures/projects/registry_provider/main.tf
+++ terraform/spec/fixtures/projects/registry_provider/main.tf
@@ -1,8 +1,19 @@
 terraform {
+  required_version = ">= 0.12"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "0.1.0"
     }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "eu-west-1"
 }
