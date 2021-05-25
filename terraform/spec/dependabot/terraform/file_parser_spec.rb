1095425ba2b7f7b7d7a0e86f9cf73c308c393da8
mo khan <mo@mokhan.ca>
2021-05-25 13:23:17 -0600

    test: check if version constraint is fixed

--- terraform/spec/dependabot/terraform/file_parser_spec.rb
+++ terraform/spec/dependabot/terraform/file_parser_spec.rb
@@ -130,12 +130,32 @@ RSpec.describe Dependabot::Terraform::FileParser do
     context "with a private registry" do
       let(:files) { project_dependency_files("private_registry") }
 
-      it "parses the host correctly" do
+      it "parses the dependency correctly" do
         expect(subject.length).to eq(1)
         expect(subject[0].name).to eq("namespace/name")
         expect(subject[0].version).to eq("0.1.0")
         expect(subject[0].requirements).to eq([{
           requirement: "0.1.0",
+          groups: [],
+          file: "main.tf",
+          source: {
+            type: "provider",
+            registry_hostname: "registry.example.org",
+            module_identifier: "namespace/name"
+          }
+        }])
+      end
+    end
+
+    context "with a private registry using a pessimistic version constraint" do
+      let(:files) { project_dependency_files("private_registry_pessimistic_constraint") }
+
+      it "parses the dependency correctly" do
+        expect(subject.length).to eq(1)
+        expect(subject[0].name).to eq("namespace/name")
+        expect(subject[0].version).to be_nil
+        expect(subject[0].requirements).to eq([{
+          requirement: "~> 0.1",
           groups: [],
           file: "main.tf",
           source: {
