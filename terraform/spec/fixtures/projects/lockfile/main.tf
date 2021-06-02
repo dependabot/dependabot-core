0e7fcf72699f1def697975f3373a2e53161e865c
Nish Sinha <nishnha@github.com>
2021-06-02 17:12:00 -0400

    Create versions.tf, remove versioning from main.tf

--- terraform/spec/fixtures/projects/lockfile/main.tf
+++ terraform/spec/fixtures/projects/lockfile/main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
+provider "aws" {
+  region = "us-west-2"
 }
 
-provider "aws" {
-  profile = "default"
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+  acl    = "public-read"
+
   region = "us-west-2"
 }
