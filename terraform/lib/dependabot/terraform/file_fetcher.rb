b3c774247f3ff1ec38e1dfbb8fd6851b51f02150
Nish Sinha <nishnha@github.com>
2021-06-08 12:11:23 -0400

    Refactor FileFetcher#lock_file
    Co-authored-by: Philip Harrison <feelepxyz@github.com>

--- terraform/lib/dependabot/terraform/file_fetcher.rb
+++ terraform/lib/dependabot/terraform/file_fetcher.rb
@@ -23,7 +23,7 @@ module Dependabot
         fetched_files = []
         fetched_files += terraform_files
         fetched_files += terragrunt_files
-        fetched_files += lock_file
+        fetched_files += lock_file if lock_file
 
         return fetched_files if fetched_files.any?
 
@@ -48,10 +48,7 @@ module Dependabot
       end
 
       def lock_file
-        @lock_file ||=
-          repo_contents(raise_errors: false).
-          select { |f| f.type == "file" && lock_file?(f.name) }.
-          map { |f| fetch_file_from_host(f.name) }
+        @lock_file ||= fetch_file_if_present(".terraform.lock.hcl")
       end
     end
   end
