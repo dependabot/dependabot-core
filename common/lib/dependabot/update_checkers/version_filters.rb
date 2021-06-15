851ae941a9d1358ca1be01f8a44d88a294b29ca5
Nish Sinha <nishnha@github.com>
2021-06-15 10:21:40 -0400

    Check if the version is a Gem::Version, not the advisory

--- common/lib/dependabot/update_checkers/version_filters.rb
+++ common/lib/dependabot/update_checkers/version_filters.rb
@@ -4,7 +4,7 @@ module Dependabot
       def filter_vulnerable_versions(versions_array)
         versions_array.reject do |v|
           security_advisories.any? do |a|
-            if a.is_a?(Gem::Version)
+            if v.is_a?(Gem::Version)
               a.vulnerable?(v)
             else
               a.vulnerable?(v.fetch(:version))
