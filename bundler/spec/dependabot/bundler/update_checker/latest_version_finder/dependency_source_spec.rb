8940d70c5b8b880675100684d22d0633d86c0ba8
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-06-21 12:54:10 -0400

    expected credentials

--- bundler/spec/dependabot/bundler/update_checker/latest_version_finder/dependency_source_spec.rb
+++ bundler/spec/dependabot/bundler/update_checker/latest_version_finder/dependency_source_spec.rb
@@ -6,7 +6,15 @@ require "dependabot/bundler/update_checker"
 RSpec.describe Dependabot::Bundler::UpdateChecker::LatestVersionFinder::DependencySource do
   let(:project_name) { "git_source" }
   let(:files) { project_dependency_files(File.join("bundler2", project_name)) }
-  let(:source) { described_class.new(dependency: nil, dependency_files: files, credentials: [], options: {}) }
+  let(:credentials) do
+    [{
+      "type" => "git_source",
+      "host" => "github.com",
+      "username" => "x-access-token",
+      "password" => "token"
+    }]
+  end
+  let(:source) { described_class.new(dependency: nil, dependency_files: files, credentials: credentials, options: {}) }
 
   describe "#inaccessible_git_dependencies", :vcr do
     subject(:inaccessible_git_dependencies) { source.inaccessible_git_dependencies }
