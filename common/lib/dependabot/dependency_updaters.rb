2264b0b32094faf90354e7c573fe60b976724f84
Mattt <mattt@me.com>
2021-12-17 03:42:08 -0800

    Register package manager as always clone when registering dependency updater

--- common/lib/dependabot/dependency_updaters.rb
+++ common/lib/dependabot/dependency_updaters.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "dependabot/utils"
+
 module Dependabot
   module DependencyUpdaters
     @dependency_updaters = {}
@@ -13,6 +15,8 @@ module Dependabot
 
     def self.register(package_manager, dependency_updater)
       @dependency_updaters[package_manager] = dependency_updater
+
+      Dependabot::Utils.register_always_clone(package_manager)
     end
   end
 end
