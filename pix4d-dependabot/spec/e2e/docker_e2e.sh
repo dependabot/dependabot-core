3f9efd13f68cde853e54476e60c6a9db9b1e4fbc
aliculPix4D <aleksandar.licul_ext@pix4d.com>
2022-01-17 11:11:30 +0100

    [e2e-docker-script] Use correct configuration for REPOSITORY_DATA env variable

--- pix4d-dependabot/spec/e2e/docker_e2e.sh
+++ pix4d-dependabot/spec/e2e/docker_e2e.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Before running the script, setup your secret in pass. They can be found in the Vault
-# Github/pix4d-dependabot    --> ./tools/vault_helper.py read-secret /concourse/developers/janus_developers_rw_token
-# Artifactory/concourse_pass --> ./tools/vault_helper.py read-secret /concourse/shared/concourse_artifactory_password
+# github/pix4d-janus-main    --> ./tools/vault_helper.py read-secret /concourse/developers/janus_developers_rw_token
+# artifactory/concourse_pass --> ./tools/vault_helper.py read-secret /concourse/shared/concourse_artifactory_password
 # docker/pix4d-pass --> ./tools/vault_helper.py read-secret /concourse/shared/docker_cloud_pix4d_password
 
 TAG=$1
 
 docker run --name PM_DOCKER -d \
-    -e GITHUB_ACCESS_TOKEN=$(pass Github/pix4d-dependabot) \
+    -e GITHUB_ACCESS_TOKEN=$(gopass show github/pix4d-janus-main) \
     -e DOCKER_REGISTRY="docker.ci.pix4d.com" \
     -e DOCKER_USER="pix4d" \
-    -e DOCKER_PASS=$(pass docker/pix4d-pass) \
-    -e REPOSITORY_DATA="[{'branch':'staging','dependency_dir':'ci/pipelines/','module':'concourse','repo':'Pix4D/github-automation-playground'},{'branch':'staging','dependency_dir':'dockerfiles/','module':'docker','repo':'Pix4D/github-automation-playground'}]" \
+    -e DOCKER_PASS=$(gopass show docker/pix4d-pass) \
+    -e REPOSITORY_DATA="[{'branch':'staging','dependency_dirs':['ci/pipelines/'],'module':'concourse','repo':'Pix4D/github-automation-playground'},{'branch':'staging','dependency_dirs':['dockerfiles/'],'module':'docker','repo':'Pix4D/github-automation-playground'}]" \
     -it docker.ci.pix4d.com/pix4d-dependabot:$TAG /bin/bash
 
 docker exec -it PM_DOCKER sh -c "cd pix4d-dependabot && bundle exec ruby pix4d-dependabot.rb"
@@ -20,11 +20,11 @@ docker stop PM_DOCKER
 docker rm PM_DOCKER
 
 docker run --name PM_PIP -d \
-    -e GITHUB_ACCESS_TOKEN=$(pass Github/pix4d-dependabot) \
+    -e GITHUB_ACCESS_TOKEN=$(gopass show github/pix4d-janus-main) \
     -e ARTIFACTORY_USERNAME="concourse" \
-    -e ARTIFACTORY_PASSWORD=$(pass Artifactory/concourse_pass) \
+    -e ARTIFACTORY_PASSWORD=$(gopass show artifactory/concourse_pass) \
     -e EXTRA_INDEX_URL="https://artifactory.ci.pix4d.com/artifactory/api/pypi/pix4d-pypi-local/simple/" \
-    -e REPOSITORY_DATA="[{'module':'pip', 'repo':'Pix4D/github-automation-playground','branch':'staging','dependency_dir':'/python/pip_example/'}]" \
+    -e REPOSITORY_DATA="[{'module':'pip', 'repo':'Pix4D/github-automation-playground','branch':'staging','dependency_dirs':['/python/pip_example/']}]" \
     -it docker.ci.pix4d.com/pix4d-dependabot:$TAG /bin/bash
 
 docker exec -it PM_PIP sh -c "cd pix4d-dependabot && bundle exec ruby pix4d-dependabot.rb"
