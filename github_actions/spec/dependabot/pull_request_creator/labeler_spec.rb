24c467a2726b8409c42c8ffe868b1cd9d5588269
mo khan <mo@mokhan.ca>
2022-01-24 17:38:32 -0700

    style: fix linter errors

--- github_actions/spec/dependabot/pull_request_creator/labeler_spec.rb
+++ github_actions/spec/dependabot/pull_request_creator/labeler_spec.rb
@@ -15,27 +15,37 @@ RSpec.describe Dependabot::PullRequestCreator::Labeler do
     )
   end
 
-  let(:dependency) { Dependabot::Dependency.new(name: "dependabot/updater-action", package_manager: "github_actions", requirements: []) }
+  let(:dependency) do
+    Dependabot::Dependency.new(name: "dependabot/updater-action", package_manager: "github_actions", requirements: [])
+  end
   let(:source) { Dependabot::Source.new(provider: "github", repo: "dependabot/dependabot-core") }
 
   describe "#create_default_labels_if_required" do
     context "when the 'github_actions' label doesn't yet exist" do
       before do
-        allow(described_class).to receive(:label_details_for_package_manager)
-          .with("github_actions")
-          .and_return({ colour: "000000", name: "github_actions", description: "Pull requests that update GitHub action code" })
+        allow(described_class).to receive(:label_details_for_package_manager).
+          with("github_actions").
+          and_return({
+            colour: "000000",
+            name: "github_actions",
+            description: "Pull requests that update GitHub action code"
+          })
 
-        stub_request(:get, "https://api.github.com/repos/#{source.repo}/labels?per_page=100")
-          .to_return(status: 200, headers: { "Content-Type" => "application/json" }, body: JSON.generate([]))
-        stub_request(:post, "https://api.github.com/repos/#{source.repo}/labels")
-          .to_return( status: 201, headers: { "Content-Type" => "application/json" }, body: JSON.generate({ id: 1, name: "github_actions", color: "000000" }))
+        stub_request(:get, "https://api.github.com/repos/#{source.repo}/labels?per_page=100").
+          to_return(status: 200, headers: { "Content-Type" => "application/json" }, body: JSON.generate([]))
+        stub_request(:post, "https://api.github.com/repos/#{source.repo}/labels").
+          to_return(
+            status: 201,
+            headers: { "Content-Type" => "application/json" },
+            body: JSON.generate({ id: 1, name: "github_actions", color: "000000" })
+          )
       end
 
       it "creates a label" do
         labeler.create_default_labels_if_required
 
-        expect(WebMock).to have_requested(:post, "https://api.github.com/repos/#{source.repo}/labels")
-          .with(body: {
+        expect(WebMock).to have_requested(:post, "https://api.github.com/repos/#{source.repo}/labels").
+          with(body: {
             name: "github_actions",
             color: "000000",
             description: "Pull requests that update GitHub action code"
