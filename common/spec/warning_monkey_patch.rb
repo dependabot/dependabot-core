d754631a332062704e0e493ca3046438ba4d7f00
Jeff Widman <jeff@jeffwidman.com>
2022-04-22 12:55:30 -0700

    Clarify comment and remove TODO
    The current version of the warning:
    ```
    warning: parser/current is loading parser/ruby27, which recognizes2.7.6-compliant syntax, but you are running 2.7.5.
    Please see https://github.com/whitequark/parser#compatibility-with-ruby-mri.
    ```
    
    So I removed the TODO comment because it's outdated.
    But I left the actual exclusion in because it will be a recurring issue from time to time.

--- common/spec/warning_monkey_patch.rb
+++ common/spec/warning_monkey_patch.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 ALLOW_PATTERNS = [
-  # Ignore parser warnings for ruby 2.7 minor version mismatches
-  # TODO: Fix these by upgrading to ruby 2.7.3 (requires ubuntu upgrade)
+  # Ignore parser warnings for ruby 2.7 minor version mismatches.
+  # This is a recurring issue that occurs whenever the parser gets
+  # ahead of our installed ruby version.
   %r{parser/current is loading parser/ruby27},
   /2.7.\d-compliant syntax, but you are running 2.7.\d/,
   %r{whitequark/parser}
