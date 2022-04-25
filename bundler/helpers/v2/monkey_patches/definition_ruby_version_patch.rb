b27c092dfdc3a6d55219462e70999781a44261ae
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-04-25 15:17:28 -0700

    Revert "Merge pull request #5018 from deivid-rodriguez/bundler-2-3-12"
    This reverts commit 93e1c50b6d45d96d06240d348024e6af5c699e1d, reversing
    changes made to 51a9cdd3da1a3b9f9cfdb1bca8d3f930df68059a.

--- bundler/helpers/v2/monkey_patches/definition_ruby_version_patch.rb
+++ bundler/helpers/v2/monkey_patches/definition_ruby_version_patch.rb
@@ -5,31 +5,15 @@ require "bundler/definition"
 module BundlerDefinitionRubyVersionPatch
   def source_requirements
     if ruby_version
-      requested_version = ruby_version.gem_version
+      requested_version = ruby_version.to_gem_version_with_patchlevel
       sources.metadata_source.specs <<
         Gem::Specification.new("Ruby\0", requested_version)
     end
 
     sources.metadata_source.specs <<
-      Gem::Specification.new("Ruby\0", "2.5.3")
+      Gem::Specification.new("Ruby\0", "2.5.3p105")
 
     super
-  end
-
-  def metadata_dependencies
-    @metadata_dependencies ||=
-      [
-        Bundler::Dependency.new("Ruby\0", ruby_version_requirements),
-        Bundler::Dependency.new("RubyGems\0", Gem::VERSION)
-      ]
-  end
-
-  def ruby_version_requirements
-    return [] unless ruby_version
-
-    ruby_version.versions.map do |version|
-      Gem::Requirement.new(version)
-    end
   end
 end
 
