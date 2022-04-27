22a6ae429945a8e01b60e69b16576071a3c22698
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-04-27 14:24:17 -0700

    Basic attempt to update the parent to unlock a transitive dependency update

--- npm_and_yarn/lib/dependabot/npm_and_yarn/update_checker/version_resolver.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/update_checker/version_resolver.rb
@@ -91,6 +91,7 @@ module Dependabot
         end
 
         def dependency_updates_from_full_unlock
+          # TODO: investigate how these special cases will affect transitive dependency updates
           return if git_dependency?(dependency)
           return updated_monorepo_dependencies if part_of_tightly_locked_monorepo?
           return if newly_broken_peer_reqs_from_dep.any?
