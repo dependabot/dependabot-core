a9811a2c11fad978153bf34fbc62dd897b2652db
Dennis Carey <dwc0011@users.noreply.github.com>
2022-05-23 20:48:17 -0400

    Make dependency cloudposse/terraform-null-label.git

--- terraform/spec/fixtures/projects/duplicate_child_modules/child_module_one/main.tf
+++ terraform/spec/fixtures/projects/duplicate_child_modules/child_module_one/main.tf
@@ -1,3 +1,9 @@
-module "dns" {
-  source = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git//some/dir?ref=tags/0.2.5"
+module "origin_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.7"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = ["${compact(concat(var.attributes, list("origin")))}"]
+  tags       = "${var.tags}"
 }
