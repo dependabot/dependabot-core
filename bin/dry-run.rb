49e41c70b67ca707b9b21eecfc7f901b1f37d817
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-05-26 16:54:07 -0700

    Remove LockedSubdependencyVersionResolver experiment

--- bin/dry-run.rb
+++ bin/dry-run.rb
@@ -681,7 +681,6 @@ dependencies.each do |dep|
 
     conflicting_dependencies.each do |conflicting_dep|
       puts "   #{conflicting_dep['explanation']}"
-      puts "   parent: #{conflicting_dep['parent_name']}:#{conflicting_dep['parent_version']}"
     end
   end
 
