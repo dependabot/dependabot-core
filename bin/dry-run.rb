2b2cc38665b037dd07e9b18d492d85a2789595bc
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-06-21 16:26:54 -0700

    Remove extra call to fetch updated dependencies

--- bin/dry-run.rb
+++ bin/dry-run.rb
@@ -593,12 +593,12 @@ end
 
 # If a version update for a peer dependency is possible we should
 # defer to the PR that will be created for it to avoid duplicate PRs.
-def peer_dependency_should_update_instead?(checker, reqs_to_unlock)
+def peer_dependency_should_update_instead?(updated_deps)
   # This doesn't apply to security updates as we can't rely on the
   # peer dependency getting updated.
   return false if $options[:security_updates_only]
 
-  checker.updated_dependencies(requirements_to_unlock: reqs_to_unlock).
+  updated_deps.
     reject { |dep| dep.name == checker.dependency.name }.
     any? do |dep|
       original_peer_dep = ::Dependabot::Dependency.new(
@@ -726,7 +726,7 @@ dependencies.each do |dep|
     requirements_to_unlock: requirements_to_unlock
   )
 
-  if peer_dependency_should_update_instead?(checker, requirements_to_unlock)
+  if peer_dependency_should_update_instead?(updated_deps)
     puts "    (no update possible, peer dependency can be updated)"
     next
   end
