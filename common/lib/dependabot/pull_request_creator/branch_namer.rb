6ba5296f550ade045187a1bd8425912faed8009b
Tom Naessens <tom.naessens@silverfin.com>
2022-06-30 21:23:35 +0200

    Add max length option to BranchNamer

--- common/lib/dependabot/pull_request_creator/branch_namer.rb
+++ common/lib/dependabot/pull_request_creator/branch_namer.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
+require "digest"
+
 require "dependabot/metadata_finders"
 require "dependabot/pull_request_creator"
 
 module Dependabot
   class PullRequestCreator
     class BranchNamer
-      attr_reader :dependencies, :files, :target_branch, :separator, :prefix
+      attr_reader :dependencies, :files, :target_branch, :separator, :prefix, :max_length
 
       def initialize(dependencies:, files:, target_branch:, separator: "/",
-                     prefix: "dependabot")
+                     prefix: "dependabot", max_length: nil)
         @dependencies  = dependencies
         @files         = files
         @target_branch = target_branch
         @separator     = separator
         @prefix        = prefix
+        @max_length    = max_length
       end
 
       def new_branch_name
@@ -37,7 +40,15 @@ module Dependabot
           end
 
         # Some users need branch names without slashes
-        sanitize_ref(File.join(prefixes, @name).gsub("/", separator))
+        sanitized_name = sanitize_ref(File.join(prefixes, @name).gsub("/", separator))
+
+        # Shorten the ref in case users refs have length limits
+        if @max_length && (sanitized_name.length > @max_length)
+          sha = Digest::SHA1.hexdigest(sanitized_name)[0, @max_length]
+          sanitized_name[[@max_length - sha.size, 0].max..] = sha
+        end
+
+        sanitized_name
       end
 
       private
@@ -181,7 +192,7 @@ module Dependabot
 
       def sanitize_ref(ref)
         # This isn't a complete implementation of git's ref validation, but it
-        # covers most cases that crop up. Its list of allowed charactersr is a
+        # covers most cases that crop up. Its list of allowed characters is a
         # bit stricter than git's, but that's for cosmetic reasons.
         ref.
           # Remove forbidden characters (those not already replaced elsewhere)
