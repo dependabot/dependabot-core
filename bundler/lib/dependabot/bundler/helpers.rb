4d60b969644504e53564fbe8cf6979832410ad82
Jurre Stender <jurre@github.com>
2022-07-14 18:36:29 +0200

    Bump bundler from 2.3.14 to 2.3.18
    [Changelog](https://github.com/rubygems/rubygems/blob/db374bc25bbe024d02787336e557d9e31efc9f6d/bundler/CHANGELOG.md#2318-july-14-2022)

--- bundler/lib/dependabot/bundler/helpers.rb
+++ bundler/lib/dependabot/bundler/helpers.rb
@@ -4,7 +4,7 @@ module Dependabot
   module Bundler
     module Helpers
       V1 = "1.17.3"
-      V2 = "2.3.13"
+      V2 = "2.3.18"
       # If we are updating a project with no Gemfile.lock, we default to the
       # newest version we support
       DEFAULT = V2
