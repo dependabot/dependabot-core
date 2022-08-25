1a8f15450072e34533a8945d9c32a28dbd5d626d
Nish Sinha <nishnha@github.com>
2022-08-25 15:41:54 -0400

    conflicting_dependencies also returns vulnerablilty_audit

--- npm_and_yarn/lib/dependabot/npm_and_yarn/update_checker.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/update_checker.rb
@@ -96,13 +96,20 @@ module Dependabot
       end
 
       def conflicting_dependencies
-        ConflictingDependencyResolver.new(
+        conflicts = ConflictingDependencyResolver.new(
           dependency_files: dependency_files,
           credentials: credentials
         ).conflicting_dependencies(
           dependency: dependency,
           target_version: lowest_security_fix_version
         )
+
+        vulnerable = vulnerability_audit.select do |v|
+          v["fix_available"] == false &&
+            v["explanation"]
+        end
+
+        conflicts + vulnerable
       end
 
       private
