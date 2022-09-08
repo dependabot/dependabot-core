fbb15bd0d5a32946351602b2d13154b4bcebf763
Jurre Stender <jurre@github.com>
2022-09-08 16:15:06 +0200

    We do not need the yarn berry parser
    The v1 parser can handle the files just fine

--- npm_and_yarn/helpers/lib/yarn/lockfile-parser.js
+++ npm_and_yarn/helpers/lib/yarn/lockfile-parser.js
@@ -10,22 +10,12 @@ const fs = require("fs");
 const path = require("path");
 const parseLockfile = require("@dependabot/yarn-lib/lib/lockfile/parse")
   .default;
-// const { parseSyml } = require("@yarnpkg/parsers")
 
 async function parse(directory) {
   const readFile = (fileName) =>
     fs.readFileSync(path.join(directory, fileName)).toString();
   const data = readFile("yarn.lock");
-  let lockfile;
-  lockfile = parseLockfile(data).object;
-
-  // // If the lockfile contains a "__metadata" key, it's safe to assume it's a
-  // // yarn Berry lockfile, and we should use its parser.
-  // if (lockfile && lockfile.__metadata) {
-  //   lockfile = parseSyml(data);
-  // }
-
-  return lockfile
+  return parseLockfile(data).object;
 }
 
 module.exports = { parse };
