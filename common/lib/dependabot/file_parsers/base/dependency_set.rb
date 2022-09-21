f531131fe1e0e2f6145a40393401ee8951b3143e
Bryan Dragon <25506+bdragon@users.noreply.github.com>
2022-09-21 16:33:16 -0600

    Refactor to preserve all added versions of each dependency in the set

--- common/lib/dependabot/file_parsers/base/dependency_set.rb
+++ common/lib/dependabot/file_parsers/base/dependency_set.rb
@@ -14,26 +14,19 @@ module Dependabot
             raise ArgumentError, "must be an array of Dependency objects"
           end
 
-          @dependencies = dependencies
           @case_sensitive = case_sensitive
+          @dependencies = Hash.new { |hsh, key| hsh[key] = DependencySlot.new }
+          dependencies.each { |dep| self << dep }
         end
 
-        attr_reader :dependencies
+        def dependencies
+          @dependencies.values.filter_map(&:combined)
+        end
 
         def <<(dep)
           raise ArgumentError, "must be a Dependency object" unless dep.is_a?(Dependency)
 
-          existing_dependency = dependency_for_name(dep.name)
-
-          return self if existing_dependency&.to_h == dep.to_h
-
-          if existing_dependency
-            dependencies[dependencies.index(existing_dependency)] =
-              combined_dependency(existing_dependency, dep)
-          else
-            dependencies << dep
-          end
-
+          @dependencies[key_for(dep)] << dep
           self
         end
 
@@ -44,47 +37,115 @@ module Dependabot
           self
         end
 
+        def all_versions_for_name(name)
+          key = key_for_name(name)
+          @dependencies.key?(key) ? @dependencies[key].all_versions : []
+        end
+
+        def dependency_for_name(name)
+          key = key_for_name(name)
+          @dependencies.key?(key) ? @dependencies[key].combined : nil
+        end
+
         private
 
         def case_sensitive?
           @case_sensitive
         end
 
-        def dependency_for_name(name)
-          return dependencies.find { |d| d.name == name } if case_sensitive?
-
-          dependencies.find { |d| d.name&.downcase == name&.downcase }
+        def key_for_name(name)
+          case_sensitive? ? name : name.downcase
         end
 
-        def combined_dependency(old_dep, new_dep)
-          package_manager = old_dep.package_manager
-          v_cls = Utils.version_class_for_package_manager(package_manager)
+        def key_for(dep)
+          key_for_name(dep.name)
+        end
 
-          # If we already have a requirement use the existing version
-          # (if present). Otherwise, use whatever the lowest version is
-          new_version =
-            if old_dep.requirements.any? then old_dep.version || new_dep.version
-            elsif !v_cls.correct?(new_dep.version) then old_dep.version
-            elsif !v_cls.correct?(old_dep.version) then new_dep.version
-            elsif v_cls.new(new_dep.version) > v_cls.new(old_dep.version)
-              old_dep.version
+        class DependencySlot
+          attr_reader :all_versions, :combined
+
+          def initialize
+            @all_versions = []
+            @combined = nil
+          end
+
+          def <<(dep)
+            return self if @all_versions.include?(dep)
+
+            @combined = if @combined
+                          combine(@combined, dep)
+                        else
+                          Dependency.new(
+                            name: dep.name,
+                            version: dep.version,
+                            requirements: dep.requirements,
+                            package_manager: dep.package_manager,
+                            subdependency_metadata: dep.subdependency_metadata
+                          )
+                        end
+
+            index_of_same_version =
+              @all_versions.find_index { |other| other.version == dep.version }
+
+            if index_of_same_version.nil?
+              @all_versions << dep
             else
-              new_dep.version
+              same_version = @all_versions[index_of_same_version]
+              @all_versions[index_of_same_version] = combine(same_version, dep)
             end
 
-          subdependency_metadata = (
-            (old_dep.subdependency_metadata || []) +
-            (new_dep.subdependency_metadata || [])
-          ).uniq
+            sort!
 
-          Dependency.new(
-            name: old_dep.name,
-            version: new_version,
-            requirements: (old_dep.requirements + new_dep.requirements).uniq,
-            package_manager: package_manager,
-            subdependency_metadata: subdependency_metadata
-          )
+            self
+          end
+
+          private
+
+          def combine(old_dep, new_dep)
+            version = if old_dep.requirements.any?
+                        old_dep.version || new_dep.version
+                      elsif !version_class.correct?(new_dep.version)
+                        old_dep.version
+                      elsif !version_class.correct?(old_dep.version)
+                        new_dep.version
+                      elsif version_class.new(new_dep.version) > version_class.new(old_dep.version)
+                        old_dep.version
+                      else
+                        new_dep.version
+                      end
+            requirements = (old_dep.requirements + new_dep.requirements).uniq
+            subdependency_metadata = (
+              (old_dep.subdependency_metadata || []) +
+              (new_dep.subdependency_metadata || [])
+            ).uniq
+
+            Dependency.new(
+              name: old_dep.name,
+              version: version,
+              requirements: requirements,
+              package_manager: old_dep.package_manager,
+              subdependency_metadata: subdependency_metadata
+            )
+          end
+
+          def sort!
+            @all_versions.sort! do |a, b|
+              a_ok = version_class.correct?(a.version)
+              b_ok = version_class.correct?(b.version)
+
+              next version_class.new(a.version) <=> version_class.new(b.version) if a_ok && b_ok
+              next 1 if b_ok
+              next -1 if a_ok
+
+              0
+            end
+          end
+
+          def version_class
+            @version_class ||= Utils.version_class_for_package_manager(@combined.package_manager)
+          end
         end
+        private_constant :DependencySlot
       end
     end
   end
