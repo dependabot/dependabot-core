7b6e9fbcd247943d191049d219962850c923281e
Kurt von Laven <kurt.von.laven@gmail.com>
2022-09-21 07:58:28 -0700

    Add support for Python 3.10.6
    Bump pyenv from v2.3.2 to v2.3.4. Python 3.10.7, 3.9.14, 3.8.14, and
    3.7.14 have all been released, but are not yet supported by a released
    version of pyenv.

--- python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
+++ python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
@@ -218,7 +218,7 @@ RSpec.describe Dependabot::Python::UpdateChecker::PipenvVersionResolver do
                 to start_with("Dependabot detected the following Python")
               expect(error.message).to include("3.4.*")
               expect(error.message).
-                to include("supported in Dependabot: 3.10.5, 3.10.4, 3.10.3")
+                to include("supported in Dependabot: 3.10.6, 3.10.5, 3.10.4")
             end
         end
       end
