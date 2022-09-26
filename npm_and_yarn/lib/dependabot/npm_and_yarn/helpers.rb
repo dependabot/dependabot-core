936a32f94bbfc550be45e4ff22482a7eef01b96a
Jurre Stender <jurre@github.com>
2022-09-26 15:45:58 +0200

    Set yarn CA file path config based on yarn version

--- npm_and_yarn/lib/dependabot/npm_and_yarn/helpers.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/helpers.rb
@@ -29,6 +29,15 @@ module Dependabot
         if (https_proxy = ENV.fetch("HTTPS_PROXY", false))
           SharedHelpers.run_shell_command("yarn config set httpsProxy #{https_proxy}")
         end
+        if (ca_file_path = ENV.fetch("NODE_EXTRA_CA_CERTS", false))
+          output = SharedHelpers.run_shell_command("yarn --version")
+          major_version = Version.new(output).major
+          if major_version >= 4
+            SharedHelpers.run_shell_command("yarn config set httpsCaFilePath #{ca_file_path}")
+          else
+            SharedHelpers.run_shell_command("yarn config set caFilePath #{ca_file_path}")
+          end
+        end
         commands.each { |cmd| SharedHelpers.run_shell_command(cmd) }
       end
     end
