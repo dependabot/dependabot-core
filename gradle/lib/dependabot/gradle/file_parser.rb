5c72577b2f20fc4ff220784c6b269d1391295408
jakecoffman <jake@jakecoffman.com>
2022-10-18 16:29:37 -0500

    oops

--- gradle/lib/dependabot/gradle/file_parser.rb
+++ gradle/lib/dependabot/gradle/file_parser.rb
@@ -277,7 +277,7 @@ module Dependabot
 
       def property_value_finder
         @property_value_finder ||=
-          PropertyValueFinder.new(dependency_files: dependency_files, credentials: credentials)
+          PropertyValueFinder.new(dependency_files: dependency_files)
       end
 
       def prepared_content(buildfile)
