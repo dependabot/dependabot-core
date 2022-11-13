4dc97e41a4ec75808876249b0786d648627df0c6
David Rodríguez <deivid.rodriguez@riseup.net>
2022-11-13 22:43:53 +0100

    Make docker version specs more clear
    Currently only the numeric part of tags is instantiated as a version.
    Make that more clear by feeding version specs "real life" versions.

--- docker/spec/dependabot/docker/version_spec.rb
+++ docker/spec/dependabot/docker/version_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Dependabot::Docker::Version do
     end
 
     it "sorts properly when it uses underscores" do
-      expect(described_class.new("11.0.16_8-jdk")).to be < described_class.new("11.0.16.1-jdk")
-      expect(described_class.new("17.0.2_8-jdk")).to be > described_class.new("17.0.1_12-jdk")
+      expect(described_class.new("11.0.16_8")).to be < described_class.new("11.0.16.1")
+      expect(described_class.new("17.0.2_8")).to be > described_class.new("17.0.1_12")
     end
   end
 end
