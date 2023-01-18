67583efda736a3391632d268c681d54819d49efa
Stuart Chinery <163900+schinery@users.noreply.github.com>
2023-01-18 23:50:27 +0000

    Bumped RubyRequirementSetter RUBY_VERSIONS to latest (#6460)

--- bundler/lib/dependabot/bundler/file_updater/ruby_requirement_setter.rb
+++ bundler/lib/dependabot/bundler/file_updater/ruby_requirement_setter.rb
@@ -11,7 +11,7 @@ module Dependabot
         class RubyVersionNotFound < StandardError; end
 
         RUBY_VERSIONS = %w(
-          1.8.7 1.9.3 2.0.0 2.1.10 2.2.10 2.3.8 2.4.10 2.5.9 2.6.7 2.7.3 3.0.1 3.1.1 3.2.0
+          1.8.7 1.9.3 2.0.0 2.1.10 2.2.10 2.3.8 2.4.10 2.5.9 2.6.9 2.7.6 3.0.5 3.1.3 3.2.0
         ).freeze
 
         attr_reader :gemspec
--- bundler/spec/dependabot/bundler/file_updater/ruby_requirement_setter_spec.rb
+++ bundler/spec/dependabot/bundler/file_updater/ruby_requirement_setter_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe Dependabot::Bundler::FileUpdater::RubyRequirementSetter do
         let(:content) do
           bundler_project_dependency_file("gemfile", filename: "Gemfile").content
         end
-        it { is_expected.to include("ruby '3.0.1'\n") }
+        it { is_expected.to include("ruby '3.0.5'\n") }
         it { is_expected.to include(%(gem "business", "~> 1.4.0")) }
       end
 
@@ -110,7 +110,7 @@ RSpec.describe Dependabot::Bundler::FileUpdater::RubyRequirementSetter do
         let(:content) do
           bundler_project_dependency_file("gemfile", filename: "Gemfile").content
         end
-        it { is_expected.to include("ruby '3.1.1'\n") }
+        it { is_expected.to include("ruby '3.1.3'\n") }
         it { is_expected.to include(%(gem "business", "~> 1.4.0")) }
       end
 
