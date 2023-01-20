a7403a1d4ddecf00fb9382c986881de6ce80f361
Jake Coffman <jake@jakecoffman.com>
2023-01-20 21:57:59 +0000

    fix CLI installation in devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gh release download --repo dependabot/cli -p "*linux-amd64.tar.gz"
-tar xzvf "*.tar.gz" >/dev/null 2>&1
+tar xzvf ./*.tar.gz >/dev/null 2>&1
 sudo mv dependabot /usr/local/bin
-rm "*.tar.gz"
+rm ./*.tar.gz
 
 echo "export LOCAL_GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN" >> ~/.bashrc
