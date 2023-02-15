c3847d2f7105784cb6151a7d82f58fa8c5ce68ee
Jake Coffman <jake@jakecoffman.com>
2023-02-15 16:23:01 -0600

    provide the Go modules version

--- go_modules/lib/dependabot/go_modules/file_fetcher.rb
+++ go_modules/lib/dependabot/go_modules/file_fetcher.rb
@@ -14,6 +14,15 @@ module Dependabot
         "Repo must contain a go.mod."
       end
 
+      def package_manager_version
+        {
+          ecosystem: "gomod",
+          package_managers: {
+            "gomod" => go_mod.content.match(/^go\s(\d+\.\d+)/)&.captures&.first || "unknown"
+          }
+        }
+      end
+
       private
 
       def fetch_files
@@ -32,10 +41,8 @@ module Dependabot
           end
 
           fetched_files = [go_mod]
-
           # Fetch the (optional) go.sum
           fetched_files << go_sum if go_sum
-
           fetched_files
         end
       end
