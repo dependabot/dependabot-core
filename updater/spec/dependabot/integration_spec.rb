eac05688a4f53d7c03ae9c6d009bc51bf76828c9
Barry Gordon <brrygrdn@github.com>
2023-03-01 16:57:16 +0000

    Switch the integration script to use the separate fetch and update

--- updater/spec/dependabot/integration_spec.rb
+++ updater/spec/dependabot/integration_spec.rb
@@ -4,26 +4,51 @@ require "spec_helper"
 require "dependabot/dependency"
 require "dependabot/dependency_file"
 require "dependabot/file_fetchers"
-require "dependabot/end_to_end_command"
+
+require "dependabot/file_fetcher_command"
+require "dependabot/update_files_command"
+
 require "dependabot/api_client"
 
-RSpec.describe Dependabot::EndToEndCommand do
-  subject(:end_to_end_job) { Dependabot::EndToEndCommand.new }
+RSpec.describe "Dependabot Updates" do
+  let(:fetch_files) { Dependabot::FileFetcherCommand.new }
+  let(:update_files) { Dependabot::UpdateFilesCommand.new }
+
+  let(:run_job) do
+    fetch_files.run
+    update_files.run
+  end
 
   before { WebMock.disable! }
   after { WebMock.enable! }
 
   let(:job_id) { 1 }
   let(:api_client) { double(Dependabot::ApiClient) }
+  let(:job_path) do
+    File.join("spec", "fixtures", "file_fetcher_output/output.json")
+  end
+  let(:output_path) do
+    File.join(Dir.mktmpdir, "output.json")
+  end
 
   before do
-    allow(end_to_end_job).to receive(:api_client).and_return(api_client)
-    allow(end_to_end_job).to receive(:job).and_return(job)
-    allow(end_to_end_job).to receive(:job_id).and_return(1)
-    allow(end_to_end_job).to receive(:token).and_return("token")
-    allow(end_to_end_job).
-      to receive(:dependency_files).and_return(dependency_files)
-    allow(end_to_end_job).to receive(:base_commit_sha).and_return("sha")
+    allow(Dependabot::ApiClient).to receive(:new).and_return(api_client)
+
+    # Stub out the environment
+    allow(Dependabot::Environment).to receive(:job_id).and_return(job_id)
+    allow(Dependabot::Environment).to receive(:job_path).and_return(job_path)
+    allow(Dependabot::Environment).to receive(:output_path).and_return(output_path)
+    allow(Dependabot::Environment).to receive(:token).and_return("token")
+
+    # TODO: Wrap job-from-file processing in a class
+    allow(fetch_files).to receive(:job).and_return(job)
+    allow(update_files).to receive(:job).and_return(job)
+
+    # TODO: Stub the file fetcher instead
+    allow(fetch_files).to receive(:dependency_files).and_return(dependency_files)
+    allow(update_files).to receive(:dependency_files).and_return(dependency_files)
+
+    allow(update_files).to receive(:base_commit_sha).and_return("sha")
 
     allow(api_client).to receive(:create_pull_request)
     allow(api_client).to receive(:update_pull_request)
@@ -35,8 +60,6 @@ RSpec.describe Dependabot::EndToEndCommand do
     # Recording the package manager happens via an observer so the instantiated `api_client` does not receive this call
     allow_any_instance_of(Dependabot::ApiClient).to receive(:record_package_manager_version)
 
-    allow(Dependabot::Environment).to receive(:token).and_return("some_token")
-    allow(Dependabot::Environment).to receive(:job_id).and_return(job_id)
     allow(Dependabot.logger).to receive(:info).and_call_original
     message_builder = double(Dependabot::PullRequestCreator::MessageBuilder)
     allow(Dependabot::PullRequestCreator::MessageBuilder).to receive(:new).and_return(message_builder)
@@ -151,7 +174,7 @@ RSpec.describe Dependabot::EndToEndCommand do
           )
           expect(commit_sha).to eq("sha")
         end
-      end_to_end_job.run
+      run_job
     end
 
     it "summarizes the changes" do
@@ -159,7 +182,7 @@ RSpec.describe Dependabot::EndToEndCommand do
         expect(log_message).to include("created", "dummy-pkg-b ( from 1.1.0 to 1.2.0 )")
       end
 
-      end_to_end_job.run
+      run_job
     end
 
     context "when there is an exception that blocks PR creation" do
@@ -171,18 +194,18 @@ RSpec.describe Dependabot::EndToEndCommand do
         expect(api_client).to receive(:record_update_job_error).
           with(job_id, { error_type: "unknown_error", error_details: nil })
 
-        expect { end_to_end_job.run }.to output(/oh no!/).to_stdout_from_any_process
+        expect { run_job }.to output(/oh no!/).to_stdout_from_any_process
       end
 
       it "indicates there was an error in the summary" do
         expect(Dependabot.logger).not_to receive(:info).with(/Changes to Dependabot Pull Requests/)
         expect(Dependabot.logger).to receive(:info).with(/Dependabot encountered '1' error/)
 
-        expect { end_to_end_job.run }.to output(/oh no!/).to_stdout_from_any_process
+        expect { run_job }.to output(/oh no!/).to_stdout_from_any_process
       end
 
       it "does not raise an exception" do
-        expect { end_to_end_job.run }.to output(/oh no!/).to_stdout_from_any_process
+        expect { run_job }.to output(/oh no!/).to_stdout_from_any_process
       end
 
       context "when GITHUB_ACTIONS is set" do
@@ -191,7 +214,7 @@ RSpec.describe Dependabot::EndToEndCommand do
         end
 
         it "raises an exception" do
-          expect { end_to_end_job.run }.to raise_error(Dependabot::RunFailure).
+          expect { run_job }.to raise_error(Dependabot::RunFailure).
             and output(/oh no!/).to_stdout_from_any_process
         end
       end
@@ -199,8 +222,8 @@ RSpec.describe Dependabot::EndToEndCommand do
 
     context "when there is an exception that does not block PR creation" do
       before do
-        # Pre-populate an error in the service
-        end_to_end_job.service.record_update_job_error(
+        # Pre-populate an updater error
+        update_files.service.record_update_job_error(
           job_id,
           error_type: :epoch_error,
           error_details: {
@@ -215,11 +238,11 @@ RSpec.describe Dependabot::EndToEndCommand do
           expect(log_message).to include("Dependabot encountered '1' error")
         end
 
-        end_to_end_job.run
+        run_job
       end
 
       it "does not raise an exception" do
-        expect { end_to_end_job.run }.not_to raise_error
+        expect { run_job }.not_to raise_error
       end
 
       context "when GITHUB_ACTIONS is set" do
@@ -228,7 +251,7 @@ RSpec.describe Dependabot::EndToEndCommand do
         end
 
         it "raises an exception" do
-          expect { end_to_end_job.run }.to raise_error(Dependabot::RunFailure)
+          expect { run_job }.to raise_error(Dependabot::RunFailure)
         end
       end
     end
@@ -354,7 +377,7 @@ RSpec.describe Dependabot::EndToEndCommand do
           )
           expect(commit_sha).to eq("sha")
         end
-      end_to_end_job.run
+      run_job
     end
 
     it "summarizes the changes" do
@@ -367,7 +390,7 @@ RSpec.describe Dependabot::EndToEndCommand do
         )
       end
 
-      end_to_end_job.run
+      run_job
     end
   end
 end
