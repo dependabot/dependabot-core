a79c1729c32435981b2d0eae13e40ca4b177c2cd
Landon Grindheim <landon.grindheim@gmail.com>
2023-03-03 21:00:51 +0000

    Delegate to strategy based on group rule presence

--- common/lib/dependabot/pull_request_creator/branch_namer.rb
+++ common/lib/dependabot/pull_request_creator/branch_namer.rb
@@ -9,13 +9,14 @@ require "dependabot/pull_request_creator/branch_namer/solo_strategy"
 module Dependabot
   class PullRequestCreator
     class BranchNamer
-      attr_reader :dependencies, :files, :target_branch, :separator, :prefix, :max_length
+      attr_reader :dependencies, :files, :target_branch, :separator, :prefix, :max_length, :group_rule
 
-      def initialize(dependencies:, files:, target_branch:, separator: "/",
-                     prefix: "dependabot", max_length: nil)
+      def initialize(dependencies:, files:, target_branch:, group_rule: nil,
+                     separator: "/", prefix: "dependabot", max_length: nil)
         @dependencies  = dependencies
         @files         = files
         @target_branch = target_branch
+        @group_rule    = group_rule
         @separator     = separator
         @prefix        = prefix
         @max_length    = max_length
@@ -29,14 +30,26 @@ module Dependabot
 
       def strategy
         @strategy ||=
-          SoloStrategy.new(
-            dependencies: dependencies,
-            files: files,
-            target_branch: target_branch,
-            separator: separator,
-            prefix: prefix,
-            max_length: max_length
-          )
+          if group_rule.nil?
+            SoloStrategy.new(
+              dependencies: dependencies,
+              files: files,
+              target_branch: target_branch,
+              separator: separator,
+              prefix: prefix,
+              max_length: max_length
+            )
+          else
+            GroupRuleStrategy.new(
+              dependencies: dependencies,
+              files: files,
+              target_branch: target_branch,
+              group_rule: group_rule,
+              separator: separator,
+              prefix: prefix,
+              max_length: max_length
+            )
+          end
       end
     end
   end
