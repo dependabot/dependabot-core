95e5b6f3002e1cf6b7396a4f4ade9099347f31c1
Jeff Widman <jeff@jeffwidman.com>
2023-03-02 16:14:38 -0800

    temp hack to fix unit test in previous commit until I hear from composer authors

--- composer/helpers/v1/src/UpdateChecker.php
+++ composer/helpers/v1/src/UpdateChecker.php
@@ -85,9 +85,12 @@ final class UpdateChecker
             // TODO surprisingly the returned result of getPrettyVersion depends on the PHP version:
             // - PHP 7 returns: "2.4.1"
             // - PHP 8 returns: "2.4.1@stable"
-            file_put_contents('php://stdout', $updatedPackage->getPrettyVersion());
+            // file_put_contents('php://stdout', $updatedPackage->getPrettyVersion());
+            //
+            // return ltrim($updatedPackage->getPrettyVersion(), 'v');
+            $pretty = $updatedPackage->getPrettyVersion();
 
-            return ltrim($updatedPackage->getPrettyVersion(), 'v');
+            return rtrim(ltrim($pretty, 'v'), '@stable');
         }
 
         // We didn't find the package in the list of updated packages. Check if
