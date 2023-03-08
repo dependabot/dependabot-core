48f41af199fbee3e82cac531a1313722a851b65f
Barry Gordon <brrygrdn@github.com>
2023-03-08 21:21:35 +0000

    Memoize the prefix, Handle the cli placeholder id

--- updater/lib/dependabot/logger/formats.rb
+++ updater/lib/dependabot/logger/formats.rb
@@ -14,18 +14,32 @@ module Dependabot
     end
 
     class JobFormatter < ::Logger::Formatter
+      CLI_ID = "cli"
+      UNKNOWN_ID = "unknown_id"
+
       def initialize(job_id)
         @job_id = job_id
       end
 
       def call(severity, _datetime, _progname, msg)
-        "#{severity} <job_#{job_id}> #{msg2str(msg)}\n"
+        [
+          severity,
+          job_prefix,
+          msg2str(msg)
+        ].compact.join(" ") + "\n"
       end
 
       private
 
-      def job_id
-        @job_id || "unknown_id"
+      def job_prefix
+        return @job_prefix if defined? @job_prefix
+        # The dependabot/cli tool uses a placeholder value since it does not
+        # have an actual Job ID issued by the service.
+        #
+        # Let's just omit the prefix if this is the case.
+        return @job_prefix = nil if @job_id == CLI_ID
+
+        @job_prefix = "<job_#{@job_id || UNKNOWN_ID}>"
       end
     end
   end
