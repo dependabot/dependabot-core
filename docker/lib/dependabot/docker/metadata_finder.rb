c681772a4bb967046d45dbd818aad47c22e1f2dd
David McIntosh <804610+mctofu@users.noreply.github.com>
2023-03-17 06:12:42 +0000

    Link docker image to repo via OCI source annotation
    Allows us to include richer metadata about the release when creating
    Docker update PRs (commits, release notes, repo link, etc).
    
    There isn't a simple registry API to read the source annotation. Instead
    you need to locate and download the correct blob which contains it. We
    could implement this ourselves but the [regctl](https://github.com/regclient/regclient)
    tool made this pretty easy and might be an option to replace our native
    registry calls in the future.
    
    One note is I did not implement authentication support for regctl. On
    the GitHub platform or using the CLI the authentication will be handled
    by the proxy so it wasn't needed. This could be added later by anyone
    that needs it.

--- docker/lib/dependabot/docker/metadata_finder.rb
+++ docker/lib/dependabot/docker/metadata_finder.rb
@@ -2,6 +2,7 @@
 
 require "dependabot/metadata_finders"
 require "dependabot/metadata_finders/base"
+require "dependabot/shared_helpers"
 
 module Dependabot
   module Docker
@@ -9,7 +10,20 @@ module Dependabot
       private
 
       def look_up_source
-        # TODO: Find a way to add links to PRs
+        return if dependency.requirements.empty?
+
+        new_source = dependency.requirements.first[:source]
+        return unless new_source && new_source[:registry] && new_source[:tag]
+
+        image_ref = "#{new_source[:registry]}/#{dependency.name}:#{new_source[:tag]}"
+        image_details_output = SharedHelpers.run_shell_command("regctl image inspect #{image_ref}")
+        image_details = JSON.parse(image_details_output)
+        image_source = image_details.dig("config", "Labels", "org.opencontainers.image.source")
+        return unless image_source
+
+        Dependabot::Source.from_url(image_source)
+      rescue StandardError => e
+        Dependabot.logger.warn("Error looking up Docker source: #{e.message}")
         nil
       end
     end
