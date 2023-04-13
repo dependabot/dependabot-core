0b9675f9ff7800d8992c9743da59aba1c77379a8
Nish Sinha <nishnha@github.com>
2023-04-13 16:35:45 -0400

    Use the WildcardMatcher in common

--- common/lib/dependabot/dependency_group.rb
+++ common/lib/dependabot/dependency_group.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'wildcard_matcher'
+
 module Dependabot
   class DependencyGroup
     attr_reader :name, :rules, :dependencies
@@ -14,18 +16,5 @@ module Dependabot
       @dependencies.include?(dependency) if @dependencies.any?
       rules.any? { |rule| WildcardMatcher.match?(rule, dependency.name) }
     end
-  end
-end
-
-# Copied from updater/lib/wildcard_matcher.rb
-class WildcardMatcher
-  def self.match?(wildcard_string, candidate_string)
-    return false unless wildcard_string && candidate_string
-
-    regex_string = "a#{wildcard_string.downcase}a".split("*").
-                   map { |p| Regexp.quote(p) }.
-                   join(".*").gsub(/^a|a$/, "")
-    regex = /^#{regex_string}$/
-    regex.match?(candidate_string.downcase)
   end
 end
