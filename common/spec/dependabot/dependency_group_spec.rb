df7da814220f9cf60f6dfc2561b15121de5d6a59
Nish Sinha <nishnha@github.com>
2023-04-13 10:58:25 -0400

    snake-case dependency group names
    Co-authored-by: Barry Gordon <896971+brrygrdn@users.noreply.github.com>

--- common/spec/dependabot/dependency_group_spec.rb
+++ common/spec/dependabot/dependency_group_spec.rb
@@ -5,7 +5,7 @@ require "dependabot/dependency_group"
 RSpec.describe Dependabot::DependencyGroup do
   describe "#name" do
     it "returns the name" do
-      my_dependency_group_name = "Darren from work"
+      my_dependency_group_name = "darren-from-work"
       dependency_group = described_class.new(my_dependency_group_name)
 
       expect(dependency_group.name).to eq(my_dependency_group_name)
