efae1f09e84b8daefc8d51b2bbac372edb4f29da
Nish Sinha <nishnha@github.com>
2023-04-21 16:18:21 -0400

    Pass the serialized group name to the api client when creating a PR for grouped updates

--- common/lib/dependabot/dependency_group.rb
+++ common/lib/dependabot/dependency_group.rb
@@ -4,17 +4,28 @@ require "wildcard_matcher"
 
 module Dependabot
   class DependencyGroup
-    attr_reader :name, :rules, :dependencies
+    attr_reader :name, :rules, :dependencies, :id
 
     def initialize(name:, rules:)
       @name = name
       @rules = rules
       @dependencies = []
+      @id = id_hash
     end
 
     def contains?(dependency)
       @dependencies.include?(dependency) if @dependencies.any?
       rules.any? { |rule| WildcardMatcher.match?(rule, dependency.name) }
+    end
+
+    private
+
+    def id_hash
+      { "name" => serialized_group_name }
+    end
+
+    def serialized_group_name
+      name.downcase.gsub("-", "_").to_sym
     end
   end
 end
