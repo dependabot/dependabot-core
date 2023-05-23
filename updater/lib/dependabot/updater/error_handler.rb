bafb6a3d4d76a42206433142eb70e9b697326393
Barry Gordon <brrygrdn@github.com>
2023-05-23 15:41:36 +0100

    Add unit tests for Updater::ErrorHandler

--- updater/lib/dependabot/updater/error_handler.rb
+++ updater/lib/dependabot/updater/error_handler.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "dependabot/updater/errors"
+
 # This class is responsible for determining how to present a Dependabot::Error
 # to the Service and Logger.
 #
