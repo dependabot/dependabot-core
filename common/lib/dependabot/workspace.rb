f9d55f868cdab70bcd9a3130ae3f60a36ec0faa1
Barry Gordon <brrygrdn@github.com>
2023-06-22 21:30:22 +0100

    Changes are not stored by default, it must be explicit

--- common/lib/dependabot/workspace.rb
+++ common/lib/dependabot/workspace.rb
@@ -9,5 +9,31 @@ module Dependabot
     class << self
       attr_accessor :active_workspace
     end
+
+    def self.setup(repo_contents_path:, directory:)
+      Dependabot.logger.debug("Setting up workspace in #{repo_contents_path}")
+
+      @active_workspace = Dependabot::Workspace::Git.new(
+        repo_contents_path,
+        Pathname.new(directory || "/").cleanpath
+      )
+    end
+
+    def self.store_change(memo:)
+      return unless @active_workspace
+
+      Dependabot.logger.debug("Storing change to workspace: #{memo}")
+
+      @active_workspace.store_change(memo)
+    end
+
+    def self.cleanup!
+      return unless @active_workspace
+
+      Dependabot.logger.debug("Cleaning up current workspace")
+
+      @active_workspace.reset!
+      @active_workspace = nil
+    end
   end
 end
