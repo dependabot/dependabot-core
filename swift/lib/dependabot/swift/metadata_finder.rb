0c8275fbd880306f4baec2a9b31e753908a7e48c
David Rodríguez <deivid.rodriguez@riseup.net>
2023-06-27 17:49:57 +0200

    Implement MetadataFinder for Swift

--- swift/lib/dependabot/swift/metadata_finder.rb
+++ swift/lib/dependabot/swift/metadata_finder.rb
@@ -9,6 +9,25 @@ module Dependabot
       private
 
       def look_up_source
+        case new_source_type
+        when "git" then find_source_from_git_url
+        when "registry" then find_source_from_registry
+        else raise "Unexpected source type: #{new_source_type}"
+        end
+      end
+
+      def new_source_type
+        dependency.source_type
+      end
+
+      def find_source_from_git_url
+        info = dependency.source_details
+
+        url = info[:url] || info.fetch("url")
+        Source.from_url(url)
+      end
+
+      def find_source_from_registry
         raise NotImplementedError
       end
     end
