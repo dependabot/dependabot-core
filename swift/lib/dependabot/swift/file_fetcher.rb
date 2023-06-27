533c58d1c667141eae1eed12f5691df2edd123c1
David Rodríguez <deivid.rodriguez@riseup.net>
2023-06-27 17:47:01 +0200

    Implement FileFetcher for Swift

--- swift/lib/dependabot/swift/file_fetcher.rb
+++ swift/lib/dependabot/swift/file_fetcher.rb
@@ -7,17 +7,40 @@ module Dependabot
   module Swift
     class FileFetcher < Dependabot::FileFetchers::Base
       def self.required_files_in?(filenames)
-        raise NotImplementedError
+        filenames.include?("Package.swift")
       end
 
       def self.required_files_message
-        raise NotImplementedError
+        "Repo must contain a Package.swift configuration file."
       end
 
       private
 
       def fetch_files
-        raise NotImplementedError
+        check_required_files_present
+
+        fetched_files = []
+        fetched_files << package_manifest
+        fetched_files << package_resolved if package_resolved
+        fetched_files
+      end
+
+      def package_manifest
+        @package_manifest ||= fetch_file_from_host("Package.swift")
+      end
+
+      def package_resolved
+        return @package_resolved if defined?(@package_resolved)
+
+        @package_resolved = fetch_file_if_present("Package.resolved")
+      end
+
+      def check_required_files_present
+        return if package_manifest
+
+        path = Pathname.new(File.join(directory, "Package.swift")).
+               cleanpath.to_path
+        raise Dependabot::DependencyFileNotFound, path
       end
     end
   end
