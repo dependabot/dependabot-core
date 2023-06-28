9aaa7adcb59306f677ada0e9a9d4ba56aba45b16
David Rodríguez <deivid.rodriguez@riseup.net>
2023-06-28 15:14:08 +0200

    Implement swift requirement parsing

--- swift/lib/dependabot/swift/requirement.rb
+++ swift/lib/dependabot/swift/requirement.rb
@@ -5,6 +5,22 @@ require "dependabot/utils"
 module Dependabot
   module Swift
     class Requirement < Gem::Requirement
+      # For consistency with other languages, we define a requirements array.
+      # Swift doesn't have an `OR` separator for requirements, so it
+      # always contains a single element.
+      def self.requirements_array(requirement_string)
+        [new(requirement_string)]
+      end
+
+      # Patches Gem::Requirement to make it accept requirement strings like
+      # "~> 4.2.5, >= 4.2.5.1" without first needing to split them.
+      def initialize(*requirements)
+        requirements = requirements.flatten.flat_map do |req_string|
+          req_string.split(",").map(&:strip)
+        end
+
+        super(requirements)
+      end
     end
   end
 end
