cdc2f073dee925f55de250abe1791f9a59d6b49b
David Rodríguez <deivid.rodriguez@riseup.net>
2023-07-05 14:28:53 +0200

    Implement security update support for Swift

--- swift/lib/dependabot/swift/file_updater/lockfile_updater.rb
+++ swift/lib/dependabot/swift/file_updater/lockfile_updater.rb
@@ -2,6 +2,7 @@
 
 require "dependabot/file_updaters/base"
 require "dependabot/shared_helpers"
+require "dependabot/logger"
 
 module Dependabot
   module Swift
@@ -18,11 +19,10 @@ module Dependabot
           SharedHelpers.in_a_temporary_repo_directory(manifest.directory, repo_contents_path) do
             File.write(manifest.name, manifest.content)
 
+            dependency_names = dependencies.map(&:name).join(" ")
+
             SharedHelpers.with_git_configured(credentials: credentials) do
-              SharedHelpers.run_shell_command(
-                "swift package update #{dependencies.map(&:name).join(' ')}",
-                fingerprint: "swift package update <dependency_name>"
-              )
+              try_lockfile_update(dependency_names)
 
               File.read("Package.resolved")
             end
@@ -30,6 +30,19 @@ module Dependabot
         end
 
         private
+
+        def try_lockfile_update(dependency_names)
+          SharedHelpers.run_shell_command(
+            "swift package update #{dependency_names}",
+            fingerprint: "swift package update <dependency_names>"
+          )
+        rescue SharedHelpers::HelperSubprocessFailed => e
+          # This class is not only used for final lockfile updates, but for
+          # checking resolvability. So resolvability errors here are expected in
+          # certain situations and will result in `no_update_possible` outcomes.
+          # That said, since we're swallowing all errors we at least log them to ease debugging.
+          Dependabot.logger.info("Lockfile failed to be updated due to error:\n#{e.message}")
+        end
 
         attr_reader :dependencies, :manifest, :repo_contents_path, :credentials
       end
