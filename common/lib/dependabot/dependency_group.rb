6ad41be42233d0ef3069a30d29ee725a938e2d2c
Barry Gordon <brrygrdn@github.com>
2023-07-13 16:13:21 +0100

    A DependencyGroup can generate ignored ranges for dependencies

--- common/lib/dependabot/dependency_group.rb
+++ common/lib/dependabot/dependency_group.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
+require "dependabot/config/ignore_condition"
+
 require "wildcard_matcher"
 require "yaml"
 
 module Dependabot
   class DependencyGroup
+    ANY_DEPENDENCY_NAME = "*"
+    SECURITY_UPDATES_ONLY = false
+
+    class NullIgnoreCondition
+      def ignored_versions(_dependency, _security_updates_only)
+        []
+      end
+    end
+
     attr_reader :name, :rules, :dependencies
 
     def initialize(name:, rules:)
       @name = name
       @rules = rules
       @dependencies = []
+      @ignore_condition = generate_ignore_condition!
     end
 
     def contains?(dependency)
@@ -18,6 +30,12 @@ module Dependabot
       return false if matches_excluded_pattern?(dependency.name)
 
       matches_pattern?(dependency.name) && matches_dependency_type?(dependency)
+    end
+
+    # This method generates ignored versions for the given Dependency based on
+    # the any update-types we have defined.
+    def ignored_versions_for(dependency)
+      @ignore_condition.ignored_versions(dependency, SECURITY_UPDATES_ONLY)
     end
 
     def to_h
@@ -53,6 +71,15 @@ module Dependabot
                                   else
                                     "development"
                                   end
+    end
+
+    def generate_ignore_condition!
+      return NullIgnoreCondition.new unless rules["update-types"]&.any?
+
+      Dependabot::Config::IgnoreCondition.new(
+        dependency_name: ANY_DEPENDENCY_NAME,
+        update_types: Dependabot::Config::IgnoreCondition::VERSION_UPDATE_TYPES - rules["update-types"]
+      )
     end
   end
 end
