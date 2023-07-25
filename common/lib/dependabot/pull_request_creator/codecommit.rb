cb325b857215617cbd0818fa8fdd41acfe8ab12f
Jeff Widman <jeff@jeffwidman.com>
2023-07-25 10:47:06 -0700

    Update common/lib/dependabot/pull_request_creator/codecommit.rb

--- common/lib/dependabot/pull_request_creator/codecommit.rb
+++ common/lib/dependabot/pull_request_creator/codecommit.rb
@@ -10,8 +10,9 @@ module Dependabot
                   :files, :commit_message, :pr_description, :pr_name,
                   :author_details, :labeler
 
-      PR_DESCRIPTION_MAX_LENGTH = 10_239 # CodeCommit has a max length of 10240 (0 based count)
-      # https://docs.aws.amazon.com/codecommit/latest/APIReference/API_PullRequest.html
+     # CodeCommit limits PR descriptions to a max length of 10,240 characters:
+     # https://docs.aws.amazon.com/codecommit/latest/APIReference/API_PullRequest.html
+      PR_DESCRIPTION_MAX_LENGTH = 10_239 # 0 based count
 
       def initialize(source:, branch_name:, base_commit:, credentials:,
                      files:, commit_message:, pr_description:, pr_name:,
