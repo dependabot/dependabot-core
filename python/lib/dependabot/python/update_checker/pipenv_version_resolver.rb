52d620f3dc9611ea6bb4d47e31b76108195611da
Jeff Widman <jeff@jeffwidman.com>
2023-08-09 15:19:31 -0700

    `pipenv` renamed internal `patch.notpip` -> `patch.pip`
    Upstream `pipenv` renamed their internal patched version of `pip` folder
    to be called `patch.pip`:
    * https://github.com/pypa/pipenv/pull/5199
    
    So this updates our code as well.

--- python/lib/dependabot/python/update_checker/pipenv_version_resolver.rb
+++ python/lib/dependabot/python/update_checker/pipenv_version_resolver.rb
@@ -28,10 +28,9 @@ module Dependabot
       # just raise if the latest version can't be resolved. Knowing that is
       # still better than nothing, though.
       class PipenvVersionResolver
-        # rubocop:disable Layout/LineLength
         GIT_DEPENDENCY_UNREACHABLE_REGEX = /git clone --filter=blob:none (?<url>[^\s]+).*/
         GIT_REFERENCE_NOT_FOUND_REGEX = /git checkout -q (?<tag>[^\s]+).*/
-        PIPENV_INSTALLATION_ERROR = "pipenv.patched.notpip._internal.exceptions.InstallationError: Command errored out" \
+        PIPENV_INSTALLATION_ERROR = "pipenv.patched.pip._internal.exceptions.InstallationError: Command errored out" \
                                     " with exit status 1: python setup.py egg_info"
         TRACEBACK = "Traceback (most recent call last):"
         PIPENV_INSTALLATION_ERROR_REGEX =
@@ -41,7 +40,6 @@ module Dependabot
         UNSUPPORTED_DEP_REGEX =
           /Could not find a version that satisfies the requirement.*(?:#{UNSUPPORTED_DEPS.join("|")})/
         PIPENV_RANGE_WARNING = /Warning:\sPython\s[<>].* was not found/
-        # rubocop:enable Layout/LineLength
 
         DEPENDENCY_TYPES = %w(packages dev-packages).freeze
 
@@ -260,7 +258,7 @@ module Dependabot
                   next false if l.start_with?("CRITICAL:")
                   next false if l.start_with?("ERROR:")
                   next false if l.start_with?("packaging.specifiers")
-                  next false if l.start_with?("pipenv.patched.notpip._internal")
+                  next false if l.start_with?("pipenv.patched.pip._internal")
                   next false if l.include?("Max retries exceeded")
 
                   true
