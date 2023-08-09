a651b058790ef9e330b11e7367d51f2b6663adfc
David Rodríguez <deivid.rodriguez@riseup.net>
2023-08-09 22:22:44 +0200

    `fetch_file_if_present` should not raise (#7774)

--- maven/lib/dependabot/maven/file_fetcher.rb
+++ maven/lib/dependabot/maven/file_fetcher.rb
@@ -36,11 +36,7 @@ module Dependabot
       def extensions
         return @extensions if defined?(@extensions)
 
-        begin
-          fetch_file_if_present(".mvn/extensions.xml")
-        rescue Dependabot::DependencyFileNotFound
-          nil
-        end
+        fetch_file_if_present(".mvn/extensions.xml")
       end
 
       def child_poms
