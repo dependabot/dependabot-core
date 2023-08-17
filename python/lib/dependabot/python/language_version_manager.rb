ab766758961efb46d29419c20d48f2536385b17c
Jeff Widman <jeff@jeffwidman.com>
2023-08-16 23:37:08 -0700

    Stop manually installing Python (#7613)
    In the python dockerfile, we pre-install python 3.7-3.11, then `tar` it
    to save space.
    
    Now that we're not supporting python <= 3.6, we no longer have any
    reason to run `pyenv install`, so we can dop that and simplify our code.

--- python/lib/dependabot/python/language_version_manager.rb
+++ python/lib/dependabot/python/language_version_manager.rb
@@ -14,22 +14,9 @@ module Dependabot
         # The leading space is important in the version check
         return if SharedHelpers.run_shell_command("pyenv versions").include?(" #{python_major_minor}.")
 
-        if File.exist?("/usr/local/.pyenv/#{python_major_minor}.tar.gz")
-          SharedHelpers.run_shell_command(
-            "tar xzf /usr/local/.pyenv/#{python_major_minor}.tar.gz -C /usr/local/.pyenv/"
-          )
-          return if SharedHelpers.run_shell_command("pyenv versions").
-                    include?(" #{python_major_minor}.")
-        end
-
-        Dependabot.logger.info("Installing required Python #{python_version}.")
-        start = Time.now
-        SharedHelpers.run_shell_command("pyenv install -s #{python_version}")
-        SharedHelpers.run_shell_command("pyenv exec pip install --upgrade pip")
-        SharedHelpers.run_shell_command("pyenv exec pip install -r" \
-                                        "#{NativeHelpers.python_requirements_path}")
-        time_taken = Time.now - start
-        Dependabot.logger.info("Installing Python #{python_version} took #{time_taken}s.")
+        SharedHelpers.run_shell_command(
+          "tar xzf /usr/local/.pyenv/#{python_major_minor}.tar.gz -C /usr/local/.pyenv/"
+        )
       end
 
       def python_major_minor
