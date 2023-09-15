26f6eb801e401f120a4e6f2ab1ee8ddfadd85777
Jamie Magee <jamagee@microsoft.com>
2023-09-15 16:00:10 -0700

    Add sigs for version.rb

--- pub/lib/dependabot/pub/version.rb
+++ pub/lib/dependabot/pub/version.rb
@@ -17,26 +17,33 @@ require "dependabot/utils"
 module Dependabot
   module Pub
     class Version < Dependabot::Version
-      VERSION_PATTERN = Gem::Version::VERSION_PATTERN + "(\\+[0-9a-zA-Z\\-.]+)?"
+      extend T::Sig
+
+      VERSION_PATTERN = T.let(Gem::Version::VERSION_PATTERN + "(\\+[0-9a-zA-Z\\-.]+)?", String)
       ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})?\s*\z/
 
+      sig { returns(String) }
       attr_reader :build_info
 
+      sig { override.params(version: T.any(String, Gem::Version)).void }
       def initialize(version)
-        @version_string = version.to_s
+        @version_string = T.let(version.to_s, String)
         version, @build_info = version.to_s.split("+") if version.to_s.include?("+")
 
         super
       end
 
+      sig { override.returns(String) }
       def to_s
         @version_string
       end
 
+      sig { override.returns(String) }
       def inspect # :nodoc:
         "#<#{self.class} #{@version_string}>"
       end
 
+      sig { override.params(version: T.any(String, Gem::Version)).returns(T::Boolean) }
       def self.correct?(version)
         return false if version.nil?
 
