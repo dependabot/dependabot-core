395228beaf10c9f6934efb32a0fda43817c8a61e
Jake Coffman <jake@jakecoffman.com>
2023-10-20 11:45:07 -0500

    add types to service

--- updater/lib/dependabot/service.rb
+++ updater/lib/dependabot/service.rb
@@ -1,9 +1,10 @@
-# typed: false
+# typed: strict
 # frozen_string_literal: true
 
 require "raven"
 require "terminal-table"
 require "dependabot/api_client"
+require "sorbet-runtime"
 
 # This class provides an output adapter for the Dependabot Service which manages
 # communication with the private API as well as consolidated error handling.
@@ -13,46 +14,61 @@ require "dependabot/api_client"
 #
 module Dependabot
   class Service
+    extend T::Sig
     extend Forwardable
-    attr_reader :pull_requests, :errors
 
+    sig { returns(T::Array[T::Array[String]]) }
+    attr_reader :pull_requests
+
+    sig { returns(T::Array[T::Array[T.any(String, Dependabot::Dependency)]]) }
+    attr_reader :errors
+
+    sig { params(client: Dependabot::ApiClient).void }
     def initialize(client:)
       @client = client
-      @pull_requests = []
-      @errors = []
+      @pull_requests = T.let([], T::Array[T.untyped])
+      @errors = T.let([], T::Array[T.untyped])
     end
 
     def_delegators :client,
                    :mark_job_as_processed,
-                   :update_dependency_list,
                    :record_ecosystem_versions,
                    :increment_metric
 
+    sig { params(dependency_change: Dependabot::DependencyChange, base_commit_sha: String).void }
     def create_pull_request(dependency_change, base_commit_sha)
       client.create_pull_request(dependency_change, base_commit_sha)
-      @pull_requests << [dependency_change.humanized, :created]
+      pull_requests << [dependency_change.humanized, :created]
     end
 
+    sig { params(dependency_change: Dependabot::DependencyChange, base_commit_sha: String).void }
     def update_pull_request(dependency_change, base_commit_sha)
       client.update_pull_request(dependency_change, base_commit_sha)
-      @pull_requests << [dependency_change.humanized, :updated]
+      pull_requests << [dependency_change.humanized, :updated]
     end
 
+    sig { params(dependencies: T.any(String, T::Array[String]), reason: Symbol).void }
     def close_pull_request(dependencies, reason)
       client.close_pull_request(dependencies, reason)
       humanized_deps = dependencies.is_a?(String) ? dependencies : dependencies.join(",")
-      @pull_requests << [humanized_deps, "closed: #{reason}"]
+      pull_requests << [humanized_deps, "closed: #{reason}"]
     end
 
+    sig do
+      params(error_type: String, error_details: T::Hash[T.untyped, T.untyped],
+             dependency: T.nilable(Dependabot::Dependency)).void
+    end
     def record_update_job_error(error_type:, error_details:, dependency: nil)
-      @errors << [error_type.to_s, dependency]
+      errors << [error_type.to_s, dependency]
       client.record_update_job_error(error_type: error_type, error_details: error_details)
     end
 
+    sig { params(error_type: String, error_details: T::Hash[T.untyped, T.untyped]).void }
     def record_update_job_unknown_error(error_type:, error_details:)
       client.record_update_job_unknown_error(error_type: error_type, error_details: error_details)
     end
 
+    sig { params(dependency_snapshot: Dependabot::DependencySnapshot).void }
     def update_dependency_list(dependency_snapshot:)
       dependency_payload = dependency_snapshot.dependencies.map do |dep|
         {
@@ -71,8 +87,18 @@ module Dependabot
     #
     # This should be called as an alternative/in addition to record_update_job_error
     # for cases where an error could indicate a problem with the service.
+    sig do
+      params(
+        error: StandardError,
+        job: T.untyped,
+        dependency: T.nilable(Dependabot::Dependency),
+        dependency_group: T.nilable(Dependabot::DependencyGroup),
+        tags: T::Hash[String, T.untyped],
+        extra: T::Hash[String, T.untyped]
+      ).void
+    end
     def capture_exception(error:, job: nil, dependency: nil, dependency_group: nil, tags: {}, extra: {})
-      Raven.capture_exception(
+      T.unsafe(Raven).capture_exception(
         error,
         {
           tags: tags.merge({
@@ -88,10 +114,12 @@ module Dependabot
       )
     end
 
+    sig { returns(T::Boolean) }
     def noop?
       pull_requests.empty? && errors.empty?
     end
 
+    sig { returns(T::Boolean) }
     def failure?
       errors.any?
     end
@@ -106,6 +134,7 @@ module Dependabot
     # | closed:dependency-removed  | package-c                         |
     # +----------------------------+-----------------------------------+
     #
+    sig { returns(T.nilable(String)) }
     def summary
       return if noop?
 
@@ -120,17 +149,20 @@ module Dependabot
 
     private
 
+    sig { returns(Dependabot::ApiClient) }
     attr_reader :client
 
+    sig { returns(T.nilable(Terminal::Table)) }
     def pull_request_summary
       return unless pull_requests.any?
 
-      Terminal::Table.new do |t|
+      T.unsafe(Terminal::Table).new do |t|
         t.title = "Changes to Dependabot Pull Requests"
         t.rows = pull_requests.map { |deps, action| [action, truncate(deps)] }
       end
     end
 
+    sig { returns(T.nilable(String)) }
     def error_summary
       return unless errors.any?
 
@@ -144,11 +176,12 @@ module Dependabot
     # +--------------------+
     # | job_repo_not_found |
     # +--------------------+
+    sig { returns(T.nilable(Terminal::Table)) }
     def job_error_type_summary
       job_error_types = errors.filter_map { |error_type, dependency| [error_type] if dependency.nil? }
       return if job_error_types.none?
 
-      Terminal::Table.new do |t|
+      T.unsafe(Terminal::Table).new do |t|
         t.title = "Errors"
         t.rows = job_error_types
       end
@@ -161,18 +194,20 @@ module Dependabot
     # +---------------------+---------------+
     # | best_dependency_yay | unknown_error |
     # +---------------------+---------------+
+    sig { returns(T.nilable(Terminal::Table)) }
     def dependency_error_summary
       dependency_errors = errors.filter_map do |error_type, dependency|
         [dependency.name, error_type] unless dependency.nil?
       end
       return if dependency_errors.none?
 
-      Terminal::Table.new do |t|
+      T.unsafe(Terminal::Table).new do |t|
         t.title = "Dependencies failed to update"
         t.rows = dependency_errors
       end
     end
 
+    sig { params(string: String, max: Integer).returns(String) }
     def truncate(string, max: 120)
       snip = max - 3
       string.length > max ? "#{string[0...snip]}..." : string
