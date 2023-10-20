418ff5f0b7e982e4364db113369e7cfbfb1710f8
Jamie Magee <jamagee@microsoft.com>
2023-10-20 14:55:56 -0700

    `spoom bump --from=false --to=true`

--- updater/lib/dependabot/dependency_group_engine.rb
+++ updater/lib/dependabot/dependency_group_engine.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 require "dependabot/dependency_group"
--- updater/lib/dependabot/updater/dependency_group_change_batch.rb
+++ updater/lib/dependabot/updater/dependency_group_change_batch.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 # This class is responsible for aggregating individual DependencyChange objects
