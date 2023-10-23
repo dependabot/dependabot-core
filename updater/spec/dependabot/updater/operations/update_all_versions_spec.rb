f388c0697af00852afe0b497692c7a4027a764b4
Ryan Brandenburg <rybrande@microsoft.com>
2023-10-23 15:22:56 -0700

    PR feedback

--- updater/spec/dependabot/updater/operations/update_all_versions_spec.rb
+++ updater/spec/dependabot/updater/operations/update_all_versions_spec.rb
@@ -5,6 +5,9 @@ require "spec_helper"
 require "support/dummy_pkg_helpers"
 require "support/dependency_file_helpers"
 
+require "dependabot/service"
+require "dependabot/dependency_snapshot"
+require "dependabot/updater/error_handler"
 require "dependabot/updater/operations/update_all_versions"
 require_relative "empty_return_update_checker"
 
@@ -38,7 +41,7 @@ RSpec.describe Dependabot::Updater::Operations::UpdateAllVersions do
 
   let(:job) do
     instance_double(Dependabot::Job,
-                    package_manager: "emptyreturn",
+                    package_manager: "bundler",
                     repo_contents_path: nil,
                     credentials: [],
                     reject_external_code?: false,
@@ -77,10 +80,19 @@ RSpec.describe Dependabot::Updater::Operations::UpdateAllVersions do
 
   before do
     allow(Dependabot.logger).to receive(:info)
-    stub_request(:get, "https://rubygems.org/api/v1/gems/business.json")
-      .to_return(status: 200, body: fixture("ruby/rubygems_response.json"))
-    stub_request(:get, "https://rubygems.org/api/v1/versions/business.json")
-      .to_return(status: 200, body: fixture("ruby/versions-business.json"))
+    allow_any_instance_of(Dependabot::Bundler::UpdateChecker)
+      .to receive(:latest_version).and_return("")
+    allow_any_instance_of(Dependabot::Bundler::UpdateChecker)
+      .to receive(:can_update?).and_return(true)
+    allow_any_instance_of(Dependabot::Bundler::UpdateChecker)
+      .to receive(:up_to_date?).and_return(false)
+    allow_any_instance_of(Dependabot::Bundler::UpdateChecker)
+      .to receive(:updated_dependencies).and_return([])
+
+    allow_any_instance_of(Dependabot::Bundler::FileUpdater)
+      .to receive(:parse).and_return([])
+    allow_any_instance_of(Dependabot::Bundler::FileUpdater)
+      .to receive(:check_required_files)
   end
 
   after do
@@ -104,7 +116,7 @@ RSpec.describe Dependabot::Updater::Operations::UpdateAllVersions do
         previous_version: dependency_previous_version,
         requirements: requirements,
         previous_requirements: previous_requirements,
-        package_manager: "emptyreturn"
+        package_manager: "bundler"
       )
     end
 
