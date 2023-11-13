93421fdd4a2642fe509fe0539a5691c374e96204
Jake Coffman <jake@jakecoffman.com>
2023-11-13 11:42:28 -0600

    didn't mean to do this yet

--- go_modules/lib/dependabot/go_modules/file_fetcher.rb
+++ go_modules/lib/dependabot/go_modules/file_fetcher.rb
@@ -30,35 +30,35 @@ module Dependabot
       def fetch_files
         # Ensure we always check out the full repo contents for go_module
         # updates.
-        unless go_mod
-          raise(
-            Dependabot::DependencyFileNotFound,
-            Pathname.new(File.join(directory, "go.mod"))
-            .cleanpath.to_path
-          )
+        SharedHelpers.in_a_temporary_repo_directory(
+          directory,
+          clone_repo_contents
+        ) do
+          unless go_mod
+            raise(
+              Dependabot::DependencyFileNotFound,
+              Pathname.new(File.join(directory, "go.mod"))
+                      .cleanpath.to_path
+            )
+          end
+
+          fetched_files = [go_mod]
+          # Fetch the (optional) go.sum
+          fetched_files << go_sum if go_sum
+          fetched_files
         end
-
-        fetched_files = [go_mod]
-        # Fetch the (optional) go.sum
-        fetched_files << go_sum if go_sum
-
-        fetched_files
       end
 
       def go_mod
         return @go_mod if defined?(@go_mod)
 
-        SharedHelpers.in_a_temporary_repo_directory(directory, clone_repo_contents) do
-          @go_mod = fetch_file_if_present("go.mod")
-        end
+        @go_mod = fetch_file_if_present("go.mod")
       end
 
       def go_sum
         return @go_sum if defined?(@go_sum)
 
-        SharedHelpers.in_a_temporary_repo_directory(directory, clone_repo_contents) do
-          @go_sum = fetch_file_if_present("go.sum")
-        end
+        @go_sum = fetch_file_if_present("go.sum")
       end
 
       def recurse_submodules_when_cloning?
