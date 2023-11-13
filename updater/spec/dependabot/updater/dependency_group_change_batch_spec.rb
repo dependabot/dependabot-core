a86730244724330bf10554ee427cbb33c0bc29a2
Jake Coffman <jake@jakecoffman.com>
2023-11-13 13:00:09 -0600

    lint

--- updater/spec/dependabot/updater/dependency_group_change_batch_spec.rb
+++ updater/spec/dependabot/updater/dependency_group_change_batch_spec.rb
@@ -14,22 +14,21 @@ RSpec.describe Dependabot::Updater::DependencyGroupChangeBatch do
         Dependabot::DependencyFile.new(name: "Gemfile.lock", content: "mock-gemfile-lock", directory: "/hello/.."),
         Dependabot::DependencyFile.new(name: "Gemfile", content: "mock-package-json", directory: "/elsewhere"),
         Dependabot::DependencyFile.new(name: "Gemfile", content: "mock-package-json", directory: "unknown"),
-        Dependabot::DependencyFile.new(name: "Gemfile", content: "mock-package-json", directory: "../../oob"),
+        Dependabot::DependencyFile.new(name: "Gemfile", content: "mock-package-json", directory: "../../oob")
       ]
     end
 
     it "returns the current dependency files filtered by directory" do
-      expect(described_class.new(initial_dependency_files: files).
-        current_dependency_files('/').map { |f| f.name }).to eq(%w[Gemfile Gemfile.lock])
+      expect(described_class.new(initial_dependency_files: files)
+        .current_dependency_files("/").map(&:name)).to eq(%w(Gemfile Gemfile.lock))
     end
 
     it "normalizes the directory" do
-      expect(described_class.new(initial_dependency_files: files).
-        current_dependency_files('/.').map { |f| f.name }).to eq(%w[Gemfile Gemfile.lock])
+      expect(described_class.new(initial_dependency_files: files)
+        .current_dependency_files("/.").map(&:name)).to eq(%w(Gemfile Gemfile.lock))
 
-      expect(described_class.new(initial_dependency_files: files).
-        current_dependency_files('/hello/..').map { |f| f.name }).to eq(%w[Gemfile Gemfile.lock])
+      expect(described_class.new(initial_dependency_files: files)
+        .current_dependency_files("/hello/..").map(&:name)).to eq(%w(Gemfile Gemfile.lock))
     end
-
   end
 end
