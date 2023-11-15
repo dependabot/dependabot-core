b829319c0f096bdee65129afbeafac39be1c78f1
Jake Coffman <jake@jakecoffman.com>
2023-11-15 05:34:14 -0600

    allow each result to be memoized

--- common/lib/dependabot/file_fetchers/base.rb
+++ common/lib/dependabot/file_fetchers/base.rb
@@ -104,9 +104,10 @@ module Dependabot
 
       sig { returns(T::Array[DependencyFile]) }
       def files
-        @files ||= T.let(fetch_files, T.nilable(T::Array[DependencyFile]))
-        @files.each { |f| f.job_directory = directory }
-        @files
+        @files ||= T.let(
+          fetch_files.each { |f| f.job_directory = directory },
+          T.nilable(T::Array[DependencyFile])
+        )
       end
 
       sig { abstract.returns(T::Array[DependencyFile]) }
