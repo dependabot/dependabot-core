42e6aa7ba056955534a3a4de70061c2ec23fb3ef
Nish Sinha <nishnha@github.com>
2023-11-30 12:58:27 -0500

    DRY up the grouped security update group creation

--- updater/lib/dependabot/updater/operations/create_group_security_update_pull_request.rb
+++ updater/lib/dependabot/updater/operations/create_group_security_update_pull_request.rb
@@ -68,12 +68,7 @@ module Dependabot
           return @group if defined?(@group)
 
           # make a temporary fake group to use the existing logic
-          @group = Dependabot::DependencyGroup.new(
-            name: "#{job.package_manager} group across #{job&.directories.count || 1} directories",
-            rules: {
-              "patterns" => "*" # The grouping is more dictated by the dependencies passed in.
-            }
-          )
+          @group = grouped_security_update_group(job) 
           dependency_snapshot.job_dependencies.each do |dep|
             @group.dependencies << dep
           end
