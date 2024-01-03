966fc036192c6f10a58314857d692a6e68456321
Ryan Brandenburg <rybrande@microsoft.com>
2024-01-03 13:59:39 -0800

    fix paged version

--- nuget/lib/dependabot/nuget/update_checker/version_finder.rb
+++ nuget/lib/dependabot/nuget/update_checker/version_finder.rb
@@ -363,7 +363,7 @@ module Dependabot
           versions = Set.new
           items.each do |item|
             catalog_entry = item.fetch("catalogEntry")
-            versions << item.fetch("version") if catalog_entry["listed"] == true
+            versions << catalog_entry.fetch("version") if catalog_entry["listed"] == true
           end
         rescue Excon::Error::Timeout, Excon::Error::Socket
           repo_url = repository_details[:repository_url]
