44e95fc677ffd49d03d9200a518f354a74dc8905
Ryan Brandenburg <rybrande@microsoft.com>
2024-01-03 10:35:58 -0800

    Fix tests

--- nuget/spec/dependabot/nuget/update_checker/version_finder_spec.rb
+++ nuget/spec/dependabot/nuget/update_checker/version_finder_spec.rb
@@ -257,7 +257,7 @@ RSpec.describe Dependabot::Nuget::UpdateChecker::VersionFinder do
       end
       let(:custom_nuget_search_url) do
         "https://www.myget.org/F/exceptionless/api/v3/" \
-          "query?q=microsoft.extensions.dependencymodel&prerelease=true&semVerLevel=2.0.0"
+          "registration1/microsoft.extensions.dependencymodel"
       end
       before do
         stub_request(:get, nuget_versions_url).to_return(status: 404)
