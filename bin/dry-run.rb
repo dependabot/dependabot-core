1ff67a9058c7f42d82923c4ca3e90f692ce2895d
Ryan Brandenburg <rybrande@microsoft.com>
2024-01-04 11:51:20 -0800

    Lint

--- bin/dry-run.rb
+++ bin/dry-run.rb
@@ -143,16 +143,13 @@ unless ENV["LOCAL_GITHUB_ACCESS_TOKEN"].to_s.strip.empty?
 end
 
 unless ENV["LOCAL_AZURE_ACCESS_TOKEN"].to_s.strip.empty?
-  if ENV["LOCAL_AZURE_FEED_URL"]
-    $options[:credentials] << {
-      "type" => "nuget_feed",
-      "host" => "pkgs.dev.azure.com",
-      "url" => ENV.fetch("LOCAL_AZURE_FEED_URL", nil),
-      "token" => ":#{ENV.fetch('LOCAL_AZURE_ACCESS_TOKEN', nil)}"
-    }
-  else
-    raise "LOCAL_AZURE_ACCESS_TOKEN supplied without LOCAL_AZURE_FEED_URL"
-  end
+  raise "LOCAL_AZURE_ACCESS_TOKEN supplied without LOCAL_AZURE_FEED_URL" unless ENV["LOCAL_AZURE_FEED_URL"]
+  $options[:credentials] << {
+    "type" => "nuget_feed",
+    "host" => "pkgs.dev.azure.com",
+    "url" => ENV.fetch("LOCAL_AZURE_FEED_URL", nil),
+    "token" => ":#{ENV.fetch('LOCAL_AZURE_ACCESS_TOKEN', nil)}"
+  }
 end
 
 unless ENV["LOCAL_CONFIG_VARIABLES"].to_s.strip.empty?
