d7ab8ec5a19627d4f13953b5d9ce76c1c04b69d9
Yaroslav Halchenko <debian@onerussian.com>
2024-01-29 18:07:13 -0500

    [DATALAD RUNCMD] run codespell throughout fixing additional new typos automagically
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "codespell -w",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
+++ python/spec/dependabot/python/update_checker/pipenv_version_resolver_spec.rb
@@ -397,7 +397,7 @@ RSpec.describe Dependabot::Python::UpdateChecker::PipenvVersionResolver do
       end
     end
 
-    context "with a missing system libary, and when running python older than 3.12" do
+    context "with a missing system library, and when running python older than 3.12" do
       # NOTE: Attempt to update an unrelated dependency (requests) to cause
       # resolution to fail for rtree which has a system dependency on
       # libspatialindex which isn't installed in dependabot-core's Dockerfile.
