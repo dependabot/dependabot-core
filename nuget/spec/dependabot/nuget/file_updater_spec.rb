7afd6abc49033e758ec1e9728e048e7e3cead3b3
Ryan Brandenburg <rybrande@microsoft.com>
2024-01-30 16:13:57 -0800

    Match array

--- nuget/spec/dependabot/nuget/file_updater_spec.rb
+++ nuget/spec/dependabot/nuget/file_updater_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe Dependabot::Nuget::FileUpdater do
 
     context "with a dirs.proj" do
       it "does not repeatedly update the same project", focus: true do
-        expect(updated_files).to eq([
+        expect(updated_files.map(&:name)).to match_array([
           "Dir1/Dir1a/Dir1a.csproj"
         ])
 
