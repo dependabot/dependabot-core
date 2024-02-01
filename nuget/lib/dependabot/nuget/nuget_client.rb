7117df961f93be4442b137c6cd30f1ea2b81a93d
Ryan Brandenburg <rybrande@microsoft.com>
2024-02-01 09:26:13 -0800

    Sorbet types for nuget_client

--- nuget/lib/dependabot/nuget/nuget_client.rb
+++ nuget/lib/dependabot/nuget/nuget_client.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "dependabot/nuget/cache_manager"
@@ -7,17 +7,20 @@ require "dependabot/nuget/update_checker/repository_finder"
 module Dependabot
   module Nuget
     class NugetClient
+      extend T::Sig
+
+      sig { params(dependency_name: String, repository_details: T::Hash[Symbol, String]).returns(T::Set[String]) }
       def self.get_package_versions(dependency_name, repository_details)
         repository_type = repository_details.fetch(:repository_type)
         if repository_type == "v3"
           get_package_versions_v3(dependency_name, repository_details)
         elsif repository_type == "v2"
           get_package_versions_v2(dependency_name, repository_details)
-        else
-          raise "Unknown repository type: #{repository_type}"
         end
+        raise "Unknown repository type: #{repository_type}"
       end
 
+      sig { params(dependency_name: String, repository_details: T::Hash[Symbol, String]).returns(T::Set[String]) }
       private_class_method def self.get_package_versions_v3(dependency_name, repository_details)
         # Use the registration URL if possible because it is fast and correct
         if repository_details[:registration_url]
@@ -29,8 +32,14 @@ module Dependabot
         elsif repository_details[:versions_url]
           get_versions_from_versions_url_v3(repository_details)
         end
+
+        raise "No version sources were available for #{dependency_name} in #{repository_details}"
       end
 
+      sig do
+        params(dependency_name: String, repository_details: T::Hash[Symbol, String])
+          .returns(T.nilable(T::Set[String]))
+      end
       private_class_method def self.get_package_versions_v2(dependency_name, repository_details)
         doc = execute_xml_nuget_request(repository_details.fetch(:versions_url), repository_details)
         return unless doc
@@ -49,38 +58,32 @@ module Dependabot
         matching_versions
       end
 
+      sig { params(repository_details: T::Hash[Symbol, String]).returns(T.nilable(T::Array[String])) }
       private_class_method def self.get_versions_from_versions_url_v3(repository_details)
-        body = execute_json_nuget_request(repository_details[:versions_url], repository_details)
+        body = execute_json_nuget_request(repository_details.fetch(:versions_url), repository_details)
         body&.fetch("versions")
       end
 
+      sig { params(repository_details: T::Hash[Symbol, String]).returns(T.nilable(T::Set[String])) }
       private_class_method def self.get_versions_from_registration_v3(repository_details)
-        url = repository_details[:registration_url]
+        url = repository_details.fetch(:registration_url)
         body = execute_json_nuget_request(url, repository_details)
 
         return unless body
 
         pages = body.fetch("items")
-        versions = Set.new
+        versions = T.let(Set.new, T::Set[String])
         pages.each do |page|
           items = page["items"]
           if items
             # inlined entries
-            items.each do |item|
-              catalog_entry = item["catalogEntry"]
-
-              # a package is considered listed if the `listed` property is either `true` or missing
-              listed_property = catalog_entry["listed"]
-              is_listed = listed_property.nil? || listed_property == true
-              if is_listed
-                vers = catalog_entry["version"]
-                versions << vers
-              end
-            end
+            get_versions_from_inline_page(items, versions)
           else
             # paged entries
             page_url = page["@id"]
             page_body = execute_json_nuget_request(page_url, repository_details)
+            next unless page_body
+
             items = page_body.fetch("items")
             items.each do |item|
               catalog_entry = item.fetch("catalogEntry")
@@ -92,8 +95,27 @@ module Dependabot
         versions
       end
 
+      sig { params(items: T::Array[T::Hash[String, T.untyped]], versions: T::Set[String]).void }
+      private_class_method def self.get_versions_from_inline_page(items, versions)
+        items.each do |item|
+          catalog_entry = item["catalogEntry"]
+
+          # a package is considered listed if the `listed` property is either `true` or missing
+          listed_property = catalog_entry["listed"]
+          is_listed = listed_property.nil? || listed_property == true
+          if is_listed
+            vers = catalog_entry["version"]
+            versions << vers
+          end
+        end
+      end
+
+      sig do
+        params(repository_details: T::Hash[Symbol, String], dependency_name: String)
+          .returns(T.nilable(T::Set[String]))
+      end
       private_class_method def self.get_versions_from_search_url_v3(repository_details, dependency_name)
-        search_url = repository_details[:search_url]
+        search_url = repository_details.fetch(:search_url)
         body = execute_json_nuget_request(search_url, repository_details)
 
         body&.fetch("data")
@@ -102,11 +124,14 @@ module Dependabot
             &.map { |d| d.fetch("version") }
       end
 
+      sig do
+        params(url: String, repository_details: T::Hash[Symbol, T.untyped]).returns(T.nilable(Nokogiri::XML::Document))
+      end
       private_class_method def self.execute_xml_nuget_request(url, repository_details)
         response = execute_nuget_request_internal(
           url: url,
-          auth_header: repository_details[:auth_header],
-          repository_url: repository_details[:repository_url]
+          auth_header: repository_details.fetch(:auth_header),
+          repository_url: repository_details.fetch(:repository_url)
         )
         return unless response.status == 200
 
@@ -115,11 +140,16 @@ module Dependabot
         doc
       end
 
+      sig do
+        params(url: String,
+               repository_details: T::Hash[Symbol,
+                                           T.untyped]).returns(T.nilable(T::Hash[T.untyped, T.untyped]))
+      end
       private_class_method def self.execute_json_nuget_request(url, repository_details)
         response = execute_nuget_request_internal(
           url: url,
-          auth_header: repository_details[:auth_header],
-          repository_url: repository_details[:repository_url]
+          auth_header: repository_details.fetch(:auth_header),
+          repository_url: repository_details.fetch(:repository_url)
         )
         return unless response.status == 200
 
@@ -127,11 +157,10 @@ module Dependabot
         JSON.parse(body)
       end
 
-      private_class_method def self.execute_nuget_request_internal(
-        url: String,
-        auth_header: String,
-        repository_url: String
-      )
+      sig do
+        params(url: String, auth_header: T::Hash[Symbol, T.untyped], repository_url: String).returns(Excon::Response)
+      end
+      private_class_method def self.execute_nuget_request_internal(url:, auth_header:, repository_url:)
         cache = CacheManager.cache("dependency_url_search_cache")
         if cache[url].nil?
           response = Dependabot::RegistryClient.get(
@@ -156,6 +185,7 @@ module Dependabot
         raise PrivateSourceTimedOut, repo_url
       end
 
+      sig { params(string: String).returns(String) }
       private_class_method def self.remove_wrapping_zero_width_chars(string)
         string.force_encoding("UTF-8").encode
               .gsub(/\A[\u200B-\u200D\uFEFF]/, "")
