c348f2852aa0dd0fb6e40e9e96031535c5be109a
Jamie Magee <jamie.magee@gmail.com>
2024-02-04 13:11:33 -0800

    Strict type `Dependabot::Devcontainers`

--- common/lib/dependabot/file_updaters/base.rb
+++ common/lib/dependabot/file_updaters/base.rb
@@ -26,7 +26,7 @@ module Dependabot
       sig { returns(T::Hash[Symbol, T.untyped]) }
       attr_reader :options
 
-      sig { overridable.returns(String) }
+      sig { overridable.returns(T::Array[Regexp]) }
       def self.updated_files_regex
         raise NotImplementedError
       end
--- devcontainers/lib/dependabot/devcontainers/metadata_finder.rb
+++ devcontainers/lib/dependabot/devcontainers/metadata_finder.rb
@@ -1,5 +1,7 @@
-# typed: true
+# typed: strong
 # frozen_string_literal: true
+
+require "sorbet-runtime"
 
 require "dependabot/metadata_finders"
 require "dependabot/metadata_finders/base"
@@ -7,8 +9,11 @@ require "dependabot/metadata_finders/base"
 module Dependabot
   module Devcontainers
     class MetadataFinder < Dependabot::MetadataFinders::Base
+      extend T::Sig
+
       private
 
+      sig { override.returns(T.nilable(Dependabot::Source)) }
       def look_up_source
         # TODO: Make upstream changes to dev container CLI to point to docs.
         #       Specifically, 'devcontainers features info' can be augmented to expose documentationUrl
