755dbe12e93743d4c5bff3ef601af24751407e22
Jamie Magee <jamagee@microsoft.com>
2024-03-01 08:29:35 -0800

    Ensure `T::Set` from `NuGetClient.get_package_versions`

--- nuget/lib/dependabot/nuget/nuget_client.rb
+++ nuget/lib/dependabot/nuget/nuget_client.rb
@@ -132,6 +132,7 @@ module Dependabot
             &.find { |d| d.fetch("id").casecmp(dependency_name.downcase).zero? }
             &.fetch("versions")
             &.map { |d| d.fetch("version") }
+            &.to_set
       end
 
       sig do
