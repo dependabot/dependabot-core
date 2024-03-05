74eac7199e48c82889c0a5c88cecc384b1f98de8
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2024-03-05 23:39:05 +0100

    fix specs

--- common/spec/dependabot/shared_helpers_spec.rb
+++ common/spec/dependabot/shared_helpers_spec.rb
@@ -607,6 +607,13 @@ RSpec.describe Dependabot::SharedHelpers do
       Dependabot::SharedHelpers.with_git_configured(credentials: credentials, &block)
     end
 
+    before do
+      allow(SecureRandom).to receive(:hex).and_return("XXXXXXXXXXXXXXXX")
+    end
+    let(:git_config_path) { File.expand_path("XXXXXXXXXXXXXXXX.gitconfig", tmp) }
+    let(:configured_git_config) { with_git_configured { `cat #{git_config_path}` } }
+    let(:configured_git_credentials) { with_git_configured { `cat #{Dir.pwd}/git.store` } }
+
     context "when the global .gitconfig has a safe directory" do
       before do
         Open3.capture2("git config --global --add safe.directory /home/dependabot/dependabot-core/repo")
@@ -749,10 +756,10 @@ RSpec.describe Dependabot::SharedHelpers do
     context "when the host has run out of disk space" do
       before do
         allow(File).to receive(:open)
-          .with(described_class::GIT_CONFIG_GLOBAL_PATH, anything)
+          .with(git_config_path, anything)
           .and_raise(Errno::ENOSPC)
         allow(FileUtils).to receive(:rm_f)
-          .with(described_class::GIT_CONFIG_GLOBAL_PATH)
+          .with(git_config_path)
       end
 
       specify { expect { configured_git_config }.to raise_error(Dependabot::OutOfDisk) }
