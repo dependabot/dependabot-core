02f47fd95be21ba1bf7c39b0309c195f61059573
Jamie Magee <jamagee@microsoft.com>
2024-03-05 21:20:17 -0800

    Strict type some of `nuget`

--- nuget/lib/dependabot/nuget/http_response_helpers.rb
+++ nuget/lib/dependabot/nuget/http_response_helpers.rb
@@ -1,9 +1,14 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
+
+require "sorbet-runtime"
 
 module Dependabot
   module Nuget
     module HttpResponseHelpers
+      extend T::Sig
+
+      sig { params(string: String).returns(String) }
       def self.remove_wrapping_zero_width_chars(string)
         string.force_encoding("UTF-8").encode
               .gsub(/\A[\u200B-\u200D\uFEFF]/, "")
