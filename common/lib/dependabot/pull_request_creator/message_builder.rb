ac5c876220e0cbca2d6d2139dd952b56996c7560
Nish Sinha <nishnha@github.com>
2024-03-07 14:57:49 -0500

    Fix the number of updated directories in a group update

--- common/lib/dependabot/pull_request_creator/message_builder.rb
+++ common/lib/dependabot/pull_request_creator/message_builder.rb
@@ -233,10 +233,17 @@ module Dependabot
 
       sig { returns(String) }
       def group_pr_name
+        directories_from_dependencies = dependencies.map { |dep| dep.metadata[:directory] }.to_set
+
+        directories_with_updates = source.directories&.filter do |directory|
+          directories_from_dependencies.include?(directory)
+        end
+
         updates = dependencies.map(&:name).uniq.count
 
         if source.directories
-          "bump the #{T.must(dependency_group).name} across #{T.must(source.directories).count} directories " \
+          "bump the #{T.must(dependency_group).name} across #{directories_with_updates.count} "\
+            "#{if directories_with_updates.count > 1 then 'directories' else 'directory' end} "\
             "with #{updates} update#{'s' if updates > 1}"
         else
           "bump the #{T.must(dependency_group).name} group#{pr_name_directory} with #{updates} update#{if updates > 1
