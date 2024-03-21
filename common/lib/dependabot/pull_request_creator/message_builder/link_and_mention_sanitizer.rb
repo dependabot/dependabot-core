365b555ad24fdd9a6f0ed4a33a6bf9273bbb135c
Gavin <46635587+blue2cat@users.noreply.github.com>
2024-03-21 05:57:49 +0000

    Attempt to fix CodeQL test

--- common/lib/dependabot/pull_request_creator/message_builder/link_and_mention_sanitizer.rb
+++ common/lib/dependabot/pull_request_creator/message_builder/link_and_mention_sanitizer.rb
@@ -5,6 +5,7 @@ require "commonmarker"
 require "sorbet-runtime"
 require "strscan"
 require "dependabot/pull_request_creator/message_builder"
+require "timeout"
 
 module Dependabot
   class PullRequestCreator
@@ -60,9 +61,17 @@ module Dependabot
           mode = unsafe ? :UNSAFE : :DEFAULT
 
           # If the text does not contain markdown, we need to use the HARDBREAKS option
-          commonmarker_render_options = (unless text.match?(MARKDOWN_REGEX)
-                                           COMMONMARKER_OPTIONS + [:HARDBREAKS]
-                                         end) || [COMMONMARKER_OPTIONS]
+          begin
+            commonmarker_render_options = Timeout.timeout(10) do
+              if text.match?(MARKDOWN_REGEX)
+                COMMONMARKER_OPTIONS
+              else
+                COMMONMARKER_OPTIONS + [:HARDBREAKS]
+              end
+            end
+          rescue Timeout::Error
+            commonmarker_render_options = [COMMONMARKER_OPTIONS]
+          end
 
           options = [mode] + commonmarker_render_options
           options = options.flatten.uniq
