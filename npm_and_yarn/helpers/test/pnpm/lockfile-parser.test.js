d3085f3cf77fe1fe259e800e767d30e650dcaaad
“Thavachelvam <thavaahariharangit@git.com>
2024-04-24 14:33:57 +0100

    #9562: removed lodash references, As that is not part of test scenario.

--- npm_and_yarn/helpers/test/pnpm/lockfile-parser.test.js
+++ npm_and_yarn/helpers/test/pnpm/lockfile-parser.test.js
@@ -25,7 +25,7 @@ describe("parseLockfile", () => {
         copyDependencies("no_lockfile_change", tempDir);
         const result = await parseLockfile(tempDir);
 
-        expect(result.length).toEqual(400);
+        expect(result.length).toEqual(398);
     })
 
     it("only dev dependency", async () =>{
