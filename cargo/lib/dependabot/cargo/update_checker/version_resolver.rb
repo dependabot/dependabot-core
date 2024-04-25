ac93a4398a44166ba69b79e50dcd78aee30b2703
Garry L Hurley Jr <garryhurleyjr@github.com>
2024-04-25 13:45:26 -0400

    PR #9585 Fixing missed message in error.

--- cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
+++ cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
@@ -31,8 +31,8 @@ module Dependabot
           return @latest_resolvable_version if defined?(@latest_resolvable_version)
 
           @latest_resolvable_version = fetch_latest_resolvable_version
-          rescue Dependabot::SharedHelpers::HelperSubprocessFailed 
-            raise Dependabot::DependencyFileNotResolvable
+          rescue Dependabot::SharedHelpers::HelperSubprocessFailed => e 
+            raise Dependabot::DependencyFileNotResolvable e.message
         end
 
         private
