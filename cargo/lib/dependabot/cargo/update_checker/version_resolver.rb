7ccba736fa44b0bc0fdf46c112b67afdf6adedef
Garry L Hurley Jr <garryhurleyjr@github.com>
2024-04-25 13:33:22 -0400

    PR #9585 Added rescue to rethrow proper exception required by test.

--- cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
+++ cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
@@ -31,6 +31,9 @@ module Dependabot
           return @latest_resolvable_version if defined?(@latest_resolvable_version)
 
           @latest_resolvable_version = fetch_latest_resolvable_version
+          rescue Dependabot::SharedHelpers::HelperSubprocessFailed 
+            raise Dependabot::DependencyFileNotResolvable
+          end
         end
 
         private
