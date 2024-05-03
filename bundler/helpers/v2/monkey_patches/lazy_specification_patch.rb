ca660ba506d34a9c38e032b412bd83b91f1ec97f
Ankit Honey <honeyankit@github.com>
2024-05-03 00:55:40 +0000

    fix the lint failure

--- bundler/helpers/v2/monkey_patches/lazy_specification_patch.rb
+++ bundler/helpers/v2/monkey_patches/lazy_specification_patch.rb
@@ -4,7 +4,7 @@
 # Adds default_gem? method to Bundler::LazySpecification
 # https://github.com/rubygems/rubygems/blob/bundler-v2.5.9/lib/rubygems/basic_specification.rb#L96-L99
 
-require 'bundler/lazy_specification'
+require "bundler/lazy_specification"
 
 # Check if the gem is a default gem
 # Ensure loaded_from is not nil and check if the gem is located in the default specifications directory
