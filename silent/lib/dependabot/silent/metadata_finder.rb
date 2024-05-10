fc191befa8940f5251d8d3c4a10a49aed3d14110
Jake Coffman <jake@jakecoffman.com>
2024-05-10 10:34:04 -0500

    fix exceptions from integration tests due to no metadata finders

---
+++ silent/lib/dependabot/silent/metadata_finder.rb
@@ -0,0 +1,30 @@
+# typed: true
+# frozen_string_literal: true
+
+require "dependabot/metadata_finders"
+require "dependabot/metadata_finders/base"
+
+module Dependabot
+  module Silent
+    class MetadataFinder < Dependabot::MetadataFinders::Base
+      extend T::Sig
+
+      def homepage_url
+        ""
+      end
+
+      private
+
+      def look_up_source
+        Dependabot::Source.new(
+          provider: "example",
+          hostname: "example.com",
+          api_endpoint: "https://example.com/api/v3",
+          repo: dependency.name,
+          directory: nil,
+          branch: nil
+        )
+      end
+    end
+  end
+end
