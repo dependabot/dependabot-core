796216dc12a2bd975903c4c4d84399e6a3f47194
“Thavachelvam <thavaahariharangit@git.com>
2024-05-14 13:57:18 +0100

    #6017: Address RSpec/ContextWording Rubocop violations.

--- bundler/helpers/v2/spec/ruby_version_spec.rb
+++ bundler/helpers/v2/spec/ruby_version_spec.rb
@@ -6,7 +6,7 @@ require "shared_contexts"
 
 RSpec.describe BundlerDefinitionRubyVersionPatch do
   include_context "when in a temporary bundler directory"
-  include_context "when stub rubygems compact index"
+  include_context "stub rubygems compact index"
 
   let(:project_name) { "ruby_version_implied" }
   before do
