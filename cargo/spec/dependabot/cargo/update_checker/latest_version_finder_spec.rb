c390fc2ad80e25b4600ad510e348d01791574a00
Ankit Honey <honeyankit@github.com>
2024-05-20 16:25:03 -0700

    Cargo implementation must support Cargo sparse index file format (#9783)
    * parge the multiple json object response and handle both default and sparse registry responses in available_versions

--- cargo/spec/dependabot/cargo/update_checker/latest_version_finder_spec.rb
+++ cargo/spec/dependabot/cargo/update_checker/latest_version_finder_spec.rb
@@ -7,13 +7,12 @@ require "dependabot/dependency_file"
 require "dependabot/cargo/update_checker/latest_version_finder"
 
 RSpec.describe Dependabot::Cargo::UpdateChecker::LatestVersionFinder do
-  before do
-    stub_request(:get, crates_url).to_return(status: 200, body: crates_response)
-  end
   let(:crates_url) { "https://crates.io/api/v1/crates/#{dependency_name}" }
   let(:crates_response) { fixture("crates_io_responses", crates_fixture_name) }
   let(:crates_fixture_name) { "#{dependency_name}.json" }
-
+  let(:ignored_versions) { [] }
+  let(:raise_on_ignored) { false }
+  let(:security_advisories) { [] }
   let(:finder) do
     described_class.new(
       dependency: dependency,
@@ -24,10 +23,6 @@ RSpec.describe Dependabot::Cargo::UpdateChecker::LatestVersionFinder do
       security_advisories: security_advisories
     )
   end
-
-  let(:ignored_versions) { [] }
-  let(:raise_on_ignored) { false }
-  let(:security_advisories) { [] }
   let(:credentials) do
     [{
       "type" => "git_source",
@@ -66,6 +61,10 @@ RSpec.describe Dependabot::Cargo::UpdateChecker::LatestVersionFinder do
 
   describe "#latest_version" do
     subject { finder.latest_version }
+    before do
+      stub_request(:get, crates_url).to_return(status: 200, body: crates_response)
+    end
+
     it { is_expected.to eq(Gem::Version.new("0.1.40")) }
 
     context "when the latest version is being ignored" do
@@ -177,6 +176,10 @@ RSpec.describe Dependabot::Cargo::UpdateChecker::LatestVersionFinder do
   describe "#lowest_security_fix_version" do
     subject { finder.lowest_security_fix_version }
 
+    before do
+      stub_request(:get, crates_url).to_return(status: 200, body: crates_response)
+    end
+
     let(:dependency_name) { "time" }
     let(:dependency_version) { "0.1.12" }
     let(:security_advisories) do
@@ -239,6 +242,194 @@ RSpec.describe Dependabot::Cargo::UpdateChecker::LatestVersionFinder do
             }]
           end
           it { is_expected.to eq(Gem::Version.new("2.0.0-pre3")) }
+        end
+      end
+    end
+  end
+
+  # Tests for sparse registry responses
+  describe "Sparse registry response handling" do
+    let(:sparse_registry_url) { "https://cargo.cloudsmith.io/honeyankit/test/he/ll/hello-world" }
+    let(:sparse_registry_response) { fixture("private_registry_responses", crates_fixture_name) }
+    let(:crates_fixture_name) { "#{dependency_name}.json" }
+
+    let(:credentials) do
+      [{
+        "type" => "cargo_registry",
+        "cargo_registry" => "honeyankit-test",
+        "url" => "https://cargo.cloudsmith.io/honeyankit/test/",
+        "token" => "token"
+      }]
+    end
+    let(:dependency_name) { "hello-world" }
+    let(:dependency_version) { "1.0.0" }
+    let(:requirements) do
+      [{
+        file: "Cargo.toml",
+        requirement: "1.0.0",
+        groups: ["dependencies"],
+        source: {
+          type: "registry",
+          name: "honeyankit-test",
+          index: "sparse+https://cargo.cloudsmith.io/honeyankit/test/",
+          dl: "https://dl.cloudsmith.io/basic/honeyankit/test/cargo/{crate}-{version}.crate",
+          api: "https://cargo.cloudsmith.io/honeyankit/test"
+        }
+      }]
+    end
+
+    describe "#latest_version" do
+      subject { finder.latest_version }
+      before do
+        stub_request(:get, sparse_registry_url).to_return(status: 200, body: sparse_registry_response)
+      end
+
+      it { is_expected.to eq(Gem::Version.new("1.0.1")) }
+
+      context "when the latest version is being ignored" do
+        let(:ignored_versions) { [">= 1.0.1, < 2.0"] }
+        it { is_expected.to eq(Gem::Version.new("1.0.0")) }
+      end
+
+      context "when the sparse registry link resolves to a 'Not Found' page" do
+        before do
+          stub_request(:get, sparse_registry_url)
+            .to_return(status: 404, body: sparse_registry_response)
+        end
+        let(:crates_fixture_name) { "not_found.json" }
+
+        it { is_expected.to be_nil }
+      end
+
+      context "when the latest version is a pre-release" do
+        let(:sparse_registry_response) do
+          <<~BODY
+            {"name": "hello-world", "vers": "1.0.0", "deps": [], "cksum": "b2c263921f1114820f4acc6b542d72bbc859ce7023c5b235346b157074dcccc7", "features": {}, "yanked": false, "links": null}
+            {"name": "hello-world", "vers": "2.0.0-pre1", "deps": [], "cksum": "8a55b58def1ecc7aa8590c7078f379ec9a85328363ffb81d4354314b132b95c4", "features": {}, "yanked": false, "links": null}
+          BODY
+        end
+        it { is_expected.to eq(Gem::Version.new("1.0.0")) }
+
+        context "with the user wants a pre-release" do
+          let(:requirements) do
+            [{
+              file: "Cargo.toml",
+              requirement: "~2.0.0-pre1",
+              groups: ["dependencies"],
+              source: {
+                type: "registry",
+                name: "honeyankit-test",
+                index: "sparse+https://cargo.cloudsmith.io/honeyankit/test/",
+                dl: "https://dl.cloudsmith.io/basic/honeyankit/test/cargo/{crate}-{version}.crate",
+                api: "https://cargo.cloudsmith.io/honeyankit/test"
+              }
+            }]
+          end
+          it { is_expected.to eq(Gem::Version.new("2.0.0-pre1")) }
+        end
+      end
+
+      context "when already on the latest version" do
+        let(:dependency_version) { "1.0.1" }
+        it { is_expected.to eq(Gem::Version.new("1.0.1")) }
+      end
+
+      context "when all later versions are being ignored" do
+        let(:ignored_versions) { ["> 1.0.0"] }
+        it { is_expected.to eq(Gem::Version.new("1.0.0")) }
+
+        context "with raise_on_ignored" do
+          let(:raise_on_ignored) { true }
+          it "raises an error" do
+            expect { subject }.to raise_error(Dependabot::AllVersionsIgnored)
+          end
+        end
+      end
+    end
+
+    describe "#lowest_security_fix_version" do
+      subject { finder.lowest_security_fix_version }
+
+      before do
+        stub_request(:get, sparse_registry_url).to_return(status: 200, body: sparse_registry_response)
+      end
+
+      let(:dependency_name) { "hello-world" }
+      let(:dependency_version) { "1.0.0" }
+      let(:security_advisories) do
+        [
+          Dependabot::SecurityAdvisory.new(
+            dependency_name: dependency_name,
+            package_manager: "cargo",
+            vulnerable_versions: ["<= 1.0.0"]
+          )
+        ]
+      end
+      it { is_expected.to eq(Gem::Version.new("1.0.1")) }
+
+      context "when the lowest version is being ignored" do
+        let(:ignored_versions) { [">= 1.0.0, < 1.0.1"] }
+        it { is_expected.to eq(Gem::Version.new("1.0.1")) }
+      end
+
+      context "when all versions are being ignored" do
+        let(:ignored_versions) { [">= 0"] }
+        it "returns nil" do
+          expect(subject).to be_nil
+        end
+
+        context "with raise_on_ignored" do
+          let(:raise_on_ignored) { true }
+          it "raises an error" do
+            expect { subject }.to raise_error(Dependabot::AllVersionsIgnored)
+          end
+        end
+      end
+
+      context "when the lowest fixed version is a pre-release" do
+        let(:sparse_registry_response) do
+          <<~BODY
+            {"name": "hello-world", "vers": "1.0.0", "deps": [], "cksum": "b2c263921f1114820f4acc6b542d72bbc859ce7023c5b235346b157074dcccc7", "features": {}, "yanked": false, "links": null}
+            {"name": "hello-world", "vers": "2.0.0", "deps": [], "cksum": "b2c263921f1114820f4acc6b542d72bbc859ce7023c5b235346b157074dcccc8", "features": {}, "yanked": false, "links": null}
+            {"name": "hello-world", "vers": "2.0.0-pre1", "deps": [], "cksum": "8a55b58def1ecc7aa8590c7078f379ec9a85328363ffb81d4354314b132b95c4", "features": {}, "yanked": false, "links": null}
+            {"name": "hello-world", "vers": "2.0.0-pre2", "deps": [], "cksum": "8a55b58def1ecc7aa8590c7078f379ec9a85328363ffb81d4354314b132b95f6", "features": {}, "yanked": false, "links": null}
+            {"name": "hello-world", "vers": "2.0.0-pre3", "deps": [], "cksum": "8a55b58def1ecc7aa8590c7078f379ec9a85328363ffb81d4354314b132b95d6", "features": {}, "yanked": false, "links": null}
+          BODY
+        end
+        let(:security_advisories) do
+          [
+            Dependabot::SecurityAdvisory.new(
+              dependency_name: dependency_name,
+              package_manager: "cargo",
+              vulnerable_versions: ["<= 2.0.0-pre2"]
+            )
+          ]
+        end
+        it { is_expected.to eq(Gem::Version.new("2.0.0")) }
+
+        context "with the user wants a pre-release" do
+          context "when their current version is a pre-release" do
+            let(:dependency_version) { "2.0.0-pre1" }
+            it { is_expected.to eq(Gem::Version.new("2.0.0-pre3")) }
+          end
+
+          context "when their requirements say they want pre-releases" do
+            let(:requirements) do
+              [{
+                file: "Cargo.toml",
+                requirement: "~2.0.0-pre1",
+                groups: ["dependencies"],
+                source: {
+                  type: "registry",
+                  name: "honeyankit-test",
+                  index: "sparse+https://cargo.cloudsmith.io/honeyankit/test/",
+                  dl: "https://dl.cloudsmith.io/basic/honeyankit/test/cargo/{crate}-{version}.crate",
+                  api: "https://cargo.cloudsmith.io/honeyankit/test"
+                }
+              }]
+            end
+            it { is_expected.to eq(Gem::Version.new("2.0.0-pre3")) }
+          end
         end
       end
     end
