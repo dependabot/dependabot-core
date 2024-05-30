50467a194919724b74e9ae2bdaffbf449462c8d6
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-05-30 17:20:57 -0400

    raj-meka/Strict-type-Dependabot::Docker::Version:sorbet fixes

--- docker/lib/dependabot/docker/version.rb
+++ docker/lib/dependabot/docker/version.rb
@@ -19,52 +19,60 @@ module Dependabot
       # A docker image cannot be longer than 255 characters anyways.
       DOCKER_VERSION_REGEX = /^(?<prefix>[a-z._\-]{0,255})[_\-v]?(?<version>.{1,255})$/
 
-      sig { params(version: String).void }
+      sig { override.params(version: T.nilable(T.any(String, Integer, Gem::Version))).void }
       def initialize(version)
-        parsed_version = version.match(DOCKER_VERSION_REGEX)
-        release_part, update_part = parsed_version[:version].split("_", 2)
+        parsed_version = version.to_s.match(DOCKER_VERSION_REGEX)
+        release_part, update_part = T.must(T.must(parsed_version)[:version]).split("_", 2)
 
         # The numeric_version is needed here to validate the version string (ex: 20.9.0-alpine3.18)
         # when the call is made via Depenedabot Api to convert the image version to semver.
-        release_part = Tag.new(release_part.chomp(".").chomp("-").chomp("_")).numeric_version
+        release_part = Tag.new(T.must(release_part).chomp(".").chomp("-").chomp("_")).numeric_version
 
-        @release_part = Dependabot::Version.new(release_part.tr("-", "."))
-        @update_part = Dependabot::Version.new(update_part&.start_with?(/[0-9]/) ? update_part : 0)
+        @release_part = T.let(Dependabot::Version.new(release_part.tr("-", ".")), Dependabot::Version)
+        @update_part = T.let(
+          Dependabot::Version.new(update_part&.start_with?(/[0-9]/) ? update_part : 0),
+          Dependabot::Version
+        )
 
         super(@release_part)
       end
 
-      sig { params(version: String).returns(T::Boolean) }
+      sig { override.params(version: T.nilable(T.any(String, Integer, Gem::Version))).returns(T::Boolean) }
       def self.correct?(version)
         return true if version.is_a?(Gem::Version)
 
         # We can't call new here because Gem::Version calls self.correct? in its initialize method
         # causing an infinite loop, so instead we check if the release_part of the version is correct
-        parsed_version = version.match(DOCKER_VERSION_REGEX)
+        parsed_version = version.to_s.match(DOCKER_VERSION_REGEX)
         return false if parsed_version.nil?
 
-        release_part, = parsed_version[:version].split("_", 2)
-        release_part = Tag.new(release_part.chomp(".").chomp("-").chomp("_")).numeric_version || parsed_version
+        release_part, = T.must(parsed_version[:version]).split("_", 2)
+        release_part = Tag.new(T.must(release_part).chomp(".").chomp("-").chomp("_")).numeric_version || parsed_version
         super(release_part.to_s)
       rescue ArgumentError
         # if we can't instantiate a version, it can't be correct
         false
       end
 
+      sig { override.returns(String) }
       def to_semver
         @release_part.to_semver
       end
 
+      sig { returns(T::Array[String]) }
       def segments
         @release_part.segments
       end
 
+      sig { returns(Dependabot::Version) }
       attr_reader :release_part
 
+      sig { params(other: Dependabot::Docker::Version).returns(T.nilable(Integer)) }
       def <=>(other)
         sort_criteria <=> other.sort_criteria
       end
 
+      sig { returns(T::Array[Dependabot::Version]) }
       def sort_criteria
         [@release_part, @update_part]
       end
