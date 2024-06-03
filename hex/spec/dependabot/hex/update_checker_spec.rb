5746c8d397f9cc74244acca27a6845f2f82f7240
“Thavachelvam <thavaahariharangit@git.com>
2024-06-03 12:16:30 +0100

    TICtxtWrd-hex: Lint error fixes.

--- hex/spec/dependabot/hex/update_checker_spec.rb
+++ hex/spec/dependabot/hex/update_checker_spec.rb
@@ -477,8 +477,6 @@ RSpec.describe Dependabot::Hex::UpdateChecker do
         describe "Skipping the failing test", pending: "https://github.com/dependabot/dependabot-core/issues/9884" do
           it { is_expected.to eq(Dependabot::Hex::Version.new("1.1.0")) }
         end
-
-
       end
     end
 
