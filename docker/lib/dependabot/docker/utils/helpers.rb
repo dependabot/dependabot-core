1cef24efcc76737954b622438dc7589a3a6d035f
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-06-04 16:13:27 -0400

    Strict type Dependabot::Docker::Requirement::CredentialsFinder.

---
+++ docker/lib/dependabot/docker/utils/helpers.rb
@@ -0,0 +1,19 @@
+# typed: strong
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+
+module Dependabot
+  module Docker
+    module Utils
+      HELM_REGEXP = /values[\-a-zA-Z_0-9]*\.ya?ml$/i
+
+      extend T::Sig
+
+      sig { params(file: Dependabot::DependencyFile).returns(T::Boolean) }
+      def self.likely_helm_chart?(file)
+        file.name.match?(HELM_REGEXP)
+      end
+    end
+  end
+end
