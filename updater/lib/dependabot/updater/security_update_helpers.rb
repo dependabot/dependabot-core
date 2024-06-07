b63ddec810d4e23225eb7f0546fcbc7d290eef2d
Jamie Magee <jamagee@microsoft.com>
2024-06-06 21:37:43 -0700

    Strict type `Dependabot::Updater::SecurityUpdateHelpers`

--- updater/lib/dependabot/updater/security_update_helpers.rb
+++ updater/lib/dependabot/updater/security_update_helpers.rb
@@ -17,6 +17,7 @@ module Dependabot
       sig { returns(Dependabot::Service) }
       attr_reader :service
 
+      sig { params(dependency: Dependabot::Dependency).void }
       def record_security_update_not_needed_error(dependency)
         Dependabot.logger.info(
           "no security update needed as #{dependency.name} " \
@@ -31,6 +32,7 @@ module Dependabot
         )
       end
 
+      sig { params(checker: Dependabot::UpdateCheckers::Base).void }
       def record_security_update_ignored(checker)
         Dependabot.logger.info(
           "Dependabot cannot update to the required version as all versions " \
@@ -45,6 +47,7 @@ module Dependabot
         )
       end
 
+      sig { params(checker: Dependabot::UpdateCheckers::Base).void }
       def record_dependency_file_not_supported_error(checker)
         Dependabot.logger.info(
           "Dependabot can't update vulnerable dependencies for projects " \
@@ -60,16 +63,17 @@ module Dependabot
         )
       end
 
+      sig { params(checker: Dependabot::UpdateCheckers::Base).void }
       def record_security_update_not_possible_error(checker)
         latest_allowed_version =
           (checker.lowest_resolvable_security_fix_version ||
            checker.dependency.version)&.to_s
         lowest_non_vulnerable_version =
-          checker.lowest_security_fix_version&.to_s
+          checker.lowest_security_fix_version.to_s
         conflicting_dependencies = checker.conflicting_dependencies
 
         Dependabot.logger.info(
-          security_update_not_possible_message(checker, latest_allowed_version, conflicting_dependencies)
+          security_update_not_possible_message(checker, T.must(latest_allowed_version), conflicting_dependencies)
         )
         Dependabot.logger.info(
           earliest_fixed_version_message(lowest_non_vulnerable_version)
@@ -86,6 +90,7 @@ module Dependabot
         )
       end
 
+      sig { params(checker: Dependabot::UpdateCheckers::Base).void }
       def record_security_update_not_found(checker)
         Dependabot.logger.info(
           "Dependabot can't find a published or compatible non-vulnerable " \
@@ -103,6 +108,7 @@ module Dependabot
         )
       end
 
+      sig { params(checker: Dependabot::UpdateCheckers::Base).void }
       def record_pull_request_exists_for_latest_version(checker)
         service.record_update_job_error(
           error_type: "pull_request_exists_for_latest_version",
@@ -114,6 +120,7 @@ module Dependabot
         )
       end
 
+      sig { params(existing_pull_request: T::Array[T::Hash[String, String]]).void }
       def record_pull_request_exists_for_security_update(existing_pull_request)
         updated_dependencies = existing_pull_request.map do |dep|
           {
@@ -131,6 +138,7 @@ module Dependabot
         )
       end
 
+      sig { void }
       def record_security_update_dependency_not_found
         service.record_update_job_error(
           error_type: "security_update_dependency_not_found",
@@ -138,6 +146,7 @@ module Dependabot
         )
       end
 
+      sig { params(lowest_non_vulnerable_version: T.nilable(String)).returns(String) }
       def earliest_fixed_version_message(lowest_non_vulnerable_version)
         if lowest_non_vulnerable_version
           "The earliest fixed version is #{lowest_non_vulnerable_version}."
@@ -146,8 +155,15 @@ module Dependabot
         end
       end
 
-      def security_update_not_possible_message(checker, latest_allowed_version,
-                                               conflicting_dependencies)
+      sig do
+        params(
+          checker: Dependabot::UpdateCheckers::Base,
+          latest_allowed_version: String,
+          conflicting_dependencies: T::Array[T::Hash[String, String]]
+        )
+          .returns(String)
+      end
+      def security_update_not_possible_message(checker, latest_allowed_version, conflicting_dependencies)
         if conflicting_dependencies.any?
           dep_messages = conflicting_dependencies.map do |dep|
             "  #{dep['explanation']}"
