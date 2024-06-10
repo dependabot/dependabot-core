8f1c6c341d547fcb620dd292fb174b9ba5a55971
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-06-10 18:08:35 -0400

    Strict type Dependabot::Pub::FileUpdater.

--- pub/lib/dependabot/pub/file_updater.rb
+++ pub/lib/dependabot/pub/file_updater.rb
@@ -1,15 +1,19 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "dependabot/file_updaters"
 require "dependabot/file_updaters/base"
 require "dependabot/pub/helpers"
+require "sorbet-runtime"
 
 module Dependabot
   module Pub
     class FileUpdater < Dependabot::FileUpdaters::Base
+      extend T::Sig
+
       include Dependabot::Pub::Helpers
 
+      sig { override.returns(T::Array[Regexp]) }
       def self.updated_files_regex
         [
           /^pubspec\.yaml$/,
@@ -17,12 +21,14 @@ module Dependabot
         ]
       end
 
+      sig { override.returns(T::Array[DependencyFile]) }
       def updated_dependency_files
         dependency_services_apply(@dependencies)
       end
 
       private
 
+      sig { override.void }
       def check_required_files
         raise "No pubspec.yaml!" unless get_original_file("pubspec.yaml")
       end
