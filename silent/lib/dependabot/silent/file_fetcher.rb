fceb37bc15cf78cb566c643ef5e962abd6c7117d
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-06-11 08:17:51 -0400

    Strict type Dependabot::Silent::FileFetcher.

--- silent/lib/dependabot/silent/file_fetcher.rb
+++ silent/lib/dependabot/silent/file_fetcher.rb
@@ -1,17 +1,22 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "dependabot/file_fetchers"
 require "dependabot/file_fetchers/base"
+require "sorbet-runtime"
 
 module SilentPackageManager
   class FileFetcher < Dependabot::FileFetchers::Base
+    extend T::Sig
+
+    sig { override.returns(T::Array[Dependabot::DependencyFile]) }
     def fetch_files
       [manifest].compact
     end
 
     private
 
+    sig { returns(T.nilable(Dependabot::DependencyFile)) }
     def manifest
       fetch_file_if_present("manifest.json")
     end
