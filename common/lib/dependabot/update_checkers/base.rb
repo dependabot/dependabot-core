f4049e340b4aab11720703b84a19b0478c1e3da2
AbdulFattaah Popoola <abdulapopoola@github.com>
2024-06-11 14:12:09 -0700

    Address Sorbet error

--- common/lib/dependabot/update_checkers/base.rb
+++ common/lib/dependabot/update_checkers/base.rb
@@ -136,7 +136,7 @@ module Dependabot
 
       # Lowest available security fix version not checking resolvability
       # @return [Dependabot::<package manager>::Version, #to_s] version class
-      sig { overridable.returns(Dependabot::Version) }
+      sig { overridable.returns(T.nilable(Dependabot::Version)) }
       def lowest_security_fix_version
         raise NotImplementedError, "#{self.class} must implement #lowest_security_fix_version"
       end
