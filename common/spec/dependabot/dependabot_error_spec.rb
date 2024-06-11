ef883dc1d9fa590af0485c156adb8a014d8ce107
garryhurleyjr <garryhurleyjr@github.com>
2024-06-11 13:02:11 -0400

    Updating dependabot_error_spec.rb to satisfy code review comments

--- common/spec/dependabot/dependabot_error_spec.rb
+++ common/spec/dependabot/dependabot_error_spec.rb
@@ -5,82 +5,81 @@ require "spec_helper"
 require "dependabot/errors"
 
 RSpec.describe Dependabot::DependabotError do
-  describe Dependabot::DependabotError do
-    let(:error) { described_class.new(message) }
-    let(:message) do
-      "some error"
+  let(:error) { described_class.new(message) }
+  let(:message) do
+    "some error"
+  end
+
+  describe "#message" do
+    subject { error.message }
+
+    let(:tmp) { Dependabot::Utils::BUMP_TMP_DIR_PATH }
+
+    it { is_expected.to eq("some error") }
+
+    context "with dependabot temp path" do
+      let(:message) do
+        "#{tmp}/dependabot_20201218-14100-y0d218/path error"
+      end
+
+      it { is_expected.to eq("dependabot_tmp_dir/path error") }
     end
 
-    describe "#message" do
-      subject { error.message }
-
-      it { is_expected.to eq("some error") }
-
-      let(:tmp) { Dependabot::Utils::BUMP_TMP_DIR_PATH }
-      context "with dependabot temp path" do
-        let(:message) do
-          "#{tmp}/dependabot_20201218-14100-y0d218/path error"
-        end
-
-        it { is_expected.to eq("dependabot_tmp_dir/path error") }
+    context "with dependabot temp path" do
+      let(:message) do
+        "Error (/Users/x/code/dependabot-core/cargo/#{tmp}/dependabot_20201218-14100-y0d218) " \
+          "failed to load https://github.com/dependabot"
       end
 
-      context "with dependabot temp path" do
-        let(:message) do
-          "Error (/Users/x/code/dependabot-core/cargo/#{tmp}/dependabot_20201218-14100-y0d218) " \
-            "failed to load https://github.com/dependabot"
-        end
+      it do
+        expect(subject).to eq(
+          "Error (/Users/x/code/dependabot-core/cargo/dependabot_tmp_dir) " \
+          "failed to load https://github.com/dependabot"
+        )
+      end
+    end
 
-        it do
-          is_expected.to eq(
-            "Error (/Users/x/code/dependabot-core/cargo/dependabot_tmp_dir) " \
-            "failed to load https://github.com/dependabot"
-          )
-        end
+    context "without http basic auth token" do
+      let(:message) do
+        "git://user@github.com error"
       end
 
-      context "without http basic auth token" do
-        let(:message) do
-          "git://user@github.com error"
-        end
+      it { is_expected.to eq("git://user@github.com error") }
+    end
 
-        it { is_expected.to eq("git://user@github.com error") }
+    context "with http basic auth missing username" do
+      let(:message) do
+        "git://:token@github.com error"
       end
 
-      context "with http basic auth missing username" do
-        let(:message) do
-          "git://:token@github.com error"
-        end
+      it { is_expected.to eq("git://github.com error") }
+    end
 
-        it { is_expected.to eq("git://github.com error") }
+    context "with http basic auth" do
+      let(:message) do
+        "git://user:token@github.com error"
       end
 
-      context "with http basic auth" do
-        let(:message) do
-          "git://user:token@github.com error"
-        end
+      it { is_expected.to eq("git://github.com error") }
+    end
 
-        it { is_expected.to eq("git://github.com error") }
+    context "with escaped basic auth uri" do
+      let(:message) do
+        "git://user:token%40github.com error"
       end
 
-      context "with escaped basic auth uri" do
-        let(:message) do
-          "git://user:token%40github.com error"
-        end
+      it { is_expected.to eq("git://github.com error") }
+    end
 
-        it { is_expected.to eq("git://github.com error") }
+    context "with multiple uri's include @ in their fragment, but no auth" do
+      let(:message) do
+        <<~MESSAGE.strip
+          https://github.com/EspressoSystems/tide-disco.git#tide-disco@0.4.1
+          https://github.com/EspressoSystems/tide-disco.git#tide-disco@0.4.1
+        MESSAGE
       end
 
-      context "with multiple uri's include @ in their fragment, but no auth" do
-        let(:message) do
-          <<~MESSAGE.strip
-            https://github.com/EspressoSystems/tide-disco.git#tide-disco@0.4.1
-            https://github.com/EspressoSystems/tide-disco.git#tide-disco@0.4.1
-          MESSAGE
-        end
-
-        it { is_expected.to eq(message) }
-      end
+      it { is_expected.to eq(message) }
     end
   end
 
@@ -90,25 +89,30 @@ RSpec.describe Dependabot::DependabotError do
 
     describe "#file_name" do
       subject { error.file_name }
+
       it { is_expected.to eq("Gemfile") }
     end
 
     describe "#directory" do
       subject { error.directory }
+
       it { is_expected.to eq("/path/to") }
 
       context "with the root directory" do
         let(:file_path) { "Gemfile" }
+
         it { is_expected.to eq("/") }
       end
 
       context "with a root level file whose path starts with a slash" do
         let(:file_path) { "/Gemfile" }
+
         it { is_expected.to eq("/") }
       end
 
       context "with a nested file whose path starts with a slash" do
         let(:file_path) { "/path/to/Gemfile" }
+
         it { is_expected.to eq("/path/to") }
       end
     end
@@ -122,7 +126,7 @@ RSpec.describe Dependabot::DependabotError do
       subject { error.message }
 
       it do
-        is_expected.to eq(
+        expect(subject).to eq(
           "The following source could not be reached as it requires authentication (and any provided details were " \
           "invalid or lacked the required permissions): source"
         )
@@ -134,7 +138,7 @@ RSpec.describe Dependabot::DependabotError do
         end
 
         it do
-          is_expected.to eq(
+          expect(subject).to eq(
             "The following source could not be reached as it requires authentication (and any provided details were " \
             "invalid or lacked the required permissions): npm.fury.io/<redacted>"
           )
@@ -151,7 +155,7 @@ RSpec.describe Dependabot::DependabotError do
       subject { error.message }
 
       it do
-        is_expected.to eq(
+        expect(subject).to eq(
           "The following source timed out: source"
         )
       end
@@ -162,7 +166,7 @@ RSpec.describe Dependabot::DependabotError do
         end
 
         it do
-          is_expected.to eq(
+          expect(subject).to eq(
             "The following source timed out: npm.fury.io/<redacted>"
           )
         end
@@ -178,7 +182,7 @@ RSpec.describe Dependabot::DependabotError do
       subject { error.message }
 
       it do
-        is_expected.to eq(
+        expect(subject).to eq(
           "Could not verify the SSL certificate for source"
         )
       end
@@ -189,7 +193,7 @@ RSpec.describe Dependabot::DependabotError do
         end
 
         it do
-          is_expected.to eq(
+          expect(subject).to eq(
             "Could not verify the SSL certificate for npm.fury.io/<redacted>"
           )
         end
@@ -207,7 +211,7 @@ RSpec.describe Dependabot::DependabotError do
       subject { error.message }
 
       it do
-        is_expected.to eq(
+        expect(subject).to eq(
           "The following git URLs could not be retrieved: " \
           "https://bitbucket.org/gocardless/"
         )
