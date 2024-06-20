d6b271bc9bca79c273bc855e7617b794a4493878
Duncan Smith <duncan.smith@freeagent.com>
2024-06-20 10:45:54 +0100

    Update Ruby version for Dependabot itself

--- bundler/helpers/v1/monkey_patches/definition_ruby_version_patch.rb
+++ bundler/helpers/v1/monkey_patches/definition_ruby_version_patch.rb
@@ -12,7 +12,7 @@ module BundlerDefinitionRubyVersionPatch
           Gem::Specification.new("ruby\0", requested_version)
       end
 
-      %w(2.5.3p105 2.6.10p210 2.7.6p219 3.0.7p220 3.1.5p252 3.2.4p170).each do |version|
+      %w(2.5.3p105 2.6.10p210 2.7.6p219 3.0.7p220 3.1.5p252 3.2.4p170 3.3.2p78).each do |version|
         sources.metadata_source.specs << Gem::Specification.new("ruby\0", version)
       end
     end
