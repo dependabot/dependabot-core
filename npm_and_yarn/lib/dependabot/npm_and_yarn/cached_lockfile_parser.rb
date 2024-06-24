7cd0d646543fd567c9abc94eb7f3cf17b9a2164c
Ankit Kumar Honey <honeyankit@github.com>
2024-06-24 16:36:43 -0700

    fixed all the lint errors

--- npm_and_yarn/lib/dependabot/npm_and_yarn/cached_lockfile_parser.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/cached_lockfile_parser.rb
@@ -11,14 +11,19 @@ module Dependabot
     class CachedLockfileParser
       extend T::Sig
 
-      sig { params(dependency_files: T::Array[Dependabot::DependencyFile]).returns(Dependabot::NpmAndYarn::FileParser::LockfileParser) }
+      sig do
+        params(dependency_files: T::Array[Dependabot::DependencyFile]).returns(
+          Dependabot::NpmAndYarn::FileParser::LockfileParser
+        )
+      end
       def self.parse(dependency_files:)
         lockfile_content = dependency_files.map(&:content).join
         lockfile_hash = Digest::SHA2.hexdigest(lockfile_content)
         @cache ||= T.let({}, T.nilable(T::Hash[String, Dependabot::NpmAndYarn::FileParser::LockfileParser]))
         return T.must(@cache[lockfile_hash]) if @cache.key?(lockfile_hash)
 
-        @cache[lockfile_hash] = Dependabot::NpmAndYarn::FileParser::LockfileParser.new(dependency_files: dependency_files)
+        @cache[lockfile_hash] =
+          Dependabot::NpmAndYarn::FileParser::LockfileParser.new(dependency_files: dependency_files)
       end
     end
   end
