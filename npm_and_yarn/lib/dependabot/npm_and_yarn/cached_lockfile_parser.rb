e4785f842dabc6355f5afc0b605045ea80723717
Ankit Kumar Honey <honeyankit@github.com>
2024-06-24 10:59:58 -0700

    Missed so adding cached_lockfile_parser file

---
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/cached_lockfile_parser.rb
@@ -0,0 +1,25 @@
+# typed: strong
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require "digest"
+require "digest/sha2"
+require "dependabot/npm_and_yarn/file_parser/lockfile_parser"
+
+module Dependabot
+  module NpmAndYarn
+    class CachedLockfileParser
+      extend T::Sig
+
+      sig { params(dependency_files: T::Array[Dependabot::DependencyFile]).returns(Dependabot::NpmAndYarn::FileParser::LockfileParser) }
+      def self.parse(dependency_files:)
+        lockfile_content = dependency_files.map(&:content).join
+        lockfile_hash = Digest::SHA2.hexdigest(lockfile_content)
+        @cache ||= T.let({}, T.nilable(T::Hash[String, Dependabot::NpmAndYarn::FileParser::LockfileParser]))
+        return T.must(@cache[lockfile_hash]) if @cache.key?(lockfile_hash)
+
+        @cache[lockfile_hash] = Dependabot::NpmAndYarn::FileParser::LockfileParser.new(dependency_files: dependency_files)
+      end
+    end
+  end
+end
