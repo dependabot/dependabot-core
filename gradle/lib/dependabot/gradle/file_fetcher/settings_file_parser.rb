dff860afa1875175d6f31bb2f2ff0fba467bf5c8
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-06-25 20:03:51 -0400

    Strict type Dependabot::Gradle::SettingsFileParser.

--- gradle/lib/dependabot/gradle/file_fetcher/settings_file_parser.rb
+++ gradle/lib/dependabot/gradle/file_fetcher/settings_file_parser.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "sorbet-runtime"
@@ -11,23 +11,26 @@ module Dependabot
       class SettingsFileParser
         extend T::Sig
 
+        sig { params(settings_file: Dependabot::DependencyFile).void }
         def initialize(settings_file:)
           @settings_file = settings_file
         end
 
+        sig { returns(T::Array[String]) }
         def included_build_paths
           paths = []
-          comment_free_content.scan(function_regex("includeBuild")) do
+          comment_free_content&.scan(function_regex("includeBuild")) do
             arg = T.must(Regexp.last_match).named_captures.fetch("args")
             paths << T.must(arg).gsub(/["']/, "").strip
           end
           paths.uniq
         end
 
+        sig { returns(T::Array[T.nilable(String)]) }
         def subproject_paths
           subprojects = T.let([], T::Array[String])
 
-          comment_free_content.scan(function_regex("include")) do
+          comment_free_content&.scan(function_regex("include")) do
             args = T.must(Regexp.last_match).named_captures.fetch("args")
             args = T.must(args).split(",")
             args = args.filter_map { |p| p.gsub(/["']/, "").strip }
@@ -37,9 +40,9 @@ module Dependabot
           subprojects = subprojects.uniq
 
           subproject_dirs = subprojects.map do |proj|
-            if comment_free_content.match?(project_dir_regex(proj))
-              comment_free_content.match(project_dir_regex(proj))
-                                  .named_captures.fetch("path").sub(%r{^/}, "")
+            if comment_free_content&.match?(project_dir_regex(proj))
+              comment_free_content&.match(project_dir_regex(proj))
+                                  &.named_captures&.fetch("path")&.sub(%r{^/}, "")
             else
               proj.tr(":", "/").sub(%r{^/}, "")
             end
@@ -50,14 +53,17 @@ module Dependabot
 
         private
 
+        sig { returns(Dependabot::DependencyFile) }
         attr_reader :settings_file
 
+        sig { returns(T.nilable(String)) }
         def comment_free_content
           settings_file.content
-                       .gsub(%r{(?<=^|\s)//.*$}, "\n")
-                       .gsub(%r{(?<=^|\s)/\*.*?\*/}m, "")
+                       &.gsub(%r{(?<=^|\s)//.*$}, "\n")
+                       &.gsub(%r{(?<=^|\s)/\*.*?\*/}m, "")
         end
 
+        sig { params(function_name: T.any(String, Symbol)).returns(Regexp) }
         def function_regex(function_name)
           /
             (?:^|\s)#{Regexp.quote(function_name)}(?:\s*\(|\s)
@@ -65,6 +71,7 @@ module Dependabot
           /mx
         end
 
+        sig { params(proj: String).returns(Regexp) }
         def project_dir_regex(proj)
           prefixed_proj = Regexp.quote(":#{proj.gsub(/^:/, '')}")
           /['"]#{prefixed_proj}['"].*dir\s*=.*['"](?<path>.*?)['"]/i
