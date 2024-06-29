d6d9248d469ddf1ea3a211f5a83d7fd24ed21979
Jamie Magee <jamagee@microsoft.com>
2024-06-28 21:50:08 -0700

    Strict type `go_modules`

--- go_modules/lib/dependabot/go_modules/path_converter.rb
+++ go_modules/lib/dependabot/go_modules/path_converter.rb
@@ -1,19 +1,32 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
+
+require "sorbet-runtime"
 
 require "dependabot/go_modules/native_helpers"
 
 module Dependabot
   module GoModules
     module PathConverter
+      extend T::Sig
+
+      sig do
+        params(path: T.untyped)
+          .returns(
+            T.nilable(String)
+          )
+      end
       def self.git_url_for_path(path)
         # Save a query by manually converting golang.org/x names
         import_path = path.gsub(%r{^golang\.org/x}, "github.com/golang")
 
-        SharedHelpers.run_helper_subprocess(
-          command: NativeHelpers.helper_path,
-          function: "getVcsRemoteForImport",
-          args: { import: import_path }
+        T.cast(
+          SharedHelpers.run_helper_subprocess(
+            command: NativeHelpers.helper_path,
+            function: "getVcsRemoteForImport",
+            args: { import: import_path }
+          ),
+          T.nilable(String)
         )
       end
     end
