6877d26db4615aa630c1b0492d29ff52c4274cf5
raj-meka <166544023+raj-meka@users.noreply.github.com>
2024-07-01 23:17:54 -0400

    Strict type Dependabot::Pub::FileUpdater.

--- python/lib/dependabot/python/file_updater.rb
+++ python/lib/dependabot/python/file_updater.rb
@@ -1,19 +1,23 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "toml-rb"
 require "dependabot/file_updaters"
 require "dependabot/file_updaters/base"
 require "dependabot/shared_helpers"
+require "sorbet-runtime"
 
 module Dependabot
   module Python
     class FileUpdater < Dependabot::FileUpdaters::Base
+      extend T::Sig
+
       require_relative "file_updater/pipfile_file_updater"
       require_relative "file_updater/pip_compile_file_updater"
       require_relative "file_updater/poetry_file_updater"
       require_relative "file_updater/requirement_file_updater"
 
+      sig { override.returns(T::Array[Regexp]) }
       def self.updated_files_regex
         [
           /^Pipfile$/,
@@ -27,6 +31,7 @@ module Dependabot
         ]
       end
 
+      sig { override.returns(T::Array[DependencyFile]) }
       def updated_dependency_files
         updated_files =
           case resolver_type
@@ -48,6 +53,8 @@ module Dependabot
       private
 
       # rubocop:disable Metrics/PerceivedComplexity
+
+      sig { returns(Symbol) }
       def resolver_type
         reqs = dependencies.flat_map(&:requirements)
         changed_reqs = reqs.zip(dependencies.flat_map(&:previous_requirements))
@@ -76,6 +83,7 @@ module Dependabot
       end
       # rubocop:enable Metrics/PerceivedComplexity
 
+      sig { returns(Symbol) }
       def subdependency_resolver
         return :pipfile if pipfile_lock
         return :poetry if poetry_lock
@@ -84,6 +92,7 @@ module Dependabot
         raise "Claimed to be a sub-dependency, but no lockfile exists!"
       end
 
+      sig { returns(T::Array[DependencyFile]) }
       def updated_pipfile_based_files
         PipfileFileUpdater.new(
           dependencies: dependencies,
@@ -93,6 +102,7 @@ module Dependabot
         ).updated_dependency_files
       end
 
+      sig { returns(T::Array[DependencyFile]) }
       def updated_poetry_based_files
         PoetryFileUpdater.new(
           dependencies: dependencies,
@@ -101,6 +111,7 @@ module Dependabot
         ).updated_dependency_files
       end
 
+      sig { returns(T::Array[DependencyFile]) }
       def updated_pip_compile_based_files
         PipCompileFileUpdater.new(
           dependencies: dependencies,
@@ -110,6 +121,7 @@ module Dependabot
         ).updated_dependency_files
       end
 
+      sig { returns(T::Array[DependencyFile]) }
       def updated_requirement_based_files
         RequirementFileUpdater.new(
           dependencies: dependencies,
@@ -119,6 +131,7 @@ module Dependabot
         ).updated_dependency_files
       end
 
+      sig { returns(T::Array[String]) }
       def pip_compile_index_urls
         if credentials.any?(&:replaces_base?)
           credentials.select(&:replaces_base?).map { |cred| AuthedUrlBuilder.authed_url(credential: cred) }
@@ -130,6 +143,7 @@ module Dependabot
         end
       end
 
+      sig { override.void }
       def check_required_files
         filenames = dependency_files.map(&:name)
         return if filenames.any? { |name| name.end_with?(".txt", ".in") }
@@ -141,31 +155,39 @@ module Dependabot
         raise "Missing required files!"
       end
 
+      sig { returns(T::Boolean) }
       def poetry_based?
         return false unless pyproject
 
-        !TomlRB.parse(pyproject.content).dig("tool", "poetry").nil?
+        !TomlRB.parse(pyproject&.content).dig("tool", "poetry").nil?
       end
 
+      sig { returns(T.nilable(Dependabot::DependencyFile)) }
       def pipfile
-        @pipfile ||= get_original_file("Pipfile")
+        @pipfile ||= T.let(get_original_file("Pipfile"), T.nilable(Dependabot::DependencyFile))
       end
 
+      sig { returns(T.nilable(Dependabot::DependencyFile)) }
       def pipfile_lock
-        @pipfile_lock ||= get_original_file("Pipfile.lock")
+        @pipfile_lock ||= T.let(get_original_file("Pipfile.lock"), T.nilable(Dependabot::DependencyFile))
       end
 
+      sig { returns(T.nilable(Dependabot::DependencyFile)) }
       def pyproject
-        @pyproject ||= get_original_file("pyproject.toml")
+        @pyproject ||= T.let(get_original_file("pyproject.toml"), T.nilable(Dependabot::DependencyFile))
       end
 
+      sig { returns(T.nilable(Dependabot::DependencyFile)) }
       def poetry_lock
-        @poetry_lock ||= get_original_file("poetry.lock")
+        @poetry_lock ||= T.let(get_original_file("poetry.lock"), T.nilable(Dependabot::DependencyFile))
       end
 
+      sig { returns(T::Array[DependencyFile]) }
       def pip_compile_files
-        @pip_compile_files ||=
-          dependency_files.select { |f| f.name.end_with?(".in") }
+        @pip_compile_files ||= T.let(
+          dependency_files.select { |f| f.name.end_with?(".in") },
+          T.nilable(T::Array[DependencyFile])
+        )
       end
     end
   end
