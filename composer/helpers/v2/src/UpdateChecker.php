0e39ada2724ac85403a6f83f686c85854fe1e25f
“Thavachelvam <thavaahariharangit@git.com>
2024-07-05 16:09:06 +0100

    Setting the latest allowable version as contraint.

--- composer/helpers/v2/src/UpdateChecker.php
+++ composer/helpers/v2/src/UpdateChecker.php
@@ -11,7 +11,6 @@ use Composer\Installer;
 use Composer\Package\Link;
 use Composer\Package\PackageInterface;
 use Composer\Semver\Constraint\Constraint;
-use Composer\Semver\VersionParser;
 
 final class UpdateChecker
 {
@@ -25,6 +24,7 @@ final class UpdateChecker
             $httpBasicCredentials[$credentials['host']] = [
                 'username' => $credentials['username'],
                 'password' => $credentials['password'],
+
             ];
         }
 
@@ -52,25 +52,10 @@ final class UpdateChecker
         }
 
         $package = $composer->getPackage();
-        $versionParser = new VersionParser();
-        $version = $package->getPrettyVersion();
 
-        try {
-            // Try to normalize the version string
-            $normalizedVersion = $versionParser->normalize($version);
-
-            // If the normalized version string is the same as the original version string,
-            // then the original version string represents an exact version
-            if ($normalizedVersion === $version) {
-                // The version string represents an exact version
-                // constraint added to check possible update for any version greater than or equal to the current version
-                $constraint = new Constraint('>=', $normalizedVersion);
-                $link = new Link($package->getName(), $dependencyName, $constraint, 'requires', $constraint->getPrettyString());
-                $package->setRequires([$dependencyName => $link]);
-            }
-        } catch (\UnexpectedValueException $e) {
-            // The version string does not represent an exact version
-        }
+        $constraint = new Constraint('==', $latestAllowableVersion);
+        $link = new Link($package->getName(), $dependencyName, $constraint);
+        $package->setRequires([$dependencyName => $link]);
 
         $install = new Installer(
             $io,
@@ -99,7 +84,7 @@ final class UpdateChecker
         // if no lock is present, we do not do a partial update as
         // this is not supported by the Installer
         if ($composer->getLocker()->isLocked()) {
-            $install->setUpdateAllowList([$dependencyName => $latestAllowableVersion]);
+            $install->setUpdateAllowList([$dependencyName]);
         }
 
         $install->run();
