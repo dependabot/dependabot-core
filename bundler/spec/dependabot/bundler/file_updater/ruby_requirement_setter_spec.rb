0dfb18adf7fa90e61e3966c25ce12694d6b8b7e1
David Rodríguez <deivid.rodriguez@riseup.net>
2024-07-09 12:58:42 +0200

    Bump Ruby 3.1 patchlevel to 3.1.6 everywhere

--- bundler/spec/dependabot/bundler/file_updater/ruby_requirement_setter_spec.rb
+++ bundler/spec/dependabot/bundler/file_updater/ruby_requirement_setter_spec.rb
@@ -119,7 +119,7 @@ RSpec.describe Dependabot::Bundler::FileUpdater::RubyRequirementSetter do
           bundler_project_dependency_file("gemfile", filename: "Gemfile").content
         end
 
-        it { is_expected.to include("ruby '3.1.4'\n") }
+        it { is_expected.to include("ruby '3.1.6'\n") }
         it { is_expected.to include(%(gem "business", "~> 1.4.0")) }
       end
 
