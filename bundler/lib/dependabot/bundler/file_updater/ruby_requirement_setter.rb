17c85ff94357f1409016e537f814cf56ed73ed0f
David Rodríguez <deivid.rodriguez@riseup.net>
2024-07-09 12:58:42 +0200

    Bump Ruby 3.1 patchlevel to 3.1.6 everywhere

--- bundler/lib/dependabot/bundler/file_updater/ruby_requirement_setter.rb
+++ bundler/lib/dependabot/bundler/file_updater/ruby_requirement_setter.rb
@@ -12,7 +12,7 @@ module Dependabot
         class RubyVersionNotFound < StandardError; end
 
         RUBY_VERSIONS = %w(
-          1.8.7 1.9.3 2.0.0 2.1.10 2.2.10 2.3.8 2.4.10 2.5.9 2.6.9 2.7.6 3.0.6 3.1.4 3.2.2 3.3.3
+          1.8.7 1.9.3 2.0.0 2.1.10 2.2.10 2.3.8 2.4.10 2.5.9 2.6.9 2.7.6 3.0.6 3.1.6 3.2.2 3.3.3
         ).freeze
 
         attr_reader :gemspec
