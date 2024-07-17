a279ac111e87b5610a17d472c33870b2ef9b22d7
Kamil Bukum <kbukum1@github.com>
2024-07-17 15:31:28 -0700

    updated the list in alphabetic order and added comments for tags

--- npm_and_yarn/lib/dependabot/npm_and_yarn/version.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/version.rb
@@ -21,20 +21,20 @@ module Dependabot
 
       # These are possible npm versioning tags that can be used in place of a version.
       # See https://docs.npmjs.com/cli/v10/commands/npm-dist-tag#purpose for more details.
-      VERSION_TAGS = T.let(%w(
-        latest
-        stable
-        beta
-        dev
-        canary
-        next
-        rc
-        alpha
-        experimental
-        nightly
-        release
-        legacy
-      ).freeze.map(&:freeze), T::Array[String])
+      VERSION_TAGS = T.let([
+        "alpha",        # Alpha version, early testing phase
+        "beta",         # Beta version, more stable than alpha
+        "canary",       # Canary version, often used for cutting-edge builds
+        "dev",          # Development version, ongoing development
+        "experimental", # Experimental version, unstable and new features
+        "latest",       # Latest stable version, used by npm to identify the current version of a package
+        "legacy",       # Legacy version, older version maintained for compatibility
+        "next",         # Next version, used by some projects to identify the upcoming version
+        "nightly",      # Nightly build, daily builds often including latest changes
+        "rc",           # Release candidate, potential final version
+        "release",      # General release version
+        "stable"        # Stable version, thoroughly tested and stable
+      ].freeze.map(&:freeze), T::Array[String])
 
       VERSION_PATTERN = T.let(Gem::Version::VERSION_PATTERN + '(\+[0-9a-zA-Z\-.]+)?', String)
       ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})?\s*\z/
