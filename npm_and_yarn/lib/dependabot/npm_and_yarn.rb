89342b25b4f3f6d77c1898fb6053facfd2cbeb9b
Kamil Bukum <kbukum1@github.com>
2024-07-22 14:46:45 -0700

    The type for function replaced to match the business logic.

--- npm_and_yarn/lib/dependabot/npm_and_yarn.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn.rb
@@ -27,7 +27,7 @@ Dependabot::Dependency.register_production_check(
 )
 
 ## A type used for defining a proc that creates a new error object
-NewErrorProc = T.type_alias do
+HandleError = T.type_alias do
   T.proc
    .params(message: String, error: Dependabot::DependabotError, params: T::Hash[Symbol, T.untyped])
    .returns(Dependabot::DependabotError)
@@ -198,7 +198,7 @@ module Dependabot
       }
     }.freeze, T::Hash[String, {
       message: T.any(String, NilClass),
-      handler: NewErrorProc
+      handler: HandleError
     }])
 
     # Group of patterns to validate error message and raise specific error
@@ -289,7 +289,7 @@ module Dependabot
       }
     ].freeze, T::Array[{
       patterns: T::Array[T.any(String, Regexp)],
-      handler: NewErrorProc,
+      handler: HandleError,
       in_usage: T.nilable(T::Boolean),
       matchfn: T.nilable(T.proc.params(usage: String, message: String).returns(T::Boolean))
     }])
