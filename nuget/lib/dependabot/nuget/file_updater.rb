cf590035abc470187f6571f1579d1dc754feeb20
Rhys Koedijk <rhys@koedijk.co.nz>
2024-08-01 00:27:17 +1200

    Fix linting

--- nuget/lib/dependabot/nuget/file_updater.rb
+++ nuget/lib/dependabot/nuget/file_updater.rb
@@ -223,7 +223,7 @@ module Dependabot
         # Compare the line counts of the original and updated content, but ignore lines only containing white-space.
         # This prevents false positives when there are trailing empty lines in the original content, for example.
         original_lines = (original_content&.lines || []).map(&:strip).reject(&:empty?)
-        updated_lines = (updated_content&.lines || []).map(&:strip).reject(&:empty?)
+        updated_lines = updated_content.lines.map(&:strip).reject(&:empty?)
         original_lines.count > updated_lines.count
       end
     end
