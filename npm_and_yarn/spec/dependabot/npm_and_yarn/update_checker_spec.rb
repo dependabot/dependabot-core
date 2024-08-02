e247eaca2312ddfe2d4c4fc3f1224d0c95136615
Kamil Bukum <kbukum1@github.com>
2024-08-01 18:30:20 -0700

    added spec tests for `#lowest_resolvable_security_fix_version`

--- npm_and_yarn/spec/dependabot/npm_and_yarn/update_checker_spec.rb
+++ npm_and_yarn/spec/dependabot/npm_and_yarn/update_checker_spec.rb
@@ -675,6 +675,62 @@ RSpec.describe Dependabot::NpmAndYarn::UpdateChecker do
     end
   end
 
+  describe "#lowest_resolvable_security_fix_version" do
+    subject(:lowest_resolvable_security_fix_version) { checker.lowest_resolvable_security_fix_version }
+
+    let(:dependency_files) { project_dependency_files("npm8/locked_transitive_dependency") }
+    let(:dependency_name) { "@dependabot-fixtures/npm-transitive-dependency" }
+    let(:target_version) { "1.2.1" }
+    let(:dependency) do
+      Dependabot::Dependency.new(
+        name: dependency_name,
+        version: "1.0.0",
+        requirements: [],
+        package_manager: "npm_and_yarn"
+      )
+    end
+
+    context "when the dependency is not vulnerable" do
+      before { allow(checker).to receive(:vulnerable?).and_return(false) }
+
+      it "raises an error" do
+        expect { lowest_resolvable_security_fix_version }.to raise_error("Dependency not vulnerable!")
+      end
+    end
+
+    context "when the dependency is vulnerable" do
+      before { allow(checker).to receive(:vulnerable?).and_return(true) }
+
+      context "when the dependency is top-level" do
+        before { allow(dependency).to receive(:top_level?).and_return(true) }
+
+        it "returns the lowest security fix version" do
+          allow(checker).to receive(:lowest_security_fix_version).and_return(Gem::Version.new(target_version))
+          expect(lowest_resolvable_security_fix_version).to eq(Gem::Version.new(target_version))
+        end
+      end
+
+      context "when the dependency is not top-level" do
+        before { allow(dependency).to receive(:top_level?).and_return(false) }
+
+        context "when there are conflicting dependencies" do
+          before { allow(checker).to receive(:conflicting_dependencies).and_return(["conflict"]) }
+
+          it { is_expected.to be_nil }
+        end
+
+        context "when there are no conflicting dependencies" do
+          before { allow(checker).to receive(:conflicting_dependencies).and_return([]) }
+
+          it "returns the latest resolvable transitive security fix version with no unlock" do
+            allow(checker).to receive(:latest_resolvable_transitive_security_fix_version_with_no_unlock).and_return(Gem::Version.new(target_version))
+            expect(lowest_resolvable_security_fix_version).to eq(Gem::Version.new(target_version))
+          end
+        end
+      end
+    end
+  end
+
   describe "#latest_resolvable_version_with_no_unlock" do
     subject { checker.latest_resolvable_version_with_no_unlock }
 
