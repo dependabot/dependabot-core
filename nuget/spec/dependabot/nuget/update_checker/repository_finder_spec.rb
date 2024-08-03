f92e7422e7630e8a7523543f1b85cac724fe8f0f
Rhys Koedijk <rhys@koedijk.co.nz>
2024-08-03 23:55:37 +1200

    Fix linting issues

--- nuget/spec/dependabot/nuget/update_checker/repository_finder_spec.rb
+++ nuget/spec/dependabot/nuget/update_checker/repository_finder_spec.rb
@@ -6,7 +6,7 @@ require "dependabot/nuget/update_checker/repository_finder"
 
 RSpec.describe Dependabot::Nuget::RepositoryFinder do
   describe "#credential_repositories" do
-    subject(:result) {
+    subject(:credential_repositories) do
       described_class.new(
         dependency: Dependabot::Dependency.new(
           name: "Microsoft.Extensions.DependencyModel",
@@ -17,85 +17,92 @@ RSpec.describe Dependabot::Nuget::RepositoryFinder do
         credentials: credentials,
         config_files: []
       ).send(:credential_repositories)
-    }
+    end
 
     context "when credentials token is `nil`" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://api.nuget.org/v3/index.json" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://api.nuget.org/v3/index.json", :token => nil }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://api.nuget.org/v3/index.json", token: nil }]
         )
-      }
+      end
     end
 
     context "when credentials token is `nil`, username is `nil`, and password is non-empty" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "password" => "github_pat_secret" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => ":github_pat_secret" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: ":github_pat_secret" }]
         )
-      }
+      end
     end
 
     context "when credentials token is `nil`, username and password are non-empty" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "username" => "user",
            "password" => "password" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => "user:password" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: "user:password" }]
         )
-      }
+      end
     end
 
     context "when credentials token, username, and password are all non-empty" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "token" => "github_pat_secret",
            "username" => "user", "password" => "password" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => "github_pat_secret" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: "github_pat_secret" }]
         )
-      }
+      end
     end
 
     context "when credentials token is access token" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "token" => "github_pat_secret" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => "github_pat_secret" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: "github_pat_secret" }]
         )
-      }
+      end
     end
 
     context "when credentials token is basic access auth" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "token" => "user:password" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => "user:password" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: "user:password" }]
         )
-      }
+      end
     end
 
     context "when credentials token is basic access auth with no username" do
-      let(:credentials) {
+      let(:credentials) do
         [{ "type" => "nuget_feed", "url" => "https://my.nuget.com/v3/index.json", "token" => ":password_only" }]
-      }
-      it {
-        is_expected.to eq(
-          [{ :url => "https://my.nuget.com/v3/index.json", :token => ":password_only" }]
+      end
+
+      it "returns the repository with credentials as a token" do
+        expect(credential_repositories).to eq(
+          [{ url: "https://my.nuget.com/v3/index.json", token: ":password_only" }]
         )
-      }
+      end
     end
   end
 end
