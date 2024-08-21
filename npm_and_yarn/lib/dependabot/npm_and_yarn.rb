0edf2e59fefe6d47b9d04d8666a93d15018e4f24
S.Sandhu <167903774+sachin-sandhu@users.noreply.github.com>
2024-08-21 14:05:16 -0400

    Fix : Dependabot::DependabotError: [YN0001] issues (#10473)
    * Fix : Dependabot:DependabotError: [YN0001] issues

--- npm_and_yarn/lib/dependabot/npm_and_yarn.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn.rb
@@ -34,6 +34,7 @@ ErrorHandler = T.type_alias do
 end
 
 module Dependabot
+  # rubocop:disable Metrics/ModuleLength
   module NpmAndYarn
     NODE_VERSION_NOT_SATISFY_REGEX = /The current Node version (?<current_version>v?\d+\.\d+\.\d+) does not satisfy the required version (?<required_version>v?\d+\.\d+\.\d+)\./ # rubocop:disable Layout/LineLength
 
@@ -125,6 +126,19 @@ module Dependabot
     YARNRC_ENOENT = /Internal Error: ENOENT/
     YARNRC_ENOENT_REGEX = /Internal Error: ENOENT: no such file or directory, stat '(?<filename>.*?)'/
 
+    YN0001_FILE_NOT_RESOLVED_CODES = T.let({
+      FIND_PACKAGE_LOCATION: /YN0001: UsageError: Couldn't find the (?<pkg>.*) state file/,
+      NO_CANDIDATE_FOUND: /YN0001: Error: (?<pkg>.*): No candidates found/,
+      NO_SUPPORTED_RESOLVER: /YN0001:*.*Error: (?<pkg>.*) isn't supported by any available resolver/,
+      WORKSPACE_NOT_FOUND: /YN0001: Error: (?<pkg>.*): Workspace not found/,
+      ENOENT: /YN0001:*.*Thrown Error: (?<pkg>.*) ENOENT/,
+      MANIFEST_NOT_FOUND: /YN0001: Error: (?<pkg>.*): Manifest not found/
+    }.freeze, T::Hash[String, Regexp])
+
+    YN0001_AUTH_ERROR_CODES = T.let({
+      AUTH_ERROR: /YN0001:*.*Fatal Error: could not read Username for '(?<url>.*)': terminal prompts disabled/
+    }.freeze, T::Hash[String, Regexp])
+
     class Utils
       extend T::Sig
 
@@ -165,6 +179,18 @@ module Dependabot
       "YN0001" => {
         message: "Exception error",
         handler: lambda { |message, _error, _params|
+          YN0001_FILE_NOT_RESOLVED_CODES.each do |(_yn0001_key, yn0001_regex)|
+            if (msg = message.match(yn0001_regex))
+              return Dependabot::DependencyFileNotResolvable.new(msg)
+            end
+          end
+
+          YN0001_AUTH_ERROR_CODES.each do |(_yn0001_key, yn0001_regex)|
+            if (msg = message.match(yn0001_regex))
+              url = msg.named_captures.fetch(URL_CAPTURE)
+              return Dependabot::PrivateSourceAuthenticationFailure.new(url)
+            end
+          end
           Dependabot::DependabotError.new(message)
         }
       },
@@ -460,4 +486,5 @@ module Dependabot
       matchfn: T.nilable(T.proc.params(usage: String, message: String).returns(T::Boolean))
     }])
   end
+  # rubocop:enable Metrics/ModuleLength
 end
