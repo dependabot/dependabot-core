512fec2e01473cc77cfd46b36e5909452fa7d79c
sachin-sandhu <sachin-sandhu@github.com>
2024-08-28 15:28:06 -0400

    draft#2

--- npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
@@ -6,23 +6,29 @@ require "dependabot/shared_helpers"
 module Dependabot
   module NpmAndYarn
     class VersionSelector
+      # For limited testing, we are allowing only specific versions defined in engines in package.json
+      # such as "v20.8.7", "20.1.2", "V20.21.2",
+      NODE_ENGINE_SUPPORTED_REGEX = /^(v|V?)\d*(?:\.\d*\.\d*)?$/
+
       def setup(manifest_json)
         package_manager = manifest_json["engines"]
 
         return unless package_manager
 
         package_manager.each do |key, value|
-          Dependabot.logger.info("Engine configuration found : \"#{key}\" : \"#{value}\"")
+          package_manager[key] = extracted_version(value)
+          Dependabot.logger.info("Engine configuration found : \"#{key}\" : \"#{extracted_version(value)}\"")
         end
       end
 
-      def validate(_name, _version)
-        empty
+      def extracted_version(value)
+        if (value.match?(NODE_ENGINE_SUPPORTED_REGEX))
+          return value
+        else 
+          return 0
+        end
+        
       end
-
-      def requested_version(name); end
-
-      def guessed_version(name); end
     end
   end
 end
