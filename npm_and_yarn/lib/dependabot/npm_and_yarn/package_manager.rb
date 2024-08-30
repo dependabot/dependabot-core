680784c95d00c9354df9a13754e24463f78fc733
sachin-sandhu <sachin-sandhu@github.com>
2024-08-30 01:16:59 -0400

    Adds checks for packageManager

--- npm_and_yarn/lib/dependabot/npm_and_yarn/package_manager.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/package_manager.rb
@@ -14,11 +14,25 @@ module Dependabot
       end
 
       def setup(name)
-        return unless @package_manager.nil? || @package_manager.start_with?("#{name}@")
+        # we prioritize version mentioned in "packageManager" instead of "engines"
+        # i.e. if { engines : "pnpm" : "6" } and { packageManager: "pnpm@6.0.2" }
+        # we go for the sepcificity mentioned in packageManager (6.0.2)
 
-        version_selector = VersionSelector.new
-        @engine_versions = version_selector.setup(@package_json)
-        version = requested_version(name)
+        # if (@package_manager&.include?("yarn"))
+        #   @package_manager = "yarn" 
+        # end
+
+
+        if @package_manager.nil?
+          version = check_engine_version(name)
+        elsif @package_manager&.==name.to_s
+          Dependabot.logger.info("Found \"packageManager\" : \"#{@package_manager}\"")
+          version = check_engine_version(name)
+        elsif @package_manager&.start_with?("#{name}@")
+          Dependabot.logger.info("Found \"packageManager\" : \"#{@package_manager}\"")
+        end
+
+        version = requested_version(name) if version.nil?
 
         if version
           raise_if_unsupported!(name, version)
@@ -47,7 +61,8 @@ module Dependabot
       end
 
       def install(name, version)
-        puts(@engine_versions)
+        Dependabot.logger.info("Installing \"#{name}@#{version}\"")
+
         SharedHelpers.run_shell_command(
           "corepack install #{name}@#{version} --global --cache-only",
           fingerprint: "corepack install <name>@<version> --global --cache-only"
@@ -57,7 +72,7 @@ module Dependabot
       def requested_version(name)
         return unless @package_manager
 
-        match = @package_manager.match(/#{name}@(?<version>\d+.\d+.\d+)/)
+        match = @package_manager.match(/^#{name}@(?<version>\d+.\d+.\d+)/)
         return unless match
 
         match["version"]
@@ -68,6 +83,21 @@ module Dependabot
         return unless lockfile
 
         Helpers.send(:"#{name}_version_numeric", lockfile)
+      end
+
+      def check_engine_version(name)
+        Dependabot.logger.info("Fetching \"engines\" info")
+        version_selector = VersionSelector.new()
+        @engine_versions = version_selector.setup(@package_json, name)
+
+        if @engine_versions.empty?
+          Dependabot.logger.info("No relevant (engines) info for \"#{name}\"")
+          return
+        end
+
+        version = @engine_versions[name]
+        Dependabot.logger.info("Returned (engines) \"#{name}\" : \"#{version}\"")
+        version
       end
     end
   end
--- npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
@@ -6,24 +6,28 @@ require "dependabot/shared_helpers"
 module Dependabot
   module NpmAndYarn
     class VersionSelector
-      # For limited testing, we are allowing only specific versions defined in engines in package.json
+      # For limited testing, allowing only specific versions defined in engines in package.json
       # such as "v20.8.7", "20.1.2", "V20.21.2",
       NODE_ENGINE_SUPPORTED_REGEX = /^(v|V?)\d*(?:\.\d*\.\d*)?$/
 
-      def setup(manifest_json)
+      def setup(manifest_json, name)
         package_manager = manifest_json["engines"]
 
         return unless package_manager
 
-        package_manager.delete_if { |_key, value| !extracted_version(value) }
 
+        # Only keep matching specs versions i.e. "V20.21.2", "20.21.2", 
+        # Additional specs can be added later
+        package_manager.delete_if { |_key, value| !valid_extracted_version(value) }
+        package_manager.delete_if { |key, _value| key != name }
         package_manager.each do |key, value|
-          Dependabot.logger.info("Engine configuration found : \"#{key}\" : \"#{value}\"")
+          Dependabot.logger.info("Found (engines) { \"#{key}\" : \"#{value}\" }")
         end
+
         package_manager
       end
 
-      def extracted_version(value)
+      def valid_extracted_version(value)
         return true if value.match?(NODE_ENGINE_SUPPORTED_REGEX)
 
         false
