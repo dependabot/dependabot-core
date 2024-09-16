c84a7405984d96cbc50a30da879d311e5a74181f
Kamil Bukum <kbukum1@github.com>
2024-09-16 15:30:15 -0700

    change default to v2 when bundler v1 is not supported

--- bundler/lib/dependabot/bundler/helpers.rb
+++ bundler/lib/dependabot/bundler/helpers.rb
@@ -4,26 +4,35 @@
 module Dependabot
   module Bundler
     module Helpers
+      extend T::Sig
+
       V1 = "1"
       V2 = "2"
       # If we are updating a project with no Gemfile.lock, we default to the
       # newest version we support
       DEFAULT = V2
-      # If we are updating a project with a Gemfile.lock that does not specify
-      # the version it was bundled with, we failover to V1 on the assumption
-      # it was created with an old version that didn't add this information
-      FAILOVER = V1
-
       BUNDLER_MAJOR_VERSION_REGEX = /BUNDLED WITH\s+(?<version>\d+)\./m
 
+      sig { params(lockfile: T.untyped).returns(String) }
       def self.bundler_version(lockfile)
         return DEFAULT unless lockfile
 
         if (matches = lockfile.content.match(BUNDLER_MAJOR_VERSION_REGEX))
           matches[:version].to_i >= 2 ? V2 : V1
+        elsif Dependabot::Experiments.enabled?(:bundler_v1_unsupported_error)
+          DEFAULT
         else
-          FAILOVER
+          failover_version
         end
+      end
+
+      # If we are updating a project with a Gemfile.lock that does not specify
+      # the version it was bundled with, we failover to V1 on the assumption
+      # it was created with an old version that didn't add this information
+      def self.failover_version
+        return V2 if Dependabot::Experiments.enabled?(:bundler_v1_unsupported_error)
+
+        V1
       end
 
       def self.detected_bundler_version(lockfile)
