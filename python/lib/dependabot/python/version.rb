ac8b08478b6b624b45f3a2c7b214b14649c00112
Alfred Mazimbe <amazimbe@github.com>
2024-10-01 10:14:13 +0100

    WIP version ordering

--- python/lib/dependabot/python/version.rb
+++ python/lib/dependabot/python/version.rb
@@ -11,9 +11,21 @@ require "dependabot/utils"
 module Dependabot
   module Python
     class Version < Dependabot::Version
+      sig { returns(Integer) }
       attr_reader :epoch
-      attr_reader :local_version
-      attr_reader :post_release_version
+
+      sig { returns(T::Array[String]) }
+      attr_reader :release
+      attr_reader :dev
+
+      sig { returns(T.nilable(T::Array[T.any(String, Integer)])) }
+      attr_reader :pre
+
+      attr_reader :post
+      attr_reader :local
+
+      NEGATIVE_INFINITY = -10
+      INFINITY = 10
 
       # See https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions
       VERSION_PATTERN = /
@@ -49,28 +61,45 @@ module Dependabot
 
       ANCHORED_VERSION_PATTERN = /\A\s*#{VERSION_PATTERN}\s*\z/
 
+      sig { override.params(version: VersionParameter).returns(T::Boolean) }
       def self.correct?(version)
         return false if version.nil?
 
         version.to_s.match?(ANCHORED_VERSION_PATTERN)
       end
 
+      sig { override.params(version: VersionParameter).void }
       def initialize(version)
         @version_string = version.to_s
-        version, @local_version = @version_string.split("+")
-        version ||= ""
-        version = version.gsub(/^v/, "")
-        if version.include?("!")
-          @epoch, version = version.split("!")
-        else
-          @epoch = "0"
-        end
-        version = normalise_prerelease(version)
-        version, @post_release_version = version.split(/\.r(?=\d)/)
-        version ||= ""
-        @local_version = normalise_prerelease(@local_version) if @local_version
-        super
+
+        matches = ANCHORED_VERSION_PATTERN.match(@version_string.downcase)
+
+        raise Dependabot::BadRequirementError unless matches
+
+        @epoch = matches["epoch"].to_i
+        @release = matches["release"]&.split(".")&.map(&:to_i) || []
+        @pre = parse_letter_version(matches["pre_l"], matches["pre_n"])
+        @post = parse_letter_version(matches["post_l"], matches["post_n1"] || matches["post_n2"])
+        @dev = parse_letter_version(matches["dev_l"], matches["dev_n"])
+        @local = parse_local_version(matches["local"])
       end
+
+      # def initialize(version)
+      #   @version_string = version.to_s
+      #   version, @local_version = @version_string.split("+")
+      #   version ||= ""
+      #   version = version.gsub(/^v/, "")
+      #   if version.include?("!")
+      #     @epoch, version = version.split("!")
+      #   else
+      #     @epoch = "0"
+      #   end
+      #   version = normalise_prerelease(version)
+      #   version, @post_release_version = version.split(/\.r(?=\d)/)
+      #   version ||= ""
+      #   @local_version = normalise_prerelease(@local_version) if @local_version
+      #   super
+      # end
 
       def to_s
         @version_string
@@ -80,71 +109,148 @@ module Dependabot
         "#<#{self.class} #{@version_string}>"
       end
 
+      sig { params(other: T.any(String, Dependabot::Python::Version)).returns(Integer) }
       def <=>(other)
-        other = Version.new(other.to_s) unless other.is_a?(Python::Version)
+        other = Dependabot::Python::Version.new(other.to_s) unless other.is_a?(Python::Version)
 
-        epoch_comparison = epoch_comparison(other)
+        epoch_comparison = epoch <=> other.epoch
         return epoch_comparison unless epoch_comparison.zero?
 
-        version_comparison = super
-        return version_comparison unless version_comparison&.zero?
+        release_comparison = release_version_comparison(other)
+        return release_comparison unless release_comparison.zero?
 
-        post_version_comparison = post_version_comparison(other)
-        return post_version_comparison unless post_version_comparison.zero?
+        pre_comparison = pre_version_comparison(other)
+        return pre_comparison unless pre_comparison.zero?
+
+        post_comparison = post_cmp_key <=> other.post_cmp_key
+        return post_comparison unless post_comparison.zero?
 
         local_version_comparison(other)
       end
 
+      sig { params(other: Dependabot::Python::Version).returns(Integer) }
+      def pre_version_comparison(other)
+        key = pre_cmp_key
+        other_key = other.pre_cmp_key
+
+        if key.is_a?(Integer) && other_key.is_a?(Integer)
+          key <=> other_key
+        elsif key.is_a?(Array) && other_key.is_a?(Array)
+          key <=> other_key
+        elsif key.is_a?(Integer)
+          key == NEGATIVE_INFINITY ? -1 : 1
+        elsif other_key.is_a?(Integer)
+          other_key == NEGATIVE_INFINITY ? 1 : -1
+        end
+      end
+
+      sig { returns(T.any(Integer, T::Array[T.any(String, Integer)])) }
+      def pre_cmp_key
+        if pre.nil? && post.nil? && dev # sort 1.0.dev0 before 1.0a0
+          NEGATIVE_INFINITY
+        elsif pre.nil?
+          INFINITY # versions without a pre-release should sort after those with one.
+        else
+          T.must(pre)
+        end
+      end
+
       private
 
-      def epoch_comparison(other)
-        epoch.to_i <=> other.epoch.to_i
+      def local_cmp_key
+        if local.nil?
+          # Versions without a local segment should sort before those with one.
+          NEGATIVE_INFINITY
+        else
+          # According to PEP440.
+          # - Alphanumeric segments sort before numeric segments
+          # - Alphanumeric segments sort lexicographically
+          # - Numeric segments sort numerically
+          # - Shorter versions sort before longer versions when the prefixes match exactly
+          local.map do |token|
+            if token.is_a?(Integer)
+              [token, ""]
+            else
+              [NEGATIVE_INFINITY, token]
+            end
+          end
+        end
       end
 
-      def post_version_comparison(other)
-        unless other.post_release_version
-          return post_release_version.nil? ? 0 : 1
+      def post_cmp_key
+        # Versions without a post segment should sort before those with one.
+        if post.nil?
+          NEGATIVE_INFINITY
+        else
+          post
+        end
+      end
+
+      def release_version_comparison(other)
+        tokens, other_tokens = pad_for_comparison(release, other.release)
+
+        tokens <=> other_tokens
+
+        # tokens.zip(other_tokens).each do |token, other_token|
+        #   result = token <=> other_token
+        #   return result unless result.zero?
+        # end
+
+        # 0
+      end
+
+      def pad_for_comparison(tokens, other_tokens)
+        tokens = tokens.dup
+        other_tokens = other_tokens.dup
+
+        longer = [tokens, other_tokens].max_by(&:count)
+        shorter = [tokens, other_tokens].min_by(&:count)
+
+        difference = longer.length - shorter.length
+
+        difference.times { shorter << 0 }
+
+        [tokens, other_tokens]
+      end
+
+      sig { params(local: T.nilable(String)).returns(T.nilable(T::Array[T.any(String, Integer)])) }
+      def parse_local_version(local)
+        return if local.nil?
+
+        # Takes a string like abc.1.twelve and turns it into ["abc", 1, "twelve"]
+        local.split(/[\._-]/).map { |s| /\d+/.match?(s) ? s.to_i : s }
+      end
+
+      sig do
+        params(
+          letter: T.nilable(String), number: T.nilable(String)
+        ).returns(T.nilable(T::Array[T.any(String, Integer)]))
+      end
+      def parse_letter_version(letter = nil, number = nil)
+        return if letter.nil? && number.nil?
+
+        if letter
+          # Implicit 0 for cases where prerelease has no numeral
+          number ||= 0
+
+          # Normalize alternate spellings
+          if letter == "alpha"
+            letter = "a"
+          elsif letter == "beta"
+            letter = "b"
+          elsif letter in %w(c pre preview)
+            letter = "rc"
+          elsif letter in %w(rev r)
+            letter = "post"
+          end
+
+          return letter, number.to_i
         end
 
-        return -1 if post_release_version.nil?
+        # Number but no letter i.e. implicit post release syntax (e.g. 1.0-1)
+        letter = "post"
 
-        post_release_version.to_i <=> other.post_release_version.to_i
-      end
-
-      def local_version_comparison(other)
-        # Local version comparison works differently in Python: `1.0.beta`
-        # compares as greater than `1.0`. To accommodate, we make the
-        # strings the same length before comparing.
-        lhsegments = local_version.to_s.split(".").map(&:downcase)
-        rhsegments = other.local_version.to_s.split(".").map(&:downcase)
-        limit = [lhsegments.count, rhsegments.count].min
-
-        lhs = ["1", *lhsegments.first(limit)].join(".")
-        rhs = ["1", *rhsegments.first(limit)].join(".")
-
-        local_comparison = Gem::Version.new(lhs) <=> Gem::Version.new(rhs)
-
-        return local_comparison unless local_comparison&.zero?
-
-        lhsegments.count <=> rhsegments.count
-      end
-
-      def normalise_prerelease(version)
-        # Python has reserved words for release states, which are treated
-        # as equal (e.g., preview, pre and rc).
-        # Further, Python treats dashes as a separator between version
-        # parts and treats the alphabetical characters in strings as the
-        # start of a new version part (so 1.1a2 == 1.1.alpha.2).
-        version
-          .gsub("alpha", "a")
-          .gsub("beta", "b")
-          .gsub("preview", "c")
-          .gsub("pre", "c")
-          .gsub("post", "r")
-          .gsub("rev", "r")
-          .gsub(/([\d.\-_])rc([\d.\-_])?/, '\1c\2')
-          .tr("-", ".")
-          .gsub(/(\d)([a-z])/i, '\1.\2')
+        [letter, number.to_i]
       end
     end
   end
