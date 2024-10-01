1367b192f34f3da100f1c4f82fe86d159ba2195f
Jamie Magee <jamagee@microsoft.com>
2024-09-30 22:12:11 -0700

    Initial `dotnet_sdk` updater
    Closes #2442

---
+++ dotnet_sdk/lib/dependabot/dotnet_sdk.rb
@@ -0,0 +1,20 @@
+# typed: strong
+# frozen_string_literal: true
+
+# These all need to be required so the various classes can be registered in a
+# lookup table of package manager names to concrete classes.
+require "dependabot/dotnet_sdk/file_fetcher"
+require "dependabot/dotnet_sdk/file_parser"
+require "dependabot/dotnet_sdk/update_checker"
+require "dependabot/dotnet_sdk/file_updater"
+require "dependabot/dotnet_sdk/metadata_finder"
+require "dependabot/dotnet_sdk/requirement"
+require "dependabot/dotnet_sdk/version"
+
+require "dependabot/pull_request_creator/labeler"
+Dependabot::PullRequestCreator::Labeler
+  .register_label_details("dotnet_sdk", name: "dotnet_sdk_package_manager", colour: "2753E3")
+
+require "dependabot/dependency"
+Dependabot::Dependency
+  .register_production_check("dotnet_sdk", ->(_) { true })
---
+++ dotnet_sdk/lib/dependabot/dotnet_sdk/file_updater.rb
@@ -0,0 +1,60 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "dependabot/file_updaters"
+require "dependabot/file_updaters/base"
+require "sorbet-runtime"
+
+module Dependabot
+  module DotnetSdk
+    class FileUpdater < Dependabot::FileUpdaters::Base
+      extend T::Sig
+
+      sig { override.returns(T::Array[Regexp]) }
+      def self.updated_files_regex
+        [/^global\.json$/]
+      end
+
+      sig { override.returns(T::Array[Dependabot::DependencyFile]) }
+      def updated_dependency_files
+        updated_files = []
+
+        contents = update
+
+        updated_files << updated_file(file: global_json, content: contents) if file_changed?(global_json)
+
+        updated_files
+      end
+
+      private
+
+      sig { returns(Dependabot::Dependency) }
+      def dependency
+        # Dockerfiles will only ever be updating a single dependency
+        T.must(dependencies.first)
+      end
+
+      sig { override.void }
+      def check_required_files
+        # Just check if there are any files at all.
+        return if dependency_files.any?
+
+        raise "No global.json configuration!"
+      end
+
+      sig { returns(Dependabot::DependencyFile) }
+      def global_json
+        T.must(dependency_files.find { |f| f.name == "global.json" })
+      end
+
+      sig { returns(String) }
+      def update
+        previous_json = JSON.parse(T.must(global_json.content))
+        previous_json["sdk"]["version"] = T.must(dependency.requirements.first)[:requirement]
+        JSON.pretty_generate(previous_json)
+      end
+    end
+  end
+end
+
+Dependabot::FileUpdaters.register("dotnet_sdk", Dependabot::DotnetSdk::FileUpdater)
