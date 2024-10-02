a55a214daa0b350ed886da950818b3df8a7e17c8
Rob Jellinghaus <rjelling@microsoft.com>
2024-10-02 16:07:21 -0700

    Separate out the diff.

--- cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
+++ cargo/lib/dependabot/cargo/update_checker/version_resolver.rb
@@ -13,18 +13,18 @@ module Dependabot
   module Cargo
     class UpdateChecker
       class VersionResolver
-        # Note that as of Rust 1.80, git error message handling in the `cargo update` command changed.
-        # This change causes the NOT_OUR_REF error to appear *before* the UNABLE_TO_UPDATE error.
-        # Issue filed in Cargo project: https://github.com/rust-lang/cargo/issues/14621
-        NOT_OUR_REF = /fatal: remote error: upload-pack: not our ref/
-
         UNABLE_TO_UPDATE = /Unable to update (?<url>.*?)$/
         BRANCH_NOT_FOUND_REGEX = /#{UNABLE_TO_UPDATE}.*to find branch `(?<branch>[^`]+)`/m
         REVSPEC_PATTERN = /revspec '.*' not found/
         OBJECT_PATTERN = /object not found - no match for id \(.*\)/
         REF_NOT_FOUND_REGEX = /#{UNABLE_TO_UPDATE}.*(#{REVSPEC_PATTERN}|#{OBJECT_PATTERN})/m
-        NOT_OUR_REF_REGEX = /#{NOT_OUR_REF}.*#{UNABLE_TO_UPDATE}/m
         GIT_REF_NOT_FOUND_REGEX = /Updating git repository `(?<url>[^`]*)`.*fatal: couldn't find remote ref/m
+
+        # Note that as of Rust 1.80, git error message handling in the `cargo update` command changed.
+        # This change causes the NOT_OUR_REF error to appear *before* the UNABLE_TO_UPDATE error.
+        # Issue filed in Cargo project: https://github.com/rust-lang/cargo/issues/14621
+        NOT_OUR_REF = /fatal: remote error: upload-pack: not our ref/
+        NOT_OUR_REF_REGEX = /#{NOT_OUR_REF}.*#{UNABLE_TO_UPDATE}/m
 
         def initialize(dependency:, credentials:,
                        original_dependency_files:, prepared_dependency_files:)
