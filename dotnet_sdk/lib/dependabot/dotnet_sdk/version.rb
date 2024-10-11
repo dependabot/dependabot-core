6b236bca72e3a4a5a1ff9508cad0a311f4ed6da8
Jamie Magee <jamagee@microsoft.com>
2024-10-10 21:30:14 -0700

    Document that we are consciously using semver for .NET SDK versioning

--- dotnet_sdk/lib/dependabot/dotnet_sdk/version.rb
+++ dotnet_sdk/lib/dependabot/dotnet_sdk/version.rb
@@ -6,6 +6,9 @@ require "dependabot/utils"
 
 module Dependabot
   module DotnetSdk
+    # The .NET SDK versioning scheme is not semver compliant.
+    # However, for simpliticy, we will treat it as semver.
+    # See: https://learn.microsoft.com/en-us/dotnet/core/versions/#versioning-details
     class Version < Dependabot::Version
     end
   end
