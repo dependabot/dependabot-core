2baa809f30a0b71fc7605553b9091485eaefff70
Sebastian Gomez <sgome121@fiu.edu>
2024-10-16 18:22:15 -0400

    update git submodules

--- nuget/spec/fixtures/github/multiple_packages_config/src/a/obj/Debug/net8.0/projecta.AssemblyInfo.cs
+++ nuget/spec/fixtures/github/multiple_packages_config/src/a/obj/Debug/net8.0/projecta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projecta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf42a1a8e048f8caba5ed8a0ab1c34013415282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ceeee663dddd6ba50fdc3b82004931fed0393fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("projecta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projecta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
