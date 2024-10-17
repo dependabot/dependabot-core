3d6dfee3948e002257aa275886876182b25b005f
Yurick Hauschild <yhauschi@volvocars.com>
2024-10-17 11:30:04 +0200

    Address linting changes

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -233,7 +233,7 @@ module Dependabot
         # Loop through parent directories looking for a pnpm-lock
         (1..directory.split("/").count).each do |i|
           @pnpm_lock = fetch_file_from_host(("../" * i) + "pnpm-lock.yaml")
-                   .tap { |f| f.support_file = true }
+                       .tap { |f| f.support_file = true }
           break if @pnpm_lock
         rescue Dependabot::DependencyFileNotFound
           # Ignore errors (pnpm_lock.yaml may not be present)
