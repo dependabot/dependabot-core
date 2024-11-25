4cc0e843402adc7c6f8379864d7fe7efbe8d4d07
Alfred Mazimbe <alfred.mazimbe@gmail.com>
2024-11-25 15:36:36 +0000

    Add support for cargo ecosystem metrics collection (#11009)

---
+++ cargo/lib/dependabot/cargo/package_manager.rb
@@ -0,0 +1,41 @@
+# typed: strong
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require "dependabot/ecosystem"
+require "dependabot/cargo/version"
+
+module Dependabot
+  module Cargo
+    ECOSYSTEM = "rust"
+    PACKAGE_MANAGER = "cargo"
+    SUPPORTED_CARGO_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+    # When a version is going to be unsupported, it will be added here
+    DEPRECATED_CARGO_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+    class PackageManager < Dependabot::Ecosystem::VersionManager
+      extend T::Sig
+
+      sig { params(raw_version: String).void }
+      def initialize(raw_version)
+        super(
+          PACKAGE_MANAGER,
+          Version.new(raw_version),
+          DEPRECATED_CARGO_VERSIONS,
+          SUPPORTED_CARGO_VERSIONS
+        )
+      end
+
+      sig { returns(T::Boolean) }
+      def deprecated?
+        false
+      end
+
+      sig { returns(T::Boolean) }
+      def unsupported?
+        false
+      end
+    end
+  end
+end
