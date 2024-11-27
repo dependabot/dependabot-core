c78c833d48be04e110cd1bd818aa1192fc6025d5
sachin-sandhu <sachin-sandhu@github.com>
2024-11-27 13:24:36 -0500

    more fixes

--- python/lib/dependabot/python/package_manager.rb
+++ python/lib/dependabot/python/package_manager.rb
@@ -161,41 +161,5 @@ module Dependabot
         false
       end
     end
-
-    class UnsupportedPackageManager < Dependabot::Ecosystem::VersionManager
-      extend T::Sig
-
-      NAME = "unsupported"
-
-      SUPPORTED_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
-
-      DEPRECATED_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
-
-      sig do
-        params(
-          raw_version: String,
-          requirement: T.nilable(Requirement)
-        ).void
-      end
-      def initialize(raw_version, requirement = nil)
-        super(
-          NAME,
-          Version.new(raw_version),
-          SUPPORTED_VERSIONS,
-          DEPRECATED_VERSIONS,
-          requirement,
-       )
-      end
-
-      sig { override.returns(T::Boolean) }
-      def deprecated?
-        false
-      end
-
-      sig { override.returns(T::Boolean) }
-      def unsupported?
-        false
-      end
-    end
   end
 end
