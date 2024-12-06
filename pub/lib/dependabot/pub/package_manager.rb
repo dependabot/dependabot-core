7aba5f2795bbd06bdb1e041df5b8c9715ad208ac
S.Sandhu <167903774+sachin-sandhu@users.noreply.github.com>
2024-12-06 09:25:30 -0500

    Metric collection for Dart ecosystem (#11070)
    Metric collection for Dart ecosystem (#11070)

---
+++ pub/lib/dependabot/pub/package_manager.rb
@@ -0,0 +1,52 @@
+# typed: strong
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require "dependabot/pub/version"
+require "dependabot/ecosystem"
+require "dependabot/pub/requirement"
+
+module Dependabot
+  module Pub
+    ECOSYSTEM = "dart"
+
+    SUPPORTED_DART_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+    DEPRECATED_DART_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+    class PubPackageManager < Dependabot::Ecosystem::VersionManager
+      extend T::Sig
+
+      NAME = "pub"
+      VERSION = "0.0"
+
+      SUPPORTED_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+      DEPRECATED_VERSIONS = T.let([].freeze, T::Array[Dependabot::Version])
+
+      sig do
+        params(
+          raw_version: String
+        ).void
+      end
+      def initialize(raw_version)
+        super(
+          NAME,
+          Version.new(raw_version),
+          SUPPORTED_VERSIONS,
+          DEPRECATED_VERSIONS
+       )
+      end
+
+      sig { override.returns(T::Boolean) }
+      def deprecated?
+        false
+      end
+
+      sig { override.returns(T::Boolean) }
+      def unsupported?
+        false
+      end
+    end
+  end
+end
