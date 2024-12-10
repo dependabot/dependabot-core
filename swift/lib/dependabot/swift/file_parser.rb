5338897c0bafbb0cbf40643a6be751a9d20b39c0
Alfred Mazimbe <alfred.mazimbe@gmail.com>
2024-12-10 21:17:10 +0000

    Add support for swift ecosystem metrics collection (#11094)
    * Add support for swift ecosystem metrics collection
    
    * Extra tests for language and package manager

--- swift/lib/dependabot/swift/file_parser.rb
+++ swift/lib/dependabot/swift/file_parser.rb
@@ -6,6 +6,8 @@ require "dependabot/file_parsers"
 require "dependabot/file_parsers/base"
 require "dependabot/swift/file_parser/dependency_parser"
 require "dependabot/swift/file_parser/manifest_parser"
+require "dependabot/swift/package_manager"
+require "dependabot/swift/language"
 
 module Dependabot
   module Swift
@@ -36,6 +38,17 @@ module Dependabot
         dependency_set.dependencies
       end
 
+      sig { returns(Ecosystem) }
+      def ecosystem
+        @ecosystem ||= T.let(begin
+          Ecosystem.new(
+            name: ECOSYSTEM,
+            language: language,
+            package_manager: package_manager
+          )
+        end, T.nilable(Dependabot::Ecosystem))
+      end
+
       private
 
       def dependency_parser
@@ -53,6 +66,44 @@ module Dependabot
       def package_manifest_file
         # TODO: Select version-specific manifest
         @package_manifest_file ||= get_original_file("Package.swift")
+      end
+
+      sig { returns(Ecosystem::VersionManager) }
+      def package_manager
+        @package_manager ||= T.let(
+          PackageManager.new(T.must(package_manager_version)),
+          T.nilable(Dependabot::Swift::PackageManager)
+        )
+      end
+
+      sig { returns(T.nilable(String)) }
+      def package_manager_version
+        @package_manager_version ||= T.let(
+          begin
+            version = SharedHelpers.run_shell_command("swift package --version")
+            version.strip.gsub(/Swift Package Manager - Swift \s*/, "")
+          end,
+          T.nilable(String)
+        )
+      end
+
+      sig { returns(T.nilable(Ecosystem::VersionManager)) }
+      def language
+        @language ||= T.let(begin
+          Language.new(T.must(swift_version))
+        end, T.nilable(Dependabot::Swift::Language))
+      end
+
+      sig { returns(T.nilable(String)) }
+      def swift_version
+        @swift_version ||= T.let(
+          begin
+            version = SharedHelpers.run_shell_command("swift --version")
+            pattern = Dependabot::Ecosystem::VersionManager::DEFAULT_VERSION_PATTERN
+            version.match(/Swift version\s*#{pattern}/)&.captures&.first
+          end,
+          T.nilable(String)
+        )
       end
     end
   end
