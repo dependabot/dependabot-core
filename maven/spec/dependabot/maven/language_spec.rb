0ba6fd88ecbf93e61081b01b5ced53c39673d086
Kamil Bukum <kbukum1@github.com>
2024-12-27 12:12:44 -0800

    fix maven specs

--- maven/spec/dependabot/maven/language_spec.rb
+++ maven/spec/dependabot/maven/language_spec.rb
@@ -6,13 +6,11 @@ require "dependabot/ecosystem"
 require "spec_helper"
 
 RSpec.describe Dependabot::Maven::Language do
-  let(:language) { described_class.new(detected_version, version) }
-  let(:detected_version) { "3.0.0" }
-  let(:version) { "3.0.0" }
+  let(:language) { described_class.new }
 
   describe "#version" do
     it "returns the version" do
-      expect(language.version).to eq(Dependabot::Maven::Version.new(version))
+      expect(language.version).to be_nil
     end
   end
 
