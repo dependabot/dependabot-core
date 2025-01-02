f4c532c36373069dcc4fb7163387c15e96a6f4c9
“Thavachelvam <thavaahariharangit@git.com>
2025-01-02 17:59:11 +0000

    Selecting the correct requirement based on the given marker condition.

--- python/lib/dependabot/python/file_parser.rb
+++ python/lib/dependabot/python/file_parser.rb
@@ -294,10 +294,36 @@ module Dependabot
 
       def blocking_marker?(dep)
         return false if dep["markers"] == "None"
-        return true if dep["markers"].include?("<")
-        return false if dep["markers"].include?(">")
+        marker = dep["markers"]
+        version = python_raw_version
 
-        dep["requirement"]&.include?("<")
+        if marker.include?("python_version")
+          !marker_satisfied?(marker, version)
+        else
+          return true if dep["markers"].include?("<")
+          return false if dep["markers"].include?(">")
+          dep["requirement"]&.include?("<")
+        end
+      end
+
+      def marker_satisfied?(marker, python_version)
+        return true if marker == "None"
+        operator, version = marker.match(/([<>=!]=?)\s*"?([\d.]+)"?/).captures
+
+        case operator
+        when "<"
+          Gem::Version.new(python_version) < Gem::Version.new(version)
+        when "<="
+          Gem::Version.new(python_version) <= Gem::Version.new(version)
+        when ">"
+          Gem::Version.new(python_version) > Gem::Version.new(version)
+        when ">="
+          Gem::Version.new(python_version) >= Gem::Version.new(version)
+        when "=="
+          Gem::Version.new(python_version) == Gem::Version.new(version)
+        else
+          false
+        end
       end
 
       def setup_file_dependencies
