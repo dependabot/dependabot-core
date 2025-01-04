eb6d6fa4e95bae95927846e5866242b5a280200e
kbukum1 <kbukum1@github.com>
2025-01-03 16:06:55 -0800

    Update Bundler deprecation unsupported check to use detected version (#11222)
    * update specs to fix the re-initialite issue

--- common/spec/dependabot/ecosystem_spec.rb
+++ common/spec/dependabot/ecosystem_spec.rb
@@ -5,122 +5,226 @@ require "spec_helper"
 require "dependabot/ecosystem"
 require "dependabot/requirement"
 
-# Define an anonymous subclass of Dependabot::Requirement for testing purposes
-TestRequirement = Class.new(Dependabot::Requirement)
-
 RSpec.describe Dependabot::Ecosystem do
-  let(:supported_versions) { [Dependabot::Version.new("1"), Dependabot::Version.new("2")] }
-  let(:deprecated_versions) { [Dependabot::Version.new("1")] }
-  let(:requirement) { TestRequirement.new(">= 1.0") }
+  let(:package_manager_supported_versions) { [Dependabot::Version.new("1"), Dependabot::Version.new("2")] }
+  let(:package_manager_deprecated_versions) { [Dependabot::Version.new("1")] }
+  let(:package_manager_requirement) { TestRequirement.new(">= 1.0") }
+  let(:package_manager_detected_version) { "1.0" }
+  let(:package_manager_raw_version) { "1.0.1" }
 
-  let(:package_manager_detected_version) { "1.0.0" }
-  let(:package_manager_raw_version) { "1.0.0" }
-  let(:language_detected_version) { "3.0.0" }
-  let(:language_raw_version) { "3.0.0" }
+  let(:language_supported_versions) { [Dependabot::Version.new("3.0"), Dependabot::Version.new("3.1")] }
+  let(:language_deprecated_versions) { [Dependabot::Version.new("3.0")] }
+  let(:language_requirement) { TestRequirement.new(">= 3.0") }
+  let(:language_detected_version) { "3.0" }
+  let(:language_raw_version) { "3.0.2" }
 
   let(:package_manager) do
     Class.new(Dependabot::Ecosystem::VersionManager) do
       def initialize(detected_version, raw_version, deprecated_versions, supported_versions, requirement)
         super(
-          name: "bundler", # name
-          detected_version: Dependabot::Version.new(detected_version), # version
-          version: Dependabot::Version.new(raw_version), # version
-          deprecated_versions: deprecated_versions, # deprecated_versions
-          supported_versions: supported_versions, # supported_versions
-          requirement: requirement # requirement
+          name: "bundler",
+          detected_version: Dependabot::Version.new(detected_version),
+          version: raw_version.nil? ? nil : Dependabot::Version.new(raw_version),
+          deprecated_versions: deprecated_versions,
+          supported_versions: supported_versions,
+          requirement: requirement
         )
       end
     end.new(
       package_manager_detected_version,
       package_manager_raw_version,
-      deprecated_versions,
-      supported_versions,
-      requirement
+      package_manager_deprecated_versions,
+      package_manager_supported_versions,
+      package_manager_requirement
     )
   end
 
   let(:language) do
     Class.new(Dependabot::Ecosystem::VersionManager) do
-      def initialize(detected_version, raw_version)
+      def initialize(detected_version, raw_version, deprecated_versions, supported_versions, requirement)
         super(
-          name: "ruby", # name
-          detected_version: Dependabot::Version.new(detected_version), # version
-          version: Dependabot::Version.new(raw_version), # version
-          deprecated_versions: [], # deprecated_versions
-          supported_versions: [], # supported_versions
+          name: "ruby",
+          detected_version: Dependabot::Version.new(detected_version),
+          version: raw_version.nil? ? nil : Dependabot::Version.new(raw_version),
+          deprecated_versions: deprecated_versions,
+          supported_versions: supported_versions,
+          requirement: requirement
         )
       end
-    end.new(language_detected_version, language_raw_version)
+    end.new(
+      language_detected_version,
+      language_raw_version,
+      language_deprecated_versions,
+      language_supported_versions,
+      language_requirement
+    )
   end
 
   describe "#initialize" do
-    it "sets the correct attributes" do
+    it "sets the correct attributes for package manager and language" do
       ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
 
       expect(ecosystem.name).to eq("bundler")
+      expect(ecosystem.package_manager.name).to eq("bundler")
+      expect(ecosystem.package_manager.detected_version.to_s).to eq("1.0")
+      expect(ecosystem.package_manager.version.to_s).to eq("1.0.1")
+      expect(ecosystem.language.name).to eq("ruby")
+      expect(ecosystem.language.detected_version.to_s).to eq("3.0")
+      expect(ecosystem.language.version.to_s).to eq("3.0.2")
+    end
+  end
+
+  describe "#name" do
+    it "returns the correct name for the package manager and language" do
+      ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+
       expect(ecosystem.package_manager.name).to eq("bundler")
       expect(ecosystem.language.name).to eq("ruby")
     end
   end
 
-  describe "#deprecated?" do
-    context "when the package manager version is deprecated" do
-      let(:package_manager_detected_version) { "1" }
-      let(:package_manager_raw_version) { "1" }
+  describe "#version checks" do
+    it "returns the correct string representation of version and raw version" do
+      expect(package_manager.version.to_s).to eq("1.0.1")
+      expect(language.version.to_s).to eq("3.0.2")
+    end
+  end
 
-      it "returns true" do
-        ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect(ecosystem.deprecated?).to be true
+  describe "#deprecated?" do
+    context "when detected version is deprecated" do
+      let(:package_manager_detected_version) { "1.0" }
+
+      context "with raw version not deprecated" do
+        let(:package_manager_raw_version) { "2.0.1" }
+
+        it "returns true" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be true
+        end
+      end
+
+      context "with raw version deprecated" do
+        let(:package_manager_raw_version) { "1.0.1" }
+
+        it "returns true" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be true
+        end
+      end
+
+      context "with raw version nil" do
+        let(:package_manager_raw_version) { nil }
+
+        it "returns true" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be true
+        end
       end
     end
 
-    context "when the package manager version is not deprecated" do
-      let(:package_manager_detected_version) { "2.0.0" }
-      let(:package_manager_raw_version) { "2.0.0" }
+    context "when detected version is not deprecated" do
+      let(:package_manager_detected_version) { "2.0" }
 
-      it "returns false" do
-        ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect(ecosystem.deprecated?).to be false
+      context "with raw version not deprecated" do
+        let(:package_manager_raw_version) { "2.0.1" }
+
+        it "returns false" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be false
+        end
+      end
+
+      context "with raw version deprecated" do
+        let(:package_manager_raw_version) { "1.0.1" }
+
+        it "returns false" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be false
+        end
+      end
+
+      context "with raw version nil" do
+        let(:package_manager_raw_version) { nil }
+
+        it "returns false" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.deprecated?).to be false
+        end
       end
     end
   end
 
   describe "#unsupported?" do
-    context "when the package manager version is unsupported" do
-      let(:package_manager_detected_version) { "0.8.0" }
-      let(:package_manager_raw_version) { "0.8.0" }
+    context "when detected version is unsupported" do
+      let(:package_manager_detected_version) { "0.8" }
 
-      it "returns true" do
-        ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect(ecosystem.unsupported?).to be true
+      context "with raw version not unsupported" do
+        let(:package_manager_raw_version) { "2.0.1" }
+
+        it "returns true" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.unsupported?).to be true
+        end
+      end
+
+      context "with raw version nil" do
+        let(:package_manager_raw_version) { nil }
+
+        it "returns true" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.unsupported?).to be true
+        end
       end
     end
 
-    context "when the package manager version is supported" do
-      let(:package_manager_detected_version) { "2.0.0" }
-      let(:package_manager_raw_version) { "2.0.0" }
+    context "when detected version is supported" do
+      let(:package_manager_detected_version) { "2.0" }
 
-      it "returns false" do
-        ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect(ecosystem.unsupported?).to be false
+      context "with raw version not unsupported" do
+        let(:package_manager_raw_version) { "2.0.1" }
+
+        it "returns false" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.unsupported?).to be false
+        end
+      end
+
+      context "with raw version nil" do
+        let(:package_manager_raw_version) { nil }
+
+        it "returns false" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect(ecosystem.unsupported?).to be false
+        end
       end
     end
   end
 
   describe "#raise_if_unsupported!" do
-    context "when the package manager version is unsupported" do
-      let(:package_manager_detected_version) { "0.8.0" }
-      let(:package_manager_raw_version) { "0.8.0" }
+    context "when detected version is unsupported" do
+      let(:package_manager_detected_version) { "0.8" }
 
-      it "raises a ToolVersionNotSupported error" do
-        ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect { ecosystem.raise_if_unsupported! }.to raise_error(Dependabot::ToolVersionNotSupported)
+      context "with raw version not nil" do
+        let(:package_manager_raw_version) { "2.0.1" }
+
+        it "raises a ToolVersionNotSupported error" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect { ecosystem.raise_if_unsupported! }.to raise_error(Dependabot::ToolVersionNotSupported)
+        end
+      end
+
+      context "with raw version nil" do
+        let(:package_manager_raw_version) { nil }
+
+        it "raises a ToolVersionNotSupported error" do
+          ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
+          expect { ecosystem.raise_if_unsupported! }.to raise_error(Dependabot::ToolVersionNotSupported)
+        end
       end
     end
 
-    context "when the package manager version is supported" do
-      let(:package_manager_detected_version) { "2.0.0" }
-      let(:package_manager_raw_version) { "2.0.0" }
+    context "when detected version is supported" do
+      let(:package_manager_detected_version) { "2.0" }
 
       it "does not raise an error" do
         ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
@@ -133,16 +237,19 @@ RSpec.describe Dependabot::Ecosystem do
     context "when a requirement is provided" do
       it "sets the requirement correctly" do
         ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
-        expect(ecosystem.package_manager.requirement).to eq(requirement)
+        expect(ecosystem.package_manager.requirement).to eq(package_manager_requirement)
+        expect(ecosystem.language.requirement).to eq(language_requirement)
       end
     end
 
     context "when no requirement is provided" do
-      let(:requirement) { nil }
+      let(:package_manager_requirement) { nil }
+      let(:language_requirement) { nil }
 
       it "returns nil for the requirement" do
         ecosystem = described_class.new(name: "bundler", package_manager: package_manager, language: language)
         expect(ecosystem.package_manager.requirement).to be_nil
+        expect(ecosystem.language.requirement).to be_nil
       end
     end
   end
--- common/spec/dependabot/notices_spec.rb
+++ common/spec/dependabot/notices_spec.rb
@@ -15,28 +15,46 @@ class StubVersionManager < Dependabot::Ecosystem::VersionManager
     super(
       name: name,
       detected_version: Dependabot::Version.new(detected_version),
-      version: Dependabot::Version.new(raw_version),
+      version: raw_version.nil? ? nil : Dependabot::Version.new(raw_version),
       deprecated_versions: deprecated_versions,
       supported_versions: supported_versions
-   )
+    )
   end
 
   attr_reader :support_later_versions
 
   sig { override.returns(T::Boolean) }
   def unsupported?
-    # Determine if the version is unsupported.
+    return false unless version
+
     version < supported_versions.first
   end
 
   sig { override.returns(T::Boolean) }
   def support_later_versions?
-    # Determine if the Bundler version is unsupported.
     support_later_versions
   end
 end
 
 RSpec.describe Dependabot::Notice do
+  let(:detected_version) { Dependabot::Version.new("1") }
+  let(:raw_version) { Dependabot::Version.new("1.0.1") }
+  let(:deprecated_versions) { [Dependabot::Version.new("1")] }
+  let(:supported_versions) { [Dependabot::Version.new("2"), Dependabot::Version.new("3")] }
+  let(:support_later_versions) { false }
+  let(:version_manager_type) { :package_manager }
+
+  let(:version_manager) do
+    StubVersionManager.new(
+      name: "bundler",
+      detected_version: detected_version,
+      raw_version: raw_version,
+      deprecated_versions: deprecated_versions,
+      supported_versions: supported_versions,
+      support_later_versions: support_later_versions
+    )
+  end
+
   describe ".generate_supported_versions_description" do
     subject(:generate_supported_versions_description) do
       described_class.generate_supported_versions_description(supported_versions, support_later_versions)
@@ -44,139 +62,119 @@ RSpec.describe Dependabot::Notice do
 
     context "when supported_versions has one version" do
       let(:supported_versions) { [Dependabot::Version.new("2")] }
-      let(:support_later_versions) { false }
 
-      it "returns the correct description" do
-        expect(generate_supported_versions_description)
-          .to eq("Please upgrade to version `v2`.")
+      context "with support_later_versions as false" do
+        let(:support_later_versions) { false }
+
+        it "returns the correct description" do
+          expect(generate_supported_versions_description).to eq("Please upgrade to version `v2`.")
+        end
       end
-    end
 
-    context "when supported_versions has one version and later versions are supported" do
-      let(:supported_versions) { [Dependabot::Version.new("2")] }
-      let(:support_later_versions) { true }
+      context "with support_later_versions as true" do
+        let(:support_later_versions) { true }
 
-      it "returns the correct description" do
-        expect(generate_supported_versions_description)
-          .to eq("Please upgrade to version `v2`, or later.")
+        it "returns the correct description with later versions supported" do
+          expect(generate_supported_versions_description).to eq("Please upgrade to version `v2`, or later.")
+        end
       end
     end
 
     context "when supported_versions has multiple versions" do
-      let(:supported_versions) do
-        [Dependabot::Version.new("2"), Dependabot::Version.new("3"),
-         Dependabot::Version.new("4")]
+      let(:supported_versions) { [Dependabot::Version.new("2"), Dependabot::Version.new("3")] }
+
+      context "with support_later_versions as false" do
+        let(:support_later_versions) { false }
+
+        it "returns the correct description without later versions supported" do
+          expect(generate_supported_versions_description)
+            .to eq("Please upgrade to one of the following versions: `v2`, or `v3`.")
+        end
       end
-      let(:support_later_versions) { false }
 
-      it "returns the correct description" do
-        expect(generate_supported_versions_description)
-          .to eq("Please upgrade to one of the following versions: `v2`, `v3`, or `v4`.")
-      end
-    end
+      context "with support_later_versions as true" do
+        let(:support_later_versions) { true }
 
-    context "when supported_versions has multiple versions and later versions are supported" do
-      let(:supported_versions) do
-        [Dependabot::Version.new("2"), Dependabot::Version.new("3"),
-         Dependabot::Version.new("4")]
-      end
-      let(:support_later_versions) { true }
-
-      it "returns the correct description" do
-        expect(generate_supported_versions_description)
-          .to eq("Please upgrade to one of the following versions: `v2`, `v3`, `v4`, or later.")
-      end
-    end
-
-    context "when supported_versions is nil" do
-      let(:supported_versions) { nil }
-      let(:support_later_versions) { false }
-
-      it "returns empty string" do
-        expect(generate_supported_versions_description).to eq("Please upgrade your package manager version")
+        it "returns the correct description with later versions supported" do
+          expect(generate_supported_versions_description)
+            .to eq("Please upgrade to one of the following versions: `v2`, `v3`, or later.")
+        end
       end
     end
 
     context "when supported_versions is empty" do
       let(:supported_versions) { [] }
-      let(:support_later_versions) { false }
 
-      it "returns the correct description" do
+      it "returns a generic upgrade message" do
         expect(generate_supported_versions_description).to eq("Please upgrade your package manager version")
-      end
-
-      context "when the entity being deprecated is the language" do
-        subject(:generate_supported_versions_description) do
-          described_class.generate_supported_versions_description(
-            supported_versions, support_later_versions, :language
-          )
-        end
-
-        it "returns the correct description" do
-          expect(generate_supported_versions_description).to eq("Please upgrade your language version")
-        end
       end
     end
   end
 
   describe ".generate_deprecation_notice" do
-    subject(:package_manager_deprecation_notice) do
-      described_class.generate_deprecation_notice(package_manager)
+    subject(:deprecation_notice) do
+      described_class.generate_deprecation_notice(version_manager, version_manager_type)
     end
 
-    let(:package_manager) do
-      StubVersionManager.new(
-        name: "bundler",
-        detected_version: Dependabot::Version.new("1"),
-        raw_version: Dependabot::Version.new("1"),
-        deprecated_versions: [Dependabot::Version.new("1")],
-        supported_versions: [Dependabot::Version.new("2"), Dependabot::Version.new("3")]
-      )
-    end
+    context "when detected_version is deprecated" do
+      let(:detected_version) { Dependabot::Version.new("1") }
+      let(:deprecated_versions) { [Dependabot::Version.new("1")] }
 
-    it "returns the correct deprecation notice" do
-      allow(package_manager).to receive(:unsupported?).and_return(false)
-      expect(package_manager_deprecation_notice.to_hash)
-        .to eq({
-          mode: "WARN",
-          type: "bundler_deprecated_warn",
-          package_manager_name: "bundler",
-          title: "Package manager deprecation notice",
-          description: "Dependabot will stop supporting `bundler v1`!" \
-                       "\n\nPlease upgrade to one of the following versions: `v2`, or `v3`.\n",
-          show_in_pr: true,
-          show_alert: true
-        })
-    end
+      context "with raw_version not deprecated" do
+        let(:raw_version) { Dependabot::Version.new("2.0.1") }
 
-    context "when generating a notice for a deprecated language" do
-      subject(:deprecation_notice) do
-        described_class.generate_deprecation_notice(language_manager, :language)
+        it "returns a deprecation notice using detected_version" do
+          expect(deprecation_notice.to_hash)
+            .to eq({
+              mode: "WARN",
+              type: "bundler_deprecated_warn",
+              package_manager_name: "bundler",
+              title: "Package manager deprecation notice",
+              description: "Dependabot will stop supporting `bundler v1`!" \
+                           "\n\nPlease upgrade to one of the following versions: `v2`, or `v3`.\n",
+              show_in_pr: true,
+              show_alert: true
+            })
+        end
       end
 
-      let(:language_manager) do
-        StubVersionManager.new(
-          name: "python",
-          detected_version: Dependabot::Version.new("3.8"),
-          raw_version: Dependabot::Version.new("3.8"),
-          deprecated_versions: [Dependabot::Version.new("3.8")],
-          supported_versions: [Dependabot::Version.new("3.9")]
-        )
+      context "with raw_version nil" do
+        let(:raw_version) { nil }
+
+        it "returns a deprecation notice using detected_version" do
+          expect(deprecation_notice.to_hash)
+            .to eq({
+              mode: "WARN",
+              type: "bundler_deprecated_warn",
+              package_manager_name: "bundler",
+              title: "Package manager deprecation notice",
+              description: "Dependabot will stop supporting `bundler v1`!" \
+                           "\n\nPlease upgrade to one of the following versions: `v2`, or `v3`.\n",
+              show_in_pr: true,
+              show_alert: true
+            })
+        end
+      end
+    end
+
+    context "when detected_version is not deprecated" do
+      let(:detected_version) { Dependabot::Version.new("2") }
+      let(:deprecated_versions) { [Dependabot::Version.new("1")] }
+
+      context "with raw_version nil" do
+        let(:raw_version) { nil }
+
+        it "does not return a notice" do
+          expect(deprecation_notice).to be_nil
+        end
       end
 
-      it "returns the correct deprecation notice" do
-        allow(language_manager).to receive(:unsupported?).and_return(false)
-        expect(deprecation_notice.to_hash)
-          .to eq({
-            mode: "WARN",
-            type: "python_deprecated_warn",
-            package_manager_name: "python",
-            title: "Language deprecation notice",
-            description: "Dependabot will stop supporting `python v3.8`!" \
-                         "\n\nPlease upgrade to version `v3.9`.\n",
-            show_in_pr: true,
-            show_alert: true
-          })
+      context "with raw_version different from detected_version" do
+        let(:raw_version) { Dependabot::Version.new("1.0.1") }
+
+        it "does not return a notice" do
+          expect(deprecation_notice).to be_nil
+        end
       end
     end
   end
