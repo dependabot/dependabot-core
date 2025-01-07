66735fb2d15453bc5b362b09596c4d8119ffaf59
kbukum1 <kbukum1@github.com>
2025-01-07 10:41:34 -0800

    Update `npm_and_yarn` deprecation and unsupported checks for `npm`, `pnpm`, and `yarn` package managers (#11240)

--- npm_and_yarn/spec/dependabot/npm_and_yarn/update_checker/subdependency_version_resolver_spec.rb
+++ npm_and_yarn/spec/dependabot/npm_and_yarn/update_checker/subdependency_version_resolver_spec.rb
@@ -43,6 +43,8 @@ RSpec.describe namespace::SubdependencyVersionResolver do
       .with(:enable_corepack_for_npm_and_yarn).and_return(enable_corepack_for_npm_and_yarn)
     allow(Dependabot::Experiments).to receive(:enabled?)
       .with(:enable_shared_helpers_command_timeout).and_return(true)
+    allow(Dependabot::Experiments).to receive(:enabled?)
+      .with(:npm_v6_deprecation_warning).and_return(true)
   end
 
   after do
