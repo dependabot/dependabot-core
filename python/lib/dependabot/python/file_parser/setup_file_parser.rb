4aab29fc611e32a17635c9e3d6d9dbc5782bde2a
Randhir <randhircs@github.com>
2025-01-16 17:08:22 +0000

    Changed setup file typecheck as strict

--- python/lib/dependabot/python/file_parser/setup_file_parser.rb
+++ python/lib/dependabot/python/file_parser/setup_file_parser.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "dependabot/dependency"
@@ -20,12 +20,14 @@ module Dependabot
         TESTS_REQUIRE_REGEX = /tests_require\s*=\s*\[/m
         EXTRAS_REQUIRE_REGEX = /extras_require\s*=\s*\{/m
 
-        CLOSING_BRACKET = { "[" => "]", "{" => "}" }.freeze
+        CLOSING_BRACKET = T.let({ "[" => "]", "{" => "}" }.freeze, T.any(T.untyped, T.untyped))
 
+        sig { params(dependency_files: T.untyped).void }
         def initialize(dependency_files:)
           @dependency_files = dependency_files
         end
 
+        sig { returns(Dependabot::FileParsers::Base::DependencySet) }
         def dependency_set
           dependencies = Dependabot::FileParsers::Base::DependencySet.new
 
@@ -56,8 +58,10 @@ module Dependabot
 
         private
 
+        sig { returns(T.untyped) }
         attr_reader :dependency_files
 
+        sig { returns(T.untyped) }
         def parsed_setup_file
           SharedHelpers.in_a_temporary_directory do
             write_temporary_dependency_files
@@ -79,6 +83,7 @@ module Dependabot
           parsed_sanitized_setup_file
         end
 
+        sig { returns(T.nilable(T.any(T::Hash[String, T.untyped], String, T::Array[T::Hash[String, T.untyped]]))) }
         def parsed_sanitized_setup_file
           SharedHelpers.in_a_temporary_directory do
             write_sanitized_setup_file
@@ -100,16 +105,18 @@ module Dependabot
           []
         end
 
+        sig { params(requirements: T.untyped).returns(T.nilable(Python::Requirement)) }
         def check_requirements(requirements)
-          requirements.each do |dep|
+          requirements&.each do |dep|
             next unless dep["requirement"]
 
-            Python::Requirement.new(dep["requirement"].split(","))
+            T.let(Python::Requirement.new(dep["requirement"].split(",")), Python::Requirement)
           rescue Gem::Requirement::BadRequirementError => e
             raise Dependabot::DependencyFileNotEvaluatable, e.message
           end
         end
 
+        sig { void }
         def write_temporary_dependency_files
           dependency_files
             .reject { |f| f.name == ".python-version" }
@@ -125,6 +132,7 @@ module Dependabot
         # This sanitization is far from perfect (it will fail if any of the
         # entries are dynamic), but it is an alternative approach to the one
         # used in parser.py which sometimes succeeds when that has failed.
+        sig { void }
         def write_sanitized_setup_file
           install_requires = get_regexed_req_array(INSTALL_REQUIRES_REGEX)
           setup_requires = get_regexed_req_array(SETUP_REQUIRES_REGEX)
@@ -175,6 +183,7 @@ module Dependabot
           NameNormaliser.normalise_including_extras(name, extras)
         end
 
+        sig { returns(T.untyped) }
         def setup_file
           dependency_files.find { |f| f.name == "setup.py" }
         end
