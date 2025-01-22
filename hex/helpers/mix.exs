83b4d5da6f2647db5015b2767c45fdb87a9327f3
Tyler Witt <Ty.Mo.Witt@gmail.com>
2025-01-22 19:05:20 +0900

    Remove Hex Jason dependency (#11295)
    JSON now ships with Elixir 1.18, which means that the Elixir portion of the Hex processing can now remove Jason, and have no dependencies.
    
    This simplifies some uncommon (to most) mix environment setup that adds some friction to onboarding.
    
    Co-authored-by: Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>

--- hex/helpers/mix.exs
+++ hex/helpers/mix.exs
@@ -2,20 +2,16 @@ defmodule DependabotCore.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dependabot_core,
-     version: "0.1.0",
-     elixir: "~> 1.5",
-     start_permanent: Mix.env == :prod,
-     lockfile: System.get_env("MIX_LOCK") || "mix.lock",
-     deps_path: System.get_env("MIX_DEPS") || "deps",
-     deps: deps()]
+    [
+      app: :dependabot_core,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env == :prod,
+      deps: []
+    ]
   end
 
   def application do
     [extra_applications: [:hex, :logger, :ssh]]
-  end
-
-  defp deps() do
-    [{:jason, "~> 1.0"}]
   end
 end
