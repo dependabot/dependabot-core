029a0111162df905bc003903b88c21c5a826d1cb
Randhir <randhircs@github.com>
2025-01-22 20:42:22 +0000

    Added return type as nil to fix issue

--- python/lib/dependabot/python/file_parser/pipfile_files_parser.rb
+++ python/lib/dependabot/python/file_parser/pipfile_files_parser.rb
@@ -142,7 +142,7 @@ module Dependabot
           end
         end
 
-        sig { params(req: T.any(String, T::Hash[String, T.untyped])).returns(T.any(T::Boolean, String)) }
+        sig { params(req: T.any(String, T::Hash[String, T.untyped])).returns(T.any(T::Boolean, NilClass, String)) }
         def specifies_version?(req)
           return true if req.is_a?(String)
 
