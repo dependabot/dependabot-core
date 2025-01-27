c5e2be77d9f8ffbe0dd7b0c936f29f2a0317f937
Kamil Bukum <kbukum1@github.com>
2025-01-27 12:26:46 -0800

    refine comments and fix spellings

--- npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/version_selector.rb
@@ -10,16 +10,23 @@ module Dependabot
       extend T::Sig
       extend T::Helpers
 
+      # Sets up engine versions from the given manifest JSON.
+      #
+      # @param manifest_json [Hash] The manifest JSON containing version information.
+      # @param name [String] The engine name to match.
+      # @return [Hash] A hash with selected versions, if found.
       sig { params(manifest_json: T::Hash[String, T.untyped], name: String).returns(T::Hash[Symbol, T.untyped]) }
       def setup(manifest_json, name)
         engine_versions = manifest_json["engines"]
 
+        # Return an empty hash if no engine versions are specified
         return {} if engine_versions.nil?
 
-        # Find version from engines according to semver speficiations
-        # Additional specs can be added later
+        # Select engine versions matching the provided name and satisfying SemVer constraints
+        # This adheres to SemVer specifications, ensuring the highest valid version is chosen.
         version = engine_versions.select do |engine, value|
-          engine.to_s.match(name) && ConstraintHelper.find_highest_version_from_constraint_expression(value)
+          engine.to_s.match(name) &&
+            ConstraintHelper.find_highest_version_from_constraint_expression(value)
         end
 
         version
