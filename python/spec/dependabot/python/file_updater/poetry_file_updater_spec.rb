1dcd7f7ca0b6072ad7ce0e9f648d54d118140b2e
Alfred Mazimbe <amazimbe@github.com>
2025-02-03 16:08:38 +0000

    Handle

--- python/spec/dependabot/python/file_updater/poetry_file_updater_spec.rb
+++ python/spec/dependabot/python/file_updater/poetry_file_updater_spec.rb
@@ -729,6 +729,36 @@ RSpec.describe Dependabot::Python::FileUpdater::PoetryFileUpdater do
           end
         end
       end
+
+      context "when the requirement has not changed" do
+        let(:pyproject_fixture_name) { "caret_version.toml" }
+        let(:dependency) do
+          Dependabot::Dependency.new(
+            name: dependency_name,
+            version: "2.19.1",
+            previous_version: nil,
+            package_manager: "pip",
+            requirements: [{
+              requirement: "^2.19.1",
+              file: "pyproject.toml",
+              source: nil,
+              groups: ["dependencies"]
+            }],
+            previous_requirements: [{
+              requirement: ">=2.19.1",
+              file: "pyproject.toml",
+              source: nil,
+              groups: ["dependencies"]
+            }]
+          )
+        end
+
+        it "raises the correct error" do
+          expect do
+            updated_files.map(&:name)
+          end.to raise_error(Dependabot::DependencyFileContentNotChanged, "Content did not change!")
+        end
+      end
     end
 
     context "with a poetry.lock" do
