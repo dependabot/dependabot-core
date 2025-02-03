72b47828ff96511dfd5bd4f48c11be43b8a5fef7
Rob Aiken <robaiken@github.com>
2025-02-03 20:06:30 +0000

    fixing catalogs being removed from lockfile
    fixing catalogs being removed from lockfile

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_updater/pnpm_lockfile_updater.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_updater/pnpm_lockfile_updater.rb
@@ -24,11 +24,12 @@ module Dependabot
           )
         end
 
-        def updated_pnpm_lock_content(pnpm_lock, is_catalog)
+        def updated_pnpm_lock_content(pnpm_lock, updated_pnpm_workspace_content: nil)
           @updated_pnpm_lock_content ||= {}
           return @updated_pnpm_lock_content[pnpm_lock.name] if @updated_pnpm_lock_content[pnpm_lock.name]
 
-          new_content = run_pnpm_update(pnpm_lock: pnpm_lock, is_catalog: is_catalog)
+          new_content = run_pnpm_update(pnpm_lock: pnpm_lock,
+                                        updated_pnpm_workspace_content: updated_pnpm_workspace_content)
           @updated_pnpm_lock_content[pnpm_lock.name] = new_content
         rescue SharedHelpers::HelperSubprocessFailed => e
           handle_pnpm_lock_updater_error(e, pnpm_lock)
@@ -100,12 +101,14 @@ module Dependabot
         # Peer dependencies configuration error
         ERR_PNPM_PEER_DEP_ISSUES = /ERR_PNPM_PEER_DEP_ISSUES/
 
-        def run_pnpm_update(pnpm_lock:, is_catalog:)
+        def run_pnpm_update(pnpm_lock:, updated_pnpm_workspace_content: nil)
           SharedHelpers.in_a_temporary_repo_directory(base_dir, repo_contents_path) do
             File.write(".npmrc", npmrc_content(pnpm_lock))
 
             SharedHelpers.with_git_configured(credentials: credentials) do
-              unless is_catalog
+              if updated_pnpm_workspace_content
+                File.write("pnpm-workspace.yaml", updated_pnpm_workspace_content["pnpm-workspace.yaml"])
+              else
                 run_pnpm_update_packages
                 write_final_package_json_files
               end
