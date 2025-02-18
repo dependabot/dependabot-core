755bc76a9a1a86b1ea6b0fa0f96b482a1c928537
Derek Morris <demorri@microsoft.com>
2025-02-18 11:05:58 -0800

    implement functionality

--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/RunWorker.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/RunWorker.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 using Microsoft.Extensions.FileSystemGlobbing;
 
@@ -23,6 +24,28 @@ public class RunWorker
     private readonly IDiscoveryWorker _discoveryWorker;
     private readonly IAnalyzeWorker _analyzeWorker;
     private readonly IUpdaterWorker _updaterWorker;
+
+    /// <summary>
+    /// Used to save (and then restore) which line endings are predominant in a file.
+    /// </summary>
+    private enum EOLType
+    {
+        /// <summary>
+        /// Line feed - \n
+        /// Typical on most systems.
+        /// </summary>
+        LF,
+        /// <summary>
+        /// Carrage return - \r
+        /// Typical on older MacOS, unlikely (but possible) to come up here
+        /// </summary>
+        CR,
+        /// <summary>
+        /// Carrage return and line feed - \r\n.
+        /// Typical on Windows
+        /// </summary>
+        CRLF
+    };
 
     internal static readonly JsonSerializerOptions SerializerOptions = new()
     {
@@ -122,6 +145,7 @@ public class RunWorker
 
         // TODO: pull out relevant dependencies, then check each for updates and track the changes
         var originalDependencyFileContents = new Dictionary<string, string>();
+        var originalDependencyFileEOFs = new Dictionary<string, EOLType>();
         var actualUpdatedDependencies = new List<ReportedDependency>();
 
         // track original contents for later handling
@@ -131,6 +155,7 @@ public class RunWorker
             var localFullPath = Path.Join(repoContentsPath.FullName, repoFullPath);
             var content = await File.ReadAllTextAsync(localFullPath);
             originalDependencyFileContents[repoFullPath] = content;
+            originalDependencyFileEOFs[repoFullPath] = GetPredominantEOL(content);
         }
 
         foreach (var project in discoveryResult.Projects)
@@ -205,6 +230,9 @@ public class RunWorker
             var updatedContent = await File.ReadAllTextAsync(localFullPath);
             if (updatedContent != originalContent)
             {
+                updatedContent = FixEOL(updatedContent, originalDependencyFileEOFs[repoFullPath]);
+                await File.WriteAllTextAsync(localFullPath, updatedContent);
+
                 updatedDependencyFiles[localFullPath] = new DependencyFile()
                 {
                     Name = Path.GetFileName(repoFullPath),
@@ -264,6 +292,56 @@ public class RunWorker
             BaseCommitSha = baseCommitSha,
         };
         return result;
+    }
+
+    /// <summary>
+    /// Analyze the input string and find the most common line ending type.
+    /// </summary>
+    /// <param name="content">The string to analyze</param>
+    /// <returns>The most common type of line ending in the input string.</returns>
+    private static EOLType GetPredominantEOL(string content)
+    {
+        // Get stats on EOL characters/character sequences, if one predominates choose that for writing later.
+        var lfcount = content.Count(c => c == '\n');
+        var crcount = content.Count(c => c == '\r');
+        var crlfcount = Regex.Matches(content, "\r\n").Count();
+
+        // Since CRLF contains both a CR and a LF, subtract it from those counts
+        lfcount -= crlfcount;
+        crcount -= crlfcount;
+        if (crcount > lfcount && crcount > crlfcount)
+        {
+            return EOLType.CR;
+        }
+        else if (crlfcount > lfcount)
+        {
+            return EOLType.CRLF;
+        }
+        else
+        {
+            return EOLType.LF;
+        }
+    }
+
+    /// <summary>
+    /// Given a line ending, modify the input string to uniformly use that line ending.
+    /// </summary>
+    /// <param name="content">The input string, which may have any combination of line endings.</param>
+    /// <param name="desiredEOL">The line ending type to use across the result.</param>
+    /// <returns>The string with any line endings swapped to the desired type.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">If EOLType is an unexpected value.</exception>
+    private static string FixEOL(string content, EOLType desiredEOL)
+    {
+        switch (desiredEOL)
+        {
+            case EOLType.LF:
+                return Regex.Replace(content, "(\r\n|\r)", "\n");
+            case EOLType.CR:
+                return Regex.Replace(content, "(\r\n|\n)", "\r");
+            case EOLType.CRLF:
+                return Regex.Replace(content, "(\r\n|\r|\n)", "\r\n");
+        }
+        throw new ArgumentOutOfRangeException(nameof(desiredEOL));
     }
 
     internal static IEnumerable<(string ProjectPath, Dependency Dependency)> GetUpdateOperations(WorkspaceDiscoveryResult discovery)
