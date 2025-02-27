2df614e0c217793bd813ae90d3c5cc3803d4f01d
Alfred Mazimbe <amazimbe@github.com>
2025-02-27 10:04:15 +0000

    Update swift/requirement.rb to typed: strong

--- swift/lib/dependabot/swift/requirement.rb
+++ swift/lib/dependabot/swift/requirement.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strong
 # frozen_string_literal: true
 
 require "sorbet-runtime"
@@ -15,17 +15,18 @@ module Dependabot
       # always contains a single element.
       sig { override.params(requirement_string: T.nilable(String)).returns(T::Array[Requirement]) }
       def self.requirements_array(requirement_string)
-        [new(requirement_string)]
+        [new([T.must(requirement_string)])]
       end
 
       # Patches Gem::Requirement to make it accept requirement strings like
       # "~> 4.2.5, >= 4.2.5.1" without first needing to split them.
-      def initialize(*requirements)
+      sig { params(requirements: T::Array[String]).void }
+      def initialize(requirements)
         requirements = requirements.flatten.flat_map do |req_string|
           req_string.split(",").map(&:strip)
         end
 
-        super(requirements)
+        super
       end
     end
   end
