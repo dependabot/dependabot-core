eb54318eb7c41826d85e28357b455e2f05787b58
Alfred Mazimbe <alfred.mazimbe@gmail.com>
2025-03-06 12:51:51 +0000

    Add npm version fallback logic

--- npm_and_yarn/lib/dependabot/npm_and_yarn/helpers.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/helpers.rb
@@ -48,7 +48,11 @@ module Dependabot
       # Otherwise, we are going to use old versionining npm 6
       sig { params(lockfile: T.nilable(DependencyFile)).returns(Integer) }
       def self.npm_version_numeric(lockfile)
-        npm_version_numeric_latest(lockfile)
+        fallback_version_npm8 = Dependabot::Experiments.enabled?(:npm_fallback_version_above_v6)
+
+        return npm_version_numeric_npm8_or_higher(lockfile) if fallback_version_npm8
+
+        npm_version_numeric_npm6_or_higher(lockfile)
       end
 
       sig { params(lockfile: T.nilable(DependencyFile)).returns(Integer) }
