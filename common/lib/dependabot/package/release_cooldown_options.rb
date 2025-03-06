bd9e70e9ee78140662b50fd4ca011c35de7bd4bb
kbukum1 <kbukum1@github.com>
2025-03-06 13:03:53 -0800

    fix naming for cooldown semver fields (#11753)

--- common/lib/dependabot/package/release_cooldown_options.rb
+++ common/lib/dependabot/package/release_cooldown_options.rb
@@ -11,34 +11,34 @@ module Dependabot
       sig do
         params(
           default_days: T.nilable(Integer),
-          major_days: T.nilable(Integer),
-          minor_days: T.nilable(Integer),
-          patch_days: T.nilable(Integer),
+          semver_major_days: T.nilable(Integer),
+          semver_minor_days: T.nilable(Integer),
+          semver_patch_days: T.nilable(Integer),
           include: T.nilable(T::Array[String]),
           exclude: T.nilable(T::Array[String])
         ).void
       end
       def initialize(
-        default_days: 0, major_days: 0, minor_days: 0, patch_days: 0,
+        default_days: 0, semver_major_days: 0, semver_minor_days: 0, semver_patch_days: 0,
         include: [], exclude: []
       )
         default_days ||= 0
-        major_days ||= 0
-        minor_days ||= 0
-        patch_days ||= 0
+        semver_major_days ||= 0
+        semver_minor_days ||= 0
+        semver_patch_days ||= 0
         include ||= []
         exclude ||= []
 
         @default_days = T.let(default_days, Integer)
-        @major_days = T.let(major_days.positive? ? major_days : default_days, Integer)
-        @minor_days = T.let(minor_days.positive? ? minor_days : default_days, Integer)
-        @patch_days = T.let(patch_days.positive? ? patch_days : default_days, Integer)
+        @semver_major_days = T.let(semver_major_days.positive? ? semver_major_days : default_days, Integer)
+        @semver_minor_days = T.let(semver_minor_days.positive? ? semver_minor_days : default_days, Integer)
+        @semver_patch_days = T.let(semver_patch_days.positive? ? semver_patch_days : default_days, Integer)
         @include = T.let(include.to_set, T::Set[String])
         @exclude = T.let(exclude.to_set, T::Set[String])
       end
 
       sig { returns(Integer) }
-      attr_reader :default_days, :major_days, :minor_days, :patch_days
+      attr_reader :default_days, :semver_major_days, :semver_minor_days, :semver_patch_days
 
       sig { returns(T::Set[String]) }
       attr_reader :include, :exclude
