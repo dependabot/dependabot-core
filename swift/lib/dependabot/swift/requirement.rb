1a098e8331c856d1bfac68ee5c24ea8ab0281774
Alfred Mazimbe <amazimbe@github.com>
2025-03-17 09:58:46 +0000

    Revert array of strings type

--- swift/lib/dependabot/swift/requirement.rb
+++ swift/lib/dependabot/swift/requirement.rb
@@ -15,18 +15,18 @@ module Dependabot
       # always contains a single element.
       sig { override.params(requirement_string: T.nilable(String)).returns(T::Array[Requirement]) }
       def self.requirements_array(requirement_string)
-        [new([T.must(requirement_string)])]
+        [new(T.must(requirement_string))]
       end
 
       # Patches Gem::Requirement to make it accept requirement strings like
       # "~> 4.2.5, >= 4.2.5.1" without first needing to split them.
-      sig { params(requirements: T::Array[String]).void }
-      def initialize(requirements)
+      sig { params(requirements: String).void }
+      def initialize(*requirements)
         requirements = requirements.flatten.flat_map do |req_string|
           req_string.split(",").map(&:strip)
         end
 
-        super
+        super(requirements)
       end
     end
   end
