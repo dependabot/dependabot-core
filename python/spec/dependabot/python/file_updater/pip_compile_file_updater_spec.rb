5886d42bc6635960a99b778b6bada3c365e7a0cf
Kamil Bukum <kbukum1@github.com>
2025-03-18 19:23:02 -0700

    fix the python issue

--- python/spec/dependabot/python/file_updater/pip_compile_file_updater_spec.rb
+++ python/spec/dependabot/python/file_updater/pip_compile_file_updater_spec.rb
@@ -531,7 +531,7 @@ RSpec.describe Dependabot::Python::FileUpdater::PipCompileFileUpdater do
       let(:dependency_version) { "5.2.7" }
       let(:dependency_previous_version) { "5.2.6" }
 
-      it "adds pycurl as dependency" do
+      it "adds urllib3 as dependency" do
         expect(updated_files.count).to eq(1)
         expect(updated_files.first.content).to include("--resolver=backtracking")
         expect(updated_files.first.content).to include("urllib3")
@@ -545,10 +545,10 @@ RSpec.describe Dependabot::Python::FileUpdater::PipCompileFileUpdater do
       let(:dependency_version) { "5.2.7" }
       let(:dependency_previous_version) { "5.2.6" }
 
-      it "do not include pycurl" do
+      it "do not include urllib3" do
         expect(updated_files.count).to eq(1)
         expect(updated_files.first.content).to include("--resolver=legacy")
-        expect(updated_files.first.content).not_to include("pycurl")
+        expect(updated_files.first.content).not_to include("urllib3")
       end
     end
   end
