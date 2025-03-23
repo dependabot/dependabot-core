37edef0b135d643261248e629fc9f72bb6f0826f
Jamie Magee <jamagee@microsoft.com>
2025-03-22 19:28:06 -0700

    Strict type `silent`

--- silent/lib/dependabot/silent/requirement.rb
+++ silent/lib/dependabot/silent/requirement.rb
@@ -1,17 +1,22 @@
-# typed: true
+# typed: strong
 # frozen_string_literal: true
+
+require "sorbet-runtime"
 
 require "dependabot/requirement"
 require "dependabot/utils"
 
 module SilentPackageManager
   class Requirement < Dependabot::Requirement
+    extend T::Sig
+
     AND_SEPARATOR = /(?<=[a-zA-Z0-9*])\s+(?:&+\s+)?(?!\s*[|-])/
 
+    sig { override.params(requirement_string: T.nilable(String)).returns(T::Array[Dependabot::Requirement]) }
     def self.requirements_array(requirement_string)
-      requirements = requirement_string.split(AND_SEPARATOR).map(&:strip)
+      requirements = requirement_string&.split(AND_SEPARATOR)&.map(&:strip)
 
-      [new(*requirements)]
+      [new(requirements)]
     end
   end
 end
