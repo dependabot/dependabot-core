7e8f667c36911b8b6e0b9656e2f0d28a70cbdfcb
Randhir <randhircs@github.com>
2025-03-26 22:17:50 +0000

    Fixed nil issue at compile time as dependent file change impacted it.

--- python/lib/dependabot/python/file_updater/pip_compile_file_updater.rb
+++ python/lib/dependabot/python/file_updater/pip_compile_file_updater.rb
@@ -197,11 +197,11 @@ module Dependabot
           args[:requirements] = new_reqs
           args[:previous_requirements] = old_reqs
 
-          RequirementFileUpdater.new(
+          T.must(RequirementFileUpdater.new(
             dependencies: [Dependency.new(**T.unsafe(args))],
             dependency_files: files,
             credentials: credentials
-          ).updated_dependency_files
+          ).updated_dependency_files)
         end
 
         sig do
