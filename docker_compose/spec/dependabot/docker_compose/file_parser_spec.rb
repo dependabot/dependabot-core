591e6762a42ed072789644ad1905bfad50398a78
Rob Aiken <robaiken@github.com>
2025-04-02 17:46:17 +0100

    lint

--- docker_compose/spec/dependabot/docker_compose/file_parser_spec.rb
+++ docker_compose/spec/dependabot/docker_compose/file_parser_spec.rb
@@ -207,8 +207,9 @@ RSpec.describe Dependabot::DockerCompose::FileParser do
               it "has the right details" do
                 expect(dependency).to be_a(Dependabot::Dependency)
                 expect(dependency.name).to eq("myreg/ubuntu")
-                expect(dependency.version).to
-                eq("sha256:18305429afa14ea462f810146ba44d4363ae76e4c8dfc38288cf73aa07485005")
+                expect(dependency.version).to eq(
+                  "sha256:18305429afa14ea462f810146ba44d4363ae76e4c8dfc38288cf73aa07485005"
+                )
                 expect(dependency.requirements).to eq(expected_requirements)
               end
             end
