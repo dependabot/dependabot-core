af7d320d5cc9ea7cf0673d9a77e1952554602412
Jamie Magee <jamie.magee@gmail.com>
2025-04-15 22:35:32 -0700

    Prevent `simplecov` from spamming `stdout`
    https://github.com/simplecov-ruby/simplecov/issues/992

--- common/spec/spec_helper.rb
+++ common/spec/spec_helper.rb
@@ -28,6 +28,15 @@ SimpleCov.start do
   minimum_coverage line: 0, branch: 0
 end
 
+# Don't print the "Coverage report generated for..." messages
+# https://github.com/simplecov-ruby/simplecov/issues/992
+SimpleCov.at_exit do
+  original_file_descriptor = $stdout
+  $stdout.reopen("/dev/null")
+  SimpleCov.result.format!
+  $stdout.reopen(original_file_descriptor)
+end
+
 require "dependabot/dependency_file"
 require "dependabot/experiments"
 require "dependabot/registry_client"
