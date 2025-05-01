7eae076a2e1aa28748bc1655e00427b5d83ec3fd
Brett V. Forsgren <brettfo@microsoft.com>
2025-05-01 12:36:40 -0600

    call separate endpoints for unknown errors (#12178)
    Also simplify the API handlers by only requiring a single method; the rest are extensions to make testing more consistent.

--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/ApiModel/UnknownError.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/ApiModel/UnknownError.cs
@@ -1,10 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace NuGetUpdater.Core.Run.ApiModel;
 
 public record UnknownError : JobErrorBase
 {
+    [JsonIgnore]
+    public Exception Exception { get; init; }
+
     public UnknownError(Exception ex, string jobId)
         : base("unknown_error")
     {
+        Exception = ex;
         Details["error-class"] = ex.GetType().Name;
         Details["error-message"] = ex.Message;
         Details["error-backtrace"] = ex.StackTrace ?? "<unknown>";
