fdaade18247d9ede6ebdfd25b5a13b3a795502e7
Brett V. Forsgren <brettfo@microsoft.com>
2025-05-06 10:45:45 -0600

    include all stack information for unknown errors

--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/ApiModel/UnknownError.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Run/ApiModel/UnknownError.cs
@@ -7,8 +7,6 @@ public record UnknownError : JobErrorBase
     [JsonIgnore]
     public Exception Exception { get; init; }
 
-    public static readonly string UnknownStackTrace = "   <unknown>";
-
     public UnknownError(Exception ex, string jobId)
         : base("unknown_error")
     {
@@ -18,7 +16,7 @@ public record UnknownError : JobErrorBase
         // stacktrace.  Since we're not in Ruby we can set an empty string there and append the .NET stacktrace to
         // the message.
         Details["error-class"] = ex.GetType().Name;
-        Details["error-message"] = $"{ex.Message}\n{ex.StackTrace ?? UnknownStackTrace}";
+        Details["error-message"] = ex.ToString();
         Details["error-backtrace"] = "";
         Details["package-manager"] = "nuget";
         Details["job-id"] = jobId;
