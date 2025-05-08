8244015fa7cba263f95bc1277a38e479b7df5184
Jake Coffman <jake@jakecoffman.com>
2025-05-08 15:21:16 -0500

    pin to a commit to avoid randomly breaking later

--- go_modules/spec/dependabot/go_modules/file_parser_spec.rb
+++ go_modules/spec/dependabot/go_modules/file_parser_spec.rb
@@ -354,7 +354,7 @@ RSpec.describe Dependabot::GoModules::FileParser do
       it "returns the correct package manager" do
         expect(package_manager.name).to eq "go_modules"
         expect(package_manager.requirement).to be_nil
-        expect(package_manager.version.to_s).to eq "1.25"
+        expect(package_manager.version.to_s).to eq "1.25-12"
       end
     end
 
