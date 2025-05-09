4937ab012717782e6b901efc6bae8c7d57329b18
sachin-sandhu <sachin-sandhu@github.com>
2025-05-09 01:19:20 -0400

    package details fetcher generic use

--- gradle/lib/dependabot/gradle/update_checker/version_finder.rb
+++ gradle/lib/dependabot/gradle/update_checker/version_finder.rb
@@ -10,11 +10,13 @@ require "dependabot/gradle/version"
 require "dependabot/gradle/requirement"
 require "dependabot/maven/utils/auth_headers_finder"
 require "sorbet-runtime"
+require "dependabot/gradle/package/package_details_fetcher"
+require "dependabot/package/package_latest_version_finder"
 
 module Dependabot
   module Gradle
     class UpdateChecker
-      class VersionFinder
+      class VersionFinder < Dependabot::Package::PackageLatestVersionFinder
         extend T::Sig
 
         KOTLIN_PLUGIN_REPO_PREFIX = "org.jetbrains.kotlin"
@@ -33,20 +35,23 @@ module Dependabot
         end
 
         def latest_version_details
-          possible_versions = versions
+          possible_versions = package_release(versions)
 
-          possible_versions = filter_prereleases(possible_versions)
+          possible_versions = filter_prerelease_versions(possible_versions)
           possible_versions = filter_date_based_versions(possible_versions)
           possible_versions = filter_version_types(possible_versions)
           possible_versions = filter_ignored_versions(possible_versions)
 
-          possible_versions.last
+          return unless possible_versions.any?
+
+          { version: possible_versions.max_by(&:version)&.version,
+            source_url: possible_versions.max_by(&:version)&.url }
         end
 
         def lowest_security_fix_version_details
-          possible_versions = versions
+          possible_versions = package_release(versions)
 
-          possible_versions = filter_prereleases(possible_versions)
+          possible_versions = filter_prerelease_versions(possible_versions)
           possible_versions = filter_date_based_versions(possible_versions)
           possible_versions = filter_version_types(possible_versions)
           possible_versions = Dependabot::UpdateCheckers::VersionFilters.filter_vulnerable_versions(possible_versions,
@@ -54,24 +59,19 @@ module Dependabot
           possible_versions = filter_ignored_versions(possible_versions)
           possible_versions = filter_lower_versions(possible_versions)
 
-          possible_versions.first
+          return unless possible_versions.any?
+
+          { version: possible_versions.min_by(&:version)&.version,
+            source_url: possible_versions.min_by(&:version)&.url }
         end
 
         def versions
-          version_details =
-            repositories.map do |repository_details|
-              url = repository_details.fetch("url")
-              next google_version_details if url == Gradle::FileParser::RepositoriesFinder::GOOGLE_MAVEN_REPO
-
-              dependency_metadata(repository_details).css("versions > version")
-                                                     .select { |node| version_class.correct?(node.content) }
-                                                     .map { |node| version_class.new(node.content) }
-                                                     .map { |version| { version: version, source_url: url } }
-            end.flatten.compact
-
-          raise PrivateSourceAuthenticationFailure, forbidden_urls.first if version_details.none? && forbidden_urls.any?
-
-          version_details.sort_by { |details| details.fetch(:version) }
+          Package::PackageDetailsFetcher.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            credentials: credentials,
+            forbidden_urls: forbidden_urls
+          ).fetch_available_versions
         end
 
         private
@@ -84,21 +84,10 @@ module Dependabot
         attr_reader :security_advisories
 
         sig { params(possible_versions: T::Array[T.untyped]).returns(T::Array[T.untyped]) }
-        def filter_prereleases(possible_versions)
-          return possible_versions if wants_prerelease?
-
-          filtered = possible_versions.reject { |v| v.fetch(:version).prerelease? }
-          if possible_versions.count > filtered.count
-            Dependabot.logger.info("Filtered out #{possible_versions.count - filtered.count} pre-release versions")
-          end
-          filtered
-        end
-
-        sig { params(possible_versions: T::Array[T.untyped]).returns(T::Array[T.untyped]) }
         def filter_date_based_versions(possible_versions)
           return possible_versions if wants_date_based_version?
 
-          filtered = possible_versions.reject { |v| v.fetch(:version) > version_class.new(1900) }
+          filtered = possible_versions.reject { |release| release.version > version_class.new(1900) }
           if possible_versions.count > filtered.count
             Dependabot.logger.info("Filtered out #{possible_versions.count - filtered.count} date-based versions")
           end
@@ -107,13 +96,35 @@ module Dependabot
 
         sig { params(possible_versions: T::Array[T.untyped]).returns(T::Array[T.untyped]) }
         def filter_version_types(possible_versions)
-          filtered = possible_versions.select { |v| matches_dependency_version_type?(v.fetch(:version)) }
+          filtered = possible_versions.select { |release| matches_dependency_version_type?(release.version) }
           if possible_versions.count > filtered.count
             diff = possible_versions.count - filtered.count
             classifier = dependency.version.split(/[.\-]/).last
             Dependabot.logger.info("Filtered out #{diff} non-#{classifier} classifier versions")
           end
           filtered
+        end
+
+        def package_release(version)
+          package_releases = []
+
+          version.map do |info|
+            package_releases << Dependabot::Package::PackageRelease.new(
+              version: info[:version],
+              released_at: info[:released_at],
+              url: info[:source_url]
+            )
+          end
+          package_releases
+        end
+
+        sig { returns(Package::PackageDetailsFetcher) }
+        def package_details_fetcher
+          @package_details_fetcher ||= Package::PackageDetailsFetcher.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            credentials: credentials
+          )
         end
 
         sig { params(possible_versions: T::Array[T.untyped]).returns(T::Array[T.untyped]) }
@@ -124,7 +135,7 @@ module Dependabot
             ignore_requirements = Gradle::Requirement.requirements_array(req)
             filtered =
               filtered
-              .reject { |v| ignore_requirements.any? { |r| r.satisfied_by?(v.fetch(:version)) } }
+              .reject { |v| ignore_requirements.any? { |r| r.satisfied_by?(v.version) } }
           end
 
           if @raise_on_ignored && filter_lower_versions(filtered).empty? &&
@@ -140,15 +151,6 @@ module Dependabot
           filtered
         end
 
-        sig { params(possible_versions: T::Array[T.untyped]).returns(T::Array[T.untyped]) }
-        def filter_lower_versions(possible_versions)
-          return possible_versions unless dependency.numeric_version
-
-          possible_versions.select do |v|
-            v.fetch(:version) > dependency.numeric_version
-          end
-        end
-
         def wants_prerelease?
           return false unless dependency.numeric_version
 
@@ -159,121 +161,6 @@ module Dependabot
           return false unless dependency.numeric_version
 
           dependency.numeric_version >= version_class.new(100)
-        end
-
-        def google_version_details
-          url = Gradle::FileParser::RepositoriesFinder::GOOGLE_MAVEN_REPO
-          group_id, artifact_id = group_and_artifact_ids
-
-          dependency_metadata_url = "#{Gradle::FileParser::RepositoriesFinder::GOOGLE_MAVEN_REPO}/" \
-                                    "#{group_id.tr('.', '/')}/" \
-                                    "group-index.xml"
-
-          @google_version_details ||=
-            begin
-              response = Dependabot::RegistryClient.get(url: dependency_metadata_url)
-              Nokogiri::XML(response.body)
-            end
-
-          xpath = "/#{group_id}/#{artifact_id}"
-          return unless @google_version_details.at_xpath(xpath)
-
-          @google_version_details.at_xpath(xpath)
-                                 .attributes.fetch("versions")
-                                 .value.split(",")
-                                 .select { |v| version_class.correct?(v) }
-                                 .map { |v| version_class.new(v) }
-                                 .map { |version| { version: version, source_url: url } }
-        rescue Nokogiri::XML::XPath::SyntaxError
-          nil
-        end
-
-        def dependency_metadata(repository_details)
-          @dependency_metadata ||= {}
-          @dependency_metadata[repository_details.hash] ||=
-            begin
-              response = Dependabot::RegistryClient.get(
-                url: dependency_metadata_url(repository_details.fetch("url")),
-                headers: repository_details.fetch("auth_headers")
-              )
-              check_response(response, repository_details.fetch("url"))
-              Nokogiri::XML(response.body)
-            rescue URI::InvalidURIError
-              Nokogiri::XML("")
-            rescue Excon::Error::Socket, Excon::Error::Timeout,
-                   Excon::Error::TooManyRedirects
-              raise if central_repo_urls.include?(repository_details["url"])
-
-              Nokogiri::XML("")
-            end
-        end
-
-        def repository_urls
-          plugin? ? plugin_repository_details : dependency_repository_details
-        end
-
-        def check_response(response, repository_url)
-          return unless response.status == 401 || response.status == 403
-          return if @forbidden_urls.include?(repository_url)
-          return if central_repo_urls.include?(repository_url)
-
-          @forbidden_urls << repository_url
-        end
-
-        def repositories
-          return @repositories if @repositories
-
-          details = if plugin?
-                      plugin_repository_details +
-                        credentials_repository_details
-                    else
-                      dependency_repository_details +
-                        credentials_repository_details
-                    end
-
-          @repositories =
-            details.reject do |repo|
-              next if repo["auth_headers"]
-
-              # Reject this entry if an identical one with non-empty auth_headers exists
-              details.any? { |r| r["url"] == repo["url"] && r["auth_headers"] != {} }
-            end
-        end
-
-        def credentials_repository_details
-          credentials
-            .select { |cred| cred["type"] == "maven_repository" }
-            .map do |cred|
-            {
-              "url" => cred.fetch("url").gsub(%r{/+$}, ""),
-              "auth_headers" => auth_headers(cred.fetch("url").gsub(%r{/+$}, ""))
-            }
-          end
-        end
-
-        def dependency_repository_details
-          requirement_files =
-            dependency.requirements
-                      .map { |r| r.fetch(:file) }
-                      .map { |nm| dependency_files.find { |f| f.name == nm } }
-
-          @dependency_repository_details ||=
-            requirement_files.flat_map do |target_file|
-              Gradle::FileParser::RepositoriesFinder.new(
-                dependency_files: dependency_files,
-                target_dependency_file: target_file
-              ).repository_urls
-                                                    .map do |url|
-                { "url" => url, "auth_headers" => {} }
-              end
-            end.uniq
-        end
-
-        def plugin_repository_details
-          [{
-            "url" => Gradle::FileParser::RepositoriesFinder::GRADLE_PLUGINS_REPO,
-            "auth_headers" => {}
-          }] + dependency_repository_details
         end
 
         def matches_dependency_version_type?(comparison_version)
@@ -296,57 +183,8 @@ module Dependabot
           current_type == version_type
         end
 
-        def pom
-          filename = dependency.requirements.first.fetch(:file)
-          dependency_files.find { |f| f.name == filename }
-        end
-
-        def dependency_metadata_url(repository_url)
-          group_id, artifact_id = group_and_artifact_ids
-          group_id = "#{KOTLIN_PLUGIN_REPO_PREFIX}.#{group_id}" if kotlin_plugin?
-
-          "#{repository_url}/" \
-            "#{group_id.tr('.', '/')}/" \
-            "#{artifact_id}/" \
-            "maven-metadata.xml"
-        end
-
-        def group_and_artifact_ids
-          if kotlin_plugin?
-            [dependency.name, "#{KOTLIN_PLUGIN_REPO_PREFIX}.#{dependency.name}.gradle.plugin"]
-          elsif plugin?
-            [dependency.name, "#{dependency.name}.gradle.plugin"]
-          else
-            dependency.name.split(":")
-          end
-        end
-
-        def plugin?
-          dependency.requirements.any? { |r| r.fetch(:groups).include? "plugins" }
-        end
-
-        def kotlin_plugin?
-          plugin? && dependency.requirements.any? { |r| r.fetch(:groups).include? "kotlin" }
-        end
-
-        def central_repo_urls
-          central_url_without_protocol =
-            Gradle::FileParser::RepositoriesFinder::CENTRAL_REPO_URL
-            .gsub(%r{^.*://}, "")
-
-          %w(http:// https://).map { |p| p + central_url_without_protocol }
-        end
-
         def version_class
           dependency.version_class
-        end
-
-        def auth_headers_finder
-          @auth_headers_finder ||= Dependabot::Maven::Utils::AuthHeadersFinder.new(credentials)
-        end
-
-        def auth_headers(maven_repo_url)
-          auth_headers_finder.auth_headers(maven_repo_url)
         end
       end
     end
