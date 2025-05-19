88965f38c4e50c650c970a9c38cccafe57427360
Abhishek <abhishekbhaskar@github.com>
2025-05-19 15:10:02 -0500

    standardize code to package details structure and fetch release dates (#12269)
    * standardize code to package details structure and fetch release dates
    
    * add spec file for package details fetcher
    
    * add spec file for package details fetcher
    
    * refactor code
    
    * refactor code
    
    * refactor code
    
    * address pr comments

--- composer/lib/dependabot/composer/update_checker/latest_version_finder.rb
+++ composer/lib/dependabot/composer/update_checker/latest_version_finder.rb
@@ -9,11 +9,13 @@ require "dependabot/composer/update_checker"
 require "dependabot/update_checkers/version_filters"
 require "dependabot/shared_helpers"
 require "dependabot/errors"
+require "dependabot/composer/package/package_details_fetcher"
+require "dependabot/package/package_latest_version_finder"
 
 module Dependabot
   module Composer
     class UpdateChecker
-      class LatestVersionFinder
+      class LatestVersionFinder < Dependabot::Package::PackageLatestVersionFinder
         extend T::Sig
 
         sig do
@@ -34,22 +36,41 @@ module Dependabot
           @ignored_versions    = ignored_versions
           @raise_on_ignored    = raise_on_ignored
           @security_advisories = security_advisories
+          @fetcher = T.let(nil, T.nilable(Package::PackageDetailsFetcher))
         end
 
-        sig { returns(T.nilable(Dependabot::Version)) }
-        def latest_version
-          @latest_version ||= T.let(
-            fetch_latest_version,
-            T.nilable(Dependabot::Version)
+        sig { returns(Package::PackageDetailsFetcher) }
+        def fetcher
+          @fetcher ||= Package::PackageDetailsFetcher.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            credentials: credentials,
+            ignored_versions: ignored_versions,
+            security_advisories: security_advisories
           )
         end
 
-        sig { returns(T.nilable(Dependabot::Version)) }
-        def lowest_security_fix_version
-          @lowest_security_fix_version ||= T.let(
-            fetch_lowest_security_fix_version,
-            T.nilable(Dependabot::Version)
-          )
+        sig do
+          override.returns(T.nilable(Dependabot::Package::PackageDetails))
+        end
+        def package_details
+          @package_details ||= fetcher.fetch
+        end
+
+        sig do
+          override.params(language_version: T.nilable(T.any(String, Dependabot::Version)))
+                  .returns(T.nilable(Dependabot::Version))
+        end
+        def latest_version(language_version: nil)
+          @latest_version ||= fetch_latest_version(language_version: language_version)
+        end
+
+        sig do
+          params(language_version: T.nilable(T.any(String, Dependabot::Version)))
+            .returns(T.nilable(Dependabot::Version))
+        end
+        def lowest_security_fix_version(language_version: nil)
+          @lowest_security_fix_version ||= fetch_lowest_security_fix_version(language_version: language_version)
         end
 
         private
@@ -69,52 +90,29 @@ module Dependabot
         sig { returns(T::Array[Dependabot::SecurityAdvisory]) }
         attr_reader :security_advisories
 
-        sig { returns(T.nilable(Dependabot::Version)) }
-        def fetch_latest_version
-          versions = available_versions
-          versions = filter_prerelease_versions(versions)
-          versions = filter_ignored_versions(versions)
-          versions.max
+        sig do
+          params(language_version: T.nilable(T.any(String, Dependabot::Version)))
+            .returns(T.nilable(Dependabot::Version))
+        end
+        def fetch_latest_version(language_version: nil) # rubocop:disable Lint/UnusedMethodArgument
+          releases = available_versions
+          releases = filter_prerelease_versions(releases)
+          releases = filter_ignored_versions(releases)
+          releases.max_by(&:version)&.version
         end
 
-        sig { returns(T.nilable(Dependabot::Version)) }
-        def fetch_lowest_security_fix_version
-          versions = available_versions
-          versions = filter_prerelease_versions(versions)
-          versions = Dependabot::UpdateCheckers::VersionFilters.filter_vulnerable_versions(versions,
+        sig do
+          params(language_version: T.nilable(T.any(String, Dependabot::Version)))
+            .returns(T.nilable(Dependabot::Version))
+        end
+        def fetch_lowest_security_fix_version(language_version: nil) # rubocop:disable Lint/UnusedMethodArgument
+          releases = available_versions
+          releases = filter_prerelease_versions(releases)
+          releases = Dependabot::UpdateCheckers::VersionFilters.filter_vulnerable_versions(releases,
                                                                                            security_advisories)
-          versions = filter_ignored_versions(versions)
-          versions = filter_lower_versions(versions)
-
-          versions.min
-        end
-
-        sig { params(versions_array: T::Array[Dependabot::Version]).returns(T::Array[Dependabot::Version]) }
-        def filter_prerelease_versions(versions_array)
-          return versions_array if wants_prerelease?
-
-          versions_array.reject(&:prerelease?)
-        end
-
-        sig { params(versions_array: T::Array[Dependabot::Version]).returns(T::Array[Dependabot::Version]) }
-        def filter_ignored_versions(versions_array)
-          filtered =
-            versions_array
-            .reject { |v| ignore_requirements.any? { |r| r.satisfied_by?(v) } }
-
-          if @raise_on_ignored && filter_lower_versions(filtered).empty? && filter_lower_versions(versions_array).any?
-            raise AllVersionsIgnored
-          end
-
-          filtered
-        end
-
-        sig { params(versions_array: T::Array[Dependabot::Version]).returns(T::Array[Dependabot::Version]) }
-        def filter_lower_versions(versions_array)
-          return versions_array unless dependency.numeric_version
-
-          versions_array
-            .select { |version| version > dependency.numeric_version }
+          releases = filter_ignored_versions(releases)
+          releases = filter_lower_versions(releases)
+          releases.min_by(&:version)&.version
         end
 
         sig { returns(T::Boolean) }
@@ -127,149 +125,23 @@ module Dependabot
           end
         end
 
-        sig { returns(T::Array[Dependabot::Version]) }
+        sig { returns(T::Array[Dependabot::Package::PackageRelease]) }
         def available_versions
-          registry_version_details
-            .select { |version| version_class.correct?(version.gsub(/^v/, "")) }
-            .map { |version| version_class.new(version.gsub(/^v/, "")) }
+          Package::PackageDetailsFetcher.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            credentials: credentials,
+            ignored_versions: ignored_versions,
+            security_advisories: security_advisories,
+            raise_on_ignored: false
+          ).fetch_releases
         end
 
-        sig { returns(T::Array[String]) }
-        def registry_version_details
-          return @registry_version_details unless @registry_version_details.nil?
-
-          repositories =
-            JSON.parse(T.must(composer_file.content))
-                .fetch("repositories", [])
-                .select { |r| r.is_a?(Hash) }
-
-          urls = repositories
-                 .select { |h| h["type"] == PackageManager::NAME }
-                 .filter_map { |h| h["url"] }
-                 .map { |url| url.gsub(%r{\/$}, "") + "/packages.json" }
-
-          unless repositories.any? { |rep| rep["packagist.org"] == false }
-            urls << "https://repo.packagist.org/p2/#{dependency.name.downcase}.json"
-          end
-
-          @registry_version_details ||= T.let([], T.nilable(T::Array[String]))
-          urls.each do |url|
-            @registry_version_details += fetch_registry_versions_from_url(url)
-          end
-          @registry_version_details.uniq
-        end
-
-        sig { params(url: String).returns(T::Array[String]) }
-        def fetch_registry_versions_from_url(url)
-          url_host = URI(url).host
-          cred = registry_credentials.find do |c|
-            url_host == c["registry"] || url_host == URI(T.must(c["registry"])).host
-          end
-
-          response = Dependabot::RegistryClient.get(
-            url: url,
-            options: {
-              user: cred&.fetch("username", nil),
-              password: cred&.fetch("password", nil)
-            }
-          )
-
-          parse_registry_response(response, url)
+        sig { params(_url: String).returns(T::Array[String]) }
+        def fetch_registry_versions_from_url(_url)
+          []
         rescue Excon::Error::Socket, Excon::Error::Timeout
           []
-        end
-
-        sig { params(response: T.untyped, url: String).returns(T::Array[String]) }
-        def parse_registry_response(response, url)
-          return [] unless response.status == 200
-
-          listing = JSON.parse(response.body)
-          return [] if listing.nil?
-          return [] unless listing.is_a?(Hash)
-          return [] if listing.fetch("packages", []) == []
-          return [] unless listing.dig("packages", dependency.name.downcase)
-
-          extract_versions(listing)
-        rescue JSON::ParserError
-          msg = "'#{url}' does not contain valid JSON"
-          raise DependencyFileNotResolvable, msg
-        end
-
-        sig { params(listing: T::Hash[String, T.untyped]).returns(T::Array[String]) }
-        def extract_versions(listing)
-          # Packagist's Metadata API format:
-          # v1: "packages": {<package name>: {<version_number>: {hash of metadata for a particular release version}}}
-          # v2: "packages": {<package name>: [{hash of metadata for a particular release version}]}
-          version_listings = listing.dig("packages", dependency.name.downcase)
-
-          if version_listings.is_a?(Hash) # some private registries are still using the v1 format
-            # Regardless of API version, composer always reads the version from the metadata hash. So for the v1 API,
-            # ignore the keys as repositories other than packagist.org could be using different keys. Instead, coerce
-            # to an array of metadata hashes to match v2 format.
-            version_listings = version_listings.values
-          end
-
-          if version_listings.is_a?(Array)
-            version_listings.map { |i| i.fetch("version") }
-          else
-            []
-          end
-        end
-
-        sig { returns(T::Array[Dependabot::Credential]) }
-        def registry_credentials
-          credentials.select { |cred| cred["type"] == PackageManager::REPOSITORY_KEY } +
-            auth_json_credentials
-        end
-
-        sig { returns(T::Array[Dependabot::Credential]) }
-        def auth_json_credentials
-          json = auth_json
-          return [] unless json
-
-          parsed_auth_json = JSON.parse(T.must(json.content))
-          parsed_auth_json.fetch("http-basic", {}).map do |reg, details|
-            Dependabot::Credential.new({
-              "registry" => reg,
-              "username" => details["username"],
-              "password" => details["password"]
-            })
-          end
-        rescue JSON::ParserError
-          raise Dependabot::DependencyFileNotParseable, json.path if json
-
-          raise Dependabot::DependencyFileNotParseable, "Unknown path"
-        end
-
-        sig { returns(Dependabot::DependencyFile) }
-        def composer_file
-          composer_file =
-            dependency_files.find do |f|
-              f.name == PackageManager::MANIFEST_FILENAME
-            end
-          raise "No #{PackageManager::MANIFEST_FILENAME}!" unless composer_file
-
-          composer_file
-        end
-
-        sig { returns(T.nilable(Dependabot::DependencyFile)) }
-        def auth_json
-          dependency_files.find { |f| f.name == PackageManager::AUTH_FILENAME }
-        end
-
-        sig { returns(T::Array[Dependabot::Requirement]) }
-        def ignore_requirements
-          ignored_versions.map { |req| requirement_class.new(req.split(",")) }
-        end
-
-        sig { returns(T.class_of(Dependabot::Version)) }
-        def version_class
-          dependency.version_class
-        end
-
-        sig { returns(T.class_of(Dependabot::Requirement)) }
-        def requirement_class
-          dependency.requirement_class
         end
       end
     end
