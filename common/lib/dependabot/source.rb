f7eb95d452bccb5e82a18f657d961db79859565f
Randhir <randhircs@github.com>
2025-05-20 23:54:03 +0000

    Removing source from change list.

--- common/lib/dependabot/source.rb
+++ common/lib/dependabot/source.rb
@@ -95,6 +95,7 @@ module Dependabot
       return github_enterprise_from_url(url_string) unless url_string&.match?(SOURCE_REGEX)
 
       captures = T.must(url_string.match(SOURCE_REGEX)).named_captures
+
       new(
         provider: T.must(captures.fetch("provider")),
         repo: T.must(captures.fetch("repo")).delete_suffix(".git").delete_suffix("."),
