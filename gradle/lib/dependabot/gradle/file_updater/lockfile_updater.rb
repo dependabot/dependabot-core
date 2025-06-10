e731169182935bb71e9670ed0a3ebee22004a93a
Ryan Brandenburg (He/Him) <rybrande@microsoft.com>
2025-06-10 11:47:29 -0700

    Lint cleanup

--- gradle/lib/dependabot/gradle/file_updater/lockfile_updater.rb
+++ gradle/lib/dependabot/gradle/file_updater/lockfile_updater.rb
@@ -50,18 +50,7 @@ module Dependabot
             # when running in a Docker container behind a proxy
             # Would prefer to use command line arguments, but they don't work.
             properties_filename = File.join(temp_dir, build_file.directory, "gradle.properties")
-            httpProxy = ENV["HTTP_PROXY"]
-            httpsProxy = ENV["HTTPS_PROXY"]
-            httpProxyHost = httpProxy ? httpProxy.split(":")[1].gsub("//", "") : "host.docker.internal"
-            httpsProxyHost = httpsProxy ? httpsProxy.split(":")[1].gsub("//", "") : "host.docker.internal"
-            httpProxyPort = httpProxy ? httpProxy.split(":")[2] : "1080"
-            httpsProxyPort = httpsProxy ? httpsProxy.split(":")[2] : "1080"
-            properties_content = "
-systemProp.http.proxyHost=#{httpProxyHost}
-systemProp.http.proxyPort=#{httpProxyPort}
-systemProp.https.proxyHost=#{httpsProxyHost}
-systemProp.https.proxyPort=#{httpsProxyPort}"
-            File.write(properties_filename, properties_content)
+            write_properties_file(properties_filename)
 
             command_parts = [
               "gradle",
@@ -72,13 +61,13 @@ systemProp.https.proxyPort=#{httpsProxyPort}"
             ]
             command = Shellwords.join(command_parts)
             env = {
-              "HTTP_PROXY" => httpProxy,
-              "HTTPS_PROXY" => httpsProxy,
-              "SOCKS_PROXY" => httpsProxy,
+              "HTTP_PROXY" => http_proxy,
+              "HTTPS_PROXY" => https_proxy,
+              "SOCKS_PROXY" => https_proxy
             }
 
             Dir.chdir(cwd) do
-              stdout = SharedHelpers.run_shell_command(command, env: env, cwd: cwd)
+              SharedHelpers.run_shell_command(command, env: env, cwd: cwd)
               local_lockfiles.each do |file|
                 f_content = File.read(File.join(temp_dir, file.directory, file.name))
                 tmp_file = file.dup
@@ -91,6 +80,22 @@ systemProp.https.proxyPort=#{httpsProxyPort}"
             end
           end
           updated_lockfiles
+        end
+
+        sig { params(properties_filename: String).void }
+        def write_properties_file(file_name)
+          http_proxy = ENV["HTTP_PROXY"]
+          https_proxy = ENV["HTTPS_PROXY"]
+          http_proxy_host = http_proxy ? T.must(http_proxy.split(":")[1]).gsub("//", "") : "host.docker.internal"
+          https_proxy_host = https_proxy ? T.must(https_proxy.split(":")[1]).gsub("//", "") : "host.docker.internal"
+          http_proxy_port = http_proxy ? http_proxy.split(":")[2] : "1080"
+          https_proxy_port = https_proxy ? https_proxy.split(":")[2] : "1080"
+          properties_content = "
+systemProp.http.proxy_host=#{http_proxy_host}
+systemProp.http.proxy_port=#{http_proxy_port}
+systemProp.https.proxy_host=#{https_proxy_host}
+systemProp.https.proxy_port=#{https_proxy_port}"
+          File.write(properties_filename, properties_content)
         end
       end
     end
