df7ac4321ffe1f9729f347cfd8a7f8385b0bcb54
Sebastian Gomez <sgome121@fiu.edu>
2025-06-27 13:45:06 -0400

    fix all styling issues

--- updater/spec/dependabot/updater/group_update_creation_spec.rb
+++ updater/spec/dependabot/updater/group_update_creation_spec.rb
@@ -10,19 +10,9 @@ require "dependabot/dependency_group"
 require "dependabot/dependency"
 require "dependabot/update_checkers/base"
 require "dependabot/experiments"
+require "dependabot/service"
 
 RSpec.describe Dependabot::Updater::GroupUpdateCreation do
-  # Stub all experiment flags to avoid unexpected argument errors
-  before do
-    allow(Dependabot::Experiments).to receive(:enabled?).and_call_original
-    allow(Dependabot::Experiments).to receive(:enabled?)
-      .with("dependency_change_validation")
-      .and_return(false)
-    allow(Dependabot::Experiments).to receive(:enabled?)
-      .with(:allow_refresh_group_with_all_dependencies)
-      .and_return(false)
-  end
-
   # Create a test class that includes the module to test it
   let(:test_class) do
     Class.new do
@@ -40,12 +30,12 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
     end
   end
 
-  let(:service) { double("Service") }
+  let(:service) { instance_double(Dependabot::Service) }
   let(:test_instance) { test_class.new(dependency_snapshot, error_handler, job, group, service) }
 
   let(:dependency_snapshot) do
-    double(
-      "DependencySnapshot",
+    instance_double(
+      Dependabot::DependencySnapshot,
       dependencies: dependencies,
       dependency_files: dependency_files,
       handled_dependencies: [],
@@ -56,24 +46,24 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
   end
 
   let(:error_handler) do
-    double("ErrorHandler").tap do |handler|
+    instance_double(Dependabot::Updater::ErrorHandler).tap do |handler|
       allow(handler).to receive(:handle_job_error)
     end
   end
 
   let(:job) do
-    double(
-      "Job",
+    instance_double(
+      Dependabot::Job,
       dependencies: job_dependencies,
       security_advisories_for: security_advisories,
       updating_a_pull_request?: false,
-      source: double("Source", directory: "/")
+      source: instance_double(Dependabot::Source, directory: "/")
     )
   end
 
   let(:group) do
-    double(
-      "DependencyGroup",
+    instance_double(
+      Dependabot::DependencyGroup,
       name: "test-group",
       dependencies: group_dependencies
     )
@@ -81,8 +71,8 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
 
   let(:dependencies) do
     [
-      double("Dependency", name: "dep1", version: "1.0.0"),
-      double("Dependency", name: "dep2", version: "2.0.0")
+      instance_double(Dependabot::Dependency, name: "dep1", version: "1.0.0"),
+      instance_double(Dependabot::Dependency, name: "dep2", version: "2.0.0")
     ]
   end
 
@@ -91,15 +81,15 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
   end
 
   let(:dependency_files) do
-    [double("DependencyFile", name: "Gemfile")]
+    [instance_double(Dependabot::DependencyFile, name: "Gemfile")]
   end
 
   let(:job_dependencies) { ["dep1"] }
   let(:security_advisories) { [] }
 
   let(:checker) do
-    double(
-      "UpdateChecker",
+    instance_double(
+      Dependabot::UpdateCheckers::Base,
       dependency: dependencies.first,
       up_to_date?: false,
       conflicting_dependencies: [],
@@ -108,14 +98,23 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
   end
 
   before do
+    # Stub all experiment flags to avoid unexpected argument errors
+    allow(Dependabot::Experiments).to receive(:enabled?).and_call_original
+    allow(Dependabot::Experiments).to receive(:enabled?)
+      .with("dependency_change_validation")
+      .and_return(false)
+    allow(Dependabot::Experiments).to receive(:enabled?)
+      .with(:allow_refresh_group_with_all_dependencies)
+      .and_return(false)
+
     # Stub common methods that would be called
-    allow(test_instance).to receive(:prepare_workspace)
-    allow(test_instance).to receive(:cleanup_workspace)
-    allow(test_instance).to receive(:dependency_file_parser).and_return(
-      double("Parser", parse: dependencies)
+    allow(test_instance).to receive_messages(
+      prepare_workspace: nil,
+      cleanup_workspace: nil
     )
-    allow(test_instance).to receive(:service).and_return(
-      double("Service").tap do |service|
+    allow(test_instance).to receive_messages(
+      dependency_file_parser: instance_double(Dependabot::FileParsers::Base, parse: dependencies),
+      service: instance_double(Dependabot::Service).tap do |service|
         allow(service).to receive(:record_update_job_error)
       end
     )
@@ -172,7 +171,8 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
 
           it "logs specific explanation and records error" do
             expect(Dependabot.logger).to receive(:info).with(
-              "Security update not possible for #{dependency.name} in group #{group.name}: Vulnerability fix not available"
+              "Security update not possible for #{dependency.name} in group #{group.name}: " \
+              "Vulnerability fix not available"
             )
             expect(test_instance).to receive(:record_security_update_not_possible_error).with(checker)
 
@@ -218,15 +218,12 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
     end
 
     context "when enhanced security reporting is disabled" do
+      let(:security_advisories) { [{ "id" => "advisory-1" }] }
+
       before do
         allow(Dependabot::Experiments).to receive(:enabled?)
           .with(:enhanced_grouped_security_error_reporting)
           .and_return(false)
-      end
-
-      let(:security_advisories) { [{ "id" => "advisory-1" }] }
-
-      before do
         allow(job).to receive(:security_advisories_for).with(dependency).and_return(security_advisories)
       end
 
@@ -284,15 +281,12 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
     end
 
     context "when enhanced security reporting is disabled" do
+      let(:security_advisories) { [{ "id" => "advisory-1" }] }
+
       before do
         allow(Dependabot::Experiments).to receive(:enabled?)
           .with(:enhanced_grouped_security_error_reporting)
           .and_return(false)
-      end
-
-      let(:security_advisories) { [{ "id" => "advisory-1" }] }
-
-      before do
         allow(job).to receive(:security_advisories_for).with(dependency).and_return(security_advisories)
       end
 
@@ -349,15 +343,12 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
     end
 
     context "when enhanced security reporting is disabled" do
+      let(:security_advisories) { [{ "id" => "advisory-1" }] }
+
       before do
         allow(Dependabot::Experiments).to receive(:enabled?)
           .with(:enhanced_grouped_security_error_reporting)
           .and_return(false)
-      end
-
-      let(:security_advisories) { [{ "id" => "advisory-1" }] }
-
-      before do
         allow(job).to receive(:security_advisories_for).with(dependency).and_return(security_advisories)
       end
 
@@ -373,13 +364,16 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
   describe "feature flag behavior in compile_all_dependency_changes_for" do
     before do
       # Stub all the complex methods that would be called during the method
-      allow(test_instance).to receive(:compile_updates_for).and_return([])
-      allow(test_instance).to receive(:skip_dependency?).and_return(false)
-      allow(test_instance).to receive(:deduce_updated_dependency).and_return(nil)
-      allow(test_instance).to receive(:store_changes)
+      allow(test_instance).to receive_messages(
+        compile_updates_for: [],
+        skip_dependency?: false,
+        deduce_updated_dependency: nil,
+        store_changes: nil,
+        record_warning_notices: nil
+      )
       allow(Dependabot::Updater::DependencyGroupChangeBatch).to receive(:new).and_return(
-        double(
-          "DependencyGroupChangeBatch",
+        instance_double(
+          Dependabot::Updater::DependencyGroupChangeBatch,
           current_dependency_files: dependency_files,
           updated_dependencies: [],
           updated_dependency_files: dependency_files,
@@ -388,9 +382,8 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
         )
       )
       allow(Dependabot::DependencyChange).to receive(:new).and_return(
-        double("DependencyChange", all_have_previous_version?: true)
+        instance_double(Dependabot::DependencyChange, all_have_previous_version?: true)
       )
-      allow(test_instance).to receive(:record_warning_notices)
     end
 
     context "when enhanced security reporting is enabled" do
@@ -401,7 +394,7 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
       end
 
       context "when job dependencies are missing from dependency snapshot" do
-        let(:job_dependencies) { ["dep1", "missing_dep"] }
+        let(:job_dependencies) { %w(dep1 missing_dep) }
 
         it "records missing dependency error for non-PR updates" do
           expect(error_handler).to receive(:handle_job_error) do |error:|
@@ -444,7 +437,7 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
       end
 
       context "when job dependencies are missing from dependency snapshot" do
-        let(:job_dependencies) { ["dep1", "missing_dep"] }
+        let(:job_dependencies) { %w(dep1 missing_dep) }
 
         it "does not record missing dependency error" do
           expect(error_handler).not_to receive(:handle_job_error)
@@ -460,14 +453,16 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
 
     before do
       # Stub the complex methods that would be called
-      allow(test_instance).to receive(:update_checker_for).and_return(checker)
-      allow(test_instance).to receive(:raise_on_ignored?).and_return(false)
-      allow(test_instance).to receive(:log_checking_for_update)
-      allow(test_instance).to receive(:all_versions_ignored?).and_return(false)
-      allow(test_instance).to receive(:semver_rules_allow_grouping?).and_return(true)
-      allow(test_instance).to receive(:log_up_to_date)
-      allow(test_instance).to receive(:requirements_to_unlock).and_return([])
-      allow(test_instance).to receive(:log_requirements_for_update)
+      allow(test_instance).to receive_messages(
+        update_checker_for: checker,
+        raise_on_ignored?: false,
+        log_checking_for_update: nil,
+        all_versions_ignored?: false,
+        semver_rules_allow_grouping?: true,
+        log_up_to_date: nil,
+        requirements_to_unlock: [],
+        log_requirements_for_update: nil
+      )
       allow(checker).to receive(:up_to_date?).and_return(false)
     end
 
@@ -535,7 +530,7 @@ RSpec.describe Dependabot::Updater::GroupUpdateCreation do
         end
 
         it "does not log or record any error even with security advisories" do
-          expect(Dependabot.logger).not_to receive(:info).with(/Security update not found/)
+          expect(Dependabot.logger).not_to receive(:info)
           expect(test_instance).not_to receive(:record_security_update_not_found)
 
           test_instance.compile_updates_for(dependency, dependency_files, group)
