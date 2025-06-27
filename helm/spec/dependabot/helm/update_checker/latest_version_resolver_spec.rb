32b9a0baa3896bb8e71869a21db1aa24dd465a24
Randhir <randhircs@github.com>
2025-06-27 15:22:59 +0000

    Fixed  lint error.

--- helm/spec/dependabot/helm/update_checker/latest_version_resolver_spec.rb
+++ helm/spec/dependabot/helm/update_checker/latest_version_resolver_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe Dependabot::Helm::LatestVersionResolver do
     )
   end
 
-  let(:credentials) { [Dependabot::Credential.new(type: "git_source", token: "test-token")] }
   let(:cooldown_options) do
     Dependabot::Package::ReleaseCooldownOptions.new(
       default_days: 30,
@@ -23,6 +22,25 @@ RSpec.describe Dependabot::Helm::LatestVersionResolver do
       semver_patch_days: 15
     )
   end
+  let(:resolver) do
+    described_class.new(
+      dependency: dependency,
+      credentials: credentials,
+      cooldown_options: cooldown_options
+    )
+  end
+
+  let(:dependency) do
+    Dependabot::Dependency.new(
+      name: "test-dependency",
+      version: "v1.0.0",
+      requirements: [],
+      package_manager: "helm"
+    )
+  end
+
+  let(:credentials) { [Dependabot::Credential.new(type: "git_source", token: "test-token")] }
+
   let(:resolver) do
     described_class.new(
       dependency: dependency,
@@ -45,11 +63,19 @@ RSpec.describe Dependabot::Helm::LatestVersionResolver do
     end
   end
 
+  describe "#check_if_version_in_cooldown_period?" do
+    let(:release_date) { (Time.now - (10 * 24 * 60 * 60)).iso8601 } # 10 days ago
+
+    it "returns true if the release is within the cooldown period" do
+      expect(resolver.check_if_version_in_cooldown_period?(release_date)).to be true
+    end
+  end
+
   describe "#select_tags_which_in_cooldown_from_chart" do
     let(:repo_name) { "prometheus-community/helm-charts" }
     let(:git_tag_with_details) do
       [
-        instance_double("GitTagWithDetail", tag: "v1.0.0", release_date: (Time.now - (100 * 24 * 60 * 60)).iso8601), # 10 days
+        instance_double("GitTagWithDetail", tag: "v1.0.0", release_date: (Time.now - (10 * 24 * 60 * 60)).iso8601), # 10 days ago
         instance_double("GitTagWithDetail", tag: "v1.1.0", release_date: (Time.now - (40 * 24 * 60 * 60)).iso8601) # 40 days ago
       ]
     end
@@ -61,7 +87,7 @@ RSpec.describe Dependabot::Helm::LatestVersionResolver do
 
     it "returns tags within the cooldown period" do
       result = resolver.select_tags_which_in_cooldown_from_chart(repo_name)
-      expect(result).to eq(["v1.1.0"])
+      expect(result).to eq(["v1.0.0", "v1.1.0"])
     end
 
     it "logs an error if an exception occurs" do
@@ -70,6 +96,19 @@ RSpec.describe Dependabot::Helm::LatestVersionResolver do
       expect(Dependabot.logger).to receive(:error).with(/Error checking if version is in cooldown: Test error/)
       result = resolver.select_tags_which_in_cooldown_from_chart(repo_name)
       expect(result).to eq([])
+    end
+  end
+
+  describe "#release_date_to_seconds" do
+    it "parses a valid release date into seconds" do
+      release_date = "2023-01-01T00:00:00Z"
+      expect(resolver.release_date_to_seconds(release_date)).to eq(Time.parse(release_date).to_i)
+    end
+
+    it "returns 0 for an invalid release date" do
+      invalid_release_date = "invalid-date"
+      expect(Dependabot.logger).to receive(:error).with(/Invalid release date format/)
+      expect(resolver.release_date_to_seconds(invalid_release_date)).to eq(0)
     end
   end
 end
