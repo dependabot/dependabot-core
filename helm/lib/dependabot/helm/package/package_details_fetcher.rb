28d605b3cf4352743f6f509fb4246ddf35dc3cae
Randhir <randhircs@github.com>
2025-06-27 14:39:42 +0000

    Cooldown feature for helm module.

--- helm/lib/dependabot/helm/package/package_details_fetcher.rb
+++ helm/lib/dependabot/helm/package/package_details_fetcher.rb
@@ -13,20 +13,10 @@ module Dependabot
     module Package
       class PackageDetailsFetcher
         extend T::Sig
-
+        # https://api.github.com/repos/prometheus-community/helm-charts/releases
         RELEASES_URL_GIT = "https://api.github.com/repos/"
-        RELEASE_URL_FOR_PROVIDER = "https://registry..io/v2/providers/"
-        RELEASE_URL_FOR_MODULE = "https://registry..io/v2/modules/"
         APPLICATION_JSON = "JSON"
-        INCLUDE_FOR_PROVIDER = "?include=provider-versions"
-        INCLUDE_FOR_MODULE = "?include=module-versions"
-        # https://registry..io/v2/providers/hashicorp/aws?include=provider-versions
-        # https://registry..io/v2/modules/-aws-modules/iam/aws?include=module-versions
-
-        ELIGIBLE_SOURCE_TYPES = T.let(
-          %w(git provider registry).freeze,
-          T::Array[String]
-        )
+        HELM_CHART_RELEASE = "/helm-charts/releases"
 
         sig do
           params(
@@ -45,101 +35,32 @@ module Dependabot
         sig { returns(Dependabot::Dependency) }
         attr_reader :dependency
 
-        sig { returns(T::Array[GitTagWithDetail]) }
-        def fetch_tag_and_release_date
-          truncate_github_url = @dependency.name.gsub("github.com/", "")
-          url = RELEASES_URL_GIT + "#{truncate_github_url}/releases"
+        sig { params(repo_name: String).returns(T::Array[GitTagWithDetail]) }
+        def fetch_tag_and_release_date_from_chart(repo_name) # rubocop:disable Metrics/AbcSize,Metrics/PerceivedComplexity
+          return [] unless repo_name.empty?
+
+          url = RELEASES_URL_GIT + repo_name + HELM_CHART_RELEASE
+          Dependabot.logger.info("Fetching graph release details from URL: #{url}")
           result_lines = T.let([], T::Array[GitTagWithDetail])
-          # Fetch the releases from the GitHub API
+          # Fetch the releases from the provider API
           response = Excon.get(url, headers: { "Accept" => "application/vnd.github.v3+json" })
           Dependabot.logger.error("Failed call details: #{response.body}") unless response.status == 200
           return result_lines unless response.status == 200
 
           # Parse the JSON response
           releases = JSON.parse(response.body)
-
-          # Extract version names and release dates into a hash
-          releases.map do |release|
-            result_lines << GitTagWithDetail.new(
+          # Extract tag_name and published_at from each release
+          result_lines = releases.map do |release|
+            GitTagWithDetail.new({
               tag: release["tag_name"],
               release_date: release["published_at"]
-            )
+            })
           end
-
-          # sort the result lines by tag in descending order
-          result_lines = result_lines.sort_by(&:tag).reverse
-          # Log the extracted details for debugging
           Dependabot.logger.info("Extracted release details: #{result_lines}")
-          result_lines
-        end
-
-        sig { returns(T::Array[GitTagWithDetail]) }
-        def fetch_tag_and_release_date_from_chart # rubocop:disable Metrics/AbcSize,Metrics/PerceivedComplexity
-          return [] unless dependency_source_details
-
-          url = RELEASE_URL_FOR_PROVIDER + dependency_source_details&.fetch(:module_identifier) +
-                INCLUDE_FOR_PROVIDER
-          Dependabot.logger.info("Fetching provider release details from URL: #{url}")
-          result_lines = T.let([], T::Array[GitTagWithDetail])
-          # Fetch the releases from the provider API
-          response = Excon.get(url, headers: { "Accept" => "application/vnd.github.v3+json" })
-          Dependabot.logger.error("Failed call details: #{response.body}") unless response.status == 200
-          return result_lines unless response.status == 200
-
-          # Parse the JSON response
-          releases = JSON.parse(response.body).fetch("included", [])
-                         .select { |item| item["type"] == "provider-versions" }
-          releases = releases.map { |release| release.fetch("attributes", {}) }
-          # Check if releases is an array and not empty
-          return result_lines unless releases.is_a?(Array) && !releases.empty?
-
-          # Extract version names and release dates into result_lines
-          releases.each do |release|
-            result_lines << GitTagWithDetail.new(
-              tag: release["version"],
-              release_date: release["published-at"]
-            )
-          end
           # Sort the result lines by tag in descending order
           result_lines.sort_by(&:tag).reverse
         end
         # RuboCop:enable Metrics/AbcSize, Metrics/MethodLength
-
-        sig { returns(T::Array[GitTagWithDetail]) }
-        def fetch_tag_and_release_date_from_module
-          return [] unless dependency_source_details
-
-          url = RELEASE_URL_FOR_MODULE + dependency_source_details&.fetch(:module_identifier) +
-                INCLUDE_FOR_MODULE
-          Dependabot.logger.info("Fetching provider release details from URL: #{url}")
-          result_lines = T.let([], T::Array[GitTagWithDetail])
-          # Fetch the releases from the provider API
-          response = Excon.get(url, headers: { "Accept" => "application/vnd.github.v3+json" })
-          Dependabot.logger.error("Failed call details: #{response.body}") unless response.status == 200
-          return result_lines unless response.status == 200
-
-          # Parse the JSON response
-          releases = JSON.parse(response.body).fetch("included", [])
-                         .select { |item| item["type"] == "module-versions" }
-          releases = releases.map { |release| release.fetch("attributes", {}) }
-
-          # Extract version names and release dates into result_lines
-          releases.each do |release|
-            result_lines << GitTagWithDetail.new(
-              tag: release["version"],
-              release_date: release["published-at"]
-            )
-          end
-          # Sort the result lines by tag in descending order
-          result_lines.sort_by(&:tag).reverse
-        end
-
-        sig { returns(T.nilable(T::Hash[T.any(String, Symbol), T.untyped])) }
-        def dependency_source_details
-          return nil unless @dependency.source_details
-
-          @dependency.source_details(allowed_types: ELIGIBLE_SOURCE_TYPES)
-        end
       end
     end
   end
