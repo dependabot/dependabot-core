7df5992e75d2f971b8479f78618250043543d140
Brett V. Forsgren <brettfo@microsoft.com>
2025-07-03 16:43:59 -0600

    allow updating child element for version

--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core.Test/Update/FileWriters/XmlFileWriterTests.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core.Test/Update/FileWriters/XmlFileWriterTests.cs
@@ -9,7 +9,7 @@ public class XmlFileWriterTests : FileWriterTestsBase
     public override IFileWriter FileWriter => new XmlFileWriter();
 
     [Fact]
-    public async Task SingleDependency_SingleFile_DirectUpdate()
+    public async Task SingleDependency_SingleFile_AttributeDirectUpdate()
     {
         await TestAsync(
             files: [
@@ -26,7 +26,7 @@ public class XmlFileWriterTests : FileWriterTestsBase
             projectDiscovery: new()
             {
                 FilePath = "project.csproj",
-                Dependencies = [], // unused
+                Dependencies = [new Dependency("Some.Dependency", "1.0.0", DependencyType.PackageReference)],
                 ImportedFiles = [],
                 AdditionalFiles = [],
             },
@@ -38,6 +38,161 @@ public class XmlFileWriterTests : FileWriterTestsBase
                         <PackageReference Include="Ignored.Dependency" Version="7.0.0" />
                         <PackageReference Include="Some.Dependency" Version="2.0.0" />
                         <PackageReference Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ]
+        );
+    }
+
+    [Fact]
+    public async Task SingleDependency_SingleFile_ElementDirectUpdate()
+    {
+        await TestAsync(
+            files: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageReference Include="Some.Dependency">
+                          <Version>1.0.0</Version>
+                        </PackageReference>
+                        <PackageReference Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ],
+            projectDiscovery: new()
+            {
+                FilePath = "project.csproj",
+                Dependencies = [new Dependency("Some.Dependency", "1.0.0", DependencyType.PackageReference)],
+                ImportedFiles = [],
+                AdditionalFiles = [],
+            },
+            requiredDependencies: [new Dependency("Some.Dependency", "2.0.0", DependencyType.PackageReference)],
+            expectedFiles: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageReference Include="Some.Dependency">
+                          <Version>2.0.0</Version>
+                        </PackageReference>
+                        <PackageReference Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ]
+        );
+    }
+
+    [Fact]
+    public async Task SingleDependency_CentralPackageManagement_AttributeDirectUpdate()
+    {
+        await TestAsync(
+            files: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" />
+                        <PackageReference Include="Some.Dependency" />
+                        <PackageReference Include="Some.Other.Dependency" />
+                      </ItemGroup>
+                    </Project>
+                    """),
+                ("Directory.Packages.props", """
+                    <Project>
+                      <ItemGroup>
+                        <PackageVersion Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageVersion Include="Some.Dependency" Version="1.0.0" />
+                        <PackageVersion Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ],
+            projectDiscovery: new()
+            {
+                FilePath = "project.csproj",
+                Dependencies = [new Dependency("Some.Dependency", "1.0.0", DependencyType.PackageReference)],
+                ImportedFiles = ["Directory.Packages.props"],
+                AdditionalFiles = [],
+            },
+            requiredDependencies: [new Dependency("Some.Dependency", "2.0.0", DependencyType.PackageReference)],
+            expectedFiles: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" />
+                        <PackageReference Include="Some.Dependency" />
+                        <PackageReference Include="Some.Other.Dependency" />
+                      </ItemGroup>
+                    </Project>
+                    """),
+                ("Directory.Packages.props", """
+                    <Project>
+                      <ItemGroup>
+                        <PackageVersion Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageVersion Include="Some.Dependency" Version="2.0.0" />
+                        <PackageVersion Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ]
+        );
+    }
+
+    [Fact]
+    public async Task SingleDependency_CentralPackageManagement_ElementDirectUpdate()
+    {
+        await TestAsync(
+            files: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" />
+                        <PackageReference Include="Some.Dependency" />
+                        <PackageReference Include="Some.Other.Dependency" />
+                      </ItemGroup>
+                    </Project>
+                    """),
+                ("Directory.Packages.props", """
+                    <Project>
+                      <ItemGroup>
+                        <PackageVersion Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageVersion Include="Some.Dependency">
+                          <Version>1.0.0</Version>
+                        </PackageVersion>
+                        <PackageVersion Include="Some.Other.Dependency" Version="8.0.0" />
+                      </ItemGroup>
+                    </Project>
+                    """)
+            ],
+            projectDiscovery: new()
+            {
+                FilePath = "project.csproj",
+                Dependencies = [new Dependency("Some.Dependency", "1.0.0", DependencyType.PackageReference)],
+                ImportedFiles = ["Directory.Packages.props"],
+                AdditionalFiles = [],
+            },
+            requiredDependencies: [new Dependency("Some.Dependency", "2.0.0", DependencyType.PackageReference)],
+            expectedFiles: [
+                ("project.csproj", """
+                    <Project Sdk="Microsoft.NET.Sdk">
+                      <ItemGroup>
+                        <PackageReference Include="Ignored.Dependency" />
+                        <PackageReference Include="Some.Dependency" />
+                        <PackageReference Include="Some.Other.Dependency" />
+                      </ItemGroup>
+                    </Project>
+                    """),
+                ("Directory.Packages.props", """
+                    <Project>
+                      <ItemGroup>
+                        <PackageVersion Include="Ignored.Dependency" Version="7.0.0" />
+                        <PackageVersion Include="Some.Dependency">
+                          <Version>2.0.0</Version>
+                        </PackageVersion>
+                        <PackageVersion Include="Some.Other.Dependency" Version="8.0.0" />
                       </ItemGroup>
                     </Project>
                     """)
--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Updater/FileWriters/XmlFileWriter.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/Updater/FileWriters/XmlFileWriter.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Xml.XPath;
 
 using NuGet.Versioning;
 
@@ -22,30 +16,83 @@ public class XmlFileWriter : IFileWriter
             .ToDictionary(path => path, path => XDocument.Parse(ReadFileContents(repoContentsPath, path)));
         foreach (var requiredPackageVersion in requiredPackageVersions)
         {
-            var packageHandled = false;
-            var filesAndContentsEnumerator = filesAndContents.GetEnumerator();
-            while (!packageHandled && filesAndContentsEnumerator.MoveNext())
+            var oldVersionString = projectDiscovery.Dependencies.FirstOrDefault(d => d.Name.Equals(requiredPackageVersion.Name, StringComparison.OrdinalIgnoreCase))?.Version;
+            if (oldVersionString is null)
             {
-                var (path, document) = filesAndContentsEnumerator.Current;
-                var candidateElements = document.Descendants()
-                    .Where(e => e.Name.LocalName == "PackageReference")
-                    .Where(e => (e.Attribute("Include")?.Value ?? string.Empty).Trim().Equals(requiredPackageVersion.Name, StringComparison.OrdinalIgnoreCase))
-                    .ToArray();
-                foreach (var candidateElement in candidateElements)
+                // TODO: log?
+                continue;
+            }
+
+            var oldVersion = NuGetVersion.Parse(oldVersionString);
+            var requiredVersion = NuGetVersion.Parse(requiredPackageVersion.Version!);
+
+            var packageReferenceElements = filesAndContents.Values
+                .SelectMany(doc => doc.Descendants().Where(e => e.Name.LocalName == "PackageReference"))
+                .Where(e => (e.Attribute("Include")?.Value ?? string.Empty).Trim().Equals(requiredPackageVersion.Name, StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+            if (packageReferenceElements.Length == 1)
+            {
+                var packageReferenceElement = packageReferenceElements[0];
+
+                // first check for matching `Version` attribute
+                var versionAttribute = packageReferenceElement.Attribute("Version");
+                if (versionAttribute is not null)
                 {
-                    var candidateVersionAttribute = candidateElement.Attribute("Version");
-                    if (candidateVersionAttribute is null)
+                    var parsedVersion = NuGetVersion.Parse(versionAttribute.Value);
+                    if (parsedVersion < requiredVersion)
                     {
+                        // found inline version attribute; do direct update
+                        versionAttribute.Value = requiredVersion.ToString();
+                        updatesPerformed[requiredPackageVersion.Name] = true;
                         continue;
                     }
+                }
 
-                    var candidateVersion = NuGetVersion.Parse(candidateVersionAttribute.Value);
-                    var requiredVersion = NuGetVersion.Parse(requiredPackageVersion.Version!);
-                    if (candidateVersion < requiredVersion)
+                // next check for `Version` child element
+                var versionElement = packageReferenceElement.Elements().FirstOrDefault(e => e.Name.LocalName == "Version");
+                if (versionElement is not null)
+                {
+                    var parsedVersion = NuGetVersion.Parse(versionElement.Value);
+                    if (parsedVersion < requiredVersion)
                     {
-                        candidateVersionAttribute.Value = requiredVersion.ToString();
+                        // found inline version element; do direct update
+                        versionElement.Value = requiredVersion.ToString();
                         updatesPerformed[requiredPackageVersion.Name] = true;
-                        packageHandled = true;
+                        continue;
+                    }
+                }
+
+                // check for matching `<PackageVersion>` element
+                var packageVersionElement = filesAndContents.Values
+                    .SelectMany(doc => doc.Descendants().Where(e => e.Name.LocalName == "PackageVersion"))
+                    .FirstOrDefault(e => (e.Attribute("Include")?.Value ?? string.Empty).Trim().Equals(requiredPackageVersion.Name, StringComparison.OrdinalIgnoreCase));
+                if (packageVersionElement is not null)
+                {
+                    if (packageVersionElement.Attribute("Version") is { } packageVersionAttribute)
+                    {
+                        var parsedVersion = NuGetVersion.Parse(packageVersionAttribute.Value);
+                        if (parsedVersion == oldVersion)
+                        {
+                            // found the correct elemtnt to update
+                            packageVersionAttribute.Value = requiredVersion.ToString();
+                            updatesPerformed[requiredPackageVersion.Name] = true;
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        var cpmVersionElement = packageVersionElement.Elements().FirstOrDefault(e => e.Name.LocalName == "Version");
+                        if (cpmVersionElement is not null)
+                        {
+                            var parsedVersion = NuGetVersion.Parse(cpmVersionElement.Value);
+                            if (parsedVersion == oldVersion)
+                            {
+                                // found the correct element to update
+                                cpmVersionElement.Value = requiredVersion.ToString();
+                                updatesPerformed[requiredPackageVersion.Name] = true;
+                                continue;
+                            }
+                        }
                     }
                 }
             }
