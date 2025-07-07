5a333d150c16f8695e587c14cee99fbcfdea2d6b
sachin-sandhu <sachin-sandhu@github.com>
2025-07-07 16:39:15 -0400

    cooldown fetaure flag removal-github_actions

--- github_actions/spec/dependabot/github_actions/update_checker_spec.rb
+++ github_actions/spec/dependabot/github_actions/update_checker_spec.rb
@@ -447,8 +447,6 @@ RSpec.describe Dependabot::GithubActions::UpdateChecker do
           allow(Time).to receive(:now).and_return(Time.parse("2019-08-06 18:29:44 -0400"))
           allow(Dependabot::Experiments).to receive(:enabled?)
             .with(:enable_shared_helpers_command_timeout).and_return(true)
-          allow(Dependabot::Experiments).to receive(:enabled?)
-            .with(:enable_cooldown_for_github_actions).and_return(true)
         end
 
         it { is_expected.to eq(Gem::Version.new("1.0.1")) }
@@ -475,8 +473,6 @@ RSpec.describe Dependabot::GithubActions::UpdateChecker do
         allow(Time).to receive(:now).and_return(Time.parse("2022-09-07 23:33:35 +0100"))
         allow(Dependabot::Experiments).to receive(:enabled?)
           .with(:enable_shared_helpers_command_timeout).and_return(true)
-        allow(Dependabot::Experiments).to receive(:enabled?)
-          .with(:enable_cooldown_for_github_actions).and_return(true)
       end
 
       context "when pinned to an up to date commit in the default branch" do
