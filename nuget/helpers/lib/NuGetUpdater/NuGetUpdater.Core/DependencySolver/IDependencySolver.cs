432446b3b12cbb715c254cbd55ec7ac734f89913
Brett V. Forsgren <brettfo@microsoft.com>
2025-07-07 14:43:28 -0600

    move dependency solver to new directory

--- nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/DependencySolver/IDependencySolver.cs
+++ nuget/helpers/lib/NuGetUpdater/NuGetUpdater.Core/DependencySolver/IDependencySolver.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NuGetUpdater.Core.Updater.FileWriters;
+namespace NuGetUpdater.Core.DependencySolver;
 
 public interface IDependencySolver
 {
