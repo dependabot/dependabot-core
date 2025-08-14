2795223ff466e1b0a838593a5ed3c176f8af3c97
Jon Ruskin <jonabc@github.com>
2025-08-14 12:31:57 -0700

    Remove blank line to fix linter

--- nuget/lib/dependabot/nuget.rb
+++ nuget/lib/dependabot/nuget.rb
@@ -7,7 +7,6 @@
 require "dependabot/nuget/requirement"
 require "dependabot/nuget/version"
 
-
 require "dependabot/pull_request_creator/labeler"
 Dependabot::PullRequestCreator::Labeler
   .register_label_details("nuget", name: ".NET", colour: "7121c6")
