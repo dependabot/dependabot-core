2dbe5271716644c70f15316e9bd64db52be27933
Dai MIKURUBE <dmikurube@acm.org>
2025-08-16 21:46:38 +0900

    Fix Gradle lockfile support against Gradle's version catalog (Fix #12557)

--- gradle/lib/dependabot/gradle/file_updater/lockfile_updater.rb
+++ gradle/lib/dependabot/gradle/file_updater/lockfile_updater.rb
@@ -31,7 +31,11 @@ module Dependabot
           SharedHelpers.in_a_temporary_directory do |temp_dir|
             populate_temp_directory(temp_dir)
             cwd = File.join(temp_dir, build_file.directory, build_file.name)
-            cwd = File.dirname(cwd)
+            if build_file.path.end_with?("/gradle/libs.versions.toml")
+              cwd = File.dirname(cwd, 2)
+            else
+              cwd = File.dirname(cwd)
+            end
 
             # Create gradle.properties file with proxy settings
             # Would prefer to use command line arguments, but they don't work.
