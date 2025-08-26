a10e5b065fba16aeddf9786294627d02e5887d86
Kamil Bukum <kbukum1@github.com>
2025-08-26 14:20:06 -0500

    Add initial Go module and main file for vanity SSH import testing

---
+++ go_modules/spec/fixtures/projects/vanity_ssh_test/go.mod
@@ -0,0 +1,7 @@
+module github.com/dependabot/vanity-ssh-test
+
+go 1.20
+
+require (
+	go.testcompany.local/shared/logger v1.0.0
+)
---
+++ go_modules/spec/fixtures/projects/vanity_ssh_test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go.testcompany.local/shared/logger"
+)
+
+func main() {
+	logger.Info("Hello world")
+	fmt.Println("Using vanity import with SSH URL")
+}
