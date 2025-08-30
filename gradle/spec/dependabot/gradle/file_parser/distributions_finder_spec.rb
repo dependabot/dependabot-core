33ea70b93abbb3908d000695828e7d163d30537b
Guillermo Mazzola <gmazzo65@gmail.com>
2025-08-30 02:53:25 +0200

    Iterated distributions regex to catch any url

--- gradle/spec/dependabot/gradle/file_parser/distributions_finder_spec.rb
+++ gradle/spec/dependabot/gradle/file_parser/distributions_finder_spec.rb
@@ -5,6 +5,28 @@ require "spec_helper"
 require "dependabot/gradle/file_parser/distributions_finder"
 
 RSpec.describe Dependabot::Gradle::FileParser::DistributionsFinder do
+  describe "regex matches distribution version" do
+    [
+      %w(https://services.gradle.org/distributions/gradle-8.14.2-bin.zip 8.14.2),
+      %w(https://services.gradle.org/distributions/gradle-8.14.2-all.zip 8.14.2),
+      %w(https://services.gradle.org/distributions/gradle-9.0.0-bin.zip 9.0.0),
+      %w(https://services.gradle.org/distributions/gradle-9.0.0-all.zip 9.0.0),
+      %w(https://services.gradle.org/distributions/gradle-9.1.0-20250829161021+0000-all.zip 9.1.0-20250829161021+0000),
+      %w(https://services.gradle.org/distributions/gradle-9.1.0-rc-1-bin.zip 9.1.0-rc-1),
+      %w(https://my.company.org/dists/my-gradle-9.1.0-rc-1-bin.zip 9.1.0-rc-1),
+      %w(https://my.company.org/dists/my-gradle-9.1.0-rc-2-bin-customized.zip 9.1.0-rc-2),
+      %w(https://my.company.org/dists/my-gradle-8.1.tar 8.1),
+      %w(file://gradle-cache/gradle-8.1.zip 8.1)
+    ].each do |(url, version)|
+      it "matches #{url} and gets version #{version}" do
+        match = url.match(Dependabot::Gradle::FileParser::DistributionsFinder::DISTRIBUTION_URL_REGEX)&.named_captures
+
+        expect(match).not_to be_nil
+        expect(match&.fetch("version", nil)).to eq(version)
+      end
+    end
+  end
+
   describe ".resolve_dependency" do
     shared_examples "distribution dependency" do |version, type, checksum|
       subject { described_class.resolve_dependency(properties_file) }
