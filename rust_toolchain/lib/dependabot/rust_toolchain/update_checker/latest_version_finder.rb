32ae6f43b6fae607aed82a97208578b603a7923d
Jamie Magee <jamagee@microsoft.com>
2025-09-02 16:54:31 -0700

    Extract predicate methods

--- rust_toolchain/lib/dependabot/rust_toolchain/update_checker/latest_version_finder.rb
+++ rust_toolchain/lib/dependabot/rust_toolchain/update_checker/latest_version_finder.rb
@@ -48,7 +48,6 @@ module Dependabot
 
         private
 
-        # rubocop:disable Metrics/PerceivedComplexity
         sig do
           params(releases: T::Array[Dependabot::Package::PackageRelease])
             .returns(T::Array[Dependabot::Package::PackageRelease])
@@ -60,46 +59,72 @@ module Dependabot
           current_channel = current_version.channel
           return [] unless current_channel
 
-          current_type = current_channel.channel_type
-
-          # There are no updates for channels
-          # i.e. "stable", "beta", "nightly"
-          return [] if current_type == ChannelType::Stability
+          return [] if stability_channel_without_updates?(current_channel)
 
           is_current_major_minor = major_minor_format?(current_version.to_s)
 
-          channel_matched_releases = releases.filter_map do |release|
-            release_rust_version = T.cast(release.version, Dependabot::RustToolchain::Version)
-            release_channel = release_rust_version.channel
-            next unless release_channel
-
-            # Check that the release version is in the same channel type
-            next unless release_channel.channel_type == current_type
-
-            # For DatedStability channels, also check that the stability levels match
-            # e.g., "nightly-2025-08-19" should only match other "nightly-*" releases,
-            # not "stable-*" or "beta-*" releases
-            if current_type == ChannelType::DatedStability && release_channel.stability != current_channel.stability
-              next
-            end
-
-            next release unless release_channel.channel_type == ChannelType::Version
-
-            # For version channels, we need to ensure that the version format matches
-            is_release_major_minor = major_minor_format?(release_rust_version.to_s)
-            case [is_current_major_minor, is_release_major_minor]
-            in [true, true] | [false, false]
-              release
-            else
-              nil
-            end
+          channel_matched_releases = releases.select do |release|
+            compatible_release?(release, current_channel, is_current_major_minor)
           end
 
           channel_matched_releases.uniq do |release|
             release.version.to_s
           end
         end
-        # rubocop:enable Metrics/PerceivedComplexity
+
+        sig { params(channel: T.untyped).returns(T::Boolean) }
+        def stability_channel_without_updates?(channel)
+          channel.channel_type == ChannelType::Stability
+        end
+
+        sig do
+          params(
+            release: Dependabot::Package::PackageRelease,
+            current_channel: T.untyped,
+            is_current_major_minor: T::Boolean
+          )
+            .returns(T::Boolean)
+        end
+        def compatible_release?(release, current_channel, is_current_major_minor)
+          release_rust_version = T.cast(release.version, Dependabot::RustToolchain::Version)
+          release_channel = release_rust_version.channel
+          return false unless release_channel
+
+          return false unless matching_channel_type?(release_channel, current_channel)
+          return false unless matching_dated_stability?(release_channel, current_channel)
+          return true unless version_channel?(release_channel)
+
+          matching_version_format?(release_rust_version, is_current_major_minor)
+        end
+
+        sig { params(release_channel: T.untyped, current_channel: T.untyped).returns(T::Boolean) }
+        def matching_channel_type?(release_channel, current_channel)
+          release_channel.channel_type == current_channel.channel_type
+        end
+
+        sig { params(release_channel: T.untyped, current_channel: T.untyped).returns(T::Boolean) }
+        def matching_dated_stability?(release_channel, current_channel)
+          return true unless current_channel.channel_type == ChannelType::DatedStability
+
+          release_channel.stability == current_channel.stability
+        end
+
+        sig { params(channel: T.untyped).returns(T::Boolean) }
+        def version_channel?(channel)
+          channel.channel_type == ChannelType::Version
+        end
+
+        sig do
+          params(
+            release_version: Dependabot::RustToolchain::Version,
+            is_current_major_minor: T::Boolean
+          )
+            .returns(T::Boolean)
+        end
+        def matching_version_format?(release_version, is_current_major_minor)
+          is_release_major_minor = major_minor_format?(release_version.to_s)
+          is_current_major_minor == is_release_major_minor
+        end
 
         # Check if a version string is in major.minor format (e.g., "1.72" vs "1.72.0")
         sig { params(version_string: String).returns(T::Boolean) }
