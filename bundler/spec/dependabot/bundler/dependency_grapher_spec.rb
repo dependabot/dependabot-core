c21cf7a201c641723b09857a824fef584673b03e
Barry Gordon <brrygrdn@github.com>
2025-09-02 18:29:01 +0100

    Add an initial test case for graphing bundler generically

---
+++ bundler/spec/dependabot/bundler/dependency_grapher_spec.rb
@@ -0,0 +1,98 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/bundler"
+require "dependabot/dependency_graphers"
+
+RSpec.describe Dependabot::DependencyGraphers::Generic do
+  context "for bundler" do
+    subject(:grapher) do
+      Dependabot::DependencyGraphers.for_package_manager("bundler").new(
+        dependency_files:,
+        dependencies:
+      )
+    end
+
+    let(:parser) do
+      Dependabot::FileParsers.for_package_manager("bundler").new(
+        dependency_files:,
+        repo_contents_path: nil,
+        source: source,
+        credentials: [],
+        reject_external_code: false
+      )
+    end
+
+    let(:source) do
+      Dependabot::Source.new(
+        provider: "github",
+        repo: "dependabot-fixtures/dependabot-test-ruby-package",
+        directory: "/",
+        branch: "main"
+      )
+    end
+
+    let(:dependencies) { parser.parse }
+
+    context "with a Gemfile only" do
+      let(:gemfile) do
+        bundler_project_dependency_file("subdependency", filename: "Gemfile")
+      end
+
+      let(:dependency_files) do
+        [gemfile]
+      end
+
+      it "specifies the Gemfile as the relevant dependency file" do
+        expect(grapher.relevant_dependency_file).to eql(gemfile)
+      end
+
+      it "correctly serializes the resolved dependencies" do
+        expect(grapher.resolved_dependencies.count).to eql(1)
+
+        ibandit = grapher.resolved_dependencies["ibandit"]
+        expect(ibandit[:package_url]).to eql("pkg:gem/ibandit")
+        expect(ibandit[:relationship]).to eql("direct")
+        expect(ibandit[:scope]).to eql("runtime")
+        expect(ibandit[:dependencies]).to be_empty
+      end
+    end
+
+    context "with a Gemfile and Gemfile.lock" do
+      let(:gemfile) do
+        bundler_project_dependency_file("subdependency", filename: "Gemfile")
+      end
+
+      let(:gemfile_lock) do
+        bundler_project_dependency_file("subdependency", filename: "Gemfile.lock")
+      end
+
+      let(:dependency_files) do
+        [gemfile, gemfile_lock]
+      end
+
+      it "specifies the Gemfile.lock as the relevant dependency file" do
+        expect(grapher.relevant_dependency_file).to eql(gemfile_lock)
+      end
+
+      it "correctly serializes the resolved dependencies" do
+        expect(grapher.resolved_dependencies.count).to eql(2)
+
+        expect(grapher.resolved_dependencies.keys).to eql(%w(ibandit i18n))
+
+        ibandit = grapher.resolved_dependencies["ibandit"]
+        expect(ibandit[:package_url]).to eql("pkg:gem/ibandit@0.7.0")
+        expect(ibandit[:relationship]).to eql("direct")
+        expect(ibandit[:scope]).to eql("runtime")
+        expect(ibandit[:dependencies]).to be_empty # NYI: We don't set any subdependencies yet, this should contain i18n
+
+        i18n = grapher.resolved_dependencies["i18n"]
+        expect(i18n[:package_url]).to eql("pkg:gem/i18n@0.7.0.beta1")
+        expect(i18n[:relationship]).to eql("indirect")
+        expect(i18n[:scope]).to eql("runtime")
+        expect(i18n[:dependencies]).to be_empty
+      end
+    end
+  end
+end
