f732ede24ec5c7ae9b0293208be7da29cc3cf41d
Jamie Magee <jamie.magee@gmail.com>
2025-09-16 21:40:55 -0700

    Complete strict typing `hex` ecosystem

--- hex/lib/dependabot/hex/requirement.rb
+++ hex/lib/dependabot/hex/requirement.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "sorbet-runtime"
@@ -16,11 +16,16 @@ module Dependabot
       OR_SEPARATOR = /\s+or\s+/
 
       # Add the double-equality matcher to the list of allowed operations
-      OPS = OPS.merge("==" => ->(v, r) { v == r })
+      OPS = T.let(
+        OPS.merge("==" => lambda { |v, r|
+          v == r
+        }),
+        T::Hash[String, T.proc.params(arg0: Gem::Version, arg1: Gem::Version).returns(T::Boolean)]
+      )
 
       # Override the version pattern to allow local versions
-      quoted = OPS.keys.map { |k| Regexp.quote k }.join "|"
-      PATTERN_RAW = "\\s*(#{quoted})?\\s*(#{Hex::Version::VERSION_PATTERN})\\s*".freeze
+      quoted = OPS.keys.map { |k| Regexp.quote k }.join("|")
+      PATTERN_RAW = T.let("\\s*(#{quoted})?\\s*(#{Hex::Version::VERSION_PATTERN})\\s*".freeze, String)
       PATTERN = /\A#{PATTERN_RAW}\z/
 
       # Returns an array of requirements. At least one requirement from the
@@ -35,6 +40,7 @@ module Dependabot
 
       # Patches Gem::Requirement to make it accept requirement strings like
       # "~> 4.2.5, >= 4.2.5.1" without first needing to split them.
+      sig { params(requirements: T.any(String, T::Array[String])).void }
       def initialize(*requirements)
         requirements = requirements.flatten.flat_map do |req_string|
           req_string.split(",").map(&:strip)
@@ -44,6 +50,10 @@ module Dependabot
       end
 
       # Override the parser to create Hex::Versions
+      sig do
+        params(obj: T.any(String, Gem::Version))
+          .returns(T::Array[T.any(String, Gem::Version)])
+      end
       def self.parse(obj)
         return ["=", Hex::Version.new(obj.to_s)] if obj.is_a?(Gem::Version)
 
@@ -57,10 +67,11 @@ module Dependabot
         [matches[1] || "=", Hex::Version.new(T.must(matches[2]))]
       end
 
+      sig { params(version: T.any(String, Gem::Version, Dependabot::Version)).returns(T::Boolean) }
       def satisfied_by?(version)
         version = Hex::Version.new(version.to_s)
 
-        requirements.all? { |op, rv| (OPS[op] || OPS["="]).call(version, rv) }
+        requirements.all? { |op, rv| T.must(OPS[op] || OPS["="]).call(version, rv) }
       end
     end
   end
