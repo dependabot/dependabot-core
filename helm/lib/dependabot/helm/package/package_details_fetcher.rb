f94250eddf49c168fb1dcc2dfdd4a924ce648370
Jamie Magee <jamie.magee@gmail.com>
2025-09-22 10:24:32 -0700

    Run `rubocop -a`

--- helm/lib/dependabot/helm/package/package_details_fetcher.rb
+++ helm/lib/dependabot/helm/package/package_details_fetcher.rb
@@ -67,9 +67,9 @@ module Dependabot
             )
           end
           result_lines.sort_by(&:tag).reverse
-          rescue JSON::ParserError => e
-            Dependabot.logger.error("Failed to parse JSON response: #{e.message} response body #{response.body}")
-            []
+        rescue JSON::ParserError => e
+          Dependabot.logger.error("Failed to parse JSON response: #{e.message} response body #{response.body}")
+          []
         end
 
         sig { params(index_url: T.nilable(String), chart_name: String).returns(T::Array[GitTagWithDetail]) }
