9f6a420f85f8cbf3ef34b9544d9e3fd076c173a8
Rob Aiken <rob@aiken.im>
2025-10-08 14:47:36 +0000

    bazel file parser

--- bazel/lib/dependabot/bazel/version.rb
+++ bazel/lib/dependabot/bazel/version.rb
@@ -1,10 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-# NOTE: This file was scaffolded automatically but is OPTIONAL.
-# If your ecosystem uses standard semantic versioning without special logic,
-# you can safely delete this file and remove the require from lib/dependabot/bazel.rb
-
 require "dependabot/version"
 require "dependabot/utils"
 
@@ -13,9 +9,51 @@ module Dependabot
     class Version < Dependabot::Version
       extend T::Sig
 
-      # TODO: Implement custom version comparison logic if needed
-      # Example: Handle pre-release versions, build metadata, etc.
-      # If standard semantic versioning is sufficient, delete this file
+      VERSION_PATTERN = T.let(/^v?(\d+(?:\.\d+)*(?:[-.]?(?:alpha|beta|rc|pre|dev|snapshot)\d*)?(?:\+[a-zA-Z0-9\-_.]+)?)$/i.freeze, Regexp)
+
+      sig { override.params(version: VersionParameter).returns(T::Boolean) }
+      def self.correct?(version)
+        return false if version.nil?
+
+        version.to_s.match?(VERSION_PATTERN)
+      end
+
+      sig { override.params(version: VersionParameter).void }
+      def initialize(version)
+        @version_string = T.let(version.to_s, String)
+
+        # Normalize the version string (remove 'v' prefix if present)
+        normalized_version = @version_string.sub(/^v/, "")
+
+        super(normalized_version)
+      end
+
+      sig { returns(String) }
+      def to_s
+        @version_string
+      end
+
+      sig { params(other: T.untyped).returns(T.nilable(Integer)) }
+      def <=>(other)
+        return nil unless other.is_a?(Dependabot::Version)
+
+        version_without_prefix = @version_string.sub(/^v/, "")
+        other_version_without_prefix = other.to_s.sub(/^v/, "")
+
+        # Handle semantic versioning comparison using Gem::Version
+        Gem::Version.new(version_without_prefix) <=>
+          Gem::Version.new(other_version_without_prefix)
+      end
+
+      sig { override.returns(String) }
+      def inspect
+        "#<#{self.class} #{@version_string}>"
+      end
+
+      private
+
+      sig { returns(String) }
+      attr_reader :version_string
     end
   end
 end
--- bazel/spec/dependabot/bazel/file_parser_spec.rb
+++ bazel/spec/dependabot/bazel/file_parser_spec.rb
@@ -2,13 +2,151 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "dependabot/dependency_file"
 require "dependabot/bazel/file_parser"
 require_common_spec "file_parsers/shared_examples_for_file_parsers"
 
 RSpec.describe Dependabot::Bazel::FileParser do
-  # TODO: Add test cases
-  # Example:
-  # it "parses dependencies correctly" do
-  #   # Test implementation
-  # end
+  subject(:parser) do
+    described_class.new(
+      dependency_files: dependency_files,
+      source: source
+    )
+  end
+
+  let(:source) do
+    Dependabot::Source.new(
+      provider: "github",
+      repo: "example/bazel-project",
+      directory: "/"
+    )
+  end
+
+  let(:dependency_files) { [module_file] }
+
+  let(:module_file) do
+    Dependabot::DependencyFile.new(
+      name: "MODULE.bazel",
+      content: module_file_content
+    )
+  end
+
+  let(:module_file_content) do
+    <<~BAZEL
+      module(name = "my-module", version = "1.0")
+
+      bazel_dep(name = "rules_cc", version = "0.1.1")
+      bazel_dep(name = "platforms", version = "0.0.11")
+      bazel_dep(name = "abseil-cpp", version = "20230125.3")
+    BAZEL
+  end
+
+  describe "#parse" do
+    it "returns the expected dependencies" do
+      dependencies = parser.parse
+
+      expect(dependencies.length).to eq(3)
+
+      expect(dependencies.map(&:name)).to contain_exactly(
+        "rules_cc",
+        "platforms",
+        "abseil-cpp"
+      )
+
+      rules_cc_dep = dependencies.find { |d| d.name == "rules_cc" }
+      expect(rules_cc_dep.version).to eq("0.1.1")
+      expect(rules_cc_dep.package_manager).to eq("bazel")
+      expect(rules_cc_dep.requirements.first[:file]).to eq("MODULE.bazel")
+    end
+  end
+
+  context "with WORKSPACE file" do
+    let(:dependency_files) { [workspace_file] }
+
+    let(:workspace_file) do
+      Dependabot::DependencyFile.new(
+        name: "WORKSPACE",
+        content: workspace_file_content
+      )
+    end
+
+    let(:workspace_file_content) do
+      <<~BAZEL
+        load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+        http_archive(
+            name = "rules_cc",
+            urls = ["https://github.com/bazelbuild/rules_cc/archive/v0.1.1.tar.gz"],
+            sha256 = "abc123...",
+        )
+
+        http_archive(
+            name = "abseil-cpp",
+            urls = ["https://github.com/abseil/abseil-cpp/archive/20230125.3.tar.gz"],
+            sha256 = "def456...",
+        )
+      BAZEL
+    end
+
+    it "parses WORKSPACE dependencies" do
+      dependencies = parser.parse
+
+      expect(dependencies.length).to eq(2)
+      expect(dependencies.map(&:name)).to contain_exactly("rules_cc", "abseil-cpp")
+
+      rules_cc_dep = dependencies.find { |d| d.name == "rules_cc" }
+      expect(rules_cc_dep.version).to eq("0.1.1")
+    end
+  end
+
+  context "with BUILD file" do
+    let(:dependency_files) { [module_file, build_file] }
+
+    let(:build_file) do
+      Dependabot::DependencyFile.new(
+        name: "BUILD",
+        content: build_file_content
+      )
+    end
+
+    let(:build_file_content) do
+      <<~BAZEL
+        load("@rules_go//go:def.bzl", "go_library", "go_binary")
+        load("@rules_cc//cc:defs.bzl", "cc_library")
+
+        cc_library(
+            name = "my_lib",
+            srcs = ["lib.cc"],
+            deps = [
+                "@abseil-cpp//absl/strings",
+            ],
+        )
+      BAZEL
+    end
+
+    it "parses load statements from BUILD files" do
+      dependencies = parser.parse
+
+      # Should include MODULE.bazel deps plus load references
+      expect(dependencies.length).to be >= 3
+
+      load_deps = dependencies.select { |d| d.requirements.first[:groups] == ["load_references"] }
+      expect(load_deps.map(&:name)).to include("rules_go", "rules_cc")
+    end
+  end
+
+  context "with .bazelversion file" do
+    let(:dependency_files) { [module_file, bazelversion_file] }
+
+    let(:bazelversion_file) do
+      Dependabot::DependencyFile.new(
+        name: ".bazelversion",
+        content: "6.4.0"
+      )
+    end
+
+    it "detects the Bazel version" do
+      expect(parser.send(:bazel_version)).to eq("6.4.0")
+    end
+  end
 end
