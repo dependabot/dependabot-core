a208bea73376280b84e8805f834c7b04dd1677a1
Rob Aiken <rob@aiken.im>
2025-11-10 15:21:59 +0000

    Refactor MetadataFinder

--- bazel/lib/dependabot/bazel/metadata_finder.rb
+++ bazel/lib/dependabot/bazel/metadata_finder.rb
@@ -9,6 +9,17 @@ module Dependabot
     class MetadataFinder < Dependabot::MetadataFinders::Base
       extend T::Sig
 
+      require_relative "update_checker/registry_client"
+
+      GITHUB_URL_PATTERNS = T.let(
+        [
+          %r{github\.com/([^/]+)/([^/]+)/archive},
+          %r{github\.com/([^/]+)/([^/]+)/releases},
+          %r{github\.com/([^/]+)/([^/]+)}
+        ].freeze,
+        T::Array[Regexp]
+      )
+
       private
 
       sig { override.returns(T.nilable(Dependabot::Source)) }
@@ -20,21 +31,20 @@ module Dependabot
           find_source_from_http_archive
         when :git_repository
           find_source_from_git_repository
+        when :unknown
+          nil
         end
       end
 
       sig { returns(Symbol) }
       def source_type
+        return :bazel_dep if dependency.requirements.empty?
         return :bazel_dep if dependency.requirements.any? { |r| r[:source].nil? }
 
-        source = T.let(dependency.requirements.first, T.nilable(T::Hash[Symbol, T.untyped]))
-        return :bazel_dep unless source
-
-        source_details = T.let(source[:source], T.nilable(T::Hash[Symbol, T.untyped]))
+        source_details = dependency.requirements.first&.dig(:source)
         return :bazel_dep unless source_details
 
-        source_type_value = source_details[:type]
-        case source_type_value
+        case source_details[:type]
         when "http_archive" then :http_archive
         when "git_repository" then :git_repository
         else :unknown
@@ -47,57 +57,36 @@ module Dependabot
         return nil unless version
 
         source_info = registry_client.get_source(dependency.name, version)
-        return nil unless source_info
-
-        url = T.let(source_info["url"], T.nilable(String))
+        url = source_info&.dig("url")
         return nil unless url
 
-        github_source = extract_github_source_from_url(url)
-        return github_source if github_source
-
-        Dependabot::Source.from_url(url)
+        source_from_url(url)
       end
 
       sig { returns(T.nilable(Dependabot::Source)) }
       def find_source_from_http_archive
-        source = T.let(dependency.requirements.first, T.nilable(T::Hash[Symbol, T.untyped]))
-        return nil unless source
-
-        source_details = T.let(source[:source], T.nilable(T::Hash[Symbol, T.untyped]))
-        return nil unless source_details
-
-        url = T.let(source_details[:url], T.nilable(String))
+        url = dependency.requirements.first&.dig(:source, :url)
         return nil unless url
 
-        github_source = extract_github_source_from_url(url)
-        return github_source if github_source
-
-        Dependabot::Source.from_url(url)
+        source_from_url(url)
       end
 
       sig { returns(T.nilable(Dependabot::Source)) }
       def find_source_from_git_repository
-        source = T.let(dependency.requirements.first, T.nilable(T::Hash[Symbol, T.untyped]))
-        return nil unless source
-
-        source_details = T.let(source[:source], T.nilable(T::Hash[Symbol, T.untyped]))
-        return nil unless source_details
-
-        remote_url = T.let(source_details[:remote], T.nilable(String))
+        remote_url = dependency.requirements.first&.dig(:source, :remote)
         return nil unless remote_url
 
         Dependabot::Source.from_url(remote_url)
       end
 
       sig { params(url: String).returns(T.nilable(Dependabot::Source)) }
-      def extract_github_source_from_url(url)
-        github_patterns = [
-          %r{github\.com/([^/]+)/([^/]+)/archive},
-          %r{github\.com/([^/]+)/([^/]+)/releases},
-          %r{github\.com/([^/]+)/([^/]+)}
-        ]
+      def source_from_url(url)
+        extract_github_source_from_url(url) || Dependabot::Source.from_url(url)
+      end
 
-        github_patterns.each do |pattern|
+      sig { params(url: String).returns(T.nilable(Dependabot::Source)) }
+      def extract_github_source_from_url(url)
+        GITHUB_URL_PATTERNS.each do |pattern|
           match = url.match(pattern)
           next unless match
 
@@ -111,10 +100,6 @@ module Dependabot
 
       sig { returns(Dependabot::Bazel::UpdateChecker::RegistryClient) }
       def registry_client
-        unless defined?(Dependabot::Bazel::UpdateChecker::RegistryClient)
-          require "dependabot/bazel/update_checker/registry_client"
-        end
-
         @registry_client ||= T.let(
           Dependabot::Bazel::UpdateChecker::RegistryClient.new(credentials: credentials),
           T.nilable(Dependabot::Bazel::UpdateChecker::RegistryClient)
