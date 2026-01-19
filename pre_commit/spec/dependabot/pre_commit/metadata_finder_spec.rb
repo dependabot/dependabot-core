79a41f8c9e669a59a9380835c07a4f4b84cfc6e2
Rob Aiken <6567647+robaiken@users.noreply.github.com>
2026-01-19 12:39:36 +0000

    pre-commit scaffold

---
+++ pre_commit/spec/dependabot/pre_commit/metadata_finder_spec.rb
@@ -0,0 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/pre_commit/metadata_finder"
+require_common_spec "metadata_finders/shared_examples_for_metadata_finders"
+
+RSpec.describe Dependabot::PreCommit::MetadataFinder do
+  # TODO: Add test cases
+  # Example:
+  # it "finds package source" do
+  #   # Test implementation
+  # end
+end
