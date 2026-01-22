8a4fcc64d0b7ca2d9be16eb60f2eba0e03ca1924
Rob Aiken <6567647+robaiken@users.noreply.github.com>
2026-01-22 15:53:23 +0000

    lint

--- pre_commit/spec/dependabot/pre_commit/update_checker_spec.rb
+++ pre_commit/spec/dependabot/pre_commit/update_checker_spec.rb
@@ -4,11 +4,3 @@
 require "spec_helper"
 require "dependabot/pre_commit/update_checker"
 require_common_spec "update_checkers/shared_examples_for_update_checkers"
-
-RSpec.describe Dependabot::PreCommit::UpdateChecker do
-  # TODO: Add test cases
-  # Example:
-  # it "finds latest version" do
-  #   # Test implementation
-  # end
-end
