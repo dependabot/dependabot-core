95e896db8f989bbdc89ace1a60d1c159a1180542
Abhishek <abhishekbhaskar@github.com>
2026-01-28 17:24:47 -0600

    Add pre commit file updater functionality (#14020)
    * add pre commit file updater
    
    * fix same version in different repo issue
    
    * fix lint errors
    
    * update specs

--- pre_commit/lib/dependabot/pre_commit/file_updater.rb
+++ pre_commit/lib/dependabot/pre_commit/file_updater.rb
@@ -1,6 +1,9 @@
 # typed: strong
 # frozen_string_literal: true
 
+require "sorbet-runtime"
+
+require "dependabot/errors"
 require "dependabot/file_updaters"
 require "dependabot/file_updaters/base"
 
@@ -13,29 +16,113 @@ module Dependabot
       def updated_dependency_files
         updated_files = []
 
-        # TODO: Implement file update logic
-        # For each file that needs updating:
-        # 1. Get the original file content
-        # 2. Update it with new dependency versions
-        # 3. Add to updated_files array
-        # Example:
-        # manifest = dependency_files.find { |f| f.name == "manifest.json" }
-        # updated_files << updated_file(file: manifest, content: new_content)
+        dependency_files.each do |file|
+          next unless requirement_changed?(file, dependency)
+
+          updated_files <<
+            updated_file(
+              file: file,
+              content: updated_config_file_content(file)
+            )
+        end
+
+        updated_files.reject! { |f| dependency_files.include?(T.cast(f, Dependabot::DependencyFile)) }
+        raise "No files changed!" if updated_files.none?
 
         updated_files
       end
 
       private
 
+      sig { returns(Dependabot::Dependency) }
+      def dependency
+        # Pre-commit will only ever be updating a single dependency
+        T.must(dependencies.first)
+      end
+
       sig { override.void }
       def check_required_files
-        # TODO: Verify that all required files are present
-        # Example:
-        # return if get_original_file("manifest.json")
-        # raise "No manifest.json file found!"
+        # Just check if there are any files at all.
+        return if dependency_files.any?
+
+        raise "No pre-commit config files!"
+      end
+
+      sig { params(file: Dependabot::DependencyFile).returns(String) }
+      def updated_config_file_content(file)
+        updated_requirement_pairs = requirement_pairs_for_file(file)
+        updated_content = T.must(file.content)
+
+        updated_requirement_pairs.each do |new_req, old_req|
+          updated_content = apply_requirement_update(updated_content, new_req, old_req)
+        end
+
+        updated_content
+      end
+
+      sig do
+        params(file: Dependabot::DependencyFile)
+          .returns(T::Array[[T::Hash[Symbol, T.untyped], T::Hash[Symbol, T.untyped]]])
+      end
+      def requirement_pairs_for_file(file)
+        pairs = dependency.requirements.zip(T.must(dependency.previous_requirements))
+        filtered_pairs = pairs.reject do |new_req, old_req|
+          next true unless old_req
+
+          file_name = T.cast(new_req[:file], T.nilable(String))
+          next true if file_name != file.name
+
+          new_source = T.cast(new_req[:source], T.nilable(T::Hash[Symbol, T.untyped]))
+          old_source = T.cast(old_req[:source], T.nilable(T::Hash[Symbol, T.untyped]))
+          new_source == old_source
+        end
+
+        filtered_pairs.map { |new_req, old_req| [new_req, T.must(old_req)] }
+      end
+
+      sig do
+        params(
+          content: String,
+          new_req: T::Hash[Symbol, T.untyped],
+          old_req: T::Hash[Symbol, T.untyped]
+        ).returns(String)
+      end
+      def apply_requirement_update(content, new_req, old_req)
+        new_source = T.cast(new_req.fetch(:source), T::Hash[Symbol, T.untyped])
+        return content unless T.cast(new_source.fetch(:type), String) == "git"
+
+        old_source = T.cast(old_req.fetch(:source), T::Hash[Symbol, T.untyped])
+        repo_url = T.cast(old_source.fetch(:url), String)
+        old_ref = T.cast(old_source.fetch(:ref), String)
+        new_ref = T.cast(new_source.fetch(:ref), String)
+
+        replace_ref_in_content(content, repo_url, old_ref, new_ref)
+      end
+
+      sig do
+        params(content: String, repo_url: String, old_ref: String, new_ref: String).returns(String)
+      end
+      def replace_ref_in_content(content, repo_url, old_ref, new_ref)
+        current_repo = T.let(nil, T.nilable(String))
+
+        updated_lines = content.lines.map do |line|
+          # Track the current repo based on "- repo: <url>" lines
+          repo_match = line.match(/^\s*-\s*repo:\s*(\S+)/)
+          current_repo = repo_match[1] if repo_match
+
+          if current_repo == repo_url &&
+             line.match?(/^\s*rev:\s+#{Regexp.escape(old_ref)}(\s*(?:#.*)?)?$/)
+            line.gsub(old_ref, new_ref)
+          else
+            line
+          end
+        end
+
+        updated_lines.join
       end
     end
   end
 end
 
-Dependabot::FileUpdaters.register("pre_commit", Dependabot::PreCommit::FileUpdater)
+Dependabot::FileUpdaters
+  .register("pre_commit", Dependabot::PreCommit::FileUpdater)
--- pre_commit/spec/dependabot/pre_commit/file_updater_spec.rb
+++ pre_commit/spec/dependabot/pre_commit/file_updater_spec.rb
@@ -1,6 +1,237 @@
-# typed: strong
+# typed: false
 # frozen_string_literal: true
 
 require "spec_helper"
+require "dependabot/dependency"
+require "dependabot/dependency_file"
+require "dependabot/source"
 require "dependabot/pre_commit/file_updater"
+require "dependabot/pre_commit/version"
 require_common_spec "file_updaters/shared_examples_for_file_updaters"
+
+RSpec.describe Dependabot::PreCommit::FileUpdater do
+  let(:dependency) do
+    Dependabot::Dependency.new(
+      name: "https://github.com/pre-commit/pre-commit-hooks",
+      version: "v4.5.0",
+      previous_version: "v4.4.0",
+      requirements: [{
+        requirement: nil,
+        groups: [],
+        file: ".pre-commit-config.yaml",
+        source: {
+          type: "git",
+          url: "https://github.com/pre-commit/pre-commit-hooks",
+          ref: "v4.5.0",
+          branch: nil
+        }
+      }],
+      previous_requirements: [{
+        requirement: nil,
+        groups: [],
+        file: ".pre-commit-config.yaml",
+        source: {
+          type: "git",
+          url: "https://github.com/pre-commit/pre-commit-hooks",
+          ref: "v4.4.0",
+          branch: nil
+        }
+      }],
+      package_manager: "pre_commit"
+    )
+  end
+  let(:config_file_body) { fixture("pre_commit_configs", "basic.yaml") }
+  let(:config_file) do
+    Dependabot::DependencyFile.new(
+      content: config_file_body,
+      name: ".pre-commit-config.yaml"
+    )
+  end
+  let(:credentials) do
+    [{
+      "type" => "git_source",
+      "host" => "github.com",
+      "username" => "x-access-token",
+      "password" => "token"
+    }]
+  end
+  let(:files) { [config_file] }
+  let(:updater) do
+    described_class.new(
+      dependency_files: files,
+      dependencies: [dependency],
+      credentials: credentials
+    )
+  end
+
+  it_behaves_like "a dependency file updater"
+
+  describe "#updated_dependency_files" do
+    subject(:updated_files) { updater.updated_dependency_files }
+
+    it "returns DependencyFile objects" do
+      expect(updated_files).to all(be_a(Dependabot::DependencyFile))
+    end
+
+    its(:length) { is_expected.to eq(1) }
+
+    describe "the updated config file" do
+      subject(:updated_config_file) do
+        updated_files.find { |f| f.name == ".pre-commit-config.yaml" }
+      end
+
+      its(:content) do
+        is_expected.to include "rev: v4.5.0"
+        is_expected.not_to include "rev: v4.4.0"
+      end
+
+      context "with a commit SHA" do
+        let(:dependency) do
+          Dependabot::Dependency.new(
+            name: "https://github.com/pycqa/flake8",
+            version: "abc123def456",
+            previous_version: "6f6a02c2c85a1b45e39c1aa5e6cc40f7a3d6df5e",
+            requirements: [{
+              requirement: nil,
+              groups: [],
+              file: ".pre-commit-config.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/pycqa/flake8",
+                ref: "abc123def456",
+                branch: nil
+              }
+            }],
+            previous_requirements: [{
+              requirement: nil,
+              groups: [],
+              file: ".pre-commit-config.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/pycqa/flake8",
+                ref: "6f6a02c2c85a1b45e39c1aa5e6cc40f7a3d6df5e",
+                branch: nil
+              }
+            }],
+            package_manager: "pre_commit"
+          )
+        end
+        let(:config_file_body) { fixture("pre_commit_configs", "with_hashes_and_tags.yaml") }
+
+        its(:content) do
+          is_expected.to include "rev: abc123def456"
+          is_expected.not_to include "rev: 6f6a02c2c85a1b45e39c1aa5e6cc40f7a3d6df5e"
+        end
+      end
+
+      context "with inline comments" do
+        let(:config_file_body) do
+          <<~YAML
+            repos:
+              - repo: https://github.com/pre-commit/pre-commit-hooks
+                rev: v4.4.0  # This is a comment
+                hooks:
+                  - id: trailing-whitespace
+          YAML
+        end
+
+        its(:content) do
+          is_expected.to include "rev: v4.5.0  # This is a comment"
+          is_expected.not_to include "rev: v4.4.0"
+        end
+      end
+
+      context "with multiple repos using the same ref value" do
+        let(:config_file_body) do
+          <<~YAML
+            repos:
+              - repo: https://github.com/pre-commit/pre-commit-hooks
+                rev: v4.4.0
+                hooks:
+                  - id: trailing-whitespace
+              - repo: https://github.com/psf/black
+                rev: v4.4.0
+                hooks:
+                  - id: black
+          YAML
+        end
+
+        its(:content) do
+          is_expected.to include "- repo: https://github.com/pre-commit/pre-commit-hooks"
+          is_expected.to include "  rev: v4.5.0"
+          is_expected.to include "- repo: https://github.com/psf/black"
+          is_expected.to include "  rev: v4.4.0"
+        end
+
+        it "only updates the intended repo's rev field" do
+          content = updated_config_file.content
+          pre_commit_hooks_section = content[
+            %r{- repo: https://github\.com/pre-commit/pre-commit-hooks.*?(?=- repo:|\z)}m
+          ]
+          black_section = content[
+            %r{- repo: https://github\.com/psf/black.*?(?=- repo:|\z)}m
+          ]
+
+          expect(pre_commit_hooks_section).to include("rev: v4.5.0")
+          expect(pre_commit_hooks_section).not_to include("rev: v4.4.0")
+          expect(black_section).to include("rev: v4.4.0")
+          expect(black_section).not_to include("rev: v4.5.0")
+        end
+      end
+
+      context "with same repo used multiple times with different revs" do
+        let(:dependency) do
+          Dependabot::Dependency.new(
+            name: "https://github.com/pre-commit/pre-commit-hooks",
+            version: "v4.6.0",
+            previous_version: "v4.4.0",
+            requirements: [{
+              requirement: nil,
+              groups: [],
+              file: ".pre-commit-config.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/pre-commit/pre-commit-hooks",
+                ref: "v4.6.0",
+                branch: nil
+              }
+            }],
+            previous_requirements: [{
+              requirement: nil,
+              groups: [],
+              file: ".pre-commit-config.yaml",
+              source: {
+                type: "git",
+                url: "https://github.com/pre-commit/pre-commit-hooks",
+                ref: "v4.4.0",
+                branch: nil
+              }
+            }],
+            package_manager: "pre_commit"
+          )
+        end
+        let(:config_file_body) { fixture("pre_commit_configs", "same_repo_different_revs.yaml") }
+
+        it "only updates the occurrence that matches both repo URL and old ref" do
+          content = updated_config_file.content
+          lines = content.lines
+
+          # Find the first occurrence of the repo with v4.4.0 (should be updated to v4.6.0)
+          first_repo_idx = lines.index { |l| l.include?("repo: https://github.com/pre-commit/pre-commit-hooks") }
+          first_rev_idx = lines[first_repo_idx..].index { |l| l.match?(/^\s*rev:/) }
+          first_rev_line = lines[first_repo_idx + first_rev_idx]
+
+          # Find the second occurrence of the repo with v4.5.0 (should remain unchanged)
+          second_repo_idx = lines[(first_repo_idx + 1)..].index do |l|
+            l.include?("repo: https://github.com/pre-commit/pre-commit-hooks")
+          end
+          second_rev_idx = lines[(first_repo_idx + 1 + second_repo_idx)..].index { |l| l.match?(/^\s*rev:/) }
+          second_rev_line = lines[first_repo_idx + 1 + second_repo_idx + second_rev_idx]
+
+          expect(first_rev_line).to include("v4.6.0")
+          expect(second_rev_line).to include("v4.5.0")
+        end
+      end
+    end
+  end
+end
