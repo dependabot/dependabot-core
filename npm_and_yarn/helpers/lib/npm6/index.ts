303dad61b594a0cddfde827b0cd837f35b3b426c
Jason Paulos <jasonpaulos@users.noreply.github.com>
2026-03-20 11:16:54 -0400

    Remove unused npm6:update helper

--- npm_and_yarn/helpers/lib/npm6/index.ts
+++ npm_and_yarn/helpers/lib/npm6/index.ts
@@ -1,9 +1,7 @@
-import { updateDependencyFiles } from "./updater.js";
 import { checkPeerDependencies } from "./peer-dependency-checker.js";
 import { updateDependencyFile } from "./subdependency-updater.js";
 
 export {
-  updateDependencyFiles as update,
   updateDependencyFile as updateSubdependency,
   checkPeerDependencies,
 };
