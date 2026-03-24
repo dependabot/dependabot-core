73c5b0e933b5758b7afe2e382b6c054b17ae609d
Jason Paulos <jasonpaulos@users.noreply.github.com>
2026-03-24 10:44:09 -0400

    Convert npm and yarn helpers to TypeScript & enforce prettier (#14493)
    * Convert npm and yarn helpers to typescript
    
    * Remove unused npm6:update helper
    
    * Use type packages when available
    
    * Use provided types in pnpm helper
    
    * Reduce usage of any
    
    * Apply prettier & check formatting in CI
    
    * Update README with new dev guidance
    
    * Properly lint typescript files
    
    * Remove unnecessary npm build files from the built image
    
    * Revert "Remove unnecessary npm build files from the built image"

--- npm_and_yarn/helpers/eslint.config.js
+++ npm_and_yarn/helpers/eslint.config.js
@@ -1,27 +1,37 @@
 const globals = require("globals");
 const { defineConfig } = require("eslint/config");
 const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
 const eslintConfigPrettier = require("eslint-config-prettier/flat");
 
-// Rules not included before upgrading to ESLint 9. Can be enabled later
-const temporaryDisabledRules = {
-    rules: {
-        "no-unused-vars": "off",
-        "no-extra-boolean-cast": "off",
-        "no-undef": "off"
-    },
-}
-
 module.exports = defineConfig([
-    js.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest
-            }
-        },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
-    temporaryDisabledRules,
-    eslintConfigPrettier
-])
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+  eslintConfigPrettier,
+  {
+    files: ["**/*.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+]);
