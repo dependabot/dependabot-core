0ddc72b2ab95e871b5c227292dc89a2d0a7c61b1
Mark Allen <markhallen@gmail.com>
2026-03-27 14:36:04 +0000

    test: Add socket error coverage for RegistryClient#head and PackageDetailsFetcher

--- common/spec/dependabot/registry_client_spec.rb
+++ common/spec/dependabot/registry_client_spec.rb
@@ -187,7 +187,7 @@ RSpec.describe Dependabot::RegistryClient do
     describe "when Excon encounters a socket error" do
       let(:error) { Excon::Error::Socket.new(EOFError.new) }
 
-      it "only attempts to reach it once and then plays back the first error without calling the internet" do
+      it "only attempts to reach it once via get and then plays back the first error" do
         expect(Excon).to receive(:get).with(unreachable_url, anything).once
 
         expect { described_class.get(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
@@ -195,12 +195,28 @@ RSpec.describe Dependabot::RegistryClient do
         expect { described_class.get(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
       end
 
-      it "replays the first error for the host on any request path" do
+      it "replays the first get error for the host on any request path" do
         expect(Excon).to receive(:get).with(unreachable_url, anything).once
 
         expect { described_class.get(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
         expect { described_class.get(url: "#{unreachable_url}/foos") }.to raise_error(Excon::Error::Socket)
         expect { described_class.get(url: "#{unreachable_url}/foos/bars") }.to raise_error(Excon::Error::Socket)
+      end
+
+      it "only attempts to reach it once via head and then plays back the first error" do
+        expect(Excon).to receive(:head).with(unreachable_url, anything).once
+
+        expect { described_class.head(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
+        expect { described_class.head(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
+        expect { described_class.head(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
+      end
+
+      it "replays the first head error for the host on any request path" do
+        expect(Excon).to receive(:head).with(unreachable_url, anything).once
+
+        expect { described_class.head(url: unreachable_url) }.to raise_error(Excon::Error::Socket)
+        expect { described_class.head(url: "#{unreachable_url}/foos") }.to raise_error(Excon::Error::Socket)
+        expect { described_class.head(url: "#{unreachable_url}/foos/bars") }.to raise_error(Excon::Error::Socket)
       end
     end
 
