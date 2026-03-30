83967198e1a345cfe0e3619194667690b7a710ea
Jake Coffman <jake@jakecoffman.com>
2026-03-30 09:42:22 -0500

    fix multiple dep instances with different extras

--- python/lib/dependabot/python/file_updater/requirement_file_updater.rb
+++ python/lib/dependabot/python/file_updater/requirement_file_updater.rb
@@ -57,31 +57,37 @@ module Dependabot
         def fetch_updated_dependency_files
           reqs = dependency.requirements.zip(dependency.previous_requirements || [])
 
-          reqs.filter_map do |(new_req, old_req)|
+          updated_contents = T.let({}, T::Hash[String, String])
+
+          reqs.each do |(new_req, old_req)|
             next if new_req == old_req
 
-            file = T.must(get_original_file(new_req.fetch(:file))).dup
-            updated_content =
-              updated_requirement_or_setup_file_content(new_req, old_req)
-            next if updated_content == file.content
+            filename = new_req.fetch(:file)
+            content = updated_contents[filename] || T.must(T.must(get_original_file(filename)).content)
+            updated_contents[filename] = updated_requirement_or_setup_file_content(content, new_req, old_req)
+          rescue RuntimeError => e
+            raise unless e.message.start_with?("Declaration not found for")
+          end
 
-            file.content = updated_content
+          updated_contents.filter_map do |filename, content|
+            file = T.must(get_original_file(filename)).dup
+            next if content == T.must(file.content)
+
+            file.content = content
             file
           end
         end
 
         sig do
           params(
+            content: String,
             new_req: T::Hash[Symbol, T.untyped],
             old_req: T.nilable(T::Hash[Symbol, T.untyped])
           ).returns(String)
         end
-        def updated_requirement_or_setup_file_content(new_req, old_req)
-          original_file = get_original_file(new_req.fetch(:file))
-          raise "Could not find a dependency file for #{new_req}" unless original_file
-
+        def updated_requirement_or_setup_file_content(content, new_req, old_req)
           RequirementReplacer.new(
-            content: T.must(original_file.content),
+            content: content,
             dependency_name: dependency.name,
             old_requirement: old_req&.fetch(:requirement),
             new_requirement: new_req.fetch(:requirement),
