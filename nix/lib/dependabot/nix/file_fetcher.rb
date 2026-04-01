c31600476bcd440ee36a3efbe92eb4132a462c20
Jamie Magee <jamagee@microsoft.com>
2026-04-01 08:12:39 -0700

    promote the nix ecosystem from beta to ga

--- nix/lib/dependabot/nix/file_fetcher.rb
+++ nix/lib/dependabot/nix/file_fetcher.rb
@@ -22,13 +22,6 @@ module Dependabot
 
       sig { override.returns(T::Array[DependencyFile]) }
       def fetch_files
-        unless allow_beta_ecosystems?
-          raise Dependabot::DependencyFileNotFound.new(
-            nil,
-            "Nix support is currently in beta. Set ALLOW_BETA_ECOSYSTEMS=true to enable it."
-          )
-        end
-
         fetched_files = []
         fetched_files << flake_nix
         fetched_files << flake_lock
