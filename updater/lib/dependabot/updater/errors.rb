804672cef9ca6d43b37850fe371814c2edd66ea0
Jamie Magee <jamie.magee@gmail.com>
2026-04-02 22:53:11 -0700

    Include HasSentryContext in Updater::SubprocessFailed  This class defines sentry_context but was missing the interface include, causing a T.cast failure at runtime.

--- updater/lib/dependabot/updater/errors.rb
+++ updater/lib/dependabot/updater/errors.rb
@@ -1,12 +1,15 @@
 # typed: strong
 # frozen_string_literal: true
 
+require "dependabot/errors"
+
 module Dependabot
   class Updater
     class SubprocessFailed < StandardError
       extend T::Sig
+      include Dependabot::HasSentryContext
 
-      sig { returns(T::Hash[Symbol, T.untyped]) }
+      sig { override.returns(T::Hash[Symbol, T.untyped]) }
       attr_reader :sentry_context
 
       sig { params(message: String, sentry_context: T::Hash[Symbol, T.untyped]).void }
