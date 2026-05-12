a76789d89102ddd523b7c6894bfb5345875f9a5a
Abhishek Bhaskar <abhishekbhaskar@github.com>
2026-05-12 00:18:49 -0500

    add error message type matching in error helpers

--- pub/spec/dependabot/pub/update_checker_spec.rb
+++ pub/spec/dependabot/pub/update_checker_spec.rb
@@ -817,6 +817,14 @@ RSpec.describe Dependabot::Pub::UpdateChecker do
         expect { checker.latest_version }.to raise_error(Dependabot::DependencyFileNotResolvable)
       end
     end
+
+    context "when dependency_services is not run from workspace root" do
+      let(:stderr) { "Only apply dependency_services to the root of the workspace." }
+
+      it "raises the correct error" do
+        expect { checker.latest_version }.to raise_error(Dependabot::DependencyFileNotResolvable)
+      end
+    end
   end
 
   context "with a git dependency" do
